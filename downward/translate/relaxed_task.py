4d985fd748b012eed6bedee0e04e0addf316347d
Malte Helmert <malte.helmert@unibas.ch>
2009-02-11 19:00:57 +0000

    [main] relaxed_task.py top-level script: * More robustly and more thoroughly check that the input is appropriate   (i.e., essentially plain STRIPS). * Use proper classes to represent relaxed actions and tasks so that this   is usable as a module and not just as a standalone top-level script. * Changed the output dump format (the old format was only needed for   David Goergen's bachelor's thesis).

--- downward/translate/relaxed_task.py
+++ downward/translate/relaxed_task.py
@@ -1,48 +1,110 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2.5
 # -*- coding: utf-8 -*-
 
-import sys
-import StringIO
-
 import instantiate
+import pddl
 
 
-def explore_silently(task):
-    # Call instatiate.explore, swallowing its output.
-    real_stdout = sys.stdout
-    sys.stdout = StringIO.StringIO()
-    result = instantiate.explore(task)
-    sys.stdout = real_stdout
-    return result
+class RelaxedAction(object):
+    def __init__(self, name, preconditions, effects, cost):
+        self.name = name
+        self.preconditions = preconditions
+        self.effects = effects
+        self.cost = cost
 
-    
+    def dump(self):
+        print "ACTION %s (cost %d):" % (self.name, self.cost)
+        print "PRE:"
+        for fact in self.preconditions:
+            print "    %s" % fact
+        print "EFF:"
+        for fact in self.effects:
+            print "    %s" % fact
+
+
+class RelaxedTask(object):
+    def __init__(self, init, goals, actions):
+        self.init = init
+        self.goals = goals
+        self.actions = actions
+
+    def dump(self):
+        print "INIT:"
+        for fact in self.init:
+            print "    %s" % fact
+        print
+        print "GOAL:"
+        for fact in self.goals:
+            print "    %s" % fact
+        for action in self.actions:
+            print
+            action.dump()
+
+
 def literal_to_name(literal):
-    if literal.negated:
-        raise SystemExit("negative conditions not supported")
+    assert not literal.negated
     return "%s(%s)" % (literal.predicate, ",".join(literal.args))
 
 
-def relaxed_task(actions):
-    for action in actions:
-        precond_names = [literal_to_name(literal)
-                         for literal in action.precondition]
-        for conditions, effect_literal in action.add_effects:
-            if conditions:
-                raise SystemExit("conditional effects not supported")
-            effect_name = literal_to_name(effect_literal) + "'"
-            yield ", ".join(precond_names) + " => " + effect_name
+def build_relaxed_action(action):
+    def fail():
+        raise SystemExit("not a STRIPS action: %s" % action.name)
+    preconditions = []
+    effects = []
+    for literal in action.precondition:
+        if literal.negated:
+            fail()
+        preconditions.append(literal_to_name(literal))
+    for conditions, effect_literal in action.add_effects:
+        if conditions:
+            fail()
+        effects.append(literal_to_name(effect_literal))
+    for conditions, effect_literal in action.del_effects:
+        if conditions:
+            fail()
+    return RelaxedAction(action.name, sorted(preconditions), sorted(effects), 1)
 
 
-if __name__ == "__main__":
-    import pddl
-    task = pddl.open()
-    relaxed_reachable, atoms, actions, axioms = explore_silently(task)
+def collect_init_facts(fluent_atoms, init):
+    facts = []
+    for atom in init:
+        if atom in fluent_atoms:
+            facts.append(literal_to_name(atom))
+    return sorted(facts)
+
+
+def collect_goal_facts(fluent_atoms, goal):
+    def fail():
+        raise SystemExit("not a STRIPS goal: %s" % goal)
+    facts = []
+    def recurse(condition):
+        if isinstance(condition, pddl.Conjunction):
+            for part in condition.parts:
+                recurse(part)
+        elif isinstance(condition, pddl.Literal):
+            if condition.negated:
+                fail()
+            if condition in fluent_atoms:
+                facts.append(literal_to_name(condition))
+        elif not isinstance(condition, pddl.Truth):
+            fail()
+    recurse(goal)
+    return sorted(facts)
+
+
+def build_relaxed_task(task):
+    relaxed_reachable, fluent_atoms, actions, axioms = instantiate.explore(task)
     if not relaxed_reachable:
         raise SystemExit("goal is not relaxed reachable")
     if axioms:
         raise SystemExit("axioms not supported")
+    relaxed_actions = [build_relaxed_action(action) for action in actions]
+    init = collect_init_facts(fluent_atoms, task.init)
+    goal = collect_goal_facts(fluent_atoms, task.goal)
+    return RelaxedTask(init, goal, relaxed_actions)
 
-    # Generate complete output before printing it, to see if errors occur.
-    out = list(relaxed_task(actions))
-    for line in out:
-        print line
+
+if __name__ == "__main__":
+    task = pddl.open()
+    relaxed_task = build_relaxed_task(task)
+    relaxed_task.dump()
