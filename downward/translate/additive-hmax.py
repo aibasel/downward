325018cb539bb2126a5a5bcecd9cafcfaca38e12
Malte Helmert <malte.helmert@unibas.ch>
2009-03-02 21:29:39 +0000

    [main] additive-hmax: * First, unfortunately slightly buggy implementation.   ("collect_cut" is not guaranteed to actually return a cut,   and this causes problems -- see comments in the code.)

--- downward/translate/additive-hmax.py
+++ downward/translate/additive-hmax.py
@@ -8,9 +8,6 @@ from heapq import heappush, heappop
 
 
 def hmax(task):
-    task = relaxed_tasks.build_relaxed_task(task)
-    task.convert_to_canonical_form()
-
     # hmax costs are pairs of the form (cost, depth), where "depth" is
     # the length (as opposed to cost sum) of the supporting chain. We
     # use the depth for tie-breaking to avoid problems with cycles of
@@ -56,7 +53,11 @@ def hmax(task):
                             heappush(heap, (action_hmax, effect))
                         elif action_hmax == effect.hmax:
                             effect.reached_by.append(action)
+    # chain = collect_chain(task)
+    return hmax
 
+
+def collect_chain(task):
     # NOTE: We backchain by taking the first best supporter of each
     # action and fact. All other choices would also be fine, and might
     # lead to different landmarks being extracted.
@@ -67,10 +68,53 @@ def hmax(task):
         chain.append(action.name)
         goal = action.hmax_supporters[0]
     chain.reverse()
-    return hmax, chain
+    return chain
+
+
+def collect_cut(task):
+    # BUG: Unfortunately, this is buggy -- by only considering
+    # "reached_by", which are the *best* supporters of each fact,
+    # we're not guaranteed to get a cut of the justification graph
+    # here.
+    goal, = task.goals
+    hmax = goal.hmax[0]
+    cut = set()
+    def recurse(supporter):
+        assert supporter.hmax[0] == hmax
+        for action in supporter.reached_by:
+            # NOTE: Same as in collect_chain: could just as well pick
+            # another supporter here instead of hmax_supporters[0]
+            supporter = action.hmax_supporters[0]
+            supporter_hmax = supporter.hmax[0]
+            assert supporter_hmax <= hmax
+            if supporter_hmax < hmax:
+                cut.add(action)
+            else:
+                recurse(supporter)
+    recurse(goal)
+    return cut
+
+
+def additive_hmax(task):
+    MULTIPLIER = 1
+    result = 0
+    for action in task.actions:
+        action.cost *= MULTIPLIER
+    while True:
+        cost, _ = hmax(task)
+        if cost == 0:
+            break
+        result += 1
+        cut = collect_cut(task)
+        for action in cut:
+            assert action.cost >= 1
+            action.cost -= 1
+    return (result + MULTIPLIER - 1) // MULTIPLIER
 
 
 if __name__ == "__main__":
     task = pddl.open()
+    relaxed_task = relaxed_tasks.build_relaxed_task(task)
+    relaxed_task.convert_to_canonical_form()
     # relaxed_task.dump()
-    print hmax(task)
+    print additive_hmax(relaxed_task)
