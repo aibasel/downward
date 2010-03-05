384e803e3bcb134ebc1ed2bb905d12eaa223bbc7
Malte Helmert <malte.helmert@unibas.ch>
2010-03-05 17:32:10 +0000

    [main] * Removed an include that didn't appear to be used. * Fixed indentation/spacing/line breaks.

--- downward/search/eager_greedy_best_first_search.cc
+++ downward/search/eager_greedy_best_first_search.cc
@@ -3,30 +3,30 @@
 #include "heuristic.h"
 #include "open_lists/standard_scalar_open_list.h"
 #include "open_lists/alternation_open_list.h"
-#include "open_lists/tiebreaking_open_list.h"
 
-EagerGreedyBestFirstSearchEngine::EagerGreedyBestFirstSearchEngine():
-	GeneralEagerBestFirstSearch(false)  {
+EagerGreedyBestFirstSearchEngine::EagerGreedyBestFirstSearchEngine() :
+    GeneralEagerBestFirstSearch(false)  {
 }
 
 EagerGreedyBestFirstSearchEngine::~EagerGreedyBestFirstSearchEngine() {
 }
 
 void EagerGreedyBestFirstSearchEngine::initialize() {
-	assert(heuristics.size() > 0);
-
-	OpenList<state_var_t *> *ol;
+    assert(heuristics.size() > 0);
+    
+    OpenList<state_var_t *> *ol;
 
     if (heuristics.size() + preferred_operator_heuristics.size() == 1) {
         ol = new StandardScalarOpenList<state_var_t *>(heuristics[0]);
-    }
-    else {
+    } else {
         vector<OpenList<state_var_t *>*> inner_lists;
         for (int i = 0; i < heuristics.size(); i++) {
-            inner_lists.push_back(new StandardScalarOpenList<state_var_t *>(heuristics[i], false));
+            inner_lists.push_back(new StandardScalarOpenList<state_var_t *>(
+                                      heuristics[i], false));
         }
         for (int i = 0; i < preferred_operator_heuristics.size(); i++) {
-            inner_lists.push_back(new StandardScalarOpenList<state_var_t *>(preferred_operator_heuristics[i], true));
+            inner_lists.push_back(new StandardScalarOpenList<state_var_t *>(
+                                      preferred_operator_heuristics[i], true));
         }
         ol = new AlternationOpenList<state_var_t *>(inner_lists);
     }
