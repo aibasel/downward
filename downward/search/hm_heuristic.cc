55354ba07e9660556c3f126c72ec44133c22a411
Malte Helmert <malte.helmert@unibas.ch>
2010-03-22 14:29:30 +0000

    [main] Some indentation fixes and similar cosmetic issues.

--- downward/search/hm_heuristic.cc
+++ downward/search/hm_heuristic.cc
@@ -6,111 +6,110 @@
 #include <set>
 
 HMHeuristic::HMHeuristic(int _m):m(_m) {
-	MAX_VALUE = 100000;
-	//MAX_VALUE = numeric_limits<int>::max();
+    MAX_VALUE = 100000;
+    //MAX_VALUE = numeric_limits<int>::max();
 }
 
 HMHeuristic::~HMHeuristic() {
 }
 
 void HMHeuristic::initialize() {
-    cout << "The implementation of the  h^m heuristic is preliminary" << endl;
+    cout << "The implementation of the h^m heuristic is preliminary" << endl;
     cout << "It is SLOOOOOOOOOOOW" << endl;
     cout << "Please do not use this for comparison" << endl;
-	generate_all_tuples();
+    generate_all_tuples();
 }
 
 int HMHeuristic::compute_heuristic(const State &state) {
-	if (test_goal(state)) {
-		return 0;
-	}
-	else {
-		//cout << "Evaluating state: " << endl;
-		//state.dump();
-		//cout << endl;
+    if (test_goal(state)) {
+        return 0;
+    } else {
+        //cout << "Evaluating state: " << endl;
+        //state.dump();
+        //cout << endl;
 
-	    tuple s_tup;
-		state_to_tuple(state, s_tup);
+        tuple s_tup;
+        state_to_tuple(state, s_tup);
 
-		init_hm_table(s_tup);
-		//cout << "After Init" << endl;
-		//dump_table();
-		//cout << "********************************" << endl;
+        init_hm_table(s_tup);
+        //cout << "After Init" << endl;
+        //dump_table();
+        //cout << "********************************" << endl;
 
-		update_hm_table();
-		//cout << "After Update" << endl;
-		//dump_table();
-		//cout << "********************************" << endl;
+        update_hm_table();
+        //cout << "After Update" << endl;
+        //dump_table();
+        //cout << "********************************" << endl;
 
-		int h = eval(g_goal);
-		//cout << "*** " << h << endl;
+        int h = eval(g_goal);
+        //cout << "*** " << h << endl;
 
-		// ******************************
-		//init_hm_table(g_goal);
-		//update_hm_table();
-		//int h1 = eval(s_tup);
-		//cout << "*** " << h1 << endl;
-		// ******************************
+        // ******************************
+        //init_hm_table(g_goal);
+        //update_hm_table();
+        //int h1 = eval(s_tup);
+        //cout << "*** " << h1 << endl;
+        // ******************************
 
-		if (h == MAX_VALUE) {
+        if (h == MAX_VALUE) {
             return DEAD_END;
         }
-		return h;
-	}
+        return h;
+    }
 }
 
 
 void HMHeuristic::init_hm_table(tuple &t) {
-	map<tuple, int>::iterator it;
-	for (it = hm_table.begin(); it != hm_table.end(); it++) {
-		pair<tuple, int> hm_ent = *it;
-		tuple &tup = hm_ent.first;
-		int h_val = check_tuple_in_tuple(tup, t);
-		hm_table[tup] = h_val;
-	}
+    map<tuple, int>::iterator it;
+    for (it = hm_table.begin(); it != hm_table.end(); it++) {
+        pair<tuple, int> hm_ent = *it;
+        tuple &tup = hm_ent.first;
+        int h_val = check_tuple_in_tuple(tup, t);
+        hm_table[tup] = h_val;
+    }
 }
 
 
 void HMHeuristic::update_hm_table() {
-	int round = 0;
-	do {
-		round++;
-		//cout << "Update round " << round << endl;
-		//cout << "***********************************" << endl;
-		//dump_table();
-		//cout << "***********************************" << endl;
-		was_updated = false;
+    int round = 0;
+    do {
+        round++;
+        //cout << "Update round " << round << endl;
+        //cout << "***********************************" << endl;
+        //dump_table();
+        //cout << "***********************************" << endl;
+        was_updated = false;
 
-		for (int op_id = 0; op_id < g_operators.size(); op_id++) {
-			const Operator &op = g_operators[op_id];
-			//cout << "Checking operator " << op.get_name() << endl;
-			tuple pre;
-			get_operator_pre(op, pre);
-			int c1 = eval(pre);
-			if (c1 < MAX_VALUE) {
-				tuple eff;
-				vector<tuple> partial_eff;
+        for (int op_id = 0; op_id < g_operators.size(); op_id++) {
+            const Operator &op = g_operators[op_id];
+            //cout << "Checking operator " << op.get_name() << endl;
+            tuple pre;
+            get_operator_pre(op, pre);
+            int c1 = eval(pre);
+            if (c1 < MAX_VALUE) {
+                tuple eff;
+                vector<tuple> partial_eff;
 
-				get_operator_eff(op, eff);
-				generate_all_partial_tuple(eff, partial_eff);
-				for (int i = 0; i < partial_eff.size(); i++) {
-					update_hm_entry(partial_eff[i], c1 + op.get_cost());
+                get_operator_eff(op, eff);
+                generate_all_partial_tuple(eff, partial_eff);
+                for (int i = 0; i < partial_eff.size(); i++) {
+                    update_hm_entry(partial_eff[i], c1 + op.get_cost());
 
-					if (partial_eff[i].size() < m) {
-					    //cout << "Calling extend_tuple for ";
-					    //print_tuple(partial_eff[i]);
-					    //cout << endl;
+                    if (partial_eff[i].size() < m) {
+                        //cout << "Calling extend_tuple for ";
+                        //print_tuple(partial_eff[i]);
+                        //cout << endl;
 
-					    //tuple others;
-					    //extend_tuple(partial_eff[i], op, others, 0);
+                        //tuple others;
+                        //extend_tuple(partial_eff[i], op, others, 0);
 
-					    extend_tuple(partial_eff[i], op);
-					}
-				}
-			}
-		}
+                        extend_tuple(partial_eff[i], op);
+                    }
+                }
+            }
+        }
 
-	} while (was_updated);
+    } while (was_updated);
 }
 
 void HMHeuristic::extend_tuple(tuple &t, const Operator &op) {
@@ -209,85 +208,85 @@ void HMHeuristic::extend_tuple(tuple &t, const Operator &op, tuple &others, int 
 }
 
 int HMHeuristic::eval(tuple &t) {
-	vector<tuple> partial;
-	generate_all_partial_tuple(t, partial);
-	int max = 0;
-	for (int i = 0; i < partial.size(); i++) {
-		assert(hm_table.count(partial[i]) == 1);
+    vector<tuple> partial;
+    generate_all_partial_tuple(t, partial);
+    int max = 0;
+    for (int i = 0; i < partial.size(); i++) {
+        assert(hm_table.count(partial[i]) == 1);
 
-		int h = hm_table[partial[i]];
-		if (h > max) {
-			max = h;
-		}
-	}
-	return max;
+        int h = hm_table[partial[i]];
+        if (h > max) {
+            max = h;
+        }
+    }
+    return max;
 }
 
 int HMHeuristic::update_hm_entry(tuple &t, int val) {
-	assert(hm_table.count(t) == 1);
-	if (hm_table[t] > val) {
-	    //cout << "Entry updated " << val << endl;
-		hm_table[t] = val;
-		was_updated = true;
-	}
-	return val;
+    assert(hm_table.count(t) == 1);
+    if (hm_table[t] > val) {
+        //cout << "Entry updated " << val << endl;
+        hm_table[t] = val;
+        was_updated = true;
+    }
+    return val;
 }
 
 void HMHeuristic::generate_all_tuples(int var, int sz, tuple &base) {
-	if (sz == 1) {
-		for (int i = var; i < g_variable_domain.size(); i++) {
-			for (int j = 0; j < g_variable_domain[i]; j++) {
-				tuple tup(base);
-				tup.push_back(make_pair(i,j));
-				hm_table[tup] = 0;
-			}
-		}
-	}
-	else {
-		for (int i = var; i < g_variable_domain.size(); i++) {
-			for (int j = 0; j < g_variable_domain[i]; j++) {
-				tuple tup(base);
-				tup.push_back(make_pair(i,j));
-				hm_table[tup] = 0;
-				generate_all_tuples(i+1, sz-1, tup);
-			}
-		}
-	}
+    if (sz == 1) {
+        for (int i = var; i < g_variable_domain.size(); i++) {
+            for (int j = 0; j < g_variable_domain[i]; j++) {
+                tuple tup(base);
+                tup.push_back(make_pair(i,j));
+                hm_table[tup] = 0;
+            }
+        }
+    }
+    else {
+        for (int i = var; i < g_variable_domain.size(); i++) {
+            for (int j = 0; j < g_variable_domain[i]; j++) {
+                tuple tup(base);
+                tup.push_back(make_pair(i,j));
+                hm_table[tup] = 0;
+                generate_all_tuples(i+1, sz-1, tup);
+            }
+        }
+    }
 }
 
 void HMHeuristic::generate_all_partial_tuple(tuple &base_tuple, tuple &t,
-		int index, int sz, vector<tuple>& res) {
-	if (sz == 1) {
-		for (int i = index; i < base_tuple.size(); i++) {
-			tuple tup(t);
-			tup.push_back(base_tuple[i]);
-			res.push_back(tup);
-		}
-	}
-	else {
-		for (int i = index; i < base_tuple.size(); i++) {
-			tuple tup(t);
-			tup.push_back(base_tuple[i]);
-			res.push_back(tup);
-			generate_all_partial_tuple(base_tuple, tup, i+1, sz - 1, res);
-		}
-	}
+                                             int index, int sz, vector<tuple>& res) {
+    if (sz == 1) {
+        for (int i = index; i < base_tuple.size(); i++) {
+            tuple tup(t);
+            tup.push_back(base_tuple[i]);
+            res.push_back(tup);
+        }
+    }
+    else {
+        for (int i = index; i < base_tuple.size(); i++) {
+            tuple tup(t);
+            tup.push_back(base_tuple[i]);
+            res.push_back(tup);
+            generate_all_partial_tuple(base_tuple, tup, i+1, sz - 1, res);
+        }
+    }
 }
 
 
 int HMHeuristic::check_tuple_in_tuple(const tuple &tup, const tuple& big_tuple) {
-	for (int i = 0; i < tup.size(); i++) {
-		bool found = false;
-		for (int j = 0; j < big_tuple.size(); j++) {
-			if (tup[i] == big_tuple[j]) {
-				found = true;
-				break;
-			}
-		}
-		if (!found) {
-			return MAX_VALUE;
-		}
+    for (int i = 0; i < tup.size(); i++) {
+        bool found = false;
+        for (int j = 0; j < big_tuple.size(); j++) {
+            if (tup[i] == big_tuple[j]) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            return MAX_VALUE;
+        }
 
-	}
-	return 0;
+    }
+    return 0;
 }
