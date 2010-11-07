3a2144411e91174dcd118f2cc106177de28a1c2c
Silvan Sievers <silvan.sievers@unibas.ch>
2010-11-07 17:50:28 +0100

    [main] changed/moved some code, implemented some more routines

--- src/search/pdb_heuristic.h
+++ src/search/pdb_heuristic.h
@@ -6,39 +6,48 @@ using namespace std;
 
 class Edge {
     const Operator *op;
-    size_t source;
     size_t target;
+public:
+    Edge(const Operator *o, size_t t) : op(o), target(t) {}
 };
 
 class AbstractState {
     vector<int> variable_values;
 public:
     AbstractState(vector<int> values);
+    AbstractState(const State &state);
 };
 
-class PDBHeuristic;
+class Operator;
+class AbstractOperator {
+    vector<int> pattern;
+public:
+    AbstractOperator(Operator op, vector<int> pattern);
+    bool is_applicable(AbstractState &abstract_state);
+    const AbstractState apply_operator(AbstractState &abstract_state);
+};
+
 class PDBAbstraction {
     vector<int> pattern;
     size_t size;
-    vector<int> distance;
+    vector<int> distances;
     vector<vector<Edge > > back_edges;
+    int hash_index(const AbstractState &state);
+    AbstractState *inv_hash_index(int index);
 public:
     PDBAbstraction(vector<int> pattern);
-    void createPDB(PDBHeuristic *pdbheuristic);
-
+    void create_pdb();
+    int get_goal_distance(const AbstractState &abstract_state);
 };
 
 class PDBHeuristic : public Heuristic {
+    PDBAbstraction *pdb_abstraction;
 protected:
     virtual void initialize();
     virtual int compute_heuristic(const State &state);
 public:
     PDBHeuristic();
     ~PDBHeuristic();
-
-    int hash_index(const AbstractState &state);
-    AbstractState* inv_hash_index(int index);
-
     static ScalarEvaluator *create(const std::vector<std::string> &config,
                                    int start, int &end, bool dry_run);
 };
