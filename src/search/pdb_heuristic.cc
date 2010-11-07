7894d51d92f1b9cbe001fdbf51c0638b0f16c921
Silvan Sievers <silvan.sievers@unibas.ch>
2010-11-07 13:04:23 +0100

    [main] made things compilable except warnings

--- src/search/pdb_heuristic.cc
+++ src/search/pdb_heuristic.cc
@@ -1,24 +1,29 @@
 #include "pdb_heuristic.h"
 
 #include "globals.h"
-#include "option_parser.h"
+//#include "option_parser.h"
 #include "plugin.h"
 #include "state.h"
 
+// AbstractState -----------------------------------------------------------
+
+AbstractState::AbstractState(vector<int> values) {
+    variable_values = values;
+}
 
 // PDBAbstraction -----------------------------------------------------------
 
-PDBAbstraction::PDBAbstraction(vector<int> pattern) {
-    this.pattern = pattern;
+PDBAbstraction::PDBAbstraction(vector<int> p) {
+    pattern = p;
     size = 1;
     for (size_t i = 0; i < pattern.size(); i++) {
         size *= g_variable_domain[pattern[i]];
     }
 }
 
-PDBAbstraction::createPDB() {
+void PDBAbstraction::createPDB(PDBHeuristic *pdbheuristic) {
     for (size_t i = 0; i < size; i++) {
-        AbstractState *absstate = inv_hash_index(i);
+        AbstractState *absstate = pdbheuristic->inv_hash_index(i);
         //TODO Operatoren anwenden (zustand erhalten wenn anwendbar), in back-edges eintragen
     }
 }
@@ -27,7 +32,7 @@ PDBAbstraction::createPDB() {
 // PDBHeuristic -----------------------------------------------------------
 
 static ScalarEvaluatorPlugin pdb_heuristic_plugin(
-    "goalcount", PDBHeuristic::create);
+    "pdb_heuristic", PDBHeuristic::create);
 
 
 PDBHeuristic::PDBHeuristic() {
@@ -36,12 +41,14 @@ PDBHeuristic::PDBHeuristic() {
 PDBHeuristic::~PDBHeuristic() {
 }
 
-int PDBHEeuristic::hash_index(const AbstractState &state) {
+int PDBHeuristic::hash_index(const AbstractState &absstate) {
     //TODO siehe formeln
+    return 0;
 }
 
-*AbstracState PDBHEeuristic::inv_hash_index(int index) {
-    //TODO modulorechnen
+AbstractState *PDBHeuristic::inv_hash_index(int index) {
+    //TODO modulo rechnen
+    return 0;
 }
 
 void PDBHeuristic::initialize() {
@@ -49,16 +56,17 @@ void PDBHeuristic::initialize() {
     int patt[5] = {1, 2, 5, 7, 10};
     vector<int> pattern (patt, patt + sizeof(patt) / sizeof(int));
     PDBAbstraction *abstraction = new PDBAbstraction(pattern);
-    abstraction->createPDB();
+    abstraction->createPDB(this);
 }
 
 int PDBHeuristic::compute_heuristic(const State &state) {
     //TODO index berechnen, in tabelle nachschauen
+    return 0;
 }
 
 ScalarEvaluator *PDBHeuristic::create(const std::vector<string> &config,
                                             int start, int &end, bool dry_run) {
-    OptionParser::instance()->set_end_for_simple_config(config, start, end);
+    //OptionParser::instance()->set_end_for_simple_config(config, start, end);
     if (dry_run)
         return 0;
     else
