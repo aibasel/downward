b90fb466bb086e11ebb545022c0a22013c708056
Silvan Sievers <silvan.sievers@unibas.ch>
2010-11-09 18:02:07 +0100

    [main] remaining: debug apply_operator, finish backward search and test everything

--- src/search/pdb_heuristic.h
+++ src/search/pdb_heuristic.h
@@ -12,25 +12,27 @@ public:
 };
 
 class AbstractState {
-    vector<int> variable_values; // contains entries for all variables, value = -10 for variables not contained in the abstraction/pdb
+    vector<int> variable_values;
 public:
-    AbstractState(vector<int> var_vals);
+    map<int, int> variale_to_index_mapping;
+    map<int, int> index_to_variable_mapping;
+    AbstractState(vector<int> var_vals, vector<int> pattern);
     AbstractState(const State &state, vector<int> pattern);
     vector<int> get_variable_values() const;
+    void dump() const;
 };
 
 class Operator;
 typedef pair<int, pair<int, int> > PreEffect;
 class AbstractOperator {
+    Operator *op; // just for debugging purposes
+    vector<int> pattern; // only use: in order to construct new AbstractState, which unfortunately needs a pattern for the moment
     vector<PreEffect> pre_effect;
-    string name;
-    int cost;
 public:
     AbstractOperator(Operator &op, vector<int> pattern);
     bool is_applicable(const AbstractState &abstract_state) const;
     const AbstractState apply_operator(const AbstractState &abstract_state);
-    string get_name() const { return name; }
-    int get_cost() const {return cost; } 
+    void dump() const;
 };
 
 class PDBAbstraction {
@@ -44,11 +46,11 @@ class PDBAbstraction {
 public:
     PDBAbstraction(vector<int> pattern);
     void create_pdb();
-    int get_goal_distance(const AbstractState &abstract_state);
+    int get_heuristic_value(const State &state);
+    void compute_goal_distances();
 };
 
 class PDBHeuristic : public Heuristic {
-    vector<int> pattern;
     PDBAbstraction *pdb_abstraction;
     void verify_no_axioms_no_cond_effects() const;
 protected:
