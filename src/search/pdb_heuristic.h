40531c6b6f44ac2ff1679c4aa5c878c96d4a50f5
Silvan Sievers <silvan.sievers@unibas.ch>
2010-11-24 20:37:54 +0100

    [main] fixed operator bug (prevail) and did some code refacturing; didn't optimize

--- src/search/pdb_heuristic.h
+++ src/search/pdb_heuristic.h
@@ -2,39 +2,46 @@
 #define PDB_HEURISTIC_H
 
 #include "heuristic.h"
-#include <queue>
-using namespace std;
+#include <vector>
+#include <map>
 
 class Edge {
 public:
-    const Operator *op;
+    int cost;
     size_t target;
-    Edge(const Operator *o, size_t t) : op(o), target(t) {}
-};
-
-class AbstractState {
-    map<int, int> variable_values; // maps variable to values
-public:
-    AbstractState(map<int, int> var_vals); // for construction after applying an operator
-    AbstractState(const State &state, const vector<int> &pattern); // for construction from a concrete state
-    map<int, int> get_variable_values() const;
-    bool is_goal(const vector<pair<int, int> > &abstract_goal) const;
-    void dump() const;
+    Edge(int c, size_t t) : cost(c), target(t) {}
 };
 
 class Operator;
-typedef pair<int, pair<int, int> > PreEffect;
+//typedef pair<int, pair<int, int> > PreEffect;
 class AbstractOperator {
-    vector<PreEffect> pre_effect; // pair<int, pair<int, int> > : variable with value before and after operator.
-        // Prevail (see operator.cc) is assumed to be non-existent, as we deal with SAS+ tasks.
+    //vector<PreEffect> pre_effect; // pair<int, pair<int, int> > : variable with value before and after operator.
+    // Prevail (see operator.cc) is assumed to be non-existent, as we deal with SAS+ tasks.
+    std::vector<std::pair<int, int> > conditions;
+    std::vector<std::pair<int, int> > effects;
 public:
-    AbstractOperator(const Operator &op, const vector<int> &pattern);
-    bool is_applicable(const AbstractState &abstract_state) const;
-    AbstractState apply_operator(const AbstractState &abstract_state) const;
+    AbstractOperator(const Operator &op, const std::vector<int> &pattern);
+    const std::vector<std::pair<int, int> > &get_conditions() const { return conditions; }
+    const std::vector<std::pair<int, int> > &get_effects() const { return effects; }
+    //bool is_applicable(const AbstractState &abstract_state) const;
+    //AbstractState apply_operator(const AbstractState &abstract_state) const;
     void dump() const;
 };
 
-typedef pair<int, int> Node;
+class AbstractState {
+    std::map<int, int> variable_values; // maps variable to values
+public:
+    AbstractState(std::map<int, int> var_vals); // for construction after applying an operator
+    AbstractState(const State &state, const std::vector<int> &pattern); // for construction from a concrete state
+    std::map<int, int> get_variable_values() const;
+    int operator[](int index) const { return variable_values.find(index)->second; }
+    bool is_applicable(const AbstractOperator &op) const;
+    void apply_operator(const AbstractOperator &op);
+    bool is_goal_state(const std::vector<std::pair<int, int> > &abstract_goal) const;
+    void dump() const;
+};
+
+typedef std::pair<int, int> Node;
 struct compare {
     bool operator()(Node a, Node b) const {
         return a.second > b.second;
@@ -43,18 +50,18 @@ struct compare {
 #define QUITE_A_LOT 1000000000
 // Impelements a single PDB
 class PDBAbstraction {
-    vector<int> pattern;
-    size_t size; // number of abstract states
-    vector<int> distances; // final h-values for abstract-states
-    vector<vector<Edge > > back_edges; // contains the abstract state space in form of a graph
-    vector<int> n_i; 
-    priority_queue<Node, vector<Node>, compare> pq;
+    std::vector<int> pattern;
+    size_t num_states;
+    std::vector<int> distances; // final h-values for abstract-states
+    //std::vector<std::vector<Edge > > back_edges; // contains the abstract state space in form of a graph
+    std::vector<int> n_i; 
+    //priority_queue<Node, std::vector<Node>, compare> pq;
     void create_pdb(); // builds the graph-structure and everything needed for the backward-search
-    void compute_goal_distances(); // does a dijkstra-backward-search
+    //void compute_goal_distances(); // does a dijkstra-backward-search
     int hash_index(const AbstractState &state) const; // maps an abstract state to an index
     AbstractState inv_hash_index(int index) const; // inverts the hash-index-function
 public:
-    PDBAbstraction(vector<int> pattern);
+    PDBAbstraction(std::vector<int> pattern);
     int get_heuristic_value(const State &state) const; // returns the precomputed h-value (optimal cost 
         // in the abstraction induced by the pattern) for a state
     void dump() const;
@@ -63,24 +70,24 @@ public:
 
 // Implements the canonical heuristic function.
 class CanonicalHeuristic {
-    vector<vector<int> > pattern_collection;
-    vector<vector<int> > cgraph; // conflict graph for the pattern collection
-    vector<vector<int> > max_cliques; // final computed max_cliques
-    vector<int> q_clique;
+    std::vector<std::vector<int> > pattern_collection;
+    std::vector<std::vector<int> > cgraph; // conflict graph for the pattern collection
+    std::vector<std::vector<int> > max_cliques; // final computed max_cliques
+    std::vector<int> q_clique;
     bool are_additive(int pattern1, int pattern2) const;
     void build_cgraph();
-    int get_maxi_vertex(const vector<int> &subg, const vector<int> &cand) const; // TODO find nicer name :)
-    void expand(vector<int> &subg, vector<int> &cand); // implements the CLIQUES-algorithmn from Tomita et al
+    int get_maxi_vertex(const std::vector<int> &subg, const std::vector<int> &cand) const; // TODO find nicer name :)
+    void expand(std::vector<int> &subg, std::vector<int> &cand); // implements the CLIQUES-algorithmn from Tomita et al
 public:
-    CanonicalHeuristic(vector<vector<int> > pat_coll);
+    CanonicalHeuristic(std::vector<std::vector<int> > pat_coll);
     int get_heuristic_value(const State &state) const; // returns the canonical heuristic value (with respect
         // to the pattern collection) for a state
     void dump() const;
 };
 
 class PDBHeuristic : public Heuristic {
-    //PDBAbstraction *pdb_abstraction;
-    CanonicalHeuristic *canonical_heuristic;
+    PDBAbstraction *pdb_abstraction;
+    //CanonicalHeuristic *canonical_heuristic;
     void verify_no_axioms_no_cond_effects() const; // SAS+ tasks only
 protected:
     virtual void initialize();
