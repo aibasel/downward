427ff646fd48ccf38fb03cde5b2e7bfabf4835e5
Manuela Ortlieb <ortlieb@informatik.uni-freiburg.de>
2010-12-12 15:00:13 +0100

    [main] Small changes

--- src/search/pdb_collection_heuristic.h
+++ src/search/pdb_collection_heuristic.h
@@ -1,15 +1,14 @@
-#ifndef CANONICAL_HEURISTIC_H
-#define CANONICAL_HEURISTIC_H
+#ifndef PDBCOLLECTION_HEURISTIC_H
+#define PDBCOLLECTION_HEURISTIC_H
 
 #include "heuristic.h"
 #include "pdb_heuristic.h"
 #include <vector>
+#include <string>
 
 // Implements the canonical heuristic function.
 class PDBCollectionHeuristic : public Heuristic {
-    //PatternCollection *pattern_collection;
     std::vector<std::vector<int> > pattern_collection;
-    //int number_patterns;
     void precompute_additive_vars();
     void compute_max_cliques();
     std::vector<std::vector<int> > cgraph; // compatibility graph for the pattern collection
@@ -19,6 +18,7 @@ class PDBCollectionHeuristic : public Heuristic {
     void build_cgraph();
     int get_maxi_vertex(const std::vector<int> &subg, const std::vector<int> &cand) const; // TODO find nicer name :)
     void max_cliques_expand(std::vector<int> &subg, std::vector<int> &cand, std::vector<int> &q_clique); // implements the CLIQUES-algorithmn from Tomita et al
+    std::vector<PDBHeuristic> pattern_databases; // final pattern databases
 protected:
     virtual void initialize();
     virtual int compute_heuristic(const State &state);
@@ -26,7 +26,6 @@ public:
     PDBCollectionHeuristic();
     virtual ~PDBCollectionHeuristic();
     static ScalarEvaluator *create(const std::vector<std::string> &config, int start, int &end, bool dry_run);
-    std::vector<PDBHeuristic> pattern_databases; // final pattern databases
     void dump() const;
 };
 
