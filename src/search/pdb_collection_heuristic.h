53b0cc2088ccc41bd20d32cfe258f003287b56bb
Silvan Sievers <silvan.sievers@unibas.ch>
2010-12-20 17:04:35 +0100

    [main] continued working on haslum's algorithm

--- src/search/pdb_collection_heuristic.h
+++ src/search/pdb_collection_heuristic.h
@@ -2,12 +2,12 @@
 #define PDBCOLLECTION_HEURISTIC_H
 
 #include "heuristic.h"
-#include "pdb_heuristic.h"
+
 #include <vector>
 
 // Implements the canonical heuristic function.
+class PDBHeuristic;
 class PDBCollectionHeuristic : public Heuristic {
-    //std::vector<std::vector<int> > pattern_collection;
     std::vector<std::vector<PDBHeuristic *> > max_cliques; // final computed max_cliques
     std::vector<std::vector<bool> > are_additive; // variables which are additive
     std::vector<PDBHeuristic *> pattern_databases; // final pattern databases
@@ -18,12 +18,12 @@ protected:
     virtual void initialize();
     virtual int compute_heuristic(const State &state);
 public:
-    PDBCollectionHeuristic();
+    PDBCollectionHeuristic(const std::vector<std::vector<int> > &pattern_collection);
     virtual ~PDBCollectionHeuristic();
     void add_new_pattern(PDBHeuristic *pdb);
+    // checks for all max cliques if they would be additive to this pattern
     void get_max_additive_subsets(const std::vector<int> &new_pattern,
-                                  std::vector<std::vector<PDBHeuristic *> > &max_additive_subsets); // checks for all max cliques if they would be additive to this pattern
-    //static ScalarEvaluator *create(const std::vector<std::string> &config, int start, int &end, bool dry_run);
+                                  std::vector<std::vector<PDBHeuristic *> > &max_additive_subsets);
     void dump(const std::vector<std::vector<int> > &cgraph) const;
 };
 
