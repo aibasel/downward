c735c349ed383f8b75ed80ad3ac55222859143e8
Malte Helmert <malte.helmert@unibas.ch>
2010-12-29 19:39:39 +0100

    [issue120] Added/fixed some comments.

--- src/search/relaxation_heuristic.cc
+++ src/search/relaxation_heuristic.cc
@@ -54,7 +54,7 @@ void RelaxationHeuristic::initialize() {
 }
 
 void RelaxationHeuristic::build_unary_operators(const Operator &op) {
-    int base_cost = op.is_axiom() ? 0 : 1; //op.get_cost();
+    int base_cost = op.is_axiom() ? 0 : 1; //get_adjusted_cost(op);
     const vector<Prevail> &prevail = op.get_prevail();
     const vector<PrePost> &pre_post = op.get_pre_post();
     vector<Proposition *> precondition;
