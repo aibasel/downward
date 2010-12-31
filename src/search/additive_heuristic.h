e372608f4ec050faa7488ec69ed9930347e91080
Malte Helmert <malte.helmert@unibas.ch>
2010-12-31 18:38:15 +0100

    [main] Removed a bogus comment.

--- src/search/additive_heuristic.h
+++ src/search/additive_heuristic.h
@@ -5,13 +5,6 @@
 #include <cassert>
 #include <ext/hash_set>
 
-/*
-  TODO:
-  Some basic data structures, like UnaryOperator, Proposition, etc.,
-  are taken from "ff_heuristic.h". This needs to be refactored,
-  along with the enormous duplication between the two heuristics.
-*/
-
 class AdditiveHeuristic : public RelaxationHeuristic {
     typedef std::vector<Proposition *> Bucket;
     std::vector<Bucket> reachable_queue;
