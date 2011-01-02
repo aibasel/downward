a9a3e849a093a6f3de9d85643399096bf4d97636
Malte Helmert <malte.helmert@unibas.ch>
2011-01-02 18:35:06 +0100

    [issue174] priority_queue.h: Speed up BucketQueue and start work on AdaptiveQueue.

--- src/search/priority_queue.h
+++ src/search/priority_queue.h
@@ -21,6 +21,7 @@ class BucketQueue {
     typedef std::vector<Value> Bucket;
     std::vector<Bucket> buckets;
     mutable size_t current_bucket_no;
+    size_t num_elements;
 
     void update_current_bucket_no() const {
         while (current_bucket_no < buckets.size() &&
@@ -28,13 +29,14 @@ class BucketQueue {
             ++current_bucket_no;
     }
 public:
-    BucketQueue() : current_bucket_no(0) {
+    BucketQueue() : current_bucket_no(0), num_elements(0) {
     }
 
     ~BucketQueue() {
     }
 
     void push(int key, Value value) {
+        ++num_elements;
         if (key >= buckets.size())
             buckets.resize(key + 1);
         else if (key < current_bucket_no)
@@ -43,17 +45,22 @@ public:
     }
 
     std::pair<int, Value> pop() {
-        update_current_bucket_no();
         assert(!empty());
+        assert(num_elements);
+        --num_elements;
+        update_current_bucket_no();
         Bucket &current_bucket = buckets[current_bucket_no];
         Value top_element = current_bucket.back();
         current_bucket.pop_back();
         return std::make_pair(current_bucket_no, top_element);
     }
 
+    size_t size() const {
+        return num_elements;
+    }
+
     bool empty() const {
-        update_current_bucket_no();
-        return current_bucket_no == buckets.size();
+        return num_elements == 0;
     }
 
     void clear() {
@@ -62,24 +69,28 @@ public:
            usage patterns and want to reduce dynamic memory time
            overhead. */
 
-        for (size_t i = 0; i < buckets.size(); ++i)
+        for (size_t i = current_bucket_no; num_elements; ++i) {
+            assert(i < buckets.size());
+            assert(buckets[i].size() <= num_elements);
+            num_elements -= buckets[i].size();
             buckets[i].clear();
+        }
         current_bucket_no = 0;
     }
 
     void clear_and_release_memory() {
         buckets.swap(std::vector<Bucket>());
         current_bucket_no = 0;
+        num_elements = 0;
     }
 };
+
 
 template<typename Value>
 class HeapQueue {
     typedef std::pair<int, Value> Entry;
 
     class compare_func {
-        // TODO: Test impact of using greater<Entry> instead (which
-        //       would use the Value elements for tie-breaking).
     public:
         bool operator()(const Entry &lhs, const Entry &rhs) const {
             return lhs.first > rhs.first;
@@ -113,9 +124,14 @@ public:
     }
 
     std::pair<int, Value> pop() {
+        assert(!heap.empty());
         std::pair<int, Value> result = heap.top();
         heap.pop();
         return result;
+    }
+
+    size_t size() const {
+        return heap.size();
     }
 
     bool empty() const {
@@ -132,9 +148,67 @@ public:
 };
 
 
+/*
+  AdaptiveQueue behaves like a BucketQueue initially, but changes into
+  a HeapQueue once the ratio of elements to buckets becomes too small.
+  Once turned into a HeapQueue, it stays a HeapQueue forever.
+ */
+
 template<typename Value>
 class AdaptiveQueue {
-    // TODO: Implement
+    bool use_bucket_queue;
+    BucketQueue<Value> bucket_queue;
+    HeapQueue<Value> heap_queue;
+
+    AdaptiveQueue() : use_bucket_queue(true) {
+    }
+
+    ~AdaptiveQueue() {
+    }
+
+    void push(int key, Value value) {
+        if (use_bucket_queue) {
+            // TODO: Switch representation if necessary.
+            bucket_queue.push(key, value);
+        } else {
+            heap_queue.push(key, value);
+        }
+    }
+
+    std::pair<int, Value> pop() {
+        if (use_bucket_queue)
+            return bucket_queue.pop();
+        else
+            return heap_queue.pop();
+    }
+
+    size_t size() const {
+        if (use_bucket_queue)
+            return bucket_queue.size();
+        else
+            return heap_queue.size();
+    }
+
+    bool empty() const {
+        if (use_bucket_queue)
+            return bucket_queue.empty();
+        else
+            return heap_queue.empty();
+    }
+
+    void clear() {
+        if (use_bucket_queue)
+            bucket_queue.clear();
+        else
+            heap_queue.clear();
+    }
+
+    void clear_and_release_memory() {
+        if (use_bucket_queue)
+            bucket_queue.clear_and_release_memory();
+        else
+            heap_queue.clear_and_release_memory();
+    }
 };
 
 #endif
