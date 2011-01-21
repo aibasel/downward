b8e77378102b4119322c7d3f3f40ec50fa302b08
Gabi Roeger <gabriele.roeger@unibas.ch>
2011-01-21 11:44:45 +0100

    [main] seq_sat: restart with true action costs when first plan found

--- src/search/seq_sat_portfolio.py
+++ src/search/seq_sat_portfolio.py
@@ -47,46 +47,64 @@ def run(nonunit_configs, unit_configs, timeout=DEFAULT_TIMEOUT):
     else:
         configs = nonunit_configs
 
-    for pos, (relative_time, args) in enumerate(configs):
-        g_bound = "infinity"
-        plan_no = 0
-        try:
-            for line in open("plan_numbers_and_cost"):
-                plan_no += 1
-                g_bound = int(line.split()[1])
-        except IOError:
-            pass
-        print "g bound: %s" % g_bound
-        print "next plan number: %d" % (plan_no + 1)
-        adapted_search = False
-        for index, arg in enumerate(args):
-            if arg == "--search":
-                search_config = args[index + 1]
-                if search_config.startswith("iterated"):
-                    extra_args[1] = plan_file
-                else:
-                    extra_args[1] = "%s.%d" % (plan_file, plan_no + 1)
-                search_config = search_config.replace("BOUND", str(g_bound))
-                search_config = search_config.replace("PLANCOUNTER", str(plan_no))
-                args[index + 1] = search_config
-                adapted_search = True
+    restarted = False
+    while True:
+        heuristic_cost_type = 1
+        search_cost_type = 1
+        second_iteration = False
+        if restarted:
+            search_cost_type = 0
+            heuristic_cost_type = 2
+            second_iteration = True
+        for pos, (relative_time, args) in enumerate(configs):
+            g_bound = "infinity"
+            plan_no = 0
+            try:
+                for line in open("plan_numbers_and_cost"):
+                    plan_no += 1
+                    g_bound = int(line.split()[1])
+            except IOError:
+                pass
+            print "g bound: %s" % g_bound
+            print "next plan number: %d" % (plan_no + 1)
+            adapted_search = False
+            for index, arg in enumerate(args):
+                if arg == "--search":
+                    search_config = args[index + 1]
+                    if search_config.startswith("iterated"):
+                        extra_args[1] = plan_file
+                    else:
+                        extra_args[1] = "%s.%d" % (plan_file, plan_no + 1)
+                    search_config = search_config.replace("BOUND", str(g_bound))
+                    search_config = search_config.replace("PLANCOUNTER", str(plan_no))
+                    args[index + 1] = search_config
+                    adapted_search = True
+                    break
+            assert adapted_search
+            remaining_time = remaining_time_at_start - sum(os.times()[:4])
+            print "remaining time: %s" % remaining_time
+            remaining_relative_time = sum(config[0] for config in configs[pos:])
+            print "config %d: relative time %d, remaining %d" % (
+                pos, relative_time, remaining_relative_time)
+            run_timeout = remaining_time * relative_time / remaining_relative_time
+            print "timeout: %.2f" % run_timeout
+            complete_args = [planner] + args + extra_args
+            print "args: %s" % complete_args
+            sys.stdout.flush()
+            if not os.fork():
+                os.close(0)
+                os.open("output", os.O_RDONLY)
+                if relative_time != remaining_relative_time:
+                    resource.setrlimit(resource.RLIMIT_CPU, (
+                        int(run_timeout), int(run_timeout)))
+                os.execl(planner, *complete_args)
+            os.wait()
+            
+            curr_plan_file = "%s.%d" % (plan_file, plan_no + 1)
+            if (not unitcost and not restarted and
+                os.path.exists(curr_plan_file)):
+                # found a plan -> restart with "true" costs
+                restarted = True
                 break
-        assert adapted_search
-        remaining_time = remaining_time_at_start - sum(os.times()[:4])
-        print "remaining time: %s" % remaining_time
-        remaining_relative_time = sum(config[0] for config in configs[pos:])
-        print "config %d: relative time %d, remaining %d" % (
-            pos, relative_time, remaining_relative_time)
-        run_timeout = remaining_time * relative_time / remaining_relative_time
-        print "timeout: %.2f" % run_timeout
-        complete_args = [planner] + args + extra_args
-        print "args: %s" % complete_args
-        sys.stdout.flush()
-        if not os.fork():
-            os.close(0)
-            os.open("output", os.O_RDONLY)
-            if relative_time != remaining_relative_time:
-                resource.setrlimit(resource.RLIMIT_CPU, (
-                    int(run_timeout), int(run_timeout)))
-            os.execl(planner, *complete_args)
-        os.wait()
+        if second_iteration or unitcost:
+            break
