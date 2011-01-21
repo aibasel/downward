de8866d6ea9573ed1199bb26abd78177c8d2cccb
Gabi Roeger <gabriele.roeger@unibas.ch>
2011-01-21 11:10:29 +0100

    [main] updated fdss-1 portfolio

--- src/search/downward-seq-sat-fdss-1.py
+++ src/search/downward-seq-sat-fdss-1.py
@@ -6,6 +6,71 @@ import seq_sat_portfolio
 # NOTE: when using iterated search included, we must include the option
 #       "plan_counter=PLANCOUNTER"
 
+CONFIGS = [
+    # alt_lazy_ff_cg
+    (49, ["--heuristic", "hff=ff(cost_type=1)",
+         "--heuristic", "hcg=cg(cost_type=1)", "--search",
+         "lazy_greedy(hff,hcg,preferred=(hff,hcg),cost_type=1,bound=BOUND)"]),
+    # lazy_greedy_ff_1
+    (171, ["--heuristic", "h=ff(cost_type=1)",
+          "--search",
+          "lazy_greedy(h,preferred=(h),cost_type=0,bound=BOUND)"]),
+    # alt_lazy_cea_cg
+    (27, ["--heuristic", "hcea=cea(cost_type=1)", 
+         "--heuristic", "hcg=cg(cost_type=1)", "--search",
+         "lazy_greedy(hcea,hcg,preferred=(hcea,hcg),cost_type=1,bound=BOUND)"]),
+    # lazy_wa3_ff_1
+    (340, ["--heuristic", "h=ff(cost_type=1)",
+          "--search",
+          "lazy_wastar(h,w=3,preferred=(h),cost_type=1,bound=BOUND)"]),
+    # alt_eager_ff_cg
+    (76, ["--heuristic", "hff=ff(cost_type=1)",
+         "--heuristic", "hcg=cg(cost_type=1)", "--search",
+         "eager_greedy(hff,hcg,preferred=(hff,hcg),cost_type=1,bound=BOUND)"]),
+    # eager_greedy_ff_1
+    (88, ["--heuristic", "h=ff(cost_type=1)",
+          "--search",
+          "eager_greedy(h,preferred=(h),cost_type=0,bound=BOUND)"]),
+    # alt_eager_ff_add
+    (90, ["--heuristic", "hff=ff(cost_type=1)",
+         "--heuristic", "hadd=add(cost_type=1)", "--search",
+         "eager_greedy(hff,hadd,preferred=(hff,hadd),cost_type=1,bound=BOUND)"]), 
+    # lazy_greedy_cea_1
+    (56, ["--heuristic", "h=cea(cost_type=1)",
+          "--search",
+          "lazy_greedy(h,preferred=(h),cost_type=0,bound=BOUND)"]),
+    # alt_eager_ff_cea_cg
+    (73, ["--heuristic", "hff=ff(cost_type=1)",
+          "--heuristic", "hcea=cea(cost_type=1)",
+          "--heuristic", "hcg=cg(cost_type=1)",
+          "--search",
+          "eager_greedy(hff,hcea,hcg,preferred=(hff,hcea,hcg),cost_type=1,bound=BOUND)"]),
+    # lazy_wa3_add_1
+    (50, ["--heuristic", "h=add(cost_type=1)",
+          "--search",
+          "lazy_wastar(h,w=3,preferred=(h),cost_type=0,bound=BOUND)"]),
+    # eager_greedy_cea_1
+    (84, ["--heuristic", "h=cea(cost_type=1)",
+          "--search",
+          "eager_greedy(h,preferred=(h),cost_type=0,bound=BOUND)"]),
+    # eager_wa3_add_1
+    (166, ["--heuristic", "h=add(cost_type=1)",
+          "--search",
+          "eager(single(sum(g(),weight(h,3))),preferred=(h),cost_type=0,bound=BOUND)"]),
+    # eager_wa3_ff_1
+    (87, ["--heuristic", "h=ff(cost_type=1)",
+          "--search",
+          "eager(single(sum(g(),weight(h,3))),preferred=(h),cost_type=1,bound=BOUND)"]),
+    # lazy_wa3_cg_1
+    (73, ["--heuristic", "h=cg(cost_type=1)",
+         "--search",
+         "lazy_wastar(h,w=3,preferred=(h),cost_type=0,bound=BOUND)"]),
+    # eager_wa3_cg_1
+    (89, ["--heuristic", "h=cg(cost_type=1)",
+          "--search",
+          "eager(single(sum(g(),weight(h,3))),preferred=(h),cost_type=0,bound=BOUND)"]),
+     ]
+
 NONUNIT_CONFIGS = [
     (91, ["--heuristic", "hff=ff(cost_type=1)",
          "--heuristic", "hcg=cg(cost_type=1)", "--search",
@@ -87,5 +152,5 @@ UNIT_CONFIGS = [
          "lazy_wastar(h,w=3,preferred=(h),cost_type=0,bound=BOUND)"]),
      ]
 
-seq_sat_portfolio.run(unit_configs=UNIT_CONFIGS,
-                      nonunit_configs=NONUNIT_CONFIGS)
+seq_sat_portfolio.run(unit_configs=CONFIGS,
+                      nonunit_configs=CONFIGS)
