311cb12bf10af2449355f4f70cb4fda2d920b1ab
Manuela Ortlieb <ortlieb@informatik.uni-freiburg.de>
2011-01-21 13:28:55 +0100

    [main] comment some cout-outputs.

--- src/search/pdb_collection_heuristic.cc
+++ src/search/pdb_collection_heuristic.cc
@@ -86,6 +86,13 @@ void PDBCollectionHeuristic::precompute_additive_vars() {
             }
         }
     }
+    /*cout << "are additive variables matrix" << endl;
+    for (size_t i = 0; i < are_additive.size(); ++i) {
+        for (size_t j = 0; j < are_additive[i].size(); ++j) {
+            cout << are_additive[i][j] << " ";
+        }
+        cout << endl;
+    }*/
 }
 
 void PDBCollectionHeuristic::initialize() {
@@ -190,6 +197,15 @@ ScalarEvaluator *create(const vector<string> &config, int start, int &end, bool 
     for (size_t i = 0; i < g_goal.size(); ++i) {
         pattern_collection.push_back(vector<int>(1, g_goal[i].first));
     }
+    cout << "goals are" << endl;
+    for (size_t i = 0; i < pattern_collection.size(); ++i) {
+        cout << "[ ";
+        for (size_t j = 0; j < pattern_collection[i].size(); ++j) {
+            cout << pattern_collection[i][j] << " ";
+        }
+        cout << "]" << endl;
+    }
+    cout << endl;
     
     return new PDBCollectionHeuristic(pattern_collection);
 }
