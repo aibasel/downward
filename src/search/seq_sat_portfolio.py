5f0551069c79cfa143b32ff5b7856750885ca902
Gabi Roeger <gabriele.roeger@unibas.ch>
2011-01-21 12:11:23 +0100

    [main] seq-sat-portfolio can run a final config forever

--- src/search/seq_sat_portfolio.py
+++ src/search/seq_sat_portfolio.py
@@ -16,8 +16,38 @@ def safe_unlink(filename):
     except EnvironmentError:
         pass
 
+def adapt_search(args, extra_args, search_cost_type, heuristic_cost_type, plan_file):
+    g_bound = "infinity"
+    plan_no = 0
+    try:
+        for line in open("plan_numbers_and_cost"):
+            plan_no += 1
+            g_bound = int(line.split()[1])
+    except IOError:
+        pass
+    for index, arg in enumerate(args):
+        if arg == "--heuristic":
+            heuristic_config = args[index + 1]
+            heuristic_config = heuristic_config.replace("H_COST_TYPE",
+                               str(heuristic_cost_type))
+            args[index + 1] = heuristic_config
+        elif arg == "--search":
+            search_config = args[index + 1]
+            if search_config.startswith("iterated"):
+                extra_args[1] = plan_file
+            else:
+                extra_args[1] = "%s.%d" % (plan_file, plan_no + 1)
+            search_config = search_config.replace("BOUND", str(g_bound))
+            search_config = search_config.replace("PLANCOUNTER", str(plan_no))
+            search_config = search_config.replace("H_COST_TYPE",
+                               str(heuristic_cost_type))
+            search_config = search_config.replace("S_COST_TYPE",
+                               str(search_cost_type))
+            args[index + 1] = search_config
+            break
+    return g_bound, plan_no
 
-def run(nonunit_configs, unit_configs, timeout=DEFAULT_TIMEOUT):
+def run(nonunit_configs, unit_configs, final_config=None, timeout=DEFAULT_TIMEOUT):
     extra_args = sys.argv[1:]
     assert len(extra_args) == 4, extra_args
     assert extra_args[0] in ["unit", "nonunit"], extra_args
@@ -48,39 +78,19 @@ def run(nonunit_configs, unit_configs, timeout=DEFAULT_TIMEOUT):
         configs = nonunit_configs
 
     restarted = False
+    heuristic_cost_type = 1
+    search_cost_type = 1
     while True:
-        heuristic_cost_type = 1
-        search_cost_type = 1
         second_iteration = False
         if restarted:
             search_cost_type = 0
             heuristic_cost_type = 2
             second_iteration = True
         for pos, (relative_time, args) in enumerate(configs):
-            g_bound = "infinity"
-            plan_no = 0
-            try:
-                for line in open("plan_numbers_and_cost"):
-                    plan_no += 1
-                    g_bound = int(line.split()[1])
-            except IOError:
-                pass
+            g_bound, plan_no = adapt_search(args, extra_args, search_cost_type, 
+                                            heuristic_cost_type, plan_file)
             print "g bound: %s" % g_bound
             print "next plan number: %d" % (plan_no + 1)
-            adapted_search = False
-            for index, arg in enumerate(args):
-                if arg == "--search":
-                    search_config = args[index + 1]
-                    if search_config.startswith("iterated"):
-                        extra_args[1] = plan_file
-                    else:
-                        extra_args[1] = "%s.%d" % (plan_file, plan_no + 1)
-                    search_config = search_config.replace("BOUND", str(g_bound))
-                    search_config = search_config.replace("PLANCOUNTER", str(plan_no))
-                    args[index + 1] = search_config
-                    adapted_search = True
-                    break
-            assert adapted_search
             remaining_time = remaining_time_at_start - sum(os.times()[:4])
             print "remaining time: %s" % remaining_time
             remaining_relative_time = sum(config[0] for config in configs[pos:])
@@ -106,5 +116,20 @@ def run(nonunit_configs, unit_configs, timeout=DEFAULT_TIMEOUT):
                 # found a plan -> restart with "true" costs
                 restarted = True
                 break
-        if second_iteration or unitcost:
+        if final_config and (second_iteration or unitcost):
             break
+
+    # run final config without time limit
+    g_bound, plan_no = adapt_search(final_config, extra_args, search_cost_type,
+                                    heuristic_cost_type, plan_file)
+    print "g bound: %s" % g_bound
+    print "next plan number: %d" % (plan_no + 1)
+    complete_args = [planner] + args + extra_args
+    print "args: %s" % complete_args
+    sys.stdout.flush()
+    if not os.fork():
+        os.close(0)
+        os.open("output", os.O_RDONLY)
+        os.execl(planner, *complete_args)
+    os.wait()
+
