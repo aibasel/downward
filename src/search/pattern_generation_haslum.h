7d0df367dd44e9ab44111cd94b697d601e9112d0
Silvan Sievers <silvan.sievers@unibas.ch>
2011-01-27 13:58:08 +0100

    [main] extra method instead of inner loops

--- src/search/pattern_generation_haslum.h
+++ src/search/pattern_generation_haslum.h
@@ -5,6 +5,7 @@
 
 class PDBCollectionHeuristic;
 class State;
+class PDBHeuristic;
 // Implementation of the pattern generation algorithm by Haslum et al.
 class PatternGenerationHaslum {
     int max_pdb_size;
@@ -16,6 +17,11 @@ class PatternGenerationHaslum {
                              std::vector<std::vector<int> > &candidate_patterns);*/
     void generate_candidate_patterns(const std::vector<int> &pattern,
                                      std::vector<std::vector<int> > &candidate_patterns);
+    void counting_approximation(PDBHeuristic &pdbheuristic,
+                                const State &sample,
+                                PDBCollectionHeuristic *current_collection,
+                                std::vector<std::vector<PDBHeuristic *> > &max_additive_subsets,
+                                int &count);
     void hill_climbing();
 public:
     PatternGenerationHaslum(int max_pdb_size, int max_collection_size, int num_samples);
