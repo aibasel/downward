e984dd56b07680ef16e2f8f9845c97e2e4024540
Silvan Sievers <silvan.sievers@unibas.ch>
2011-02-16 17:30:31 +0100

    [main] continued genectic algorithms

--- src/search/pattern_generation_edelkamp.cc
+++ src/search/pattern_generation_edelkamp.cc
@@ -1,11 +1,14 @@
 #include "pattern_generation_edelkamp.h"
 #include "globals.h"
 #include "pdb_collection_heuristic.h"
+#include "pdb_heuristic.h"
 #include "plugin.h"
+#include "rng.h"
 
 #include <algorithm>
 #include <cassert>
 #include <iostream>
+#include <limits>
 #include <vector>
 
 using namespace std;
@@ -13,7 +16,8 @@ using namespace std;
 static ScalarEvaluator *create(const vector<string> &config, int start, int &end, bool dry_run);
 static ScalarEvaluatorPlugin pattern_generation_edelkamp_plugin("gapdb", create);
 
-PatternGenerationEdelkamp::PatternGenerationEdelkamp(int initial_pdb_max_size) {
+PatternGenerationEdelkamp::PatternGenerationEdelkamp(int initial_pdb_max_size, int mcn)
+    : max_collection_number(mcn) {
     initialize(initial_pdb_max_size);
 }
 
@@ -27,27 +31,33 @@ void PatternGenerationEdelkamp::initialize(int initial_pdb_max_size) {
         variables[i] = i;
     }
     
-    int MAX = 1;
-    vector<vector<vector<int> > > pattern_collections;
-    for (size_t num_pcs = 0; num_pcs < MAX; ++num_pcs) {
-        vector<vector<int> > pattern_collection;
+    for (size_t num_pcs = 0; num_pcs < max_collection_number; ++num_pcs) {
+        vector<vector<bool> > pattern_collection;
         int i = 0;
+        int current_size = 1;
+        vector<bool> pattern;
+        pattern.resize(g_variable_name.size(), false);
         while (i < variables.size()) {
-            int current_size = 1;
-            vector<int> pattern;
-            while (i < variables.size() && current_size * g_variable_domain[variables[i]] < initial_pdb_max_size) {
-                pattern.push_back(variables[i]);
+            if (current_size * g_variable_domain[variables[i]] < initial_pdb_max_size) {
+                //pattern.push_back(variables[i]);
+                pattern[variables[i]] = true;
                 cout << "pushing variable " << variables[i] << endl;
                 current_size *= g_variable_domain[variables[i]];
                 ++i;
+            } else if (!pattern.empty()) {
+                pattern_collection.push_back(pattern);
+                cout << "pushing pattern into collection:" << endl;
+                for (size_t j = 0; j < pattern.size(); ++j) {
+                    cout << pattern[j] << " ";
+                }
+                cout << endl;
+                current_size = 1;
+                pattern.clear();
+                pattern.resize(g_variable_name.size(), false);
+            } else { //pattern.empty()
+                cout << "no more variables fitted into pattern" << endl;
+                ++i;
             }
-            assert(!pattern.empty());
-            pattern_collection.push_back(pattern);
-            cout << "pushing pattern collection:" << endl;
-            for (size_t j = 0; j < pattern.size(); ++j) {
-                cout << pattern[j] << " ";
-            }
-            cout << endl;
         }
         pattern_collections.push_back(pattern_collection);
         random_shuffle(variables.begin(), variables.end());
@@ -57,21 +67,105 @@ void PatternGenerationEdelkamp::initialize(int initial_pdb_max_size) {
         }
         cout << endl;
     }
-    
-    cout << "finished" << endl;
-    current_collection = new PDBCollectionHeuristic(pattern_collections[0]);
-    cout << "test" << endl;
+}
+
+void PatternGenerationEdelkamp::recombine() {
+    for (size_t i = 1; i < pattern_collections.size(); i += 2) {
+        vector<vector<bool> > collection1;
+        vector<vector<bool> > collection2;
+        for (size_t j = 0; j < pattern_collections[i-1].size(); ++j) {
+            if (j < pattern_collections[i-1].size() / 2)
+                collection1.push_back(pattern_collections[i-1][j]);
+            else
+                collection2.push_back(pattern_collections[i-1][j]);
+        }
+        for (size_t j = 0; j < pattern_collections[i].size(); ++j) {
+            if (j < pattern_collections[i].size() / 2)
+                collection1.push_back(pattern_collections[i][j]);
+            else
+                collection2.push_back(pattern_collections[i][j]);
+        }
+        pattern_collections.push_back(collection1);
+        pattern_collections.push_back(collection2);
+    }
+}
+
+void PatternGenerationEdelkamp::mutate() {
+    for (size_t i = 0; i < pattern_collections.size(); ++i) {
+        for (size_t j = 0; j < pattern_collections[i].size(); ++j) {
+            vector<bool> &pattern = pattern_collections[i][j];
+            for (size_t k = 0; k < pattern.size(); ++k) {
+                double random = g_rng(); // [0..1)
+                if (random < 0.01)
+                    pattern[k] = !pattern[k];
+            }
+        }
+    }
+}
+
+void PatternGenerationEdelkamp::evaluate(vector<pair<int, int> > &fitness_values) {
+    for (size_t i = 0; i < pattern_collections.size(); ++i) {
+        double fitness = 0;
+        for (size_t j = 0; j < pattern_collections[i].size(); ++j) {
+            vector<int> pattern;
+            for (size_t k = 0; k < pattern_collections[i][j].size(); ++k) {
+                if (pattern_collections[i][j][k])
+                    pattern.push_back(k);
+            }
+            PDBHeuristic pdb_heuristic(pattern);
+            const vector<int> &h_values = pdb_heuristic.get_h_values();
+            int sum = 0;
+            for (size_t k = 0; k < h_values.size(); ++k) {
+                if (h_values[k] == numeric_limits<int>::max())
+                    continue;
+                sum += h_values[k];
+            }
+            double mean_h = sum / h_values.size();
+            fitness += mean_h;
+        }
+        fitness_values.push_back(make_pair(fitness, i));
+    }
+    sort(fitness_values.begin(), fitness_values.end());
+}
+
+void PatternGenerationEdelkamp::select(const vector<pair<int, int> > &fitness_values) {
+    // TODO: how to understand the paper: is the normalized fitness function a probability distribution?
+    // if yes, how to chose the right number of pattern collections?
+    // for the moment, we just take the x best pattern collections, where x is the number of collections
+    // we should have.
+    vector<vector<vector<bool> > > new_pattern_collections;
+    for (size_t i = fitness_values.size() - 1; i > fitness_values.size() - max_collection_number; --i) {
+        new_pattern_collections.push_back(pattern_collections[fitness_values[i].second]);
+    }
+    pattern_collections = new_pattern_collections;
+}
+
+PDBCollectionHeuristic *PatternGenerationEdelkamp::get_pattern_collection_heuristic() {
+    // TODO: this is not final
+    const vector<vector<bool> > &best_collection = pattern_collections[0];
+    vector<vector<int> > pattern_collection;
+    for (size_t j = 0; j < best_collection.size(); ++j) {
+        vector<int> pattern;
+        for (size_t k = 0; k < best_collection[j].size(); ++k) {
+            if (best_collection[j][k])
+                pattern.push_back(k);
+        }
+        // TODO: handle empty patterns (probably just remove them)
+        assert(!pattern.empty());
+        pattern_collection.push_back(pattern);
+    }
+    return new PDBCollectionHeuristic(pattern_collection);
 }
 
 ScalarEvaluator *create(const vector<string> &config, int start, int &end, bool dry_run) {
     int initial_pdb_max_size = 32;
-    //int max_collection_size = 20000000;
+    int max_collection_number = 10;
     if (config.size() > start + 2 && config[start + 1] == "(") {
         end = start + 2;
         if (config[end] != ")") {
             NamedOptionParser option_parser;
-            option_parser.add_int_option("max_pdb_size", &initial_pdb_max_size, "maximum size per pdb");
-            //option_parser.add_int_option("max_collection_size", &max_collection_size, "max collection size");
+            option_parser.add_int_option("initial_pdb_max_size", &initial_pdb_max_size, "initial max size for pdbs");
+            option_parser.add_int_option("max_collection_number", &max_collection_number, "max number of pattern collections");
             option_parser.parse_options(config, end, end, dry_run);
             end++;
         }
@@ -86,11 +180,15 @@ ScalarEvaluator *create(const vector<string> &config, int start, int &end, bool 
         cerr << "error: size per pdb must be at least 10" << endl;
         exit(2);
     }
+    if (max_collection_number < 1) {
+        cerr << "error: number of pattern collections must be at least 1" << endl;
+        exit(2);
+    }
     
     if (dry_run)
         return 0;
     
-    PatternGenerationEdelkamp pge(initial_pdb_max_size);
+    PatternGenerationEdelkamp pge(initial_pdb_max_size, max_collection_number);
     cout << "Edelkamp et al. done." << endl;
     return pge.get_pattern_collection_heuristic();
 }
--- src/search/pattern_generation_edelkamp.h
+++ src/search/pattern_generation_edelkamp.h
@@ -1,15 +1,22 @@
 #ifndef PATTERN_GENERATION_EDELKAMP_H
 #define PATTERN_GENERATION_EDELKAMP_H
 
+#include <vector>
+
 class PDBCollectionHeuristic;
 // Implementation of the pattern generation algorithm by Edelkamp et al.
 class PatternGenerationEdelkamp {
-    PDBCollectionHeuristic *current_collection;
+    int max_collection_number;
+    std::vector<std::vector<std::vector<bool> > > pattern_collections;
     void initialize(int initial_pdb_max_size);
+    void recombine();
+    void mutate();
+    void evaluate(std::vector<std::pair<int, int> > &fitness_values);
+    void select(const std::vector<std::pair<int, int> > &fitness_values);
 public:
-    PatternGenerationEdelkamp(int initial_pdb_max_size);
+    PatternGenerationEdelkamp(int initial_pdb_max_size, int max_collection_number);
     virtual ~PatternGenerationEdelkamp();
-    PDBCollectionHeuristic *get_pattern_collection_heuristic() { return current_collection; }
+    PDBCollectionHeuristic *get_pattern_collection_heuristic();
 };
 
 #endif
