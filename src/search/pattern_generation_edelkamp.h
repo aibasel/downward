29912302109bb5903a96f0a12bb4511543acda62
Silvan Sievers <silvan.sievers@unibas.ch>
2011-03-01 18:40:53 +0100

    [main] no more recombine, using select with probabilities, refactored

--- src/search/pattern_generation_edelkamp.h
+++ src/search/pattern_generation_edelkamp.h
@@ -6,20 +6,29 @@
 class PDBCollectionHeuristic;
 // Implementation of the pattern generation algorithm by Edelkamp
 class PatternGenerationEdelkamp {
-    int max_collection_number;
-    std::vector<std::vector<std::vector<bool> > > pattern_collections;
-    void initialize(int initial_pdb_max_size); // bin packing with variables to determine
-    // initial pattern collections of a give size
-    void recombine();
-    void mutate(); // flip bits (= variables) with a small probability
-    void evaluate(std::vector<std::pair<int, int> > &fitness_values) const; // calculate the mean h-value
-    // (fitness function) for each pattern collection
-    void select(const std::vector<std::pair<int, int> > &fitness_values); // select each pattern collection
-    // with a probability according to its fitness function value
+    int pdb_max_size;
+    int num_collections;
+    std::vector<std::vector<std::vector<bool> > > pattern_collections; // all current pattern collections
+    //std::vector<std::vector<bool> > best_collection; // remember the best pattern collection over all episodes
+    void initialize(); // bin packing (for variables) to determine initial pattern collections
+    //void recombine();
+    void mutate(double probability); // flip bits (= variables) with a given probability
+
+    // calculate the mean h-value (fitness function) for each pattern collection
+    void evaluate(std::vector<double> &fitness_values) const;
+
+    // select each pattern collection with a probability (obtained by normalizing the fitness function)
+    void select(const std::vector<double> &fitness_values);
+
+    // transforms a vector of bools (pattern representation in this algorithm) to the "normal" pattern form
+    // vector<int> (needed for PDBHeuristic)
+    void transform_to_pattern_normal_form(const std::vector<bool> &bitvector, std::vector<int> &pattern) const;
 public:
-    PatternGenerationEdelkamp(int initial_pdb_max_size, int max_collection_number, int num_episodes);
+    PatternGenerationEdelkamp(int pdb_max_size, int num_collections, int num_episodes,
+                              double mutation_probability);
     virtual ~PatternGenerationEdelkamp();
     void dump() const;
+
     PDBCollectionHeuristic *get_pattern_collection_heuristic() const;
 };
 
