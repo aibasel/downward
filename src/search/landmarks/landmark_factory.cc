fb869ebe00a678fa492c076a45c503561cc1015a
Silvan Sievers <silvan.sievers@unibas.ch>
2011-03-03 14:12:55 +0100

    [issue139] adapted comments to the new class names

--- src/search/landmarks/landmark_factory.cc
+++ src/search/landmarks/landmark_factory.cc
@@ -18,11 +18,13 @@ LandmarkGraph *LandmarkFactory::compute_lm_graph() {
     generate();
     if (lm_graph->number_of_landmarks() == 0)
         cout << "Warning! No landmarks found. Task unsolvable?" << endl;
-    cout << "Discovered " << lm_graph->number_of_landmarks()
-    << " landmarks, of which " << lm_graph->number_of_disj_landmarks()
-    << " are disjunctive and "
-    << lm_graph->number_of_conj_landmarks() << " are conjunctive \n"
-    << lm_graph->number_of_edges() << " edges\n";
+    else {
+        cout << "Discovered " << lm_graph->number_of_landmarks()
+        << " landmarks, of which " << lm_graph->number_of_disj_landmarks()
+        << " are disjunctive and "
+        << lm_graph->number_of_conj_landmarks() << " are conjunctive \n"
+        << lm_graph->number_of_edges() << " edges\n";
+    }
     return lm_graph;
 }
 
