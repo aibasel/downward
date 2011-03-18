14730bb2bb4b1917ce3148c5ab8bb8c3a4b9412b
Malte Helmert <malte.helmert@unibas.ch>
2011-03-18 21:33:55 +0100

    [main] Uncrustified.

--- src/search/landmarks/h_m_landmarks.cc
+++ src/search/landmarks/h_m_landmarks.cc
@@ -740,10 +740,10 @@ void HMLandmarks::compute_h_m_landmarks() {
 
     // mark actions with no precondition to be applied
     for (int i = 0; i < pm_ops_.size(); i++) {
-      if (unsat_pc_count_[i].first == 0) {
-	// create empty set or clear prev entries
-	current_trigger[i].clear();
-      }
+        if (unsat_pc_count_[i].first == 0) {
+            // create empty set or clear prev entries
+            current_trigger[i].clear();
+        }
     }
 
     std::vector<int>::iterator it;
