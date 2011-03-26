3581b1a5b950d346914f28f3bd145fc9c27cd592
Moritz Gronbach <meismo@arcor.de>
2011-03-26 19:50:24 +0100

    [issue104] fixed some parsing bugs and translated the downward-seq-*.py files

--- src/search/downward-seq-opt-fdss-1.py
+++ src/search/downward-seq-opt-fdss-1.py
@@ -9,7 +9,7 @@ CONFIGS = [
     (432, ["--search",
            "astar(mas(max_states=200000,merge_strategy=5,shrink_strategy=7))"]),
     (455, ["--search",
-           "astar(lmcount(lm_merged(lm_rhw(),lm_hm(m=1)),admissible=true),mpd=true)"]),
+           "astar(lmcount([lm_merged(lm_rhw(),lm_hm(m=1)]),admissible=true),mpd=true)"]),
     (569, ["--search",
            "astar(lmcut())"]),
      ]
--- src/search/downward-seq-sat-fdss-2.py
+++ src/search/downward-seq-sat-fdss-2.py
@@ -10,35 +10,35 @@ CONFIGS = [
     # eager_greedy_ff
     (330, ["--heuristic", "h=ff(cost_type=H_COST_TYPE)",
           "--search",
-          "eager_greedy(h,preferred=(h),cost_type=S_COST_TYPE,bound=BOUND)"]),
+          "eager_greedy(h,preferred=h,cost_type=S_COST_TYPE,bound=BOUND)"]),
     # lazy_greedy_ff
     (411, ["--heuristic", "h=ff(cost_type=H_COST_TYPE)",
           "--search",
-          "lazy_greedy(h,preferred=(h),cost_type=S_COST_TYPE,bound=BOUND)"]),
+          "lazy_greedy(h,preferred=h,cost_type=S_COST_TYPE,bound=BOUND)"]),
     # eager_greedy_cea
     (213, ["--heuristic", "h=cea(cost_type=H_COST_TYPE)",
           "--search",
-          "eager_greedy(h,preferred=(h),cost_type=S_COST_TYPE,bound=BOUND)"]),
+          "eager_greedy(h,preferred=h,cost_type=S_COST_TYPE,bound=BOUND)"]),
     # lazy_greedy_cea
     (57, ["--heuristic", "h=cea(cost_type=H_COST_TYPE)",
           "--search",
-          "lazy_greedy(h,preferred=(h),cost_type=S_COST_TYPE,bound=BOUND)"]),
+          "lazy_greedy(h,preferred=h,cost_type=S_COST_TYPE,bound=BOUND)"]),
     # eager_greedy_add
     (204, ["--heuristic", "h=add(cost_type=H_COST_TYPE)",
           "--search",
-          "eager_greedy(h,preferred=(h),cost_type=S_COST_TYPE,bound=BOUND)"]),
+          "eager_greedy(h,preferred=h,cost_type=S_COST_TYPE,bound=BOUND)"]),
     # eager_greedy_cg
     (208, ["--heuristic", "h=cg(cost_type=H_COST_TYPE)",
           "--search",
-          "eager_greedy(h,preferred=(h),cost_type=S_COST_TYPE,bound=BOUND)"]),
+          "eager_greedy(h,preferred=h,cost_type=S_COST_TYPE,bound=BOUND)"]),
     # lazy_greedy_cg
     (109, ["--heuristic", "h=cg(cost_type=H_COST_TYPE)",
           "--search",
-          "lazy_greedy(h,preferred=(h),cost_type=S_COST_TYPE,bound=BOUND)"]),
+          "lazy_greedy(h,preferred=h,cost_type=S_COST_TYPE,bound=BOUND)"]),
     # lazy_greedy_add
     (63, ["--heuristic", "h=add(cost_type=H_COST_TYPE)",
           "--search",
-          "lazy_greedy(h,preferred=(h),cost_type=S_COST_TYPE,bound=BOUND)"]),
+          "lazy_greedy(h,preferred=h,cost_type=S_COST_TYPE,bound=BOUND)"]),
      ]
 
 def build_final_config(successful_args):
@@ -51,13 +51,13 @@ def build_final_config(successful_args):
             for weight in (5,3,2,1):
                 if orig_search.startswith("lazy"):
                     sub_search = \
-                        "lazy_wastar(h,preferred=(h),w=%d,cost_type=S_COST_TYPE)" % weight
+                        "lazy_wastar(h,preferred=h,w=%d,cost_type=S_COST_TYPE)" % weight
                 else:
                     sub_search = \
-                        "eager(single(sum(g(),weight(h,%d))),preferred=(h),cost_type=S_COST_TYPE)" % weight
+                        "eager(single(sum([g(),weight(h,%d)])),preferred=h,cost_type=S_COST_TYPE)" % weight
                 sub_searches.append(sub_search)
             sub_search_string = ",".join(sub_searches)
-            new_search = "iterated(%s,bound=BOUND,repeat_last=true,plan_counter=PLANCOUNTER)" % sub_search_string
+            new_search = "iterated([%s],bound=BOUND,repeat_last=true,plan_counter=PLANCOUNTER)" % sub_search_string
             new_args[pos + 1] = new_search
             break
     return new_args
