ec6e085b3669722895806c521579c3602b6b7ece
Malte Helmert <malte.helmert@unibas.ch>
2011-05-18 17:51:55 +0200

    [main] utilities.h: added a template for dumping vectors (operator <<).

--- src/search/utilities.h
+++ src/search/utilities.h
@@ -1,9 +1,26 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
+#include <vector>
+#include <ostream>
+
 extern void register_event_handlers();
 
 extern int get_peak_memory_in_kb();
 extern void print_peak_memory();
+
+namespace std {
+    template<class T>
+    ostream &operator<<(ostream &stream, const vector<T> &vec) {
+        stream << "[";
+        for (size_t i = 0; i < vec.size(); ++i) {
+            if (i != 0)
+                stream << ", ";
+            stream << vec[i];
+        }
+        stream << "]";
+        return stream;
+    }
+}
 
 #endif
