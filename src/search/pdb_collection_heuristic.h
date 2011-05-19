4f70e2548716759df7af5a90c7ac5c05c8e44c19
Manuela Ortlieb <ortlieb@informatik.uni-freiburg.de>
2011-05-19 14:20:36 +0200

    [main] Cleaned up pdb_collection_heuristic.

--- src/search/pdb_collection_heuristic.h
+++ src/search/pdb_collection_heuristic.h
@@ -1,5 +1,5 @@
-#ifndef PDBCOLLECTION_HEURISTIC_H
-#define PDBCOLLECTION_HEURISTIC_H
+#ifndef PDB_COLLECTION_HEURISTIC_H
+#define PDB_COLLECTION_HEURISTIC_H
 
 #include "heuristic.h"
 
@@ -8,16 +8,18 @@
 // Implements the canonical heuristic function.
 class PDBHeuristic;
 class PDBCollectionHeuristic : public Heuristic {
-    int size;
+    int size; // the sum of all abstract state sizes of all pdbs in the collection
     std::vector<std::vector<PDBHeuristic *> > max_cliques; // final computed max_cliques
     std::vector<std::vector<bool> > are_additive; // variables which are additive
     std::vector<PDBHeuristic *> pattern_databases; // final pattern databases
-    bool are_pattern_additive(const std::vector<int> &patt1, const std::vector<int> &patt2) const;
+    bool are_patterns_additive(const std::vector<int> &patt1, 
+                              const std::vector<int> &patt2) const;
     void compute_max_cliques();
     void compute_additive_vars();
 
     void _add_pattern(const std::vector<int> &pattern);
     // does not recompute max_cliques
+    void dump_cgraph(const std::vector<std::vector<int> > &cgraph) const;
 protected:
     virtual void initialize();
     virtual int compute_heuristic(const State &state);
@@ -31,7 +33,7 @@ public:
                                   std::vector<std::vector<PDBHeuristic *> > &max_additive_subsets);
     const std::vector<PDBHeuristic *> &get_pattern_databases() const { return pattern_databases; }
     int get_size() const { return size; }
-    void dump(const std::vector<std::vector<int> > &cgraph) const;
+    void dump() const;
 };
 
 #endif
