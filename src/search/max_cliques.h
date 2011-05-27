e3a267ff046a8a9148c8edc5a9abce5fbb1f52e7
Manuela Ortlieb <ortlieb@informatik.uni-freiburg.de>
2011-05-27 15:08:57 +0200

    [main] Some small changes.

--- src/search/max_cliques.h
+++ src/search/max_cliques.h
@@ -3,7 +3,12 @@
 
 #include <vector>
 
-// Implementation of the Max Cliques algorithm by Tomita et al.
+/* Implementation of the Max Cliques algorithm by Tomita et al.
+   In the paper the authors use a compressed output of the cliques, such that the
+   algorithm is in O(3^{n/3}).
+   This implementation is in O(n 3^{n/3}) because the cliques are outputed explicitely.
+   For a better runtime it could be useful to use bitvectors instead of vectors.
+ */
 extern void compute_max_cliques(
     const std::vector<std::vector<int> > &graph,
     std::vector<std::vector<int> > &max_cliques);
