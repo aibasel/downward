da890c03a704d687f27a28aebb096e4b4d6cb5f8
Moritz Gronbach <meismo@arcor.de>
2011-08-15 13:02:41 +0200

    [issue211] removed unused methods

--- src/search/raz_mas_heuristic.h
+++ src/search/raz_mas_heuristic.h
@@ -36,17 +36,6 @@ class MergeAndShrinkHeuristic : public Heuristic {
     void dump_options() const;
     void warn_on_unusual_options() const;
 
-    //Gives an approximation on the number of extra transitions added to the composed system,
-    //composed of abstraction and the relevant atomic abstractions, if relevant_ops are reduced.
-    int approximate_added_transitions(
-        const std::vector<int> &transitions_to_reduce, const std::vector<
-            int> &relevant_atomic_abs, const Abstraction *abstraction,
-        const std::vector<Abstraction *> &atomic_abstractions) const;
-
-    void get_ops_to_reduce(Abstraction *abs1, Abstraction *abs2, std::vector<
-                               std::vector<const Operator *> > &result, int reduce_strategy);
-    int get_distance_from_op(const Operator *op, const Operator *other_op);
-
 protected:
     virtual void initialize();
     virtual int compute_heuristic(const State &state);
