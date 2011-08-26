1d2ddc8a9ef350bac00eefca5c6b64057cc9b919
Malte Helmert <malte.helmert@unibas.ch>
2011-08-26 16:36:16 +0200

    [issue268] Adapted scripts to new merge-and-shrink option names.

--- src/search/downward-seq-opt-merge-and-shrink.py
+++ src/search/downward-seq-opt-merge-and-shrink.py
@@ -5,9 +5,9 @@ import seq_opt_portfolio
 
 CONFIGS = [
     (800, ["--search",
-           "astar(mas(max_states=1,merge_strategy=merge_linear_reverse_level,shrink_strategy=shrink_bisimulation(greedy=true,memory_limit=false)))"]),
+           "astar(merge_and_shrink(merge_strategy=merge_linear_reverse_level,shrink_strategy=shrink_bisimulation(max_states=infinity,threshold=1,greedy=true,initialize_by_h=false,group_by_h=false)))"]),
     (1000, ["--search",
-           "astar(mas(max_states=200000,merge_strategy=merge_linear_reverse_level,shrink_strategy=shrink_dfp(enable_greedy_bisimulation)))"]),
+           "astar(merge_and_shrink(merge_strategy=merge_linear_reverse_level,shrink_strategy=shrink_bisimulation(max_states=200000,greedy=false,initialize_by_h=true,group_by_h=true)))"]),
      ]
 
 seq_opt_portfolio.run(CONFIGS)
