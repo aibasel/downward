1dbccf8ecad8d9abc04772345a9b50ce24f93e83
Moritz Gronbach <meismo@arcor.de>
2011-09-01 20:56:43 +0200

    [issue232] refactored the printing methods into classes

--- src/search/option_parser_util.cc
+++ src/search/option_parser_util.cc
@@ -81,3 +81,140 @@ vector<string> DocStore::get_types() {
 }
 
 
+DocPrinter::DocPrinter(ostream &out)
+    :os(out) {
+}
+
+DocPrinter::~DocPrinter() {
+}
+
+
+void DocPrinter::print_all() {
+    DocStore *ds = DocStore::instance();
+    vector<string> types = ds->get_types();
+    for(size_t n(0); n != types.size(); ++n) {
+        print_category(types[n]);
+    }        
+}
+
+void DocPrinter::print_category(string category_name) {
+    print_category_header(category_name);
+    DocStore *ds = DocStore::instance();
+    vector<string> keys = ds->get_keys();
+    for(size_t i(0); i != keys.size(); ++i) {
+        DocStruct info = ds->get(keys[i]);
+        if(info.type.compare(category_name) != 0)
+            continue;
+        print_element(keys[i], info);
+    }
+    print_category_footer();
+}
+
+Txt2TagsPrinter::Txt2TagsPrinter(ostream &out)
+    :DocPrinter(out) {
+}
+
+Txt2TagsPrinter::~Txt2TagsPrinter() {
+}
+
+void Txt2TagsPrinter::print_element(string call_name, DocStruct &info) {
+    os << "== " << info.full_name << " ==" << endl
+         << info.synopsis << endl
+         << "``` " << call_name << "(";
+    for(size_t j(0); j != info.arg_help.size(); ++j){
+        ArgumentInfo arg = info.arg_help[j];
+        os << arg.kwd;
+        if(arg.default_value.compare("") != 0){
+            os << " = " << arg.default_value;
+        }
+        if(j != info.arg_help.size() - 1)
+            os << ", ";
+    }
+    os << ")" << endl << endl << endl;
+    for(size_t j(0); j != info.arg_help.size(); ++j){
+        ArgumentInfo arg = info.arg_help[j];
+        os << "- //" << arg.kwd << "// (" 
+             << arg.type_name << "): "
+             << arg.help << endl;
+        if(!arg.value_explanations.empty()) {
+            for(size_t k(0); k != arg.value_explanations.size(); ++k) {
+                pair<string, string> explanation = 
+                    arg.value_explanations[k];
+                os << " - //" << explanation.first << "//: "
+                     << explanation.second << endl;
+            }
+        }
+    }
+    //notes:
+    for(size_t j(0); j != info.notes.size(); ++j) {
+        NoteInfo note = info.notes[j];
+        os << "**" << note.name << ":** "
+             << note.description << endl << endl;
+    }            
+    //language features:
+    if(!info.support_help.empty()) {
+        os << "Language features supported:" << endl;
+    }
+    for(size_t j(0); j != info.support_help.size(); ++j) {
+        LanguageSupportInfo ls = info.support_help[j];
+        os << "- **" << ls.feature << ":** "
+             << ls.description << endl;
+    }
+    //properties:
+    if(!info.property_help.empty()) {
+        os << "Properties:" << endl;
+    }
+    for(size_t j(0); j != info.property_help.size(); ++j) {
+        PropertyInfo p = info.property_help[j];
+        os << "- **" << p.property << ":** "
+             << p.description << endl;
+    }   
+}
+
+void Txt2TagsPrinter::print_category_header(string category_name) {
+    os << ">>>>CATEGORY: " << category_name << "s" << "<<<<" << endl;
+}
+
+void Txt2TagsPrinter::print_category_footer() {
+    os << endl
+       << ">>>>CATEGORYEND<<<<" << endl;
+}
+
+PlainPrinter::PlainPrinter(ostream &out)
+    :DocPrinter(out) {
+}
+
+PlainPrinter::~PlainPrinter() {
+}
+
+void PlainPrinter::print_element(string call_name, DocStruct &info) {
+    os << info.full_name << " is a " << info.type << "." << endl
+         << info.synopsis << endl
+         << "Usage:" << endl
+         << call_name << "(";
+    for(size_t j(0); j != info.arg_help.size(); ++j){
+        os << info.arg_help[j].kwd;
+        if(info.arg_help[j].default_value.compare("") != 0){
+            os << " = " << info.arg_help[j].default_value;
+        }
+        if(j != info.arg_help.size() - 1)
+            os << ", ";
+    }
+    os << ")" << endl;
+    for(size_t j(0); j != info.arg_help.size(); ++j){
+        os << info.arg_help[j].kwd
+             << " (" << info.arg_help[j].type_name << "):"
+             << info.arg_help[j].help << endl;            
+    }
+    os << endl;
+}
+
+void PlainPrinter::print_category_header(string category_name) {
+    os << "Help for " << category_name << "s" << endl << endl;
+}
+
+void PlainPrinter::print_category_footer() {
+    os << endl;
+}
+
+
