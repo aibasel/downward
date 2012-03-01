6d1249f4a1e267bf959e382be5eb3649853cc709
Malte Helmert <malte.helmert@unibas.ch>
2012-03-01 20:55:50 +0100

    [main] PDBs: Add 'combo' pattern selection strategy by Patrik.

--- src/search/pdbs/util.cc
+++ src/search/pdbs/util.cc
@@ -4,12 +4,16 @@
 #include "../option_parser.h"
 #include "../utilities.h"
 
+#include "../merge_and_shrink/variable_order_finder.h"
+
 #include <vector>
 
 using namespace std;
 
 void parse_patterns(OptionParser &parser, Options &opts) {
     parser.add_list_option<vector<int> >("patterns", "the pattern collection", OptionFlags(false));
+    parser.add_option<bool>("combo", false, "use the combo strategy");
+    parser.add_option<int>("max_states", 1000000, "maximum abstraction size for combo strategy");
     opts = parser.parse();
     vector<vector<int> > pattern_collection;
     if (opts.contains("patterns"))
@@ -47,10 +51,39 @@ void parse_patterns(OptionParser &parser, Options &opts) {
 
     // if option "patterns" is not specified, use default
     if (!parser.dry_run() && !opts.contains("patterns")) {
+      if (opts.get<bool>("combo")) {
+	if (opts.get<int>("max_states") < 1)
+	  parser.error("abstraction size must be at least 1");
+	vector<int> pattern1;
+        VariableOrderFinder vof(MERGE_LINEAR_GOAL_CG_LEVEL, 0.0);
+        int var = vof.next();
+        int num_states = g_variable_domain[var];
+        while (num_states <= opts.get<int>("max_states")) {
+            pattern1.push_back(var);
+            if (vof.done())
+                break;
+            var = vof.next();
+            // test against overflow
+            if (num_states <= numeric_limits<int>::max() / g_variable_domain[var]) {
+                // num_states * g_variable_domain[var] <= numeric_limits<int>::max()
+                num_states *= g_variable_domain[var];
+            } else {
+                break;
+            }
+        }
+	pattern_collection.push_back(pattern1);
+        for (size_t i = 0; i < g_goal.size(); ++i) {
+	  if (find(pattern1.begin(), pattern1.end(), g_goal[i].first)
+	      == pattern1.end())
+            pattern_collection.push_back(vector<int>(1, g_goal[i].first));
+        }
+        opts.set("patterns", pattern_collection);
+      } else {
         // Simple selection strategy. Take all goal variables as patterns.
         for (size_t i = 0; i < g_goal.size(); ++i) {
             pattern_collection.push_back(vector<int>(1, g_goal[i].first));
         }
         opts.set("patterns", pattern_collection);
+      }
     }
 }
