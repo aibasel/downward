7f8bceb3f7f746cdc72931ab90e1b25e7148564c
Jendrik Seipp <jendrik.seipp@unibas.ch>
2012-05-01 13:52:02 +0200

    [issue332] use optparse for parsing the commandline, move version check into function

--- src/translate/translate.py
+++ src/translate/translate.py
@@ -9,6 +9,7 @@ from copy import deepcopy
 import axiom_rules
 import fact_groups
 import instantiate
+import optparse
 import pddl
 import sas_tasks
 import simplify
@@ -574,20 +575,35 @@ def dump_statistics(sas_task):
         print("Translator peak memory: %d KB" % peak_memory)
 
 
+def check_python_version(force_old_python):
+    if sys.version_info[:2] == (2, 6):
+        if force_old_python:
+            print("Warning: Running with slow Python 2.6", file=sys.stderr)
+        else:
+            print("Error: Python 2.6 runs the translator very slowly. You should "
+                  "use Python 2.7 or 3.x instead. If you really need to run it "
+                  "with Python 2.6, you can pass the --force-old-python flag.",
+                  file=sys.stderr)
+            sys.exit(1)
+
+
+def parse_options():
+    optparser = optparse.OptionParser(usage="Usage: %prog [options] [<domain.pddl>] <task.pddl>")
+    optparser.add_option('--force-old-python', action='store_true',
+                    help='Allow running the translator with slow Python 2.6')
+    options, args = optparser.parse_args()
+    # Remove the parsed options from sys.argv
+    sys.argv = [sys.argv[0]] + args
+    return options, args
+
+
+
 if __name__ == "__main__":
     import pddl
 
-    try:
-        sys.argv.remove("--force-old-python")
-        force_old_python = True
-    except ValueError:
-        force_old_python = False
+    options, args = parse_options()
 
-    if sys.version_info[:2] == (2, 6) and not force_old_python:
-        print("Error: Python 2.6 runs the translator very slowly. You should "
-              "use Python 2.7 or 3.x instead. If you really need to run it "
-              "with Python 2.6, you can pass the --force-old-python flag.")
-        sys.exit(1)
+    check_python_version(options.force_old_python)
 
     timer = timers.Timer()
     with timers.timing("Parsing"):
