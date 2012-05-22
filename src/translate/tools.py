5fa635150b1faaf4b51655b4ad0db667baadbce3
Jendrik Seipp <jendrik.seipp@unibas.ch>
2012-05-22 12:37:08 +0200

    [issue278] add missing get_peak_memory_in_kb()

--- src/translate/tools.py
+++ src/translate/tools.py
@@ -15,3 +15,16 @@ def cartesian_product(sequences):
         for item in sequences[0]:
             for sequence in temp:
                 yield item + sequence
+
+
+def get_peak_memory_in_kb():
+    try:
+        # This will only work on Linux systems.
+        with open("/proc/self/status") as status_file:
+            for line in status_file:
+                parts = line.split()
+                if parts[0] == "VmPeak:":
+                    return int(parts[1])
+    except IOError:
+        pass
+    raise Warning("warning: could not determine peak memory")
