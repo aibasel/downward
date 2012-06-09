a28b2784c8d8dd390b6bf621fa4e38513739ff19
Jendrik Seipp <jendrik.seipp@unibas.ch>
2012-06-09 14:50:07 +0200

    [issue600] move Abstraction into own files

---
+++ src/search/cegar/abstraction.cc
@@ -0,0 +1,23 @@
+#include "./cegar_heuristic.h"
+
+#include "./../globals.h"
+#include "./../operator.h"
+#include "./../option_parser.h"
+#include "./../plugin.h"
+#include "./../state.h"
+
+#include <assert.h>
+
+#include <limits>
+#include <utility>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+using namespace std;
+
+namespace cegar_heuristic {
+
+
+
+}
--- src/search/cegar/cegar_heuristic.h
+++ src/search/cegar/cegar_heuristic.h
@@ -15,16 +15,6 @@
 
 namespace cegar_heuristic {
 
-class AbstractTransitionSystem {
-    // abs_states[(1, -1, 0)] => Which abstract state has var1=1, var2=?, var3=0?
-    //std::map<std::vector<int>, AbstractState> abs_states;
-
-    // Create a vector of values (state->vars) and set all values to -1 if
-    // we haven't refined the variable yet.
-    // Lookup this vector in abs_states and return it.
-    AbstractState get_abstract_state(const State &state) const;
-};
-
 class CegarHeuristic : public Heuristic {
     int min_operator_cost;
 protected:
