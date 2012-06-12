4aed8faacf8e5ce1af3b3a7a491af36e8112111b
Jendrik Seipp <jendrik.seipp@unibas.ch>
2012-06-12 23:01:20 +0200

    [issue600] add new files utils.{h,cc}

---
+++ src/search/cegar/utils.h
@@ -0,0 +1,38 @@
+#ifndef CEGAR_UTILS_H
+#define CEGAR_UTILS_H
+
+#include <ext/slist>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <sstream>
+
+#include "../heuristic.h"
+#include "../operator.h"
+#include "./abstract_state.h"
+#include "./abstraction.h"
+
+#include "gtest/gtest_prod.h"
+
+namespace cegar_heuristic {
+
+// Gtest prevents us from defining this variable in the header.
+extern int UNDEFINED;
+
+string int_set_to_string(set<int> myset);
+
+int get_eff(Operator op, int var);
+int get_pre(Operator op, int var);
+
+// Create an operator with cost 1.
+// prevails have the form "var value".
+// pre_posts have the form "0 var pre post" (no conditional effects).
+Operator create_op(const std::string desc);
+Operator create_op(const std::string name, std::vector<string> prevail,
+                   std::vector<string> pre_post);
+
+State* create_state(const std::string desc);
+
+}
+
+#endif
