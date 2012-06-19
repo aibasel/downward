1739819d601ff8a4303a86f194c4e12ce922b820
Florian Pommerening <florian.pommerening@unibas.ch>
2012-06-19 15:25:06 +0200

    [issue344] template class for storing per-state information in a heuristic

---
+++ src/search/per_state_information.h
@@ -0,0 +1,40 @@
+#ifndef PER_STATE_INFORMATION_H
+#define PER_STATE_INFORMATION_H
+
+#include "state.h"
+
+#include <vector>
+#include <assert.h>
+
+template<class Entry>
+class PerStateInformation {
+private:
+    std::vector<bool> empty;
+    std::vector<Entry> entries;
+public:
+    PerStateInformation() {}
+
+    Entry &operator[](const State &state) {
+        assert(state.id >= 0);
+        if (entries.size() <= state.id) {
+            entries.resize(state.id +1);
+            empty.resize(state.id +1, true);
+        }
+        empty[state.id] = false;
+        return entries[state.id];
+    }
+
+    bool has_entry(const State &state) const {
+        return (state.id >= 0 && state.id < entries.size() && !empty[state.id]);
+    }
+
+    void remove_enty(const State &state) {
+        if (!has_entry(state)) {
+            return;
+        }
+        empty[state.id] = true;
+        entries[state.id] = Entry();
+    }
+};
+
+#endif
