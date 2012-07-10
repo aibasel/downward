1256584f92e21ea0e292e76ab01e9cd957d241eb
Florian Pommerening <florian.pommerening@unibas.ch>
2012-07-10 17:24:51 +0200

    [issue344] Store everything by value

--- src/search/per_state_information.h
+++ src/search/per_state_information.h
@@ -7,12 +7,15 @@
 #include <assert.h>
 
 template<class Entry>
-class PerStateInformationByValue {
+class PerStateInformation {
 private:
     Entry default_value;
     std::vector<Entry> entries;
 public:
-    PerStateInformationByValue(Entry _default_value)
+    PerStateInformation()
+        : PerStateInformation(Entry()) {
+    }
+    PerStateInformation(Entry _default_value)
         : default_value(_default_value) {
     }
 
@@ -22,53 +25,6 @@ public:
             entries.resize(state.id + 1, default_value);
         }
         return entries[state.id];
-    }
-};
-
-
-template<class Entry>
-class PerStateInformationByReference {
-private:
-    std::vector<Entry *> entries;
-    typedef typename std::vector<Entry *>::iterator EntryIterator;
-public:
-    PerStateInformationByReference() {}
-    ~PerStateInformationByReference() {
-        for (EntryIterator it = entries.begin(); it != entries.end(); ++it) {
-            Entry *e = *it;
-            if (e) {
-                delete e;
-            }
-        }
-    }
-
-    void set(const State &state, Entry &entry) {
-        assert(state.id >= 0);
-        if (entries.size() <= state.id) {
-            entries.resize(state.id + 1, 0);
-        }
-        entries[state.id] = new Entry(entry);
-    }
-
-    Entry *get(const State &state) {
-        assert(state.id >= 0);
-        if (has_entry(state)) {
-            return entries[state.id];
-        } else {
-            return 0;
-        }
-    }
-
-    bool has_entry(const State &state) const {
-        return state.id >= 0 && state.id < entries.size() && entries[state.id];
-    }
-
-    void remove_enty(const State &state) {
-        if (!has_entry(state)) {
-            return;
-        }
-        delete entries[state.id];
-        entries[state.id] = 0;
     }
 };
 
