2ded25fda0376d44cc8eb379b70b63bed857f8d5
Florian Pommerening <florian.pommerening@unibas.ch>
2012-09-03 10:59:32 +0200

    [issue344] removed state_id accessor for PerStateInformation

--- src/search/per_state_information.h
+++ src/search/per_state_information.h
@@ -16,29 +16,6 @@ private:
     const Entry default_value;
     std::vector<Entry> entries;
 
-    Entry &at(int state_id) {
-        size_t virtual_size = state_registry.size();
-        assert(state_id >= 0 && state_id < virtual_size);
-        while (entries.size() <= virtual_size) {
-            // resize() and insert() do not guarantee amortized constant time.
-            // In a lot of cases this will be called once for each state, so we
-            // have to avoid copying the vector every time.
-            entries.push_back(default_value);
-        }
-        return entries[state_id];
-    }
-
-    const Entry at(int state_id) const {
-        // We do not change the size here to avoid having to make entries
-        // mutable. Instead, we return the default value by value if the index
-        // is out of bounds.
-        assert(state_id >= 0 && state_id < state_registry.size());
-        if (state_id >= entries.size()) {
-            return default_value;
-        }
-        return entries[state_id];
-    }
-
 public:
     PerStateInformation()
         : state_registry(g_state_registry),
@@ -56,21 +33,28 @@ public:
     }
 
     Entry &operator[](const StateHandle &state_handle) {
-        return at(state_handle.get_id());
+        int state_id = state_handle.get_id();
+        size_t virtual_size = state_registry.size();
+        assert(state_id >= 0 && state_id < virtual_size);
+        while (entries.size() <= virtual_size) {
+            // resize() and insert() do not guarantee amortized constant time.
+            // In a lot of cases this will be called once for each state, so we
+            // have to avoid copying the vector every time.
+            entries.push_back(default_value);
+        }
+        return entries[state_id];
     }
 
     const Entry operator[](const StateHandle &state_handle) const {
-        return at(state_handle.get_id());
-    }
-
-    // TODO get rid of this access style in favor of using StateHandle.
-    Entry &operator[](int state_id) {
-        return at(state_id);
-    }
-
-    // TODO get rid of this access style in favor of using StateHandle.
-    const Entry operator[](int state_id) const {
-        return at(state_id);
+        // We do not change the size here to avoid having to make entries
+        // mutable. Instead, we return the default value by value if the index
+        // is out of bounds.
+        int state_id = state_handle.get_id();
+        assert(state_id >= 0 && state_id < state_registry.size());
+        if (state_id >= entries.size()) {
+            return default_value;
+        }
+        return entries[state_id];
     }
 
     void clear() {
