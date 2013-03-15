2b876008e92ba658f51b25e9540e353ceab4caee
Florian Pommerening <florian.pommerening@unibas.ch>
2013-03-15 14:40:41 +0100

    [issue344] Register every state for now to evaluate the patch

--- src/search/learning/probe_state_space_sample.cc
+++ src/search/learning/probe_state_space_sample.cc
@@ -42,7 +42,9 @@ void ProbeStateSpaceSample::send_probe(int depth_limit) {
     vector<const Operator *> applicable_ops;
     vector<int> h_s;
 
-    State s = *g_initial_state;
+    // TODO for now we use the global state registry for all states (even temporary states like this)
+    StateHandle initial_state_handle = g_state_registry.get_handle(*g_initial_state);
+    State s = State(initial_state_handle);
     sample_t::const_iterator succ_it = temporary_samp->find(s);
     if (succ_it == temporary_samp->end()) {
         (*temporary_samp)[s].reserve(0);
@@ -75,7 +77,9 @@ void ProbeStateSpaceSample::send_probe(int depth_limit) {
         for (int op_num = 0; op_num < applicable_ops.size(); op_num++) {
             // generate and add to training set all successors
             const Operator *op = applicable_ops[op_num];
-            State succ = State::construct_unregistered_successor(s, *op);
+            // TODO for now, we only generate registered successors. This is a temporary state that
+            // should should not necessarily be registered in the global registry.
+            State succ = State::construct_registered_successor(s, *op);
 
             succ_it = temporary_samp->find(succ);
             if (succ_it == temporary_samp->end()) {
@@ -115,7 +119,9 @@ void ProbeStateSpaceSample::send_probe(int depth_limit) {
 
         //cout << op->get_name() << endl;
 
-        State succ = State::construct_unregistered_successor(s, *op);
+        // TODO for now, we only generate registered successors. This is a temporary state that
+        // should should not necessarily be registered in the global registry.
+        State succ = State::construct_registered_successor(s, *op);
 
         if (test_goal(succ)) {
             //if (succ_node.is_goal()) {
