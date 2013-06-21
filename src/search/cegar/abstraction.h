5018bb4b3f4eef9ae3f268a8bc61e8170871eb63
Jendrik Seipp <jendrik.seipp@unibas.ch>
2013-06-21 20:59:14 +0200

    [issue600] only compute landmark graph if we need it

--- src/search/cegar/abstraction.h
+++ src/search/cegar/abstraction.h
@@ -176,7 +176,7 @@ public:
     void set_max_states_online(int states) {max_states_online = states; }
     void set_max_time(int time) {max_time = time; }
     void set_log_h(bool log) {log_h = log; }
-    void set_pick_strategy(PickStrategy strategy) {pick = strategy; }
+    void set_pick_strategy(PickStrategy strategy);
     void set_hadd(AdditiveHeuristic *h);
 
     // Testing.
