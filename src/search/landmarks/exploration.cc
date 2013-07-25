1b7a2ffa0ab22d7a95b755027421f35fb3cc98ca
Malte Helmert <malte.helmert@unibas.ch>
2013-07-25 16:09:16 +0200

    [issue123] Removed some dead code.

--- src/search/landmarks/exploration.cc
+++ src/search/landmarks/exploration.cc
@@ -301,26 +301,6 @@ int Exploration::compute_hsp_add_heuristic() {
     return total_cost;
 }
 
-int Exploration::compute_hsp_max_heuristic() {
-// Note: this function is currently not used
-    int maximal_cost = 0;
-    for (int i = 0; i < goal_propositions.size(); i++) {
-        int prop_cost = goal_propositions[i]->h_max_cost;
-        if (prop_cost == -1)
-            return DEAD_END;
-        maximal_cost = max(maximal_cost, prop_cost);
-    }
-    return maximal_cost;
-}
-
-int Exploration::get_lower_bound(const State &state) {
-// Note: this function is currently not used
-    prepare_heuristic_computation(state, true);
-    int h = compute_hsp_max_heuristic();
-    heuristic_recomputation_needed = true;
-    return h;
-}
-
 
 int Exploration::compute_ff_heuristic(const State &state) {
     int h_add_heuristic = compute_hsp_add_heuristic();
@@ -460,7 +440,7 @@ void Exploration::collect_ha(ExProposition *goal,
     }
 }
 
-// TODOP: this should be in landmark class
+// TODO: this should be in landmark class
 bool is_landmark(vector<pair<int, int> > &landmarks, int var, int val) {
     // TODO: change landmarks to set or hash_set
     for (int i = 0; i < landmarks.size(); i++)
