446070ff088757bace61c25e3092e1627c609086
Jendrik Seipp <jendrik.seipp@unibas.ch>
2013-09-10 16:07:30 +0200

    [issue600] add task stubs

---
+++ src/search/cegar/task.cc
@@ -0,0 +1,11 @@
+#include "task.h"
+
+using namespace std;
+
+namespace cegar_heuristic {
+
+
+Task::Task() {
+}
+
+}
---
+++ src/search/cegar/task.h
@@ -0,0 +1,15 @@
+#ifndef CEGAR_TASK_H
+#define CEGAR_TASK_H
+
+#include <vector>
+
+namespace cegar_heuristic {
+class Task {
+private:
+
+public:
+    Task();
+};
+}
+
+#endif
