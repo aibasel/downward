bcd69526bc5ea850d4f41fddd3f1ee50a277182b
Jendrik Seipp <jendrik.seipp@unibas.ch>
2013-09-17 16:49:25 +0200

    [issue600] add methods that adapt task

--- src/search/cegar/task.h
+++ src/search/cegar/task.h
@@ -11,6 +11,10 @@
 namespace cegar_heuristic {
 class Task {
 private:
+    std::vector<std::vector<int> > fact_mapping;
+
+    void rename_fact(int var, int before, int after);
+    void remove_fact(const Fact &fact);
 
 public:
     Task();
@@ -20,6 +24,9 @@ public:
     std::vector<Operator> operators;
     std::vector<int> original_operator_numbers;
     std::unordered_set<int> fact_numbers;
+
+    void remove_unreachable_facts();
+    void combine_facts(int var, const vector<int> &values);
 
     void install();
     void release_memory();
