505c3a44cc14a3ebe1361874022d452a7842ea79
Malte Helmert <malte.helmert@unibas.ch>
2014-02-09 23:44:33 +0100

    [issue414] Simplified input analyzer and made it possible to separate the two analyses.

--- src/search/input_analyzer.py
+++ src/search/input_analyzer.py
@@ -1,37 +1,70 @@
 # -*- coding: utf-8 -*-
 
+# Some performance numbers: on the largest preprocessor output file in
+# our benchmark collection as of end of 2013 (satellite #33, ~104 MB),
+# the input analyzer requires roughly 5.01 seconds for a complete
+# analysis and 0.038 seconds to only check the variable ranges. For
+# comparison: the translator requires 130 seconds and the preprocessor
+# 33 seconds on this task.
+
+
 class AnalysisError(Exception):
     pass
-
-
-class AnalysisResult(object):
-    """Used to store the result of an input file analysis. Has the
-    attributes is_unit_cost, max_var_range and state_var_size."""
-
-    def dump(self):
-        print "is_unit_cost: %s" % self.is_unit_cost
-        print "max_var_range: %d" % self.max_var_range
-        print "state_var_size: %d" % self.state_var_size
 
 
 class InputAnalyzer(object):
     """Simple parser for the preprocessor output file. Not a complete
     parser, so does not completely validate that the input is
-    syntax-correct."""
+    syntax-correct.
 
-    def analyze(self, filename):
+    The class is intended to be used through the helper functions in
+    the module rather than directly."""
+
+    def analyze(self, filename, extract_func):
         self.filename = filename
         self.curr_line = None
         self.prev_line = None
-
-        result = AnalysisResult()
         with open(filename) as input_file:
             self.input_file = input_file
-            use_metric = self._parse_metric()
-            result.max_var_range = self._parse_variables()
-            result.is_unit_cost = self._parse_actions(use_metric)
-        result.state_var_size = get_state_var_size(result.max_var_range)
-        return result
+            return extract_func()
+
+    def extract_max_var_range(self):
+        self._skip_to("end_metric")
+        num_variables = self._read_int(min_value=1)
+        max_range = 0
+        for _ in xrange(num_variables):
+            self._skip_to("begin_variable")
+            self._next_line() # Skip variable name.
+            self._next_line() # Skip axiom layer specification.
+            var_range = self._read_int(min_value=1)
+            max_range = max(max_range, var_range)
+            self._skip_to("end_variable")
+        return max_range
+
+    def extract_unit_cost(self):
+        is_unit_cost = True
+
+        self._skip_to("begin_metric")
+        use_metric = bool(self._read_int(min_value=0, max_value=1))
+
+        self._skip_to("end_goal")
+        num_operators = self._read_int(min_value=0)
+        for _ in xrange(num_operators):
+            self._skip_to("end_operator")
+            action_cost = self._parse_int(self.prev_line, min_value=0)
+            if use_metric:
+                if action_cost != 1:
+                    is_unit_cost = False
+            else:
+                if action_cost != 0:
+                    # Currently, action costs in the input must all be
+                    # specified as 0 unless the metric is set. In the
+                    # future, it might make sense to change the
+                    # entries to 1 in this case (since they are
+                    # interpreted as 1) and get rid of the metric
+                    # block.
+                    self._error("action cost set despite use_metric=False")
+        return is_unit_cost
 
     def _error(self, msg):
         raise AnalysisError("error in %s: %s" % (self.filename, msg))
@@ -61,45 +94,6 @@ class InputAnalyzer(object):
     def _read_int(self, **kwargs):
         return self._parse_int(self._next_line(), **kwargs)
 
-    def _parse_metric(self):
-        self._skip_to("begin_metric")
-        use_metric = bool(self._read_int(min_value=0, max_value=1))
-        self._skip_to("end_metric")
-        return use_metric
-
-    def _parse_variables(self):
-        num_variables = self._read_int(min_value=1)
-        max_range = 0
-        for _ in xrange(num_variables):
-            self._skip_to("begin_variable")
-            self._next_line() # Skip variable name.
-            self._next_line() # Skip axiom layer specification.
-            var_range = self._read_int(min_value=1)
-            max_range = max(max_range, var_range)
-            self._skip_to("end_variable")
-        return max_range
-
-    def _parse_actions(self, use_metric):
-        is_unit_cost = True
-        self._skip_to("end_goal")
-        num_operators = self._read_int(min_value=0)
-        for _ in xrange(num_operators):
-            self._skip_to("end_operator")
-            action_cost = self._parse_int(self.prev_line, min_value=0)
-            if use_metric:
-                if action_cost != 1:
-                    is_unit_cost = False
-            else:
-                if action_cost != 0:
-                    # Currently, action costs in the input must all be
-                    # specified as 0 unless the metric is set. In the
-                    # future, it might make sense to change the
-                    # entries to 1 in this case (since they are
-                    # interpreted as 1) and get rid of the metric
-                    # block.
-                    self._error("action cost set despite use_metric=False")
-        return is_unit_cost
-
 
 def get_state_var_size(max_range):
     # Note that the highest value in the state_var_t domain is
@@ -113,8 +107,18 @@ def get_state_var_size(max_range):
         return 4
 
 
-def analyze(filename):
-    return InputAnalyzer().analyze(filename)
+def read_max_var_range(filename):
+    analyzer = InputAnalyzer()
+    return analyzer.analyze(filename, analyzer.extract_max_var_range)
+
+
+def read_state_var_size(filename):
+    return get_state_var_size(read_max_var_range(filename))
+
+
+def read_unit_cost(filename):
+    analyzer = InputAnalyzer()
+    return analyzer.analyze(filename, analyzer.extract_unit_cost)
 
 
 def test():
@@ -123,7 +127,10 @@ def test():
     for filename in args:
         if len(args) >= 2:
             print "%s:" % filename
-        analyze(filename).dump()
+        print "is_unit_cost:", read_unit_cost(filename)
+        max_var_range = read_max_var_range(filename)
+        print "max_var_range:", max_var_range
+        print "state_var_size:", get_state_var_size(max_var_range)
         if len(args) >= 2:
             print
 
