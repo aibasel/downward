0f2d351fb8c62e1de2a3933d8ae34074b327f68e
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-03-07 15:18:00 +0100

    [issue214] use better variable names

--- src/search/int_packer.cc
+++ src/search/int_packer.cc
@@ -18,10 +18,10 @@ int get_needed_bits(int num_values) {
     return num_bits;
 }
 
-int get_max_fitting_bits(const vector<vector<int> > &ints_by_needed_bits,
+int get_max_fitting_bits(const vector<vector<int> > &bits_to_entries,
                                  int available_bits) {
     for (size_t bits = available_bits; bits != 0; --bits) {
-        if (!ints_by_needed_bits[bits].empty()) {
+        if (!bits_to_entries[bits].empty()) {
             return bits;
         }
     }
@@ -30,8 +30,8 @@ int get_max_fitting_bits(const vector<vector<int> > &ints_by_needed_bits,
 
 
 IntPacker::IntPacker(const vector<int> &ranges)
-    : packed_ints(),
-      packed_size(0) {
+    : packed_entries(),
+      num_bins(0) {
     pack_bins(ranges);
 }
 
@@ -39,55 +39,55 @@ IntPacker::~IntPacker() {
 }
 
 int IntPacker::get(const PackedStateEntry *buffer, int pos) const {
-    const PackedInt &packed_int = packed_ints[pos];
-    return (buffer[packed_int.index] & packed_int.read_mask) >> packed_int.shift;
+    const PackedEntry &packed_entry = packed_entries[pos];
+    return (buffer[packed_entry.index] & packed_entry.read_mask) >> packed_entry.shift;
 }
 
 void IntPacker::set(PackedStateEntry *buffer, int pos, int value) const {
-    const PackedInt &packed_int = packed_ints[pos];
-    PackedStateEntry before = buffer[packed_int.index];
-    buffer[packed_int.index] = (before & packed_int.clear_mask) | (value << packed_int.shift);
+    const PackedEntry &packed_entry = packed_entries[pos];
+    PackedStateEntry before = buffer[packed_entry.index];
+    buffer[packed_entry.index] = (before & packed_entry.clear_mask) | (value << packed_entry.shift);
 }
 
 void IntPacker::pack_bins(const vector<int> &ranges) {
-    assert(packed_ints.empty());
-    int num_ints = ranges.size();
-    packed_ints.resize(num_ints);
+    assert(packed_entries.empty());
+    int num_entries = ranges.size();
+    packed_entries.resize(num_entries);
 
-    int bits_per_entry = sizeof(PackedStateEntry) * 8;
-    vector<vector<int> > ints_by_needed_bits(bits_per_entry + 1);
-    for (size_t pos = 0; pos < num_ints; ++pos) {
+    int bits_per_bin = sizeof(PackedStateEntry) * 8;
+    vector<vector<int> > bits_to_entries(bits_per_bin + 1);
+    for (size_t pos = 0; pos < num_entries; ++pos) {
         int bits = get_needed_bits(ranges[pos]);
-        assert(bits < bits_per_entry);
-        ints_by_needed_bits[bits].push_back(pos);
+        assert(bits < bits_per_bin);
+        bits_to_entries[bits].push_back(pos);
     }
-    // Greedy strategy: always add the largest fitting int.
-    int num_packed_ints = 0;
-    packed_size = 1;
-    int remaining_bits = bits_per_entry;
-    while (num_packed_ints < num_ints) {
-        int bits = get_max_fitting_bits(ints_by_needed_bits, remaining_bits);
+    // Greedy strategy: always add the largest fitting entry.
+    int num_packed_entries = 0;
+    num_bins = 1;
+    int remaining_bits = bits_per_bin;
+    while (num_packed_entries < num_entries) {
+        int bits = get_max_fitting_bits(bits_to_entries, remaining_bits);
         if (bits == 0) {
             // We cannot pack another int into the current word so we add
             // an additional word.
-            ++packed_size;
-            remaining_bits = bits_per_entry;
+            ++num_bins;
+            remaining_bits = bits_per_bin;
             continue;
         }
-        // We can pack another value of size max_fitting_bits into the current word.
-        vector<int> &ints = ints_by_needed_bits[bits];
-        assert(!ints.empty());
-        int pos = ints.back();
-        ints.pop_back();
-        PackedInt &packed_int = packed_ints[pos];
-        packed_int.shift = bits_per_entry - remaining_bits;
-        packed_int.index = packed_size - 1;
-        packed_int.read_mask = 0;
-        set_bits(packed_int.read_mask, packed_int.shift, packed_int.shift + bits);
-        packed_int.clear_mask = ~packed_int.read_mask;
+        // We can pack another entry of size bits into the current word.
+        vector<int> &best_fit_entries = bits_to_entries[bits];
+        assert(!best_fit_entries.empty());
+        int pos = best_fit_entries.back();
+        best_fit_entries.pop_back();
+        PackedEntry &packed_entry = packed_entries[pos];
+        packed_entry.shift = bits_per_bin - remaining_bits;
+        packed_entry.index = num_bins - 1;
+        packed_entry.read_mask = 0;
+        set_bits(packed_entry.read_mask, packed_entry.shift, packed_entry.shift + bits);
+        packed_entry.clear_mask = ~packed_entry.read_mask;
         remaining_bits -= bits;
-        ++num_packed_ints;
+        ++num_packed_entries;
     }
     cout << "Ints: " << ranges.size() << endl;
-    cout << "Bytes per packing: " << packed_size * sizeof(PackedStateEntry) << endl;
+    cout << "Bytes per packing: " << num_bins * sizeof(PackedStateEntry) << endl;
 }
