bb457dac72ef19b1c24ad053627fa2be57f113e7
Silvan Sievers <silvan.sievers@unibas.ch>
2014-03-10 15:29:32 +0100

    [issue419] abstraction::apply_abstraction: fixed 'must_clear_distances'

--- src/search/merge_and_shrink/abstraction.cc
+++ src/search/merge_and_shrink/abstraction.cc
@@ -873,15 +873,11 @@ void Abstraction::apply_abstraction(
 
         ++pos;
         for (; pos != group.end(); ++pos) {
-            // TODO: !=, remove new_init_dist assignment
-            if (init_distances[*pos] < new_init_dist) {
+            if (init_distances[*pos] != new_init_dist) {
                 must_clear_distances = true;
-                new_init_dist = init_distances[*pos];
             }
-            // TODO: !=, remove new_goal_dist assignment
-            if (goal_distances[*pos] < new_goal_dist) {
+            if (goal_distances[*pos] != new_goal_dist) {
                 must_clear_distances = true;
-                new_goal_dist = goal_distances[*pos];
             }
             if (goal_states[*pos])
                 new_goal_states[new_state] = true;
