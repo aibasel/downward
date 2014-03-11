77c2112be9fd9c9417c70e749922f340b3688a16
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-03-11 14:10:54 +0100

    [issue214] add issue214-v4-ipdb.py exp

---
+++ experiments/issue214/issue214-v4-ipdb.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from downward.suites import suite_optimal_with_ipc11
+from downward.configs import default_configs_optimal
+from downward.reports.scatter import ScatterPlotReport
+
+import common_setup
+
+
+REVS = ["issue214-base", "issue214-v4"]
+CONFIGS = {"ipdb": ["--search", "astar(ipdb())"]}
+
+TEST_RUN = False
+
+if TEST_RUN:
+    SUITE = "gripper:prob01.pddl"
+    PRIORITY = None  # "None" means local experiment
+else:
+    SUITE = suite_optimal_with_ipc11()
+    PRIORITY = 0     # number means maia experiment
+
+
+# TODO: I'd like to specify "search_revisions" (which uses the same
+# translator and preprocessor for everything) instead of "revisions"
+# here, but I can't seem to make this work with the REVS argument for
+# CompareRevisionsReport.
+
+exp = common_setup.MyExperiment(
+    grid_priority=PRIORITY,
+    revisions=REVS,
+    configs=CONFIGS,
+    suite=SUITE,
+    parsers=['state_size_parser.py'],
+    )
+
+
+exp.add_comparison_table_step(
+    attributes=common_setup.MyExperiment.DEFAULT_TABLE_ATTRIBUTES + ['bytes_per_state', 'variables', 'state_var_t_size']
+)
+exp.add_scatter_plot_step()
+
+exp.add_report(ScatterPlotReport(
+        attributes=['bytes_per_state'],
+        filter_config_nick='astar_blind',
+    ),
+    outfile='issue214_bytes_per_state.png')
+
+for config_nick in ['astar_blind', 'astar_lmcut', 'astar_merge_and_shrink_bisim', 'astar_ipdb']:
+    for attr in ['memory', 'total_time']:
+        exp.add_report(ScatterPlotReport(
+                attributes=[attr],
+                filter_config_nick=config_nick,
+            ),
+            outfile='issue214_%s_%s.png' % (attr, config_nick))
+
+
+exp()
