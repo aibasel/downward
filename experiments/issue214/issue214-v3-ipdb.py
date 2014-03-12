331beb5098537fbb466b597a4c2df0b61ffdea52
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-03-12 22:12:59 +0100

    [issue214] remove unneeded reports

--- experiments/issue214/issue214-v3-ipdb.py
+++ experiments/issue214/issue214-v3-ipdb.py
@@ -34,25 +34,4 @@ exp = common_setup.MyExperiment(
     parsers=['state_size_parser.py'],
     )
 
-
-exp.add_comparison_table_step(
-    attributes=common_setup.MyExperiment.DEFAULT_TABLE_ATTRIBUTES + ['bytes_per_state', 'variables', 'state_var_t_size']
-)
-exp.add_scatter_plot_step()
-
-exp.add_report(ScatterPlotReport(
-        attributes=['bytes_per_state'],
-        filter_config_nick='astar_blind',
-    ),
-    outfile='issue214_bytes_per_state.png')
-
-for config_nick in ['astar_blind', 'astar_lmcut', 'astar_merge_and_shrink_bisim', 'astar_ipdb']:
-    for attr in ['memory', 'total_time']:
-        exp.add_report(ScatterPlotReport(
-                attributes=[attr],
-                filter_config_nick=config_nick,
-            ),
-            outfile='issue214_%s_%s.png' % (attr, config_nick))
-
-
 exp()
