d8a6ca988aa83fce4924793cc266991478805264
Silvan Sievers <silvan.sievers@unibas.ch>
2014-03-17 21:51:23 +0100

    [issue419] dfp merge strategy now considers abstractions in a more sophisticated order: consider composite abstractions in the inverted order of their generation (thus starting with the most recent one), but consider atomic abstractions in the regular order (thus starting with the one for variable 1).

--- src/search/merge_and_shrink/non_linear_merge_strategy.h
+++ src/search/merge_and_shrink/non_linear_merge_strategy.h
@@ -11,7 +11,12 @@ class NonLinearMergeStrategy : public MergeStrategy {
     };
     NonLinearMergeStrategyType non_linear_merge_strategy_type;
     int remaining_merges;
+    // border_atomic_composites is the first index at which a composite
+    // abstraction can be found in vector of all abstractions as passed
+    // as argument to the get_next method.
+    std::size_t border_atomics_composites;
 
+    std::size_t get_corrected_index(int index) const;
     void dump_strategy_specific_options() const;
 public:
     explicit NonLinearMergeStrategy(const Options &opts);
