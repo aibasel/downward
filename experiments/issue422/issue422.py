783607961e8007f461035cb8d0ee0e0bbf8aa6ab
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-03-17 14:57:57 +0100

    [issue422] copy experiment scripts from other experiment

---
+++ experiments/issue422/issue422.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from downward.suites import suite_optimal_with_ipc11
+from downward.configs import default_configs_optimal
+
+import common_setup
+
+
+REVS = ["issue422-base", "issue422-v1"]
+CONFIGS = default_configs_optimal()
+
+# remove config that is disabled in this branch
+del CONFIGS['astar_selmax_lmcut_lmcount']
+
+TEST_RUN = True
+
+if TEST_RUN:
+    SUITE = "gripper:prob01.pddl"
+    PRIORITY = None  # "None" means local experiment
+else:
+    SUITE = suite_optimal_with_ipc11()
+    PRIORITY = 0     # number means maia experiment
+
+
+exp = common_setup.MyExperiment(
+    grid_priority=PRIORITY,
+    revisions=REVS,
+    configs=CONFIGS,
+    suite=SUITE,
+    )
+
+
+exp.add_comparison_table_step(
+    attributes=common_setup.MyExperiment.DEFAULT_TABLE_ATTRIBUTES
+)
+exp.add_scatter_plot_step()
+
+exp()
