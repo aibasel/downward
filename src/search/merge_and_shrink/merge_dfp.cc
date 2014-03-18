5497fe2a28825d46a2fd8932fd2e08d5d1df1b2b
Silvan Sievers <silvan.sievers@unibas.ch>
2014-03-18 17:36:53 +0100

    [issue419] refactored dfp strategy

--- src/search/merge_and_shrink/merge_dfp.cc
+++ src/search/merge_and_shrink/merge_dfp.cc
@@ -19,8 +19,7 @@ using namespace std;
 static const int infinity = numeric_limits<int>::max();
 
 MergeDFP::MergeDFP()
-    : MergeStrategy(),
-      remaining_merges(-1) {
+    : MergeStrategy(), remaining_merges(-1), border_atomics_composites(-1) {
 }
 
 bool MergeDFP::done() const {
@@ -34,7 +33,9 @@ size_t MergeDFP::get_corrected_index(int index) const {
     // abstractions (these are thus traversed in order from the last one
     // to the first one) and modifies the index otherwise so that the order
     // in which atomic abstractions are considered is from the first to the
-    // last one (from front to back).
+    // last one (from front to back). This is to emulate the previous behavior
+    // when new abstractions were not inserted after existing abstractions,
+    // but rather replaced arbitrarily one of the two original abstractions.
     assert(index >= 0);
     if (index >= border_atomics_composites)
         return index;
@@ -46,58 +47,70 @@ pair<int, int> MergeDFP::get_next(const std::vector<Abstraction *> &all_abstract
         remaining_merges = all_abstractions.size() - 1;
         border_atomics_composites = all_abstractions.size();
     }
-    int first = -1;
-    int second = -1;
-    int minimum_weight = infinity;
-    if (remaining_merges > 1) {
-        vector<vector<int> > abstraction_label_ranks(all_abstractions.size());
+    assert(remaining_merges > 0);
+
+    // Precompute a vector sorted_abstrations which contains all exisiting
+    // abstractions from all_abstractions in the desired order.
+    vector<Abstraction *> sorted_abstractions;
+    vector<int> indices_mapping;
+    vector<vector<int> > abstraction_label_ranks;
+    for (int i = all_abstractions.size() - 1; i >= 0; --i) {
         // We iterate from back to front, considering the composite
         // abstractions in the order from "most recently added" (= at the back
         // of the vector) to "first added" (= at border_atomics_composites).
         // Afterwards, we consider the atomic abstrations in the "regular"
         // order from the first one until the last one. See also explanation
         // at get_corrected_index().
-        for (int i = all_abstractions.size() - 1; i >= 0; --i) {
-            size_t abs_index = get_corrected_index(i);
-            Abstraction *abstraction = all_abstractions[abs_index];
-            if (abstraction) {
-                vector<int> &label_ranks = abstraction_label_ranks[abs_index];
-                if (label_ranks.empty()) {
-                    abstraction->compute_label_ranks(label_ranks);
-                }
-                for (int j = i - 1; j >= 0; --j) {
-                    size_t other_abs_index = get_corrected_index(j);
-                    assert (other_abs_index != abs_index);
-                    Abstraction *other_abstraction = all_abstractions[other_abs_index];
-                    if (other_abstraction) {
-                        if (!abstraction->is_goal_relevant() && !other_abstraction->is_goal_relevant()) {
-                            // only consider pairs where at least one abstraction is goal relevant
-                            continue;
-                        }
-                        vector<int> &other_label_ranks = abstraction_label_ranks[other_abs_index];
-                        if (other_label_ranks.empty()) {
-                            other_abstraction->compute_label_ranks(other_label_ranks);
-                        }
+        size_t abs_index = get_corrected_index(i);
+        Abstraction *abstraction = all_abstractions[abs_index];
+        if (abstraction) {
+            sorted_abstractions.push_back(abstraction);
+            indices_mapping.push_back(abs_index);
+            abstraction_label_ranks.push_back(vector<int>());
+            vector<int> &label_ranks = abstraction_label_ranks[abstraction_label_ranks.size() - 1];
+            abstraction->compute_label_ranks(label_ranks);
+        }
+    }
 
-                        assert(label_ranks.size() == other_label_ranks.size());
-                        int pair_weight = infinity;
-                        for (size_t i = 0; i < label_ranks.size(); ++i) {
-                            if (label_ranks[i] != -1 && other_label_ranks[i] != -1) {
-                                int max_label_rank = max(label_ranks[i], other_label_ranks[i]);
-                                pair_weight = min(pair_weight, max_label_rank);
-                            }
+    int first = -1;
+    int second = -1;
+    int minimum_weight = infinity;
+    if (remaining_merges > 1) {
+        for (size_t abs_index = 0; abs_index < sorted_abstractions.size(); ++abs_index) {
+            Abstraction *abstraction = sorted_abstractions[abs_index];
+            assert(abstraction);
+            vector<int> &label_ranks = abstraction_label_ranks[abs_index];
+            assert(!label_ranks.empty());
+            for (size_t other_abs_index = abs_index + 1; other_abs_index < sorted_abstractions.size();
+                 ++other_abs_index) {
+                Abstraction *other_abstraction = sorted_abstractions[other_abs_index];
+                assert(other_abstraction);
+                if (abstraction->is_goal_relevant() || other_abstraction->is_goal_relevant()) {
+                    vector<int> &other_label_ranks = abstraction_label_ranks[other_abs_index];
+                    assert(!other_label_ranks.empty());
+                    assert(label_ranks.size() == other_label_ranks.size());
+                    int pair_weight = infinity;
+                    for (size_t i = 0; i < label_ranks.size(); ++i) {
+                        if (label_ranks[i] != -1 && other_label_ranks[i] != -1) {
+                            // label is relevant in both abstractions
+                            int max_label_rank = max(label_ranks[i], other_label_ranks[i]);
+                            pair_weight = min(pair_weight, max_label_rank);
                         }
-                        if (pair_weight < minimum_weight) {
-                            minimum_weight = pair_weight;
-                            // always return a goal relevant abstraction as a first index
-                            if (abstraction->is_goal_relevant()) {
-                                first = abs_index;
-                                second = other_abs_index;
-                            } else {
-                                assert(other_abstraction->is_goal_relevant());
-                                first = other_abs_index;
-                                second = abs_index;
-                            }
+                    }
+                    if (pair_weight < minimum_weight) {
+                        minimum_weight = pair_weight;
+                        // always return a goal relevant abstraction as a first index
+                        if (abstraction->is_goal_relevant()) {
+                            first = indices_mapping[abs_index];
+                            second = indices_mapping[other_abs_index];
+                            assert(all_abstractions[first] == abstraction);
+                            assert(all_abstractions[second] == other_abstraction);
+                        } else {
+                            assert(other_abstraction->is_goal_relevant());
+                            first = indices_mapping[other_abs_index];
+                            second = indices_mapping[abs_index];
+                            assert(all_abstractions[first] == other_abstraction);
+                            assert(all_abstractions[second] == abstraction);
                         }
                     }
                 }
