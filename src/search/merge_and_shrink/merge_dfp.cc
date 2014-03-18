f7579f3bdcf888fd744aa998fc84581dbecf7529
Silvan Sievers <silvan.sievers@unibas.ch>
2014-03-18 17:57:04 +0100

    [issue419] use the correct order for abstractions also when taking an arbitrary pair

--- src/search/merge_and_shrink/merge_dfp.cc
+++ src/search/merge_and_shrink/merge_dfp.cc
@@ -75,75 +75,86 @@ pair<int, int> MergeDFP::get_next(const std::vector<Abstraction *> &all_abstract
     int first = -1;
     int second = -1;
     int minimum_weight = infinity;
-    if (remaining_merges > 1) {
-        for (size_t abs_index = 0; abs_index < sorted_abstractions.size(); ++abs_index) {
-            Abstraction *abstraction = sorted_abstractions[abs_index];
-            assert(abstraction);
-            vector<int> &label_ranks = abstraction_label_ranks[abs_index];
-            assert(!label_ranks.empty());
-            for (size_t other_abs_index = abs_index + 1; other_abs_index < sorted_abstractions.size();
-                 ++other_abs_index) {
-                Abstraction *other_abstraction = sorted_abstractions[other_abs_index];
-                assert(other_abstraction);
-                if (abstraction->is_goal_relevant() || other_abstraction->is_goal_relevant()) {
-                    vector<int> &other_label_ranks = abstraction_label_ranks[other_abs_index];
-                    assert(!other_label_ranks.empty());
-                    assert(label_ranks.size() == other_label_ranks.size());
-                    int pair_weight = infinity;
-                    for (size_t i = 0; i < label_ranks.size(); ++i) {
-                        if (label_ranks[i] != -1 && other_label_ranks[i] != -1) {
-                            // label is relevant in both abstractions
-                            int max_label_rank = max(label_ranks[i], other_label_ranks[i]);
-                            pair_weight = min(pair_weight, max_label_rank);
-                        }
+    for (size_t abs_index = 0; abs_index < sorted_abstractions.size(); ++abs_index) {
+        Abstraction *abstraction = sorted_abstractions[abs_index];
+        assert(abstraction);
+        vector<int> &label_ranks = abstraction_label_ranks[abs_index];
+        assert(!label_ranks.empty());
+        for (size_t other_abs_index = abs_index + 1; other_abs_index < sorted_abstractions.size();
+             ++other_abs_index) {
+            Abstraction *other_abstraction = sorted_abstractions[other_abs_index];
+            assert(other_abstraction);
+            if (abstraction->is_goal_relevant() || other_abstraction->is_goal_relevant()) {
+                vector<int> &other_label_ranks = abstraction_label_ranks[other_abs_index];
+                assert(!other_label_ranks.empty());
+                assert(label_ranks.size() == other_label_ranks.size());
+                int pair_weight = infinity;
+                for (size_t i = 0; i < label_ranks.size(); ++i) {
+                    if (label_ranks[i] != -1 && other_label_ranks[i] != -1) {
+                        // label is relevant in both abstractions
+                        int max_label_rank = max(label_ranks[i], other_label_ranks[i]);
+                        pair_weight = min(pair_weight, max_label_rank);
                     }
-                    if (pair_weight < minimum_weight) {
-                        minimum_weight = pair_weight;
-                        // always return a goal relevant abstraction as a first index
-                        if (abstraction->is_goal_relevant()) {
-                            first = indices_mapping[abs_index];
-                            second = indices_mapping[other_abs_index];
-                            assert(all_abstractions[first] == abstraction);
-                            assert(all_abstractions[second] == other_abstraction);
-                        } else {
-                            assert(other_abstraction->is_goal_relevant());
-                            first = indices_mapping[other_abs_index];
-                            second = indices_mapping[abs_index];
-                            assert(all_abstractions[first] == other_abstraction);
-                            assert(all_abstractions[second] == abstraction);
-                        }
+                }
+                if (pair_weight < minimum_weight) {
+                    minimum_weight = pair_weight;
+                    // always return a goal relevant abstraction as a first index
+                    if (abstraction->is_goal_relevant()) {
+                        first = indices_mapping[abs_index];
+                        second = indices_mapping[other_abs_index];
+                        assert(all_abstractions[first] == abstraction);
+                        assert(all_abstractions[second] == other_abstraction);
+                    } else {
+                        assert(other_abstraction->is_goal_relevant());
+                        first = indices_mapping[other_abs_index];
+                        second = indices_mapping[abs_index];
+                        assert(all_abstractions[first] == other_abstraction);
+                        assert(all_abstractions[second] == abstraction);
                     }
+                    // TODO: do not care about the index at which an abstraction
+                    // is returned. Use the following code instead.
+                    //first = indices_mapping[abs_index];
+                    //second = indices_mapping[other_abs_index];
+                    //assert(all_abstractions[first] == abstraction);
+                    //assert(all_abstractions[second] == other_abstraction);
                 }
             }
         }
     }
     if (first == -1) {
+        // No pair with finite weight has been found. In this case, we simply
+        // take the first pair according to our ordering consisting of at
+        // least one goal relevant abstraction.
         assert(second == -1);
-        assert(remaining_merges == 1 || minimum_weight == infinity);
-        // we simply take the first two valid indices from the set of all
-        // abstractions (prefering goal relevant abstractions) to be merged next if:
-        // 1) remaining_merges = 1 (there are only two abstractions left)
-        // 2) all pair weights have been computed to be infinity
-        for (size_t abs_index = 0; abs_index < all_abstractions.size(); ++abs_index) {
-            Abstraction *abstraction = all_abstractions[abs_index];
-            if (abstraction) {
-                for (size_t other_abs_index = abs_index + 1; other_abs_index < all_abstractions.size(); ++other_abs_index) {
-                    Abstraction *other_abstraction = all_abstractions[other_abs_index];
-                    if (other_abstraction) {
-                        if (!abstraction->is_goal_relevant() && !other_abstraction->is_goal_relevant()) {
-                            // only consider pairs where at least one abstraction is goal relevant
-                            continue;
-                        }
-                        // always return a goal relevant abstraction as a first index
-                        if (abstraction->is_goal_relevant()) {
-                            first = abs_index;
-                            second = other_abs_index;
-                        } else {
-                            assert(other_abstraction->is_goal_relevant());
-                            first = other_abs_index;
-                            second = abs_index;
-                        }
+        assert(minimum_weight == infinity);
+
+        for (size_t abs_index = 0; abs_index < sorted_abstractions.size(); ++abs_index) {
+            Abstraction *abstraction = sorted_abstractions[abs_index];
+            assert(abstraction);
+            for (size_t other_abs_index = abs_index + 1; other_abs_index < sorted_abstractions.size();
+                 ++other_abs_index) {
+                Abstraction *other_abstraction = sorted_abstractions[other_abs_index];
+                assert(other_abstraction);
+                if (abstraction->is_goal_relevant() || other_abstraction->is_goal_relevant()) {
+                    // always return a goal relevant abstraction as a first index
+                    if (abstraction->is_goal_relevant()) {
+                        first = indices_mapping[abs_index];
+                        second = indices_mapping[other_abs_index];
+                        assert(all_abstractions[first] == abstraction);
+                        assert(all_abstractions[second] == other_abstraction);
+                    } else {
+                        assert(other_abstraction->is_goal_relevant());
+                        first = indices_mapping[other_abs_index];
+                        second = indices_mapping[abs_index];
+                        assert(all_abstractions[first] == other_abstraction);
+                        assert(all_abstractions[second] == abstraction);
                     }
+                    // TODO: do not care about the index at which an abstraction
+                    // is returned. Use the following code instead.
+                    //first = indices_mapping[abs_index];
+                    //second = indices_mapping[other_abs_index];
+                    //assert(all_abstractions[first] == abstraction);
+                    //assert(all_abstractions[second] == other_abstraction);
                 }
             }
         }
