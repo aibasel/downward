7cfd62c26b26a64c55ea3e81c152301b8d68c82e
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-03-28 10:27:18 +0100

    [issue392] -> succ_mode -> succ_order

--- src/search/lazy_search.h
+++ src/search/lazy_search.h
@@ -22,7 +22,7 @@ protected:
 
     // Search Behavior parameters
     bool reopen_closed_nodes; // whether to reopen closed nodes upon finding lower g paths
-    enum SuccMode {original, pref_first, shuffled, shuffled_pref_first} succ_mode;
+    enum SuccOrder {original, pref_first, shuffled, shuffled_pref_first} succ_order;
 
     vector<Heuristic *> heuristics;
     vector<Heuristic *> preferred_operator_heuristics;
