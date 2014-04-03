238d37db0fc8d3f2cbb5e3e61a369e16e85872c3
Silvan Sievers <silvan.sievers@unibas.ch>
2014-04-03 12:24:31 +0200

    [issue419] Backed out changeset: 08b32eebad55

--- src/search/merge_and_shrink/label_reducer.cc
+++ src/search/merge_and_shrink/label_reducer.cc
@@ -73,15 +73,10 @@ void LabelReducer::reduce_labels(int abs_start_index,
 
     size_t system_order_index = 0;
     assert(!system_order.empty());
-    while (system_order[system_order_index] >= all_abstractions.size()) {
+    while (system_order[system_order_index] != abs_start_index) {
         ++system_order_index;
-        assert(system_order_index < system_order.size());
     }
-
-    //while (system_order[system_order_index] != abs_start_index) {
-    //    ++system_order_index;
-    //}
-    //assert(system_order[system_order_index] == abs_start_index);
+    assert(system_order[system_order_index] == abs_start_index);
 
     int max_iterations;
     if (label_reduction_method == ONE_ABSTRACTION) {
