b647427e1ed28bc2800a1399a1f3b8e44c0026e0
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-04-10 14:52:30 +0200

    [issue422] return to original add_config API (without search_rev kwargs, etc.)

--- experiments/issue422/issue422.py
+++ experiments/issue422/issue422.py
@@ -14,7 +14,7 @@ del configs["astar_selmax_lmcut_lmcount"]
 
 exp = common_setup.IssueExperiment(
     grid_priority=0,
-    #search_revisions=["issue422-base", "issue422-v1"],
+    search_revisions=["issue422-base", "issue422-v1"],
     configs=configs,
     suite=suite_optimal_with_ipc11(),
     )
