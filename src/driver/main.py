c1da711182912332506355dd3816262281826cb8
Malte Helmert <malte.helmert@unibas.ch>
2014-04-12 23:48:47 +0200

    [issue414] driver: split off module for running the different search components

--- src/driver/main.py
+++ src/driver/main.py
@@ -1,46 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import os
-import os.path
 import resource
-import subprocess
 import sys
 
 from . import aliases
 from . import arguments
-from . import input_analyzer
-
-
-DRIVER_DIR = os.path.abspath(os.path.dirname(__file__))
-PARENT_DIR = os.path.dirname(DRIVER_DIR)
-TRANSLATE = os.path.join(PARENT_DIR, "translate", "translate.py")
-PREPROCESS = os.path.join(PARENT_DIR, "preprocess", "preprocess")
-SEARCH_DIR = os.path.join(PARENT_DIR, "search")
-
-
-def write_elapsed_time():
-    ## Note: According to the os.times documentation, Windows sets the
-    ## child time components to 0, so this won't work properly under
-    ## Windows.
-    ##
-    ## TODO: Find a solution for this. A simple solution might be to
-    ## just document this as a limitation under Windows that causes
-    ## time slices for portfolios to be allocated slightly wrongly.
-    ## Another solution would be to base time slices on wall-clock
-    ## time under Windows.
-    times = os.times()
-    child_elapsed = times[2] + times[3]
-    with open("elapsed.time", "w") as time_file:
-        print >> time_file, child_elapsed
-
-
-def call_cmd(cmd, args, stdin=None):
-    sys.stdout.flush()
-    if stdin:
-        with open(stdin) as stdin_file:
-            subprocess.check_call([cmd] + args, stdin=stdin_file)
-    else:
-        subprocess.check_call([cmd] + args)
+from . import run_components
 
 
 def set_memory_limit():
@@ -61,52 +26,6 @@ def set_memory_limit():
         resource.setrlimit(resource.RLIMIT_AS, (ALLOWED_MEMORY, hard))
 
 
-def run_translate(args):
-    print "*** Running translator."
-    print "*** translator arguments: %s" % args.translate_args
-    call_cmd(TRANSLATE, args.translate_args)
-    print "***"
-
-
-def run_preprocess(args):
-    print "*** Running preprocessor."
-    print "*** preprocessor arguments: %s" % args.preprocess_args
-    call_cmd(PREPROCESS, args.preprocess_args, stdin="output.sas")
-    print "***"
-
-
-def run_search(args):
-    print "*** Running search."
-
-    if args.unit_cost_search_args is not None:
-        if input_analyzer.is_unit_cost(args.input_file_search):
-            print "*** using special configuration for unit-cost problems"
-            args.search_args = args.unit_cost_search_args
-
-    if args.debug:
-        executable = os.path.join(SEARCH_DIR, "downward-debug")
-    else:
-        executable = os.path.join(SEARCH_DIR, "downward-release")
-    print "*** executable:", executable
-
-    if args.portfolio_file:
-        assert not args.search_args
-        # TODO: Implement portfolios. Don't forget exit code. Take
-        # into account the time used by *this* process too for the
-        # portfolio time slices. After all, we used to take into
-        # account time for dispatch script and unitcost script, and
-        # these are now in-process.
-        raise NotImplementedError
-    else:
-        print "*** final search args:", args.search_args
-        write_elapsed_time()
-        if args.help_search:
-            call_cmd(executable, args.search_args)
-        else:
-            call_cmd(executable, args.search_args, stdin=args.input_file_search)
-    print "***"
-
-
 def main():
     args = arguments.parse_args()
     print "*** processed args:", args
@@ -121,11 +40,12 @@ def main():
     if len(args.translate_args) == 0:
         # This is of course a HACK.
         print "*** no positional filenames: run search only"
-        run_search(args)
+        run_components.run_search(args)
     else:
-        run_translate(args)
-        run_preprocess(args)
-        run_search(args)
+        print "*** run all planner components"
+        run_components.run_translate(args)
+        run_components.run_preprocess(args)
+        run_components.run_search(args)
 
     ## TODO: The old "plan" script preserved the exit code of the
     ## search component. Make sure that this one does, too, both in
---
+++ src/driver/run_components.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+import os
+import os.path
+import subprocess
+import sys
+
+from . import input_analyzer
+
+
+DRIVER_DIR = os.path.abspath(os.path.dirname(__file__))
+PARENT_DIR = os.path.dirname(DRIVER_DIR)
+TRANSLATE = os.path.join(PARENT_DIR, "translate", "translate.py")
+PREPROCESS = os.path.join(PARENT_DIR, "preprocess", "preprocess")
+SEARCH_DIR = os.path.join(PARENT_DIR, "search")
+
+
+def write_elapsed_time():
+    ## Note: According to the os.times documentation, Windows sets the
+    ## child time components to 0, so this won't work properly under
+    ## Windows.
+    ##
+    ## TODO: Find a solution for this. A simple solution might be to
+    ## just document this as a limitation under Windows that causes
+    ## time slices for portfolios to be allocated slightly wrongly.
+    ## Another solution would be to base time slices on wall-clock
+    ## time under Windows.
+    times = os.times()
+    child_elapsed = times[2] + times[3]
+    with open("elapsed.time", "w") as time_file:
+        print >> time_file, child_elapsed
+
+
+def call_cmd(cmd, args, stdin=None):
+    sys.stdout.flush()
+    if stdin:
+        with open(stdin) as stdin_file:
+            subprocess.check_call([cmd] + args, stdin=stdin_file)
+    else:
+        subprocess.check_call([cmd] + args)
+
+
+def run_translate(args):
+    print "*** Running translator."
+    print "*** translator arguments: %s" % args.translate_args
+    call_cmd(TRANSLATE, args.translate_args)
+    print "***"
+
+
+def run_preprocess(args):
+    print "*** Running preprocessor."
+    print "*** preprocessor arguments: %s" % args.preprocess_args
+    call_cmd(PREPROCESS, args.preprocess_args, stdin="output.sas")
+    print "***"
+
+
+def run_search(args):
+    print "*** Running search."
+
+    if args.unit_cost_search_args is not None:
+        if input_analyzer.is_unit_cost(args.input_file_search):
+            print "*** using special configuration for unit-cost problems"
+            args.search_args = args.unit_cost_search_args
+
+    if args.debug:
+        executable = os.path.join(SEARCH_DIR, "downward-debug")
+    else:
+        executable = os.path.join(SEARCH_DIR, "downward-release")
+    print "*** executable:", executable
+
+    if args.portfolio_file:
+        assert not args.search_args
+        # TODO: Implement portfolios. Don't forget exit code. Take
+        # into account the time used by *this* process too for the
+        # portfolio time slices. After all, we used to take into
+        # account time for dispatch script and unitcost script, and
+        # these are now in-process.
+        raise NotImplementedError
+    else:
+        print "*** final search args:", args.search_args
+        write_elapsed_time()
+        if args.help_search:
+            call_cmd(executable, args.search_args)
+        else:
+            call_cmd(executable, args.search_args, stdin=args.input_file_search)
+    print "***"
