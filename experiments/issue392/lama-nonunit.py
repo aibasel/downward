e2cf8d119ed4778acd27bf88d4a13fe00d50e65d
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-05-06 13:28:36 +0200

    [issue392] use enum instead of numerical values

--- experiments/issue392/lama-nonunit.py
+++ experiments/issue392/lama-nonunit.py
@@ -12,11 +12,11 @@ LIMITS = {"search_time": 300}
 CONFIGS = {}
 for option in ["pref_first", "original", "shuffled", "shuffled_pref_first"]:
     CONFIGS["lama-nonunit-%s" % option] = [
-        "--heuristic", "hlm1,hff1=lm_ff_syn(lm_rhw(reasonable_orders=true,lm_cost_type=1,cost_type=1))",
-        "--heuristic", "hlm2,hff2=lm_ff_syn(lm_rhw(reasonable_orders=true,lm_cost_type=2,cost_type=2))",
+        "--heuristic", "hlm1,hff1=lm_ff_syn(lm_rhw(reasonable_orders=true,lm_cost_type=ONE,cost_type=ONE))",
+        "--heuristic", "hlm2,hff2=lm_ff_syn(lm_rhw(reasonable_orders=true,lm_cost_type=PLUSONE,cost_type=PLUSONE))",
         "--search",
         "iterated(["
-            "lazy_greedy([hff1,hlm1],preferred=[hff1,hlm1],succ_order=%(option)s,cost_type=1,reopen_closed=false),"
+            "lazy_greedy([hff1,hlm1],preferred=[hff1,hlm1],succ_order=%(option)s,cost_type=ONE,reopen_closed=false),"
             "lazy_greedy([hff2,hlm2],preferred=[hff2,hlm2],succ_order=%(option)s,reopen_closed=false),"
             "lazy_wastar([hff2,hlm2],preferred=[hff2,hlm2],succ_order=%(option)s,w=5),"
             "lazy_wastar([hff2,hlm2],preferred=[hff2,hlm2],succ_order=%(option)s,w=3),"
