e6d227aea67de6849be39807f003ee5d8dbdbd25
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-05-07 10:22:37 +0200

    [issue392] update LAMA experiments

--- experiments/issue392/lama-unit.py
+++ experiments/issue392/lama-unit.py
@@ -6,37 +6,30 @@ from downward import suites
 import common_setup
 
 
-REVS = ["issue392-v1"]
+REVS = ["issue392-v2"]
 LIMITS = {"search_time": 300}
 
 CONFIGS = {}
-for option in ["pref_first", "original", "shuffled", "shuffled_pref_first"]:
-    CONFIGS["lama-unit-%s" % option] = [
+for randomize in ["false", "true"]:
+    for pref_first in ["false", "true"]:
+        CONFIGS["lama-unit-randomize-%(randomize)s-pref_first-%(pref_first)s" % locals()] = [
         "--heuristic",
         "hlm,hff=lm_ff_syn(lm_rhw(reasonable_orders=true,lm_cost_type=PLUSONE,cost_type=PLUSONE))",
         "--search",
         "iterated(["
-            "lazy_greedy([hff,hlm],preferred=[hff,hlm],succ_order=%(option)s),"
-            "lazy_wastar([hff,hlm],preferred=[hff,hlm],succ_order=%(option)s,w=5),"
-            "lazy_wastar([hff,hlm],preferred=[hff,hlm],succ_order=%(option)s,w=3),"
-            "lazy_wastar([hff,hlm],preferred=[hff,hlm],succ_order=%(option)s,w=2),"
-            "lazy_wastar([hff,hlm],preferred=[hff,hlm],succ_order=%(option)s,w=1)],"
+            "lazy_greedy([hff,hlm],preferred=[hff,hlm],randomize_successors=%(randomize)s,preferred_successors_first=%(pref_first)s),"
+            "lazy_wastar([hff,hlm],preferred=[hff,hlm],randomize_successors=%(randomize)s,preferred_successors_first=%(pref_first)s,w=5),"
+            "lazy_wastar([hff,hlm],preferred=[hff,hlm],randomize_successors=%(randomize)s,preferred_successors_first=%(pref_first)s,w=3),"
+            "lazy_wastar([hff,hlm],preferred=[hff,hlm],randomize_successors=%(randomize)s,preferred_successors_first=%(pref_first)s,w=2),"
+            "lazy_wastar([hff,hlm],preferred=[hff,hlm],randomize_successors=%(randomize)s,preferred_successors_first=%(pref_first)s,w=1)],"
             "repeat_last=true,continue_on_fail=true)" % locals()
     ]
 
-TEST_RUN = False
-
-if TEST_RUN:
-    SUITE = "gripper:prob01.pddl"
-    PRIORITY = None  # "None" means local experiment
-else:
-    SUITE = sorted(set(suites.suite_satisficing_with_ipc11()) &
-                   set(suites.suite_unit_costs()))
-    PRIORITY = 0     # number means maia experiment
+SUITE = sorted(set(suites.suite_satisficing_with_ipc11()) &
+               set(suites.suite_unit_costs()))
 
 
-exp = common_setup.MyExperiment(
-    grid_priority=PRIORITY,
+exp = common_setup.IssueExperiment(
     revisions=REVS,
     configs=CONFIGS,
     suite=SUITE,
