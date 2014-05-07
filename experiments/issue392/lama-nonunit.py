9395ad481c27a83262fe2678e736d975362888c3
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-05-07 10:22:37 +0200

    [issue392] update LAMA experiments

--- experiments/issue392/lama-nonunit.py
+++ experiments/issue392/lama-nonunit.py
@@ -6,38 +6,31 @@ from downward import suites
 import common_setup
 
 
-REVS = ["issue392-v1"]
+REVS = ["issue392-v2"]
 LIMITS = {"search_time": 300}
 
 CONFIGS = {}
-for option in ["pref_first", "original", "shuffled", "shuffled_pref_first"]:
-    CONFIGS["lama-nonunit-%s" % option] = [
-        "--heuristic", "hlm1,hff1=lm_ff_syn(lm_rhw(reasonable_orders=true,lm_cost_type=ONE,cost_type=ONE))",
-        "--heuristic", "hlm2,hff2=lm_ff_syn(lm_rhw(reasonable_orders=true,lm_cost_type=PLUSONE,cost_type=PLUSONE))",
-        "--search",
-        "iterated(["
-            "lazy_greedy([hff1,hlm1],preferred=[hff1,hlm1],succ_order=%(option)s,cost_type=ONE,reopen_closed=false),"
-            "lazy_greedy([hff2,hlm2],preferred=[hff2,hlm2],succ_order=%(option)s,reopen_closed=false),"
-            "lazy_wastar([hff2,hlm2],preferred=[hff2,hlm2],succ_order=%(option)s,w=5),"
-            "lazy_wastar([hff2,hlm2],preferred=[hff2,hlm2],succ_order=%(option)s,w=3),"
-            "lazy_wastar([hff2,hlm2],preferred=[hff2,hlm2],succ_order=%(option)s,w=2),"
-            "lazy_wastar([hff2,hlm2],preferred=[hff2,hlm2],succ_order=%(option)s,w=1)],"
-            "repeat_last=true,continue_on_fail=true)" % locals()
-    ]
+for randomize in ["false", "true"]:
+    for pref_first in ["false", "true"]:
+        CONFIGS["lama-nonunit-randomize-%(randomize)s-pref_first-%(pref_first)s" % locals()] = [
+            "--heuristic", "hlm1,hff1=lm_ff_syn(lm_rhw(reasonable_orders=true,lm_cost_type=ONE,cost_type=ONE))",
+            "--heuristic", "hlm2,hff2=lm_ff_syn(lm_rhw(reasonable_orders=true,lm_cost_type=PLUSONE,cost_type=PLUSONE))",
+            "--search",
+            "iterated(["
+                "lazy_greedy([hff1,hlm1],preferred=[hff1,hlm1],randomize_successors=%(randomize)s,preferred_successors_first=%(pref_first)s,cost_type=ONE,reopen_closed=false),"
+                "lazy_greedy([hff2,hlm2],preferred=[hff2,hlm2],randomize_successors=%(randomize)s,preferred_successors_first=%(pref_first)s,reopen_closed=false),"
+                "lazy_wastar([hff2,hlm2],preferred=[hff2,hlm2],randomize_successors=%(randomize)s,preferred_successors_first=%(pref_first)s,w=5),"
+                "lazy_wastar([hff2,hlm2],preferred=[hff2,hlm2],randomize_successors=%(randomize)s,preferred_successors_first=%(pref_first)s,w=3),"
+                "lazy_wastar([hff2,hlm2],preferred=[hff2,hlm2],randomize_successors=%(randomize)s,preferred_successors_first=%(pref_first)s,w=2),"
+                "lazy_wastar([hff2,hlm2],preferred=[hff2,hlm2],randomize_successors=%(randomize)s,preferred_successors_first=%(pref_first)s,w=1)],"
+                "repeat_last=true,continue_on_fail=true)" % locals()
+        ]
 
-TEST_RUN = False
-
-if TEST_RUN:
-    SUITE = "gripper:prob01.pddl"
-    PRIORITY = None  # "None" means local experiment
-else:
-    SUITE = sorted(set(suites.suite_satisficing_with_ipc11()) &
-                   set(suites.suite_diverse_costs()))
-    PRIORITY = 0     # number means maia experiment
+SUITE = sorted(set(suites.suite_satisficing_with_ipc11()) &
+               set(suites.suite_diverse_costs()))
 
 
-exp = common_setup.MyExperiment(
-    grid_priority=PRIORITY,
+exp = common_setup.IssueExperiment(
     revisions=REVS,
     configs=CONFIGS,
     suite=SUITE,
