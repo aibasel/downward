77db3f48b67c92c7b462a8cb452b5ea5f3c0c51a
Silvan Sievers <silvan.sievers@unibas.ch>
2014-07-12 10:30:14 +0200

    [main] fixed ipc merge-and-shrink config

--- src/search/downward-seq-opt-merge-and-shrink.py
+++ src/search/downward-seq-opt-merge-and-shrink.py
@@ -5,9 +5,9 @@ import portfolio
 
 CONFIGS = [
     (800, ["--search",
-           "astar(merge_and_shrink(merge_strategy=merge_linear_reverse_level,shrink_strategy=shrink_bisimulation(max_states=infinity,threshold=1,greedy=true,group_by_h=false)))"]),
+           "astar(merge_and_shrink(merge_strategy=merge_linear(variable_order=reverse_level),shrink_strategy=shrink_bisimulation(max_states=infinity,threshold=1,greedy=true,group_by_h=false)))"]),
     (1000, ["--search",
-           "astar(merge_and_shrink(merge_strategy=merge_linear_reverse_level,shrink_strategy=shrink_bisimulation(max_states=200000,greedy=false,group_by_h=true)))"]),
+           "astar(merge_and_shrink(merge_strategy=merge_linear(variable_order=reverse_level),shrink_strategy=shrink_bisimulation(max_states=200000,greedy=false,group_by_h=true)))"]),
      ]
 
 portfolio.run(CONFIGS, optimal=True)
