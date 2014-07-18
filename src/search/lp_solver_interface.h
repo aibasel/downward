aa23f90a5697aec98bcb025f78b615f0761b7f40
Florian Pommerening <florian.pommerening@unibas.ch>
2014-07-18 15:50:42 +0200

    [issue429] Reorder methods to have same order in header and cc file

--- src/search/lp_solver_interface.h
+++ src/search/lp_solver_interface.h
@@ -27,6 +27,18 @@ void add_lp_solver_option_to_parser(OptionParser &parser);
 
 #ifdef USE_LP
 
+OsiSolverInterface *create_lp_solver(LPSolverType solver_type);
+
+
+// Print the CoinError and then exit with EXIT_CRITICAL_ERROR.
+// We would like to also handle out-of-memory exceptions here by returning
+// EXIT_OUT_OF_MEMORY, but (at least for cplex) they do not induce
+// exceptions. Instead, they produce a warning in the message handler
+// and then continue. To handle them correctly, we use a custom handler.
+// See comment on ErrorCatchingCoinMessageHandler.
+extern void handle_coin_error(CoinError error) __attribute__((noreturn));
+
+
 // Cplex sometimes does not report errors as exceptions, and only prints an
 // error message. This class will report any error messages as usual but will
 // exit with a critical error afterwards.
@@ -39,18 +51,6 @@ public:
 
     virtual void checkSeverity();
 };
-
-
-OsiSolverInterface *create_lp_solver(LPSolverType solver_type);
-
-
-// Print the CoinError and then exit with EXIT_CRITICAL_ERROR.
-// We would like to also handle out-of-memory exceptions here by returning
-// EXIT_OUT_OF_MEMORY, but (at least for cplex) they do not induce
-// exceptions. Instead, they produce a warning in the message handler
-// and then continue. To handle them correctly, we use a custom handler.
-// See comment on ErrorCatchingCoinMessageHandler.
-extern void handle_coin_error(CoinError error) __attribute__((noreturn));
 #endif
 
 #endif
