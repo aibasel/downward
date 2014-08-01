0947f4537c0474638349deef98dc369d683dd0d9
Malte Helmert <malte.helmert@unibas.ch>
2014-08-02 00:38:13 +0200

    [issue414] driver: implemented splitting of arguments for planner components; also some minor refactoring and renaming

--- src/driver/arguments.py
+++ src/driver/arguments.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import os.path
 
 from . import aliases
 
@@ -11,7 +10,62 @@ have a special meaning for the driver (see below) are passed on to the
 search component."""
 
 
-def check_arg_conflicts(parser, possible_conflicts):
+def _split_off_filenames(planner_args):
+    """Given the list of arguments to be passed on to the planner
+    components, split it into a prefix of filenames and a suffix of
+    options. Returns a pair (filenames, options).
+
+    If a "--" separator is present, the last such separator serves as
+    the border between filenames and options. The separator itself is
+    not returned. (This implies that "--" can be a filename, but never
+    an option to a planner component.)
+
+    If no such separator is present, the first argument that begins
+    with "-" and consists of at least two characters starts the list
+    of options, and all previous arguments are filenames."""
+
+    if "--" in planner_args:
+        separator_pos = planner_args.rindex("--")
+        num_filenames = separator_pos
+        del planner_args[separator_pos]
+    else:
+        num_filenames = 0
+        for arg in planner_args:
+            # We treat "-" by itself as a filename because by common
+            # convention it denotes stdin or stdout, and we might want
+            # to support this later.
+            if arg.startswith("-") and arg != "-":
+                break
+            num_filenames += 1
+    return planner_args[:num_filenames], planner_args[num_filenames:]
+
+
+def _split_planner_args(parser, args):
+    """Partitions args.planner_args, the list of arguments for the
+    planner components, into args.filenames, args.translate_options,
+    arge.preprocess_options and args.search_options. Modifies args
+    directly and removes the original args.planner_args list."""
+
+    args.filenames, options = _split_off_filenames(args.planner_args)
+    del args.planner_args
+
+    args.translate_options = []
+    args.preprocess_options = []
+    args.search_options = []
+
+    curr_options = args.search_options
+    for option in options:
+        if option == "--translate-options":
+            curr_options = args.translate_options
+        elif option == "--preprocess-options":
+            curr_options = args.preprocess_options
+        elif option == "--search-options":
+            curr_options = args.search_options
+        else:
+            curr_options.append(option)
+
+
+def _check_arg_conflicts(parser, possible_conflicts):
     for pos, (name1, is_specified1) in enumerate(possible_conflicts):
         for name2, is_specified2 in possible_conflicts[pos + 1:]:
             if is_specified1 and is_specified2:
@@ -19,47 +73,49 @@ def check_arg_conflicts(parser, possible_conflicts):
 
 
 def parse_args():
+    # TODO: Need better usage string. We might also want to improve
+    # the help output more generally. Note that there are various ways
+    # to finetune this by including a formatter, an epilog, etc.
     parser = argparse.ArgumentParser(description=DESCRIPTION)
-    parser.add_argument("--alias",
-                        help="run a config with an alias (e.g. seq-sat-lama-2011)")
-    parser.add_argument("--debug", action="store_true",
-                        help="run search component in debug mode")
-    parser.add_argument("--help-search", action="store_true",
-                        help="pass --help argument to search component")
-    parser.add_argument("--ipc", dest="alias",
-                        help="same as --alias")
-    parser.add_argument("--portfolio-file", metavar="FILE",
-                        help="run a portfolio specified in FILE")
-    parser.add_argument("--show-aliases", action="store_true",
-                        help="show the known aliases; don't run search")
+    parser.add_argument(
+        "--alias",
+        help="run a config with an alias (e.g. seq-sat-lama-2011)")
+    parser.add_argument(
+        "--debug", action="store_true",
+        help="run search component in debug mode")
+    parser.add_argument(
+        "--ipc", dest="alias",
+        help="same as --alias")
+    parser.add_argument(
+        "--portfolio", metavar="FILE",
+        help="run a portfolio specified in FILE")
+    parser.add_argument(
+        "--show-aliases", action="store_true",
+        help="show the known aliases; don't run search")
+    parser.add_argument(
+        "planner_args", nargs=argparse.REMAINDER,
+        help="file names and options passed on to planner components")
 
-    args, unparsed_args = parser.parse_known_args()
+    # Using argparse.REMAINDER relies on the fact that the first
+    # argument that doesn't belong to the driver doesn't look like an
+    # option, i.e., doesn't start with "-". This is usually satisfied
+    # because the argument is a filename; in exceptional cases, "--"
+    # can be used as an explicit separator. For example, "./plan.py --
+    # --help" passes "--help" to the search code.
 
-    num_filenames = 0
-    for arg in unparsed_args:
-        if not os.path.exists(arg):
-            break
-        num_filenames += 1
-    if num_filenames >= 3:
-        parser.error("too many filenames: %r" % args.filenames)
+    args = parser.parse_args()
 
-    args.translate_args = unparsed_args[:num_filenames]
-    args.preprocess_args = []
-    args.search_args = unparsed_args[num_filenames:]
-    args.unit_cost_search_args = None
+    _split_planner_args(parser, args)
+    args.unit_cost_search_options = None
 
-    check_arg_conflicts(parser, [
+    _check_arg_conflicts(parser, [
             ("--alias", args.alias is not None),
-            ("--help-search", args.help_search),
-            ("--portfolio-file", args.portfolio_file is not None),
-            ("arguments to search component", bool(args.search_args))])
-
-    if args.help_search:
-        args.search_args = ["--help"]
+            ("--portfolio", args.portfolio is not None),
+            ("options for search component", bool(args.search_options))])
 
     if args.alias:
         try:
-            aliases.set_args_for_alias(args.alias, args)
+            aliases.set_options_for_alias(args.alias, args)
         except KeyError:
             parser.error("unknown alias: %r" % args.alias)
 
--- src/driver/run_components.py
+++ src/driver/run_components.py
@@ -42,15 +42,16 @@ def call_cmd(cmd, args, stdin=None):
 
 def run_translate(args):
     print "*** Running translator."
-    print "*** translator arguments: %s" % args.translate_args
-    call_cmd(TRANSLATE, args.translate_args)
+    print "*** filenames: %s" % args.filenames
+    print "*** translator arguments: %s" % args.translate_options
+    call_cmd(TRANSLATE, args.filenames + args.translate_options)
     print "***"
 
 
 def run_preprocess(args):
     print "*** Running preprocessor."
-    print "*** preprocessor arguments: %s" % args.preprocess_args
-    call_cmd(PREPROCESS, args.preprocess_args, stdin="output.sas")
+    print "*** preprocessor arguments: %s" % args.preprocess_options
+    call_cmd(PREPROCESS, args.preprocess_options, stdin="output.sas")
     print "***"
 
 
@@ -63,10 +64,8 @@ def run_search(args):
         executable = os.path.join(SEARCH_DIR, "downward-release")
     print "*** executable:", executable
 
-    if args.help_search:
-        call_cmd(executable, args.search_args)
-    elif args.portfolio_file:
-        assert not args.search_args
+    if args.portfolio_file:
+        assert not args.search_options
         # TODO: Implement portfolios. Don't forget exit code. Take
         # into account the time used by *this* process too for the
         # portfolio time slices. After all, we used to take into
@@ -79,14 +78,14 @@ def run_search(args):
     else:
         INPUT_FILE = "output"
 
-        search_args = args.search_args
+        search_options = args.search_options
 
-        if args.unit_cost_search_args is not None:
+        if args.unit_cost_search_options is not None:
             if input_analyzer.is_unit_cost(INPUT_FILE):
                 print "*** using special configuration for unit-cost problems"
-                search_args = args.unit_cost_search_args
+                search_options = args.unit_cost_search_options
 
-        print "*** final search args:", search_args
+        print "*** final search options:", search_options
         write_elapsed_time()
-        call_cmd(executable, search_args, stdin=INPUT_FILE)
+        call_cmd(executable, search_options, stdin=INPUT_FILE)
     print "***"
