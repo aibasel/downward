3164ecd479e27ef453f0c0c16ac21b32140172dd
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-08-11 14:13:01 +0200

    [issue439] add assertion

--- src/search/global_task.h
+++ src/search/global_task.h
@@ -10,7 +10,10 @@
 
 class GlobalTaskImpl : public TaskImpl {
 public:
-    int get_operator_cost(size_t index) const {return g_operators[index].get_cost(); }
+    int get_operator_cost(size_t index) const {
+        assert(index < g_operators.size());
+        return g_operators[index].get_cost();
+    }
     OperatorRef get_operator(size_t index) const {return OperatorRef(*this, index); }
     size_t get_number_of_operators() const {return g_operators.size(); }
     const OperatorsRef get_operators() const {return OperatorsRef(*this); }
