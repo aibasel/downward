d2311e02a0593ea052810d15b835b6b385a8fc68
Patrick von Reth <vonreth@kde.org>
2014-08-20 13:01:27 +0200

    [issue455] uncrustify

--- src/search/const_evaluator.cc
+++ src/search/const_evaluator.cc
@@ -4,19 +4,15 @@
 
 
 ConstEvaluator::ConstEvaluator(const Options &opts)
-    :Heuristic(opts), value(opts.get<int>("value")){
+    : Heuristic(opts), value(opts.get<int>("value")) {
 }
-
 
 ConstEvaluator::~ConstEvaluator() {
 }
 
-int ConstEvaluator::compute_heuristic(const State &)
-{
+int ConstEvaluator::compute_heuristic(const State &) {
     return value;
 }
-
-
 
 bool ConstEvaluator::is_dead_end() const {
     return false;
@@ -29,7 +25,7 @@ bool ConstEvaluator::dead_end_is_reliable() const {
 static ScalarEvaluator *_parse(OptionParser &parser) {
     parser.document_synopsis("const-value evaluator",
                              "Returns a constant value.");
-    parser.add_option<int>("value","the constant value", "1");
+    parser.add_option<int>("value", "the constant value", "1");
     Heuristic::add_options_to_parser(parser);
     Options opts = parser.parse();
     if (parser.dry_run())
