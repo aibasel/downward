cd6a80985201e219d8b44812b0c97df4159025bb
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-08-21 10:44:14 +0200

    [issue439] impl -> interface

--- src/search/task.h
+++ src/search/task.h
@@ -48,12 +48,12 @@ public:
 
 
 class Fact {
-    const TaskInterface &impl;
+    const TaskInterface &interface;
     std::size_t var_id;
     std::size_t value;
 public:
-    Fact(const TaskInterface &impl_, std::size_t var_id_, std::size_t value_)
-        : impl(impl_), var_id(var_id_), value(value_) {}
+    Fact(const TaskInterface &interface_, std::size_t var_id_, std::size_t value_)
+        : interface(interface_), var_id(var_id_), value(value_) {}
     ~Fact() {}
     std::size_t get_value() const {return value; }
     Variable get_variable() const;
@@ -61,32 +61,33 @@ public:
 
 
 class Variable {
-    const TaskInterface &impl;
+    const TaskInterface &interface;
     std::size_t id;
 public:
-    Variable(const TaskInterface &impl_, std::size_t id_) : impl(impl_), id(id_) {}
+    Variable(const TaskInterface &interface_, std::size_t id_)
+        : interface(interface_), id(id_) {}
     ~Variable() {}
     std::size_t get_id() const {return id; }
-    std::size_t get_domain_size() const {return impl.get_variable_domain_size(id); }
-    Fact get_fact(std::size_t index) const {return Fact(impl, id, index); }
+    std::size_t get_domain_size() const {return interface.get_variable_domain_size(id); }
+    Fact get_fact(std::size_t index) const {return Fact(interface, id, index); }
 };
 
 
 class Variables {
-    const TaskInterface &impl;
+    const TaskInterface &interface;
 public:
-    Variables(const TaskInterface &impl_) : impl(impl_) {}
+    Variables(const TaskInterface &interface_) : interface(interface_) {}
     ~Variables() {}
-    std::size_t size() const {return impl.get_num_variables(); }
-    Variable operator[](std::size_t index) const {return Variable(impl, index); }
+    std::size_t size() const {return interface.get_num_variables(); }
+    Variable operator[](std::size_t index) const {return Variable(interface, index); }
 };
 
 
 class Condition {
 protected:
-    const TaskInterface &impl;
+    const TaskInterface &interface;
 public:
-    Condition(const TaskInterface &impl_) : impl(impl_) {}
+    Condition(const TaskInterface &interface_) : interface(interface_) {}
     ~Condition() {}
     virtual std::size_t size() const = 0;
     virtual Fact operator[](std::size_t index) const = 0;
@@ -96,26 +97,26 @@ public:
 class Precondition : Condition {
     std::size_t op_index;
 public:
-    Precondition(const TaskInterface &impl_, std::size_t op_index_)
-        : Condition(impl_), op_index(op_index_) {}
+    Precondition(const TaskInterface &interface_, std::size_t op_index_)
+        : Condition(interface_), op_index(op_index_) {}
     ~Precondition() {}
-    std::size_t size() const {return impl.get_operator_precondition_size(op_index); }
+    std::size_t size() const {return interface.get_operator_precondition_size(op_index); }
     Fact operator[](std::size_t fact_index) const {
         std::pair<std::size_t, std::size_t> fact =
-            impl.get_operator_precondition_fact(op_index, fact_index);
-        return Fact(impl, fact.first, fact.second);
+            interface.get_operator_precondition_fact(op_index, fact_index);
+        return Fact(interface, fact.first, fact.second);
     }
 };
 
 
 class Goal : Condition {
 public:
-    Goal(const TaskInterface &impl_) : Condition(impl_) {}
+    Goal(const TaskInterface &interface_) : Condition(interface_) {}
     ~Goal() {}
-    std::size_t size() const {return impl.get_goal_size(); }
+    std::size_t size() const {return interface.get_goal_size(); }
     Fact operator[](std::size_t index) const {
-        std::pair<std::size_t, std::size_t> fact = impl.get_goal_fact(index);
-        return Fact(impl, fact.first, fact.second);
+        std::pair<std::size_t, std::size_t> fact = interface.get_goal_fact(index);
+        return Fact(interface, fact.first, fact.second);
     }
 };
 
@@ -125,106 +126,106 @@ class EffectCondition : Condition {
     std::size_t eff_index;
 public:
     EffectCondition(
-        const TaskInterface &impl_, std::size_t op_index_, std::size_t eff_index_)
-        : Condition(impl_), op_index(op_index_), eff_index(eff_index_) {}
+        const TaskInterface &interface_, std::size_t op_index_, std::size_t eff_index_)
+        : Condition(interface_), op_index(op_index_), eff_index(eff_index_) {}
     ~EffectCondition() {}
     std::size_t size() const {
-        return impl.get_operator_effect_condition_size(op_index, eff_index);
+        return interface.get_operator_effect_condition_size(op_index, eff_index);
     }
     Fact operator[](std::size_t index) const {
         std::pair<std::size_t, std::size_t> fact =
-            impl.get_operator_effect_condition(op_index, eff_index, index);
-        return Fact(impl, fact.first, fact.second);
+            interface.get_operator_effect_condition(op_index, eff_index, index);
+        return Fact(interface, fact.first, fact.second);
     }
 };
 
 
 class Effect {
-    const TaskInterface &impl;
+    const TaskInterface &interface;
     std::size_t op_index;
     std::size_t eff_index;
 public:
-    Effect(const TaskInterface &impl_, std::size_t op_index_, std::size_t eff_index_)
-        : impl(impl_), op_index(op_index_), eff_index(eff_index_) {}
+    Effect(const TaskInterface &interface_, std::size_t op_index_, std::size_t eff_index_)
+        : interface(interface_), op_index(op_index_), eff_index(eff_index_) {}
     ~Effect() {}
     EffectCondition get_condition() const {
-        return EffectCondition(impl, op_index, eff_index);
+        return EffectCondition(interface, op_index, eff_index);
     }
     Fact get_effect() const {
         std::pair<std::size_t, std::size_t> fact =
-            impl.get_operator_effect(op_index, eff_index);
-        return Fact(impl, fact.first, fact.second);
+            interface.get_operator_effect(op_index, eff_index);
+        return Fact(interface, fact.first, fact.second);
     }
 };
 
 
 class Effects {
-    const TaskInterface &impl;
+    const TaskInterface &interface;
     std::size_t op_index;
 public:
-    Effects(const TaskInterface &impl_, std::size_t op_index_)
-        : impl(impl_), op_index(op_index_) {}
+    Effects(const TaskInterface &interface_, std::size_t op_index_)
+        : interface(interface_), op_index(op_index_) {}
     ~Effects() {}
-    std::size_t size() const {return impl.get_num_operator_effects(op_index); }
+    std::size_t size() const {return interface.get_num_operator_effects(op_index); }
     Effect operator[](std::size_t eff_index) const {
-        return Effect(impl, op_index, eff_index);
+        return Effect(interface, op_index, eff_index);
     }
 };
 
 
 class OperatorRef {
-    const TaskInterface &impl;
+    const TaskInterface &interface;
     size_t index;
 public:
-    OperatorRef(const TaskInterface &impl_, std::size_t index_)
-        : impl(impl_), index(index_) {}
+    OperatorRef(const TaskInterface &interface_, std::size_t index_)
+        : interface(interface_), index(index_) {}
     ~OperatorRef() {}
-    Precondition get_precondition() const {return Precondition(impl, index); }
-    Effects get_effects() const {return Effects(impl, index); }
-    int get_cost() const {return impl.get_operator_cost(index); }
+    Precondition get_precondition() const {return Precondition(interface, index); }
+    Effects get_effects() const {return Effects(interface, index); }
+    int get_cost() const {return interface.get_operator_cost(index); }
     int get_adjusted_cost(OperatorCost cost_type) const {
-        return impl.get_adjusted_operator_cost(index, cost_type);
+        return interface.get_adjusted_operator_cost(index, cost_type);
     }
     std::size_t get_index() const {return index; }
 };
 
 
 class Operators {
-    const TaskInterface &impl;
+    const TaskInterface &interface;
 public:
-    Operators(const TaskInterface &impl_) : impl(impl_) {}
+    Operators(const TaskInterface &interface_) : interface(interface_) {}
     ~Operators() {}
-    std::size_t size() const {return impl.get_num_operators(); }
+    std::size_t size() const {return interface.get_num_operators(); }
     OperatorRef operator[](std::size_t index) const {
-        return OperatorRef(impl, index);
+        return OperatorRef(interface, index);
     }
 };
 
 
 class Axioms {
-    const TaskInterface &impl;
+    const TaskInterface &interface;
 public:
-    Axioms(const TaskInterface &impl_) : impl(impl_) {}
+    Axioms(const TaskInterface &interface_) : interface(interface_) {}
     ~Axioms() {}
-    std::size_t size() const {return impl.get_num_axioms(); }
+    std::size_t size() const {return interface.get_num_axioms(); }
     OperatorRef operator[](std::size_t index) const {
-        return OperatorRef(impl, index);
+        return OperatorRef(interface, index);
     }
 };
 
 
 class Task {
-    const TaskInterface &impl;
+    const TaskInterface &interface;
 public:
-    Task(const TaskInterface &impl_) : impl(impl_) {}
+    Task(const TaskInterface &interface_) : interface(interface_) {}
     ~Task() {}
-    Variables get_variables() const {return Variables(impl); }
-    Operators get_operators() const {return Operators(impl); }
+    Variables get_variables() const {return Variables(interface); }
+    Operators get_operators() const {return Operators(interface); }
     const Operator *get_original_operator(OperatorRef op_ref) const {
-        return impl.get_original_operator(op_ref.get_index());
+        return interface.get_original_operator(op_ref.get_index());
     }
-    Axioms get_axioms() const {return Axioms(impl); }
-    Goal get_goal() const {return Goal(impl); }
+    Axioms get_axioms() const {return Axioms(interface); }
+    Goal get_goal() const {return Goal(interface); }
 };
 
 #endif
