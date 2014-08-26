aa6e457aa5b15abd1d7c6c1b5b74481b5bc3caaa
Patrick von Reth <vonreth@kde.org>
2014-08-26 16:13:09 +0200

    [issue449] uncrustify and style fix

--- src/search/timer.cc
+++ src/search/timer.cc
@@ -28,15 +28,14 @@ double Timer::current_clock() const {
     FILETIME exitTime;
     FILETIME kernelTime;
     FILETIME userTime;
-    if ( GetProcessTimes( GetCurrentProcess( ),
-        &createTime, &exitTime, &kernelTime, &userTime ) != -1 )
-    {
+    if (GetProcessTimes(GetCurrentProcess( ),
+                        &createTime, &exitTime, &kernelTime, &userTime) != -1) {
         SYSTEMTIME userSystemTime;
-        if ( FileTimeToSystemTime( &userTime, &userSystemTime ) != -1 )
-            return (double)userSystemTime.wHour * 3600.0 +
-                (double)userSystemTime.wMinute * 60.0 +
-                (double)userSystemTime.wSecond +
-                (double)userSystemTime.wMilliseconds / 1000.0;
+        if (FileTimeToSystemTime(&userTime, &userSystemTime) != -1)
+            return double(userSystemTime.wHour) * 3600.0 +
+                   double(userSystemTime.wMinute) * 60.0 +
+                   double(userSystemTime.wSecond) +
+                   double(userSystemTime.wMilliseconds) / 1000.0;
     }
     return -1;
 #else
