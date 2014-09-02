5147cdef0619d9dcb51df76e4c0deef25b420714
Florian Pommerening <florian.pommerening@unibas.ch>
2014-09-02 22:31:01 +0200

    [issue462] Added experiment for issue462

---
+++ experiments/issue462/issue462-opt.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from downward import suites, configs
+
+import common_setup
+
+exp = common_setup.IssueExperiment(
+    search_revisions=["issue462-base", "issue462-v1"],
+    configs=configs.default_configs_optimal(),
+    suite=suites.suite_optimal_with_ipc11(),
+    limits={"search_time": 30}
+    )
+exp.add_absolute_report_step()
+exp.add_comparison_table_step()
+exp.add_scatter_plot_step()
+
+exp()
