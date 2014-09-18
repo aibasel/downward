d4467f005cc41c402307d574110d4cdd9de46bb2
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-09-18 13:22:28 +0200

    [issue439] remove dupplicate assertions

--- src/search/global_task_interface.cc
+++ src/search/global_task_interface.cc
@@ -16,8 +16,6 @@ pair<int, int> GlobalTaskInterface::get_operator_precondition(
     assert(fact_index < get_num_operator_preconditions(op_index, is_axiom));
     const GlobalOperator &op = get_operator_or_axiom(op_index, is_axiom);
     const GlobalCondition &precondition = op.get_preconditions()[fact_index];
-    assert(precondition.var >= 0 && precondition.var < g_variable_domain.size());
-    assert(precondition.val >= 0 && precondition.val < g_variable_domain[precondition.var]);
     return make_pair(precondition.var, precondition.val);
 }
 
@@ -37,8 +35,6 @@ pair<int, int> GlobalTaskInterface::get_operator_effect_condition(
     const GlobalEffect &effect = get_operator_or_axiom(op_index, is_axiom).get_effects()[eff_index];
     assert(cond_index < effect.conditions.size());
     const GlobalCondition &condition = effect.conditions[cond_index];
-    assert(condition.var >= 0 && condition.var < g_variable_domain.size());
-    assert(condition.val >= 0 && condition.val < g_variable_domain[condition.var]);
     return make_pair(condition.var, condition.val);
 }
 
@@ -46,8 +42,6 @@ pair<int, int> GlobalTaskInterface::get_operator_effect(
     int op_index, int eff_index, bool is_axiom) const {
     assert(eff_index < get_num_operator_effects(op_index, is_axiom));
     const GlobalEffect &effect = get_operator_or_axiom(op_index, is_axiom).get_effects()[eff_index];
-    assert(effect.var >= 0 && effect.var < g_variable_domain.size());
-    assert(effect.val >= 0 && effect.val < g_variable_domain[effect.var]);
     return make_pair(effect.var, effect.val);
 }
 
