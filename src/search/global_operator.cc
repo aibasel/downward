f7e6f442cb941b02e13aba961598593c1c93571f
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-09-18 13:20:46 +0200

    [issue439] check bounds for operator values when reading operators

--- src/search/global_operator.cc
+++ src/search/global_operator.cc
@@ -7,6 +7,8 @@ using namespace std;
 
 GlobalCondition::GlobalCondition(istream &in) {
     in >> var >> val;
+    assert(var >= 0 && var < g_variable_domain.size());
+    assert(val >= 0 && val < g_variable_domain[var]);
 }
 
 // TODO if the input file format has been changed, we would need something like this
@@ -27,6 +29,9 @@ void GlobalOperator::read_pre_post(istream &in) {
     for (int i = 0; i < cond_count; i++)
         conditions.push_back(GlobalCondition(in));
     in >> var >> pre >> post;
+    assert(var >= 0 && var < g_variable_domain.size());
+    assert(pre >= -1 && pre < g_variable_domain[var]);
+    assert(post >= 0 && post < g_variable_domain[var]);
     if (pre != -1)
         preconditions.push_back(GlobalCondition(var, pre));
     effects.push_back(GlobalEffect(var, post, conditions));
