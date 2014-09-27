9652ce826d8bbfceffea4003d381d967df73ce22
Florian Pommerening <florian.pommerening@unibas.ch>
2014-09-27 15:54:04 +0200

    [issue468] updated experiment (only core solvers)

--- experiments/issue468/issue468.py
+++ experiments/issue468/issue468.py
@@ -9,9 +9,6 @@ CONFIGS = {
     'astar_lmcount_lm_merged_rhw_hm': [
         '--search',
         'astar(lmcount(lm_merged([lm_rhw(),lm_hm(m=1)]),admissible=true),mpd=true)'],
-    'seq_opt_fdss_1': ['ipc', 'seq-opt-fdss-1'],
-    'seq_opt_fdss_2': ['ipc', 'seq-opt-fdss-2'],
-    'seq_opt_bjolp': ['ipc', 'seq-opt-bjolp'],
 }
 
 exp = common_setup.IssueExperiment(
@@ -20,14 +17,6 @@ exp = common_setup.IssueExperiment(
     suite=suites.suite_optimal_with_ipc11(),
     )
 
-
-def add_first_run_search_time(run):
-    if  "search_time" not in run and run.get("search_time_all", []):
-        run["search_time"] = run["search_time_all"][0]
-    return run
-
-exp.add_comparison_table_step(
-    filter=add_first_run_search_time
-)
+exp.add_comparison_table_step()
 
 exp()
