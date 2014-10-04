a8959b969a2a403d08bc023d7a4b11acb6b241ba
Malte Helmert <malte.helmert@unibas.ch>
2014-10-04 15:00:50 +0200

    [issue463] h^m heuristic: const cleanup and other minor cleanup

--- src/search/hm_heuristic.h
+++ src/search/hm_heuristic.h
@@ -24,9 +24,6 @@ class Options;
 */
 
 class HMHeuristic : public Heuristic {
-    virtual int compute_heuristic(const GlobalState &state);
-    virtual void initialize();
-
     // parameters
     int m;
 
@@ -34,32 +31,38 @@ class HMHeuristic : public Heuristic {
     std::map<Tuple, int> hm_table;
     bool was_updated;
 
+    // auxiliary methods
     void init_hm_table(Tuple &t);
     void update_hm_table();
-    int eval(Tuple &t);
+    int eval(Tuple &t) const;
     int update_hm_entry(Tuple &t, int val);
     void extend_tuple(Tuple &t, const GlobalOperator &op);
 
-    int check_tuple_in_tuple(const Tuple &tup, const Tuple &big_tuple);
-    void state_to_tuple(const GlobalState &state, Tuple &t);
+    int check_tuple_in_tuple(const Tuple &tup, const Tuple &big_tuple) const;
+    void state_to_tuple(const GlobalState &state, Tuple &t) const;
 
-    int get_operator_pre_value(const GlobalOperator &op, int var);
-    void get_operator_pre(const GlobalOperator &op, Tuple &t);
-    void get_operator_eff(const GlobalOperator &op, Tuple &t);
-    bool is_pre_of(const GlobalOperator &op, int var);
-    bool is_effect_of(const GlobalOperator &op, int var);
-    bool contradict_effect_of(const GlobalOperator &op, int var, int val);
+    int get_operator_pre_value(const GlobalOperator &op, int var) const;
+    void get_operator_pre(const GlobalOperator &op, Tuple &t) const;
+    void get_operator_eff(const GlobalOperator &op, Tuple &t) const;
+    bool is_pre_of(const GlobalOperator &op, int var) const;
+    bool is_effect_of(const GlobalOperator &op, int var) const;
+    bool contradict_effect_of(const GlobalOperator &op, int var, int val) const;
 
     void generate_all_tuples();
     void generate_all_tuples_aux(int var, int sz, Tuple &base);
 
     void generate_all_partial_tuples(Tuple &base_tuple,
-                                     std::vector<Tuple> &res);
+                                     std::vector<Tuple> &res) const;
     void generate_all_partial_tuples_aux(Tuple &base_tuple, Tuple &t, int index,
-                                         int sz, std::vector<Tuple> &res);
+                                         int sz, std::vector<Tuple> &res) const;
 
     void dump_table() const;
     void dump_tuple(Tuple &tup) const;
+
+protected:
+    virtual int compute_heuristic(const GlobalState &state);
+    virtual void initialize();
+
 public:
     HMHeuristic(const Options &opts);
     virtual ~HMHeuristic();
