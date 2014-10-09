fdc86a04c382091b88e71d5665914da2c47ad3a1
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-10-09 22:23:21 +0200

    [issue414] add tests for aliases and portfolios

--- src/driver/tests.py
+++ src/driver/tests.py
@@ -3,17 +3,43 @@
 import os
 import subprocess
 
+import pytest
+
+from .aliases import ALIASES, PORTFOLIOS
 from .arguments import EXAMPLES
+from .portfolio_runner import EXIT_PLAN_FOUND, EXIT_UNSOLVED_INCOMPLETE
 
 
 SRC_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-def test_commandline_args():
+@pytest.fixture
+def preprocess():
+    """Create preprocessed task."""
     os.chdir(SRC_DIR)
+    cmd = ["./plan.py", "--run-translator", "--run-preprocessor",
+           "../benchmarks/gripper/prob01.pddl"]
+    assert subprocess.check_call(cmd) == 0
+
+
+def test_commandline_args(preprocess):
     for description, cmd in EXAMPLES:
         assert subprocess.check_call(cmd) == 0
 
 
-if __name__ == "__main__":
-    test_commandline_args()
+def test_aliases(preprocess):
+    for alias, config in ALIASES.items():
+        # selmax is currently not supported.
+        if alias in ["seq-opt-fd-autotune", "seq-opt-selmax"]:
+            continue
+        cmd = ["./plan.py", "--alias", alias, "output"]
+        assert subprocess.check_call(cmd) == 0
+
+
+def test_portfolios(preprocess):
+    for name, portfolio in PORTFOLIOS.items():
+        if name in ["seq-opt-fd-autotune", "seq-opt-selmax"]:
+            continue
+        cmd = ["./plan.py", "--portfolio", portfolio, "output"]
+        assert subprocess.call(cmd) in [
+            EXIT_PLAN_FOUND, EXIT_UNSOLVED_INCOMPLETE]
