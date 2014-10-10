642206917b1b364a660bb2f95ef30b498a05f5ca
Florian Pommerening <florian.pommerening@unibas.ch>
2014-10-10 15:13:58 +0200

    [issue488] Add experiment for issue 488

---
+++ experiments/issue488/issue488.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from downward import suites
+
+import common_setup
+
+CONFIGS = {
+        'astar_ipdb': [
+            '--search',
+            'astar(ipdb())'],
+        'astar_pdb': [
+            '--search',
+            'astar(pdb())'],
+        'astar_gapdb': [
+            '--search',
+            'astar(gapdb())'],
+}
+
+exp = common_setup.IssueExperiment(
+    search_revisions=["issue488-base", "issue488-v1"],
+    configs=CONFIGS,
+    suite=suites.suite_optimal_with_ipc11(),
+    )
+
+exp.add_comparison_table_step()
+
+exp()
