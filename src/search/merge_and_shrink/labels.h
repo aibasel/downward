27593752527abf05eb86cab598700b89b43f60f9
Silvan Sievers <silvan.sievers@unibas.ch>
2014-10-16 12:28:46 +0200

    [issue490] integrated label reducer into labels

--- src/search/merge_and_shrink/labels.h
+++ src/search/merge_and_shrink/labels.h
@@ -5,30 +5,83 @@
 
 #include <vector>
 
-class TransitionSystem;
+class EquivalenceRelation;
 class Label;
-class LabelReducer;
 class Options;
+class TransitionSystem;
 
 /*
- The Labels class is basically a container class for the set of all
- labels used by merge-and-shrink transition_systems.
- */
+  This class serves both as a container class to handle the set of all labels
+  and to perform label reduction on this set.
+*/
 class Labels {
-    const LabelReducer *label_reducer;
-
     std::vector<Label *> labels;
+
+    /*
+      none: no label reduction will be performed
+
+      two_transition_systems: compute the 'combinable relation'
+      for labels only for the two transition_systems that will
+      be merged next and reduce labels.
+
+      all_transition_systems: compute the 'combinable relation'
+      for labels once for every transition_system and reduce
+      labels.
+
+      all_transition_systems_with_fixpoint: keep computing the
+      'combinable relation' for labels iteratively for all
+      transition systems until no more labels can be reduced.
+    */
+    enum LabelReductionMethod {
+        NONE,
+        TWO_TRANSITION_SYSTEMS,
+        ALL_TRANSITION_SYSTEMS,
+        ALL_TRANSITION_SYSTEMS_WITH_FIXPOINT
+    };
+
+    /*
+      Order in which iterations of label reduction considers the set of all
+      transition systems. Regular is the fast downward order plus appending
+      new composite transition systems after the atomic ones, revers is the
+      reversed regulard order and random is a random one. All orders are
+      precomputed and reused for every call to reduce().
+    */
+    enum LabelReductionSystemOrder {
+        REGULAR,
+        REVERSE,
+        RANDOM
+    };
+
+    LabelReductionMethod label_reduction_method;
+    LabelReductionSystemOrder label_reduction_system_order;
+    std::vector<int> transition_system_order;
+
+    // Apply the label mapping to all transition systems. Also clear cache.
+    void notify_transition_systems(
+        int ts_index,
+        const std::vector<TransitionSystem *> &all_transition_systems,
+        const std::vector<std::pair<int, std::vector<int> > > &label_mapping,
+        std::vector<EquivalenceRelation *> &cached_equivalence_relations) const;
+    // Apply the given label equivalence relation to the set of labels
+    bool apply_label_reduction(
+        const EquivalenceRelation *relation,
+        std::vector<std::pair<int, std::vector<int> > > &label_mapping);
+    EquivalenceRelation *compute_combinable_equivalence_relation(
+        int ts_index,
+        const std::vector<TransitionSystem *> &all_transition_systems,
+        std::vector<EquivalenceRelation *> &cached_local_equivalence_relations) const;
+
 public:
     Labels(const Options &options, OperatorCost cost_type);
-    ~Labels();
+    ~Labels() {}
     void reduce(std::pair<int, int> next_merge,
                 const std::vector<TransitionSystem *> &all_transition_systems);
     // TODO: consider removing get_label_by_index and forwarding all required
     // methods of Label and giving access to them by label number.
     const Label *get_label_by_index(int index) const;
     bool is_label_reduced(int label_no) const;
-    void dump() const;
-    void dump_options() const;
+    void dump_labels() const;
+    void dump_label_reduction_options() const;
 
     int get_size() const {
         return labels.size();
