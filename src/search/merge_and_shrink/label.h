2e5cb38f347773a40a3ef877ab55e5a8a1eda138
Silvan Sievers <silvan.sievers@unibas.ch>
2014-10-16 17:33:37 +0200

    [issue491] delete reduced labels when applying label reduction (no more tree-like structure for labels).

--- src/search/merge_and_shrink/label.h
+++ src/search/merge_and_shrink/label.h
@@ -1,11 +1,10 @@
 #ifndef MERGE_AND_SHRINK_LABEL_H
 #define MERGE_AND_SHRINK_LABEL_H
 
-#include "../global_operator.h"
-
 #include <vector>
 
-class CompositeLabel;
+class GlobalCondition;
+class GlobalEffect;
 
 /* This class implements labels as used by merge-and-shrink transition systems.
    It abstracts from the underlying regular operators and allows to store
@@ -13,40 +12,28 @@ class CompositeLabel;
    NOTE: operators that are axioms are currently not supported! */
 
 class Label {
-    friend class CompositeLabel; // required for update_root
     int id;
     int cost;
 protected:
-    // root is a pointer to a composite label that this label has been reduced
-    // to, if such a label exists, or to itself, if the label has not been
-    // reduced yet.
-    Label *root;
-
     Label(int id, int cost);
-    virtual ~Label() {}
-    virtual void update_root(CompositeLabel *new_root) = 0;
 public:
+    virtual ~Label() {}
     int get_id() const {
         return id;
     }
     int get_cost() const {
         return cost;
     }
-    bool is_reduced() const;
-    virtual const std::vector<Label *> &get_parents() const = 0;
     void dump() const;
 };
 
 class OperatorLabel : public Label {
     const std::vector<GlobalCondition> &preconditions;
     const std::vector<GlobalEffect> &effects;
-
-    void update_root(CompositeLabel *new_root);
 public:
     OperatorLabel(int id, int cost,
                   const std::vector<GlobalCondition> &preconditions,
                   const std::vector<GlobalEffect> &effects);
-    const std::vector<Label *> &get_parents() const;
     const std::vector<GlobalCondition> &get_preconditions() const {
         return preconditions;
     }
@@ -56,14 +43,8 @@ public:
 };
 
 class CompositeLabel : public Label {
-    std::vector<Label *> parents;
-
-    void update_root(CompositeLabel *new_root);
 public:
-    CompositeLabel(int id, const std::vector<Label *> &parents);
-    const std::vector<Label *> &get_parents() const {
-        return parents;
-    }
+    CompositeLabel(int id, int cost);
 };
 
 #endif
