e7fbc345b7a4b872a7143b6ba0abb182a7b3f0a8
Florian Pommerening <florian.pommerening@unibas.ch>
2014-10-20 14:16:07 +0200

    [issue479] Add experiment for issue479-v1 airport/30 min

---
+++ experiments/issue479/issue479.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from downward import suites
+from lab.reports import Attribute
+
+import common_setup
+
+import os
+
+
+exp = common_setup.IssueExperiment(
+    search_revisions=["issue479-v1"],
+    configs={
+        'dfp-b-50k': ['--search', 'astar(merge_and_shrink(merge_strategy=merge_dfp,shrink_strategy=shrink_bisimulation(max_states=50000,threshold=1,greedy=false,group_by_h=true),label_reduction_method=all_transition_systems_with_fixpoint))'],
+        'blind': ['--search', 'astar(blind())'],
+    },
+    suite=['airport'],
+    )
+
+exp.add_absolute_report_step(attributes=['coverage', 'error', 'run_dir'])
