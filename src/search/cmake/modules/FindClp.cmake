f094b0d5d7d10862b85f24dd40067ad4d8074a6c
Florian Pommerening <florian.pommerening@unibas.ch>
2014-10-25 00:59:08 +0200

    [issue67] Add LP code

--- src/search/cmake/modules/FindClp.cmake
+++ src/search/cmake/modules/FindClp.cmake
@@ -1,49 +1,28 @@
-# include(FindLibraryWithDebug)
+set(_CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH})
+if(NOT "$ENV{DOWNWARD_COIN_ROOT}" STREQUAL "")
+    set(CMAKE_FIND_ROOT_PATH $ENV{DOWNWARD_COIN_ROOT})
+    set(_COIN_ROOT_OPTS ONLY_CMAKE_FIND_ROOT_PATH NO_DEFAULT_PATH)
+endif(NOT "$ENV{DOWNWARD_COIN_ROOT}" STREQUAL "")
 
-if (CLP_INCLUDES AND CLP_LIBRARIES)
-  set(CLP_FIND_QUIETLY TRUE)
-endif (CLP_INCLUDES AND CLP_LIBRARIES)
+find_path(CLP_INCLUDES
+    NAMES
+    ClpSimplex.hpp
+    PATHS
+    /include/coin
+    ${_COIN_ROOT_OPTS}
+)
 
-if(LP_PREFIX_PATH)
-    
-#    message("-- Forcing the use of Clp in non-standard location ${LP_PREFIX_PATH}")
-    
-    find_path(CLP_INCLUDES
-        NAMES
-        coin/ClpSimplex.hpp
-        PATHS
-        ${LP_PREFIX_PATH}/include
-        NO_DEFAULT_PATH
-    )
-
-    find_library(CLP_LIBRARIES
-        Clp
-        PATHS
-        ${LP_PREFIX_PATH}/lib
-        NO_DEFAULT_PATH
-    )
-
-  
-else(LP_PREFIX_PATH)
-    
-    find_path(CLP_INCLUDES
-        NAMES
-        coin/ClpSimplex.hpp
-        PATHS
-        ${INCLUDE_INSTALL_DIR}
-    )
-
-    find_library(CLP_LIBRARIES
-        Clp
-        PATHS
-        ${LIB_INSTALL_DIR}
-    )
-    
-endif(LP_PREFIX_PATH)
+find_library(CLP_LIBRARIES
+    Clp
+    PATHS
+    /lib
+    ${_COIN_ROOT_OPTS}
+)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Clp DEFAULT_MSG
-                                  CLP_INCLUDES CLP_LIBRARIES)
+find_package_handle_standard_args(
+    Clp
+    REQUIRED_VARS CLP_INCLUDES CLP_LIBRARIES
+)
 
 mark_as_advanced(CLP_INCLUDES CLP_LIBRARIES)
-
--- src/search/cmake/modules/FindOSI.cmake
+++ src/search/cmake/modules/FindOSI.cmake
@@ -1,63 +1,77 @@
-# include(FindLibraryWithDebug)
+# - Find the Open Solver Interface by COIN/OR.
+# This module will search for the OSI the proxies for the solvers
+# specified as components in the FIND_PACKAGE call.
+# Possible solvers are:
+#
+#  Clp (internal solver of COIN)
+#  Cpx (CPLEX)
+#  Grb (Gurobi)
+#
+# This code defines the following variables:
+#
+#  OSI_FOUND                 - TRUE if all components are found.
+#  OSI_INCLUDE_DIRS          - Full paths to all include dirs.
+#  OSI_LIBRARY               - Full paths to the OSI library.
+#  OSI_LIBRARIES             - Full paths to all libraries.
+#  OSI_<solver>_FOUND        - TRUE if a proxy for <solver> is found.
+#  OSI_<solver>_LIBRARIES    - Full path to proxy and main libraries for <solver>.
+#
+# Example Usages:
+#  find_package(OSI)
+#  find_package(OSI COMPONENTS Clp Cpx)
+#
+# Note that the standard FIND_PACKAGE features are supported
+# (i.e., QUIET, REQUIRED, etc.).
 
-if (OSI_INCLUDES AND OSI_LIBRARIES)
-  set(OSI_FIND_QUIETLY TRUE)
-endif (OSI_INCLUDES AND OSI_LIBRARIES)
+set(_CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH})
+if(NOT "$ENV{DOWNWARD_COIN_ROOT}" STREQUAL "")
+    set(CMAKE_FIND_ROOT_PATH $ENV{DOWNWARD_COIN_ROOT})
+    set(_COIN_ROOT_OPTS ONLY_CMAKE_FIND_ROOT_PATH NO_DEFAULT_PATH)
+endif(NOT "$ENV{DOWNWARD_COIN_ROOT}" STREQUAL "")
 
-if(LP_PREFIX_PATH)
-#    message("-- Forcing the use of OSI in non-standard location ${LP_PREFIX_PATH}")
-  
-    
-    find_path(OSI_INCLUDES
-        coin/config_osi.h
+find_path(OSI_INCLUDES
+    NAMES
+    OsiConfig.h config_osi.h
+    PATHS
+    /include/coin
+    ${_COIN_ROOT_OPTS}
+)
+
+set(OSI_LIBRARIES "")
+foreach(solver ${OSI_FIND_COMPONENTS})
+    set(OSI_${solver}_FOUND FALSE)
+    find_library(OSI_${solver}_LIBRARY
+        Osi${solver}
         PATHS
-        ${LP_PREFIX_PATH}/include
-        NO_DEFAULT_PATH
+        /lib
+        ${_COIN_ROOT_OPTS}
     )
+    set(OSI_${solver}_LIBRARIES ${OSI_${solver}_LIBRARY})
+    if(OSI_${solver}_LIBRARIES)
+        list(APPEND OSI_LIBRARIES ${OSI_${solver}_LIBRARIES})
+        set(OSI_${solver}_FOUND TRUE)
+    endif()
+    mark_as_advanced(OSI_${solver}_LIBRARY)
+endforeach()
 
+find_library(OSI_LIBRARY
+    Osi
+    PATHS
+    /lib
+    ${_COIN_ROOT_OPTS}
+)
 
-    find_library(OSI_LIBRARIES
-        Osi
-        PATHS
-        ${LP_PREFIX_PATH}/lib
-        NO_DEFAULT_PATH
-    )
+## Basic Osi libs must be added after (!) all osi solver libs
+list(APPEND OSI_LIBRARIES ${OSI_LIBRARY})
 
-
-    find_library(OSICLP_LIBRARIES
-        OsiClp
-        PATHS
-        ${LP_PREFIX_PATH}/lib
-        NO_DEFAULT_PATH
-    )
-        
-else(LP_PREFIX_PATH)
-
-    find_path(OSI_INCLUDES
-        NAMES
-        coin/config_osi.h
-        PATHS
-        ${INCLUDE_INSTALL_DIR}
-    )
-
-
-    find_library(OSI_LIBRARIES
-        Osi
-        PATHS
-        ${LIB_INSTALL_DIR}
-    )
-
-
-    find_library(OSICLP_LIBRARIES
-        OsiClp
-        PATHS
-        ${LIB_INSTALL_DIR}
-    )
-
-endif(LP_PREFIX_PATH)
+# Restore the original search paths
+set(CMAKE_FIND_ROOT_PATH ${_CMAKE_FIND_ROOT_PATH})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(OSI DEFAULT_MSG
-                                  OSI_INCLUDES OSI_LIBRARIES)
+find_package_handle_standard_args(
+    OSI
+    REQUIRED_VARS OSI_INCLUDES OSI_LIBRARIES
+    HANDLE_COMPONENTS
+)
 
-mark_as_advanced(OSI_INCLUDES OSI_LIBRARIES)
+mark_as_advanced(OSI_INCLUDES OSI_LIBRARY OSI_LIBRARIES)
