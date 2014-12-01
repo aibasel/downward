5d7779a87328ccd93aff9b994bc17bd6db06a4c2
Silvan Sievers <silvan.sievers@unibas.ch>
2014-12-01 19:22:24 +0100

    [issue492] ShrinkBisimulation uses label equivalence relation to compute signatures

--- src/search/merge_and_shrink/shrink_bucket_based.cc
+++ src/search/merge_and_shrink/shrink_bucket_based.cc
@@ -24,7 +24,7 @@ void ShrinkBucketBased::shrink(TransitionSystem &ts, int threshold) {
         vector<Bucket> buckets;
         partition_into_buckets(ts, buckets);
 
-        EquivalenceRelation equiv_relation;
+        StateEquivalenceRelation equiv_relation;
         compute_abstraction(buckets, threshold, equiv_relation);
         apply(ts, equiv_relation, threshold);
     }
@@ -32,7 +32,7 @@ void ShrinkBucketBased::shrink(TransitionSystem &ts, int threshold) {
 
 void ShrinkBucketBased::compute_abstraction(
     const vector<Bucket> &buckets, int target_size,
-    EquivalenceRelation &equiv_relation) const {
+    StateEquivalenceRelation &equiv_relation) const {
     bool show_combine_buckets_warning = true;
 
     assert(equiv_relation.empty());
@@ -52,7 +52,7 @@ void ShrinkBucketBased::compute_abstraction(
         if (budget_for_this_bucket >= static_cast<int>(bucket.size())) {
             // Each state in bucket can become a singleton group.
             for (size_t i = 0; i < bucket.size(); ++i) {
-                EquivalenceClass group;
+                StateEquivalenceClass group;
                 group.push_front(bucket[i]);
                 equiv_relation.push_back(group);
             }
@@ -60,22 +60,22 @@ void ShrinkBucketBased::compute_abstraction(
             // The whole bucket must form one group.
             int remaining_buckets = buckets.size() - bucket_no;
             if (remaining_state_budget >= remaining_buckets) {
-                equiv_relation.push_back(EquivalenceClass());
+                equiv_relation.push_back(StateEquivalenceClass());
             } else {
                 if (bucket_no == 0)
-                    equiv_relation.push_back(EquivalenceClass());
+                    equiv_relation.push_back(StateEquivalenceClass());
                 if (show_combine_buckets_warning) {
                     show_combine_buckets_warning = false;
                     cout << "Very small node limit, must combine buckets."
                          << endl;
                 }
             }
-            EquivalenceClass &group = equiv_relation.back();
+            StateEquivalenceClass &group = equiv_relation.back();
             group.insert(group.begin(), bucket.begin(), bucket.end());
         } else {
             // Complicated case: must combine until bucket budget is met.
             // First create singleton groups.
-            vector<EquivalenceClass> groups(bucket.size());
+            vector<StateEquivalenceClass> groups(bucket.size());
             for (size_t i = 0; i < bucket.size(); ++i)
                 groups[i].push_front(bucket[i]);
 
@@ -98,7 +98,7 @@ void ShrinkBucketBased::compute_abstraction(
 
             // Finally add these groups to the result.
             for (size_t i = 0; i < groups.size(); ++i) {
-                equiv_relation.push_back(EquivalenceClass());
+                equiv_relation.push_back(StateEquivalenceClass());
                 equiv_relation.back().swap(groups[i]);
             }
         }
