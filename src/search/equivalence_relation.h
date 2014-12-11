62a40ed1105bdc3471a0edb5cec32bddaa5a77aa
Silvan Sievers <silvan.sievers@unibas.ch>
2014-12-11 18:53:38 +0100

    [issue492] compositetransitionsytem constructor: compute the intersection of equivalence relations and transitions

--- src/search/equivalence_relation.h
+++ src/search/equivalence_relation.h
@@ -32,7 +32,6 @@ class Block {
       is created and stored in B for easier access.
     */
     friend class EquivalenceRelation;
-    BlockListIter it_intersection_block;
 public:
     bool empty() const;
     ElementListIter insert(int element);
@@ -41,6 +40,8 @@ public:
     ElementListIter end() {return elements.end(); }
     ElementListConstIter begin() const {return elements.begin(); }
     ElementListConstIter end() const {return elements.end(); }
+    // HACK!
+    BlockListIter it_intersection_block;
 };
 
 class EquivalenceRelation {
@@ -74,6 +75,9 @@ public:
     // Cannot be declared const due to operator[]
     BlockListConstIter get_block_iterator_for_element(int element) {
         return element_positions[element].first;
+    }
+    const std::list<Block> &get_blocks() const {
+        return blocks;
     }
 
     int get_num_elements() const;
