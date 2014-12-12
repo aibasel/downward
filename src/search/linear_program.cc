a8549bb451f7cb4f737d8cec9fc3ce565e4069f8
Florian Pommerening <florian.pommerening@unibas.ch>
2014-12-12 11:15:57 +0100

    [issue443] Only create one LP interface

--- src/search/linear_program.cc
+++ src/search/linear_program.cc
@@ -68,52 +68,12 @@ LPVariable::~LPVariable() {
 
 #ifdef USE_LP
 
-LP::LP(LPSolverType solver_type,
-   const std::vector<LPVariable> &variables,
-   const std::vector<LPConstraint> &constraints,
-   LPObjectiveSense sense)
+LP::LP(LPSolverType solver_type)
     : is_initialized(false),
       is_solved(false),
+      num_permanent_constraints(0),
       has_temporary_constraints(false) {
     lp_solver = create_lp_solver(solver_type);
-    int num_columns = variables.size();
-    int num_rows = constraints.size();
-    num_permanent_constraints = num_rows;
-
-    try {
-        if (sense == LPObjectiveSense::MINIMIZE) {
-            lp_solver->setObjSense(1);
-        } else {
-            lp_solver->setObjSense(-1);
-        }
-        /*
-          Note that using assignProblem instead of loadProblem, the ownership of
-          the data is transfered to the LP solver. It will delete the matrix,
-          bounds and the objective so we do not delete them afterwards.
-        */
-        CoinPackedMatrix *matrix = new CoinPackedMatrix(false, 0, 0);
-        matrix->setDimensions(0, num_columns);
-        // TODO: delete rows or are they owned by matrix?
-        matrix->appendRows(num_rows, get_rows(constraints));
-        double *col_lb = build_array<LPVariable>(
-            variables,
-            [](const LPVariable &var) {return var.lower_bound; });
-        double *col_ub = build_array<LPVariable>(
-            variables,
-            [](const LPVariable &var) {return var.upper_bound; });
-        double *objective = build_array<LPVariable>(
-            variables,
-            [](const LPVariable &var) {return var.objective_coefficient; });
-        double *row_lb = build_array<LPConstraint>(
-            constraints,
-            [](const LPConstraint &constraint) {return constraint.lower_bound; });
-        double *row_ub = build_array<LPConstraint>(
-            constraints,
-            [](const LPConstraint &constraint) {return constraint.upper_bound; });
-        lp_solver->assignProblem(matrix, col_lb, col_ub, objective, row_lb, row_ub);
-    } catch (CoinError &error) {
-        handle_coin_error(error);
-    }
 }
 
 LP::~LP() {
@@ -154,6 +114,52 @@ CoinPackedVectorBase **LP::get_rows(const std::vector<LPConstraint> &constraints
     return rows;
 }
 
+void LP::assign_problem(LPObjectiveSense sense,
+                        const std::vector<LPVariable> &variables,
+                        const std::vector<LPConstraint> &constraints) {
+    int num_columns = variables.size();
+    int num_rows = constraints.size();
+    num_permanent_constraints = num_rows;
+    try {
+        /*
+          Note that using assignProblem instead of loadProblem, the ownership of
+          the data is transfered to the LP solver. It will delete the matrix,
+          bounds and the objective so we do not delete them afterwards.
+        */
+        CoinPackedMatrix *matrix = new CoinPackedMatrix(false, 0, 0);
+        matrix->setDimensions(0, num_columns);
+        CoinPackedVectorBase **rows = get_rows(constraints);
+        matrix->appendRows(num_rows, rows);
+        double *col_lb = build_array<LPVariable>(
+            variables,
+            [](const LPVariable &var) {return var.lower_bound; });
+        double *col_ub = build_array<LPVariable>(
+            variables,
+            [](const LPVariable &var) {return var.upper_bound; });
+        double *objective = build_array<LPVariable>(
+            variables,
+            [](const LPVariable &var) {return var.objective_coefficient; });
+        double *row_lb = build_array<LPConstraint>(
+            constraints,
+            [](const LPConstraint &constraint) {return constraint.lower_bound; });
+        double *row_ub = build_array<LPConstraint>(
+            constraints,
+            [](const LPConstraint &constraint) {return constraint.upper_bound; });
+        if (sense == LPObjectiveSense::MINIMIZE) {
+            lp_solver->setObjSense(1);
+        } else {
+            lp_solver->setObjSense(-1);
+        }
+        lp_solver->assignProblem(matrix, col_lb, col_ub, objective, row_lb, row_ub);
+        for (size_t i = 0; i < constraints.size(); ++i) {
+            delete rows[i];
+        }
+        delete[] rows;
+    } catch (CoinError &error) {
+        handle_coin_error(error);
+    }
+}
+
 int LP::add_temporary_constraints(const std::vector<LPConstraint> &constraints) {
     int index_of_first_constraint = get_num_constraints();
     if (!constraints.empty()) {
@@ -164,8 +170,12 @@ int LP::add_temporary_constraints(const std::vector<LPConstraint> &constraints) 
             double *row_ub = build_array<LPConstraint>(
                 constraints,
                 [](const LPConstraint &constraint) {return constraint.upper_bound; });
-            lp_solver->addRows(constraints.size(),
-                               get_rows(constraints), row_lb, row_ub);
+            CoinPackedVectorBase **rows = get_rows(constraints);
+            lp_solver->addRows(constraints.size(), rows, row_lb, row_ub);
+            for (size_t i = 0; i < constraints.size(); ++i) {
+                delete rows[i];
+            }
+            delete[] rows;
         } catch (CoinError &error) {
             handle_coin_error(error);
         }
@@ -306,15 +316,8 @@ void LP::print_statistics() const {
     cout << "LP constraints: " << get_num_constraints() << endl;
 }
 #else
-LP::LP(LPSolverType solver_type,
-   const std::vector<LPVariable> &variables,
-   const std::vector<LPConstraint> &constraints,
-   LPObjectiveSense sense) {
-    // silence unused variable warnings
+LP::LP(LPSolverType solver_type) {
     unused_parameter(solver_type);
-    unused_parameter(variables);
-    unused_parameter(constraints);
-    unused_parameter(sense);
     cerr << "You must build the planner with the USE_LP symbol defined" << endl;
     exit_with(EXIT_CRITICAL_ERROR);
 }
