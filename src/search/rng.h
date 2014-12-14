6935589e933f0dfb4d7194e2d91dbc5bc98bf510
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-12-14 17:41:45 +0100

    [issue269] inline methods

--- src/search/rng.h
+++ src/search/rng.h
@@ -2,22 +2,38 @@
 #define RNG_H
 
 #include <algorithm>
+#include <cassert>
 #include <random>
 #include <vector>
 
 class RandomNumberGenerator {
     // Mersenne Twister random number generator.
     std::mt19937 rng;
+
 public:
-    RandomNumberGenerator();                   // seed with time-dependent value
-    explicit RandomNumberGenerator(int seed_); // seed with integer
+    // Seed with time-dependent value.
+    RandomNumberGenerator();
+
+    // Seed with integer.
+    explicit RandomNumberGenerator(int seed_);
+
     RandomNumberGenerator(const RandomNumberGenerator &) = delete;
     RandomNumberGenerator &operator=(const RandomNumberGenerator &) = delete;
 
     void seed(int seed);
 
-    double operator()();        // random double in [0..1), 2^53 possible values
-    int operator()(int bound);  // random integer in [0..bound), bound < 2^31
+    // Return random double in [0..1).
+    double operator()() {
+        std::uniform_real_distribution<double> distribution(0.0, 1.0);
+        return distribution(rng);
+    }
+
+    // Return random integer in [0..bound).
+    int operator()(int bound) {
+        assert(bound > 0);
+        std::uniform_int_distribution<int> distribution(0, bound - 1);
+        return distribution(rng);
+    }
 
     template <class T>
     void shuffle(std::vector<T> &vec) {
