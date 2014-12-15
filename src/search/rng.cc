792460139fa6cb130d1cb3203cf9dc5550be5964
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-12-15 09:54:27 +0100

    [issue269] also use PID for seeding

--- src/search/rng.cc
+++ src/search/rng.cc
@@ -1,13 +1,14 @@
 #include "rng.h"
 
 #include <chrono>
+#include <unistd.h>
 
 using namespace std;
 
 
 RandomNumberGenerator::RandomNumberGenerator() {
     unsigned int secs = chrono::system_clock::now().time_since_epoch().count();
-    seed(secs);
+    seed(secs + getpid());
 }
 
 RandomNumberGenerator::RandomNumberGenerator(int seed_) {
