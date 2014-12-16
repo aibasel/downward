1ff7d6a1ea0e99feac13537dbb7c81e62c4b42f8
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-12-16 19:31:49 +0100

    [issue269] add/change comments

--- src/search/rng.cc
+++ src/search/rng.cc
@@ -6,6 +6,13 @@
 using namespace std;
 
 
+/*
+  Ideally, one would use true randomness here from std::random_device. However,
+  there exist platforms where this returns non-random data, which is condoned by
+  the standard. On these platforms one would need to additionally seed with time
+  and process ID (PID), and therefore generally seeding with time and PID only
+  is probably good enough.
+*/
 RandomNumberGenerator::RandomNumberGenerator() {
     unsigned int secs = chrono::system_clock::now().time_since_epoch().count();
     seed(secs + getpid());
