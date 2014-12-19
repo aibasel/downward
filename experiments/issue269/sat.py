dae3f18d575827f857ca8652ecdab3b3227a90e5
Jendrik Seipp <jendrik.seipp@unibas.ch>
2014-12-19 15:28:00 +0100

    [issue269] use ff() instead of lmcut()

--- experiments/issue269/sat.py
+++ experiments/issue269/sat.py
@@ -13,7 +13,7 @@ SUITE = suites.suite_satisficing_with_ipc11()
 CONFIGS = {
     "random-successors": ["--search", "lazy_greedy(ff(),randomize_successors=true)"],
     "pareto-open-list": [
-        "--heuristic", "h=lmcut()",
+        "--heuristic", "h=ff()",
         "--search", "eager(pareto([sum([g(), h]), h]), reopen_closed=true, pathmax=false,f_eval=sum([g(), h]))"],
 }
 
