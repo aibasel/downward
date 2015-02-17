aa1499f362d3157fba65692b0f88068c916f6482
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-02-17 12:08:27 +0100

    [issue439] add virtual keyword and make method private

--- src/search/global_task_interface.h
+++ src/search/global_task_interface.h
@@ -11,7 +11,7 @@
 
 
 class GlobalTaskInterface : public TaskInterface {
-protected:
+private:
     const GlobalOperator &get_operator_or_axiom(int index, bool is_axiom) const {
         if (is_axiom) {
             assert(in_bounds(index, g_axioms));
@@ -23,42 +23,42 @@ protected:
     }
 
 public:
-    int get_num_variables() const override {
+    virtual int get_num_variables() const override {
         return g_variable_domain.size();
     }
-    int get_variable_domain_size(int var) const override {
+    virtual int get_variable_domain_size(int var) const override {
         return g_variable_domain[var];
     }
 
-    int get_operator_cost(int index, bool is_axiom) const override {
+    virtual int get_operator_cost(int index, bool is_axiom) const override {
         return get_operator_or_axiom(index, is_axiom).get_cost();
     }
-    const std::string &get_operator_name(int index, bool is_axiom) const override {
+    virtual const std::string &get_operator_name(int index, bool is_axiom) const override {
         return get_operator_or_axiom(index, is_axiom).get_name();
     }
-    int get_num_operators() const override {
+    virtual int get_num_operators() const override {
         return g_operators.size();
     }
-    int get_num_operator_preconditions(int index, bool is_axiom) const override;
-    std::pair<int, int> get_operator_precondition(
+    virtual int get_num_operator_preconditions(int index, bool is_axiom) const override;
+    virtual std::pair<int, int> get_operator_precondition(
         int op_index, int fact_index, bool is_axiom) const override;
-    int get_num_operator_effects(int op_index, bool is_axiom) const override;
-    int get_num_operator_effect_conditions(
+    virtual int get_num_operator_effects(int op_index, bool is_axiom) const override;
+    virtual int get_num_operator_effect_conditions(
         int op_index, int eff_index, bool is_axiom) const override;
-    std::pair<int, int> get_operator_effect_condition(
+    virtual std::pair<int, int> get_operator_effect_condition(
         int op_index, int eff_index, int cond_index, bool is_axiom) const override;
-    std::pair<int, int> get_operator_effect(
+    virtual std::pair<int, int> get_operator_effect(
         int op_index, int eff_index, bool is_axiom) const override;
-    const GlobalOperator *get_global_operator(int index, bool is_axiom) const override;
+    virtual const GlobalOperator *get_global_operator(int index, bool is_axiom) const override;
 
-    int get_num_axioms() const override {
+    virtual int get_num_axioms() const override {
         return g_axioms.size();
     }
 
-    int get_num_goals() const override {
+    virtual int get_num_goals() const override {
         return g_goal.size();
     }
-    std::pair<int, int> get_goal_fact(int index) const override {
+    virtual std::pair<int, int> get_goal_fact(int index) const override {
         return g_goal[index];
     }
 };
