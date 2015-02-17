14cae11ace2624d38ffcf426b665769a340ebe44
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-02-17 11:46:00 +0100

    [issue439] move Fact methods into task.h

--- src/search/task.h
+++ src/search/task.h
@@ -267,4 +267,17 @@ public:
     }
 };
 
+
+inline Fact::Fact(const TaskInterface &interface_, int var_id_, int value_)
+    : interface(interface_), var_id(var_id_), value(value_) {
+    assert(var_id >= 0 && var_id < interface.get_num_variables());
+    assert(value >= 0 && value < get_variable().get_domain_size());
+}
+
+
+inline Variable Fact::get_variable() const {
+    return Variable(interface, var_id);
+}
+
+
 #endif
