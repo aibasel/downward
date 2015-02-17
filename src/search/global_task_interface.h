394a668773c1769ba15f47711779c5651766cde6
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-02-17 10:25:46 +0100

    [issue439] mark overriding methods and indent methods in header

--- src/search/global_task_interface.h
+++ src/search/global_task_interface.h
@@ -23,34 +23,42 @@ protected:
     }
 
 public:
-    int get_num_variables() const {return g_variable_domain.size(); }
-    int get_variable_domain_size(int var) const {
+    int get_num_variables() const override {
+        return g_variable_domain.size();
+    }
+    int get_variable_domain_size(int var) const override {
         return g_variable_domain[var];
     }
 
-    int get_operator_cost(int index, bool is_axiom) const {
+    int get_operator_cost(int index, bool is_axiom) const override {
         return get_operator_or_axiom(index, is_axiom).get_cost();
     }
-    std::string get_operator_name(int index, bool is_axiom) const {
+    std::string get_operator_name(int index, bool is_axiom) const override {
         return get_operator_or_axiom(index, is_axiom).get_name();
     }
-    int get_num_operators() const {return g_operators.size(); }
-    int get_num_operator_preconditions(int index, bool is_axiom) const;
+    int get_num_operators() const override {
+        return g_operators.size();
+    }
+    int get_num_operator_preconditions(int index, bool is_axiom) const override;
     std::pair<int, int> get_operator_precondition(
-        int op_index, int fact_index, bool is_axiom) const;
-    int get_num_operator_effects(int op_index, bool is_axiom) const;
+        int op_index, int fact_index, bool is_axiom) const override;
+    int get_num_operator_effects(int op_index, bool is_axiom) const override;
     int get_num_operator_effect_conditions(
-        int op_index, int eff_index, bool is_axiom) const;
+        int op_index, int eff_index, bool is_axiom) const override;
     std::pair<int, int> get_operator_effect_condition(
-        int op_index, int eff_index, int cond_index, bool is_axiom) const;
+        int op_index, int eff_index, int cond_index, bool is_axiom) const override;
     std::pair<int, int> get_operator_effect(
-        int op_index, int eff_index, bool is_axiom) const;
-    const GlobalOperator *get_global_operator(int index, bool is_axiom) const;
+        int op_index, int eff_index, bool is_axiom) const override;
+    const GlobalOperator *get_global_operator(int index, bool is_axiom) const override;
 
-    int get_num_axioms() const {return g_axioms.size(); }
+    int get_num_axioms() const override {
+        return g_axioms.size();
+    }
 
-    int get_num_goals() const {return g_goal.size(); }
-    std::pair<int, int> get_goal_fact(int index) const {
+    int get_num_goals() const override {
+        return g_goal.size();
+    }
+    std::pair<int, int> get_goal_fact(int index) const override {
         return g_goal[index];
     }
 };
