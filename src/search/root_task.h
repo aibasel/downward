18125b0839d8f757ed6ee27c6945ffe382cf4f33
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-03-06 17:27:41 +0100

    [issue515] var_id -> var

--- src/search/root_task.h
+++ src/search/root_task.h
@@ -13,7 +13,7 @@ public:
     virtual int get_num_variables() const override;
     virtual const std::string &get_variable_name(int var) const override;
     virtual int get_variable_domain_size(int var) const override;
-    virtual const std::string &get_fact_name(int var_id, int value) const override;
+    virtual const std::string &get_fact_name(int var, int value) const override;
 
     virtual int get_operator_cost(int index, bool is_axiom) const override;
     virtual const std::string &get_operator_name(int index, bool is_axiom) const override;
