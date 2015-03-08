a361dd5c747e05985f7f9c0553cff1824f99b383
Malte Helmert <malte.helmert@unibas.ch>
2015-03-08 15:51:46 +0100

    [issue77] work further towards using EvaluationContext instead of old evaluation model

--- src/search/evaluation_context.cc
+++ src/search/evaluation_context.cc
@@ -1,68 +1,104 @@
 #include "evaluation_context.h"
 
 #include "heuristic.h"
+#include "scalar_evaluator.h"
 
 #include <cassert>
 
 using namespace std;
 
 
+EvaluationContext::EvaluationContext(
+    const GlobalState &state, int g, bool is_preferred)
+    : state(state),
+      g_value(g),
+      preferred(is_preferred) {
+}
+
 const EvaluationContext::HeuristicResult &
-EvaluationContext::get_result(Heuristic *heur) {
+EvaluationContext::get_result(ScalarEvaluator *heur) {
     HeuristicResult &result = heuristic_results[heur];
     if (result.is_uninitialized()) {
-        heur->evaluate(state);
+        /*
+          TODO: This code is currently messy because we have to
+          support both heuristics and other kinds of scalar
+          evaluators. This messiness is temporary. See the general
+          comments in the header file.
+        */
+
+        Heuristic *h = dynamic_cast<Heuristic *>(heur);
+        if (h != nullptr) {
+            // We have an actual heuristic.
+            h->evaluate(state);
+        } else {
+            // We have non-heuristic scalar evaluator.
+            heur->evaluate(g_value, preferred);
+        }
         if (heur->is_dead_end())
             result.h_value = INFINITE;
         else
             result.h_value = heur->get_value();
-        heur->get_preferred_operators(result.preferred_operators);
+        if (h != nullptr) {
+            // We have an actual heuristic.
+            h->get_preferred_operators(result.preferred_operators);
+        }
     }
     return result;
 }
 
-EvaluationContext::EvaluationContext(const GlobalState &state)
-    : state(state) {
-}
-
-void EvaluationContext::evaluate_heuristic(Heuristic *heur) {
+void EvaluationContext::evaluate_heuristic(ScalarEvaluator *heur) {
     get_result(heur);
 }
 
-void EvaluationContext::hacky_set_evaluator_value(Heuristic *heur, int value) {
+void EvaluationContext::hacky_set_evaluator_value(ScalarEvaluator *heur, int value) {
     heuristic_results[heur].h_value = value;
 }
 
-bool EvaluationContext::is_heuristic_infinite(Heuristic *heur) {
+int EvaluationContext::get_g_value() const {
+    return g_value;
+}
+
+bool EvaluationContext::is_preferred() const {
+    return preferred;
+}
+
+bool EvaluationContext::is_heuristic_infinite(ScalarEvaluator *heur) {
     return get_result(heur).is_heuristic_infinite();
 }
 
-int EvaluationContext::get_heuristic_value(Heuristic *heur) {
+int EvaluationContext::get_heuristic_value(ScalarEvaluator *heur) {
     int h = get_result(heur).h_value;
     assert(h != INFINITE);
     return h;
 }
 
-int EvaluationContext::get_heuristic_value_or_infinity(Heuristic *heur) {
+int EvaluationContext::get_heuristic_value_or_infinity(ScalarEvaluator *heur) {
     return get_result(heur).h_value;
 }
 
 const vector<const GlobalOperator *> &
-EvaluationContext::get_preferred_operators(Heuristic *heur) {
+EvaluationContext::get_preferred_operators(ScalarEvaluator *heur) {
     return get_result(heur).preferred_operators;
 }
 
 bool EvaluationContext::is_dead_end() const {
     bool all_estimates_are_infinite = true;
+    bool at_least_one_estimate = false;
     for (const auto &entry : heuristic_results) {
-        const Heuristic *heur = entry.first;
+        const ScalarEvaluator *heur = entry.first;
         const HeuristicResult &result = entry.second;
-        if (result.is_heuristic_infinite()) {
-            if (heur->dead_ends_are_reliable())
-                return true;
-        } else {
-            all_estimates_are_infinite = false;
+        const Heuristic *h = dynamic_cast<const Heuristic *>(heur);
+        if (h != nullptr) {
+            /* Only consider actual heuristics.
+               TODO: Get rid of the cast and test once we've unified
+               Heuristic and ScalarEvaluator. */
+            if (result.is_heuristic_infinite()) {
+                if (h->dead_ends_are_reliable())
+                    return true;
+            } else {
+                all_estimates_are_infinite = false;
+            }
         }
     }
-    return all_estimates_are_infinite && !heuristic_results.empty();
+    return at_least_one_estimate && all_estimates_are_infinite;
 }
