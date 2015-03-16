4f885af8fcac7e44b51e832faeff4fe91b748437
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-03-16 13:34:54 +0100

    [main] cosmetics

--- src/search/lp_solver.h
+++ src/search/lp_solver.h
@@ -11,7 +11,7 @@
   All methods that use COIN specific classes only do something useful
   if the planner is compiled with USE_LP. Otherwise, they just print
   an error message and abort.
- */
+*/
 #ifdef USE_LP
 #define LP_METHOD(X) X;
 #else
@@ -82,7 +82,7 @@ class LPSolver {
     /*
       Temporary data for assigning a new problem. We keep the vectors
       around to avoid recreating them in every assignment.
-     */
+    */
     std::vector<double> elements;
     std::vector<int> indices;
     std::vector<int> starts;
