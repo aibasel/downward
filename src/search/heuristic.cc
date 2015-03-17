8c37a5d811ae51f3975b3972627538b1ef77e6ad
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-03-17 18:51:29 +0100

    [issue511] add get_task_from_options function

--- src/search/heuristic.cc
+++ src/search/heuristic.cc
@@ -15,37 +15,8 @@
 using namespace std;
 
 Heuristic::Heuristic(const Options &opts)
-    : cost_type(OperatorCost(opts.get_enum("cost_type"))) {
-    /*
-      If the heuristic is created by the parser, the options must contain an
-      AbstractTask. If it is created internally, the options must contain a
-      TaskProxy. It is an error if both are present.
-    */
-    assert(!(opts.contains("transform") && opts.contains("task_proxy")));
-    /*
-      This code is only intended for the transitional period while we still
-      support the "old style" of adjusting costs for the heuristics (via the
-      cost_type parameter) in parallel with the "new style" (via task
-      transformations). Once all heuristics are adapted to support task
-      transformations and we can remove the "cost_type" attribute, the options
-      should always contain a task (either an AbstractTask or a TaskProxy).
-    */
-    if (opts.contains("transform") && cost_type != NORMAL) {
-        cerr << "You may specify either the cost_type option of the heuristic "
-             << "(deprecated) or use transform=adapt_costs() (recommended), "
-             << "but not both." << endl;
-        exit_with(EXIT_INPUT_ERROR);
-    }
-    if (opts.contains("task_proxy")) {
-        task = opts.get<TaskProxy *>("task_proxy");
-    } else if (opts.contains("transform")) {
-        task = new TaskProxy(opts.get<AbstractTask *>("transform"));
-    } else {
-        Options options;
-        options.set<AbstractTask *>("transform", get_root_task().get());
-        options.set<int>("cost_type", cost_type);
-        task = new TaskProxy(new CostAdaptedTask(options));
-    }
+    : task(get_task_from_options(opts)),
+      cost_type(OperatorCost(opts.get_enum("cost_type"))) {
     heuristic = NOT_INITIALIZED;
 }
 
@@ -174,4 +145,43 @@ Options Heuristic::default_options() {
     opts.set<AbstractTask *>("transform", get_root_task().get());
     opts.set<int>("cost_type", 0);
     return opts;
+}
+
+TaskProxy *get_task_from_options(const Options &opts) {
+    /*
+      If the options are created by the parser, they must contain an
+      AbstractTask. If they are created internally, they must contain a
+      TaskProxy. It is an error if both are present.
+    */
+    assert(!(opts.contains("transform") && opts.contains("task_proxy")));
+    /*
+      TODO: This code is only intended for the transitional period while we
+      still support the "old style" of adjusting costs for the heuristics (via
+      the cost_type parameter) in parallel with the "new style" (via task
+      transformations). Once all heuristics are adapted to support task
+      transformations and we can remove the "cost_type" attribute, the options
+      should always contain a task (either an AbstractTask or a TaskProxy). When
+      that is the case, get_task_from_options() should only be integrated into
+      the Heuristic constructor and the PDB heuristic generators (ipdb, gapdb,
+      etc.) should directly call opts.get<TaskProxy *>("transform").
+    */
+    OperatorCost cost_type = OperatorCost(opts.get_enum("cost_type"));
+    if (opts.contains("transform") && cost_type != NORMAL) {
+        cerr << "You may specify either the cost_type option of the heuristic "
+             << "(deprecated) or use transform=adapt_costs() (recommended), "
+             << "but not both." << endl;
+        exit_with(EXIT_INPUT_ERROR);
+    }
+    TaskProxy *task = nullptr;
+    if (opts.contains("task_proxy")) {
+        task = opts.get<TaskProxy *>("task_proxy");
+    } else if (opts.contains("transform")) {
+        task = new TaskProxy(opts.get<AbstractTask *>("transform"));
+    } else {
+        Options options;
+        options.set<AbstractTask *>("transform", get_root_task().get());
+        options.set<int>("cost_type", cost_type);
+        task = new TaskProxy(new CostAdaptedTask(options));
+    }
+    return task;
 }
