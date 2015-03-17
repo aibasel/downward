bd1e07eaaadf664daf7edc7515e59821a232bca2
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-03-17 19:31:26 +0100

    [issue511] add docstrings

--- src/search/cost_adapted_task.h
+++ src/search/cost_adapted_task.h
@@ -6,6 +6,18 @@
 
 class Options;
 
+/*
+  Task transformation that changes operator costs. If the parent task assigns
+  costs 'c' to an operator, its adjusted costs, depending on the value of the
+  cost_type option, are:
+
+    NORMAL:  c
+    ONE:     1
+    PLUSONE: 1, if all operators have cost 1 in the parent task, else c + 1
+
+  Regardless of the cost_type value, axioms will always keep their original
+  cost, which is 0 by default.
+*/
 class CostAdaptedTask : public DelegatingTask {
     const OperatorCost cost_type;
 public:
