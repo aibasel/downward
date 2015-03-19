4c19c972e8a549b9bd1e98a9ed54b46a508569c9
Patrick von Reth <vonreth@kde.org>
2015-03-19 18:59:35 +0100

    [issue436] cleanup of UTILITIES_HASH

--- src/search/utilities_hash.h
+++ src/search/utilities_hash.h
@@ -1,5 +1,5 @@
-#ifndef HASH_UTILS
-#define HASH_UTILS
+#ifndef UTILITIES_HASH
+#define UTILITIES_HASH
 
 #include <functional>
 #include <utility>
@@ -7,14 +7,10 @@
 
 // combination of hash values is based on boost
 //TODO: move helper functions to a namespace?
-inline void hash_combine_impl(size_t &hash, size_t value) {
-    hash ^= value + 0x9e3779b9 + (hash << 6) + (hash >> 2);
-}
-
 template<typename T>
-inline void hash_combine(size_t &seed, const T &value) {
+inline void hash_combine(size_t &hash, const T &value) {
     const std::hash<T> hasher;
-    return hash_combine_impl(seed, hasher(value));
+    hash ^= hasher(value) + 0x9e3779b9 + (hash << 6) + (hash >> 2);
 }
 
 template<typename Sequence>
@@ -26,21 +22,16 @@ size_t hash_sequence(const Sequence &data, size_t length) {
     return hash;
 }
 
-template<typename Sequence>
-inline size_t hash_sequence(const Sequence &data) {
-    return ::hash_sequence(data, data.size());
-}
-
 namespace std {
-template<typename T, typename Alloc>
-struct hash<std::vector<T, Alloc> > {
-    size_t operator()(const std::vector<T, Alloc> &vec) const {
+template<typename T>
+struct hash<std::vector<T> > {
+    size_t operator()(const std::vector<T> &vec) const {
         return ::hash_sequence(vec, vec.size());
     }
 };
 
 template<typename TA, typename TB>
-struct  hash < std::pair < TA, TB > > {
+struct  hash<std::pair<TA, TB> > {
     size_t operator()(const std::pair<TA, TB> &pair) const {
         size_t hash = 0;
         hash_combine(hash, pair.first);
@@ -49,13 +40,13 @@ struct  hash < std::pair < TA, TB > > {
     }
 };
 
-template<typename T, typename Alloc>
-struct hash<pair<vector<T *, Alloc>, T *> > {
+template<typename TA, typename TB>
+struct hash<std::pair<vector<TA>, TB> > {
 public:
-    size_t operator()(const pair<vector<T *, Alloc>, T *> &pair) const {
+    size_t operator()(const pair<vector<TA>, TB> &pair) const {
         size_t hash = 0;
         hash_combine(hash, pair.second);
-        hash_combine_impl(hash, hash_sequence(pair.first));
+        hash_combine(hash, pair.first);
         return hash;
     }
 };
