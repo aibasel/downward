f7429c382e6c56486a757f896472b4d59457a346
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-03-31 20:24:35 +0200

    [issue600] add applicability test for ConcreteState and convert first function

--- src/search/cegar/concrete_state.cc
+++ src/search/cegar/concrete_state.cc
@@ -1,5 +1,6 @@
 #include "concrete_state.h"
 
+#include "../global_operator.h"
 #include "../global_state.h"
 
 using namespace std;
@@ -14,5 +15,17 @@ ConcreteState::~ConcreteState() {
 
 int ConcreteState::operator[](size_t index) const {
     return state[index];
+}
+
+bool is_applicable(const GlobalCondition &condition, const ConcreteState &state) {
+    return state[condition.var] == condition.val;
+}
+
+bool is_applicable(const GlobalOperator &op, const ConcreteState &state) {
+    for (GlobalCondition precondition : op.get_preconditions()) {
+        if (!is_applicable(precondition, state))
+            return false;
+    }
+    return true;
 }
 
