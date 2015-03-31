05123166d119a348be06c42ac293a2c89f1f85ad
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-03-31 20:11:19 +0200

    [issue600] add ConcreteState::operator[]

--- src/search/cegar/concrete_state.cc
+++ src/search/cegar/concrete_state.cc
@@ -1,10 +1,18 @@
 #include "concrete_state.h"
 
+#include "../global_state.h"
+
+using namespace std;
+
 ConcreteState::ConcreteState(const GlobalState &state)
     : state(state) {
 
 }
 
 ConcreteState::~ConcreteState() {
+}
+
+int ConcreteState::operator[](size_t index) const {
+    return state[index];
 }
 
