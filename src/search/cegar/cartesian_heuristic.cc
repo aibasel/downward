ff276e9e0a7340c10486f589f689c34e3a378ee0
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-04-03 13:11:25 +0200

    [issue600] add CartesianHeuristic

---
+++ src/search/cegar/cartesian_heuristic.cc
@@ -0,0 +1,29 @@
+#include "cartesian_heuristic.h"
+
+#include "../option_parser.h"
+
+#include <cassert>
+
+using namespace std;
+
+namespace cegar {
+CartesianHeuristic::CartesianHeuristic(const Options &opts)
+    : Heuristic(opts),
+      split_tree(opts.get<SplitTree>("split_tree")) {
+}
+
+CartesianHeuristic::~CartesianHeuristic() {
+}
+
+void CartesianHeuristic::initialize() {
+}
+
+int CartesianHeuristic::compute_heuristic(const GlobalState &global_state) {
+    State state = task->convert_global_state(global_state);
+    int h = split_tree.get_node(state)->get_h();
+    assert(h >= 0);
+    if (h == INF)
+        return DEAD_END;
+    return h;
+}
+}
