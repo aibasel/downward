cc988eac97c43654091d7cd9847a31732db851a8
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-04-08 10:54:56 +0200

    [issue600] finish flaw-selector restructuring

--- src/search/cegar/flaw_selector.cc
+++ src/search/cegar/flaw_selector.cc
@@ -1,21 +1,20 @@
 #include "flaw_selector.h"
 
 #include "abstract_state.h"
+#include "utils.h"
 
 #include "../additive_heuristic.h"
 #include "../globals.h"
 #include "../rng.h"
+#include "../utilities.h"
 
-#include <algorithm>
 #include <cassert>
 #include <iostream>
-#include <utility>
-#include <vector>
+#include <limits>
 
 using namespace std;
 
 namespace cegar {
-
 FlawSelector::FlawSelector(TaskProxy task_proxy, PickStrategy pick)
     : task_proxy(task_proxy),
       pick(pick) {
@@ -56,100 +55,71 @@ int FlawSelector::get_hadd_value(int var_id, int value) const {
 
 int FlawSelector::get_extreme_hadd_value(int var_id, const vector<int> &values) const {
     assert(pick == MIN_HADD || pick == MAX_HADD);
-    int min_hadd = INF;
-    int max_hadd = -2;
+    int min_hadd = numeric_limits<int>::max();
+    int max_hadd = -1;
     for (int value : values) {
         int hadd = get_hadd_value(var_id, value);
-        if (pick == MIN_HADD && hadd < min_hadd) {
+        if (hadd < min_hadd) {
             min_hadd = hadd;
-        } else if (pick == MAX_HADD && hadd > max_hadd) {
+        }
+        if (hadd > max_hadd) {
             max_hadd = hadd;
         }
     }
-    if (pick == MIN_HADD)
+    if (pick == MIN_HADD) {
         return min_hadd;
-    else
+    } else {
         return max_hadd;
+    }
 }
 
-const Flaw &FlawSelector::pick_flaw(const AbstractState &state, const Flaws &flaws) const {
-    // TODO: Return reference to flaw instead of index and rename method.
-    assert(!flaws.empty());
-    // Shortcut for condition lists with only one element.
-    if (flaws.size() == 1) {
-        return flaws[0];
-    }
-    if (DEBUG) {
-        cout << "Flaw: " << state.str() << endl;
-        for (const Flaw &flaw : flaws) {
-            int var = flaw.first;
-            const vector<int> &wanted = flaw.second;
-            if (DEBUG)
-                cout << var << "=" << wanted << " ";
-        }
-    }
-    if (DEBUG)
-        cout << endl;
-    int cond = -1;
-    int random_cond = g_rng(flaws.size());
-    if (pick == RANDOM) {
-        cond = random_cond;
-    } else if (pick == MIN_CONSTRAINED || pick == MAX_CONSTRAINED) {
-        int max_rest = -1;
-        int min_rest = INF;
-        int i = 0;
-        for (const Flaw &flaw : flaws) {
-            int value = get_constrainedness(state, flaw);
-            if (value > max_rest && pick == MIN_CONSTRAINED) {
-                cond = i;
-                max_rest = value;
-            }
-            if (value < min_rest && pick == MAX_CONSTRAINED) {
-                cond = i;
-                min_rest = value;
-            }
-            ++i;
-        }
+double FlawSelector::rate_flaw(const AbstractState &state, const Flaw &flaw) const {
+    double rating;
+    if (pick == MIN_CONSTRAINED || pick == MAX_CONSTRAINED) {
+        rating = get_constrainedness(state, flaw);
     } else if (pick == MIN_REFINED || pick == MAX_REFINED) {
-        double min_refinement = 0.0;
-        double max_refinement = -1.1;
-        int i = 0;
-        for (auto flaw : flaws) {
-            double refinedness = get_refinedness(state, flaw);
-            if (refinedness < min_refinement && pick == MIN_REFINED) {
-                cond = i;
-                min_refinement = refinedness;
-            }
-            if (refinedness > max_refinement && pick == MAX_REFINED) {
-                cond = i;
-                max_refinement = refinedness;
-            }
-            ++i;
-        }
-    } else if (pick == MIN_HADD || pick == MAX_HADD) {
-        int min_hadd = INF;
-        int max_hadd = -2;
-        int i = 0;
-        for (const Flaw &flaw : flaws) {
-            int var_id = flaw.first;
-            const vector<int> &values = flaw.second;
-            for (int value : values) {
-                int result = get_hadd_value(var_id, value);
-                if (pick == MIN_HADD && result < min_hadd) {
-                    cond = i;
-                    min_hadd = result;
-                } else if (pick == MAX_HADD && result > max_hadd) {
-                    cond = i;
-                    max_hadd = result;
-                }
-            }
-            ++i;
-        }
+        rating = get_refinedness(state, flaw);
+    } else if (pick == MIN_HADD || MAX_HADD) {
+        rating  = get_extreme_hadd_value(flaw.first, flaw.second);
     } else {
         cout << "Invalid pick strategy: " << pick << endl;
         exit_with(EXIT_INPUT_ERROR);
     }
-    assert(in_bounds(cond, flaws));
-    return flaws[cond];
+    if (pick == MIN_CONSTRAINED || pick == MIN_REFINED || pick == MIN_HADD) {
+        rating = -rating;
+    }
+    return rating;
+}
+
+const Flaw &FlawSelector::pick_flaw(const AbstractState &state, const Flaws &flaws) const {
+    assert(!flaws.empty());
+
+    // Use shortcut if there is only one flaw.
+    if (flaws.size() == 1) {
+        return flaws[0];
+    }
+
+    if (DEBUG) {
+        cout << "Flaws: " << state.str() << endl;
+        for (const Flaw &flaw : flaws)
+            cout << flaw.first << "=" << flaw.second << " ";
+        cout << endl;
+    }
+
+    if (pick == RANDOM) {
+        return *g_rng.choose(flaws);
+    }
+
+    double max_rating = numeric_limits<double>::lowest();
+    const Flaw *selected_flaw = nullptr;
+    for (const Flaw &flaw : flaws) {
+        double rating = rate_flaw(state, flaw);
+        if (rating > max_rating) {
+            selected_flaw = &flaw;
+            max_rating = rating;
+        }
+    }
+    assert(selected_flaw);
+    return *selected_flaw;
 }
 }
