b83a35d224b65fa932c9525a541f195fc158273a
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-04-13 11:39:24 +0200

    [issue600] Values -> Domains

--- src/search/cegar/values.h
+++ src/search/cegar/values.h
@@ -14,21 +14,21 @@ class TaskProxy;
 namespace cegar {
 typedef boost::dynamic_bitset<> Bitset;
 
-class Values {
+class Domains {
     // Possible values of each variable in this state.
     // Values are represented from right to left (least- to most-significant).
     // 11 10 001 -> var0 = {0}, var1 = {1}, var2 = {0,1}
-    Bitset values;
+    Bitset domains;
 
-    static std::vector<int> variable_domain;
+    static std::vector<int> original_domain_sizes;
     // Total number of facts and size of the values bitset.
-    static int facts;
+    static int num_facts;
     // The first bit for var is at borders[var].
     static std::vector<int> borders;
     // masks[var][pos] == true iff pos belongs to var.
     static std::vector<Bitset> masks;
     static std::vector<Bitset> inverse_masks;
-    // Temporary bitset for calculations in domains_intersect().
+    // Temporary bitset for speeding up calculations.
     static Bitset temp_values;
 
     static void initialize_static_members(TaskProxy task_proxy);
@@ -36,8 +36,8 @@ class Values {
     int pos(int var, int value) const {return borders[var] + value; }
 
 public:
-    explicit Values(TaskProxy task_proxy);
-    ~Values() = default;
+    explicit Domains(TaskProxy task_proxy);
+    ~Domains() = default;
 
     void add(int var, int value);
     void set(int var, int value);
@@ -46,11 +46,11 @@ public:
     void remove_all(int var);
     bool test(int var, int value) const;
     size_t count(int var) const;
-    bool domains_intersect(const Values &other, int var) const;
+    bool intersects(const Domains &other, int var) const;
     // Return true if all abstract domains are supersets of the
     // other's respective domains.
-    bool abstracts(const Values &other) const;
-    void get_possible_flaws(const Values &flaw, const State &conc_state, Flaws *flaws) const;
+    bool abstracts(const Domains &other) const;
+    void get_possible_flaws(const Domains &flaw, const State &conc_state, Flaws *flaws) const;
 
     std::string str() const;
 };
