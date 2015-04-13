6f1d31dede76fd0060c3092328088f0fc0ae5e93
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-04-13 10:52:13 +0200

    [issue600] further simplify includes

--- src/search/cegar/domain_abstracted_task.h
+++ src/search/cegar/domain_abstracted_task.h
@@ -7,7 +7,7 @@
 #include <cassert>
 #include <string>
 #include <unordered_map>
-#include <unordered_set>
+#include <utility>
 #include <vector>
 
 namespace cegar {
--- src/search/cegar/utils.h
+++ src/search/cegar/utils.h
@@ -3,7 +3,6 @@
 
 #include <limits>
 #include <memory>
-#include <string>
 #include <unordered_map>
 #include <unordered_set>
 #include <utility>
