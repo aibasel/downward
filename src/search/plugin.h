d7140e2fdb50552d2aa2626482c653a3f4675197
Patrick von Reth <vonreth@kde.org>
2015-04-14 14:34:20 +0200

    [issue456] style

--- src/search/plugin.h
+++ src/search/plugin.h
@@ -17,8 +17,7 @@ template <class T>
 class Plugin {
 public:
     Plugin(const std::string &key, typename Registry<T *>::Factory factory) {
-        Registry<T *>::
-        instance()->register_object(key, factory);
+        Registry<T *>::instance()->register_object(key, factory);
     }
     ~Plugin() = default;
     Plugin(const Plugin<T> &other) = delete;
@@ -29,8 +28,7 @@ template <class T>
 class PluginShared {
 public:
     PluginShared(const std::string &key, typename Registry<std::shared_ptr<T>>::Factory factory) {
-        Registry<std::shared_ptr<T>>::
-        instance()->register_object(key, factory);
+        Registry<std::shared_ptr<T>>::instance()->register_object(key, factory);
     }
     ~PluginShared() = default;
     PluginShared(const PluginShared<T> &other) = delete;
