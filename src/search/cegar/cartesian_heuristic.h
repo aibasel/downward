30b9df0c97716d46cbdfbe189cc5582f7c5599ec
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-04-22 15:28:18 +0200

    [issue600] use task_proxy from Heuristic in CartesianHeuristic

--- src/search/cegar/cartesian_heuristic.h
+++ src/search/cegar/cartesian_heuristic.h
@@ -5,15 +5,11 @@
 
 #include "../heuristic.h"
 
-#include <memory>
-
 class GlobalState;
 class Options;
 
 namespace cegar {
 class CartesianHeuristic : public Heuristic {
-    // TODO: We only save this to keep the task from being deleted. Avoid this.
-    const std::shared_ptr<AbstractTask> abstract_task;
     // TODO: Store as unique_ptr.
     const SplitTree split_tree;
 
@@ -22,10 +18,8 @@ protected:
     virtual int compute_heuristic(const GlobalState &global_state);
 
 public:
-    // TODO: Move parameters into options once they support smart pointers.
-    explicit CartesianHeuristic(std::shared_ptr<AbstractTask> abstract_task,
-                                const Options &options);
-    ~CartesianHeuristic();
+    explicit CartesianHeuristic(const Options &options);
+    ~CartesianHeuristic() = default;
 };
 }
 
