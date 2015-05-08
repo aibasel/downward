4e8bba6ac56f33a18d1e11b575a0246d24d9aa30
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-05-08 13:56:13 +0200

    [issue77] update ehc for evaluation contexts

--- src/search/enforced_hill_climbing_search.h
+++ src/search/enforced_hill_climbing_search.h
@@ -1,28 +1,24 @@
 #ifndef ENFORCED_HILL_CLIMBING_SEARCH_H
 #define ENFORCED_HILL_CLIMBING_SEARCH_H
 
-#include "g_evaluator.h"
-#include "global_operator.h"
-#include "global_state.h"
-#include "globals.h"
+#include "eager_evaluation_context.h"
 #include "search_engine.h"
-#include "search_node_info.h"
-#include "search_progress.h"
-#include "search_space.h"
 
 #include "open_lists/open_list.h"
 
 #include <map>
+#include <set>
+#include <utility>
 #include <vector>
 
-using namespace std;
-
+class GEvaluator;
 class Options;
 
-typedef pair<StateID, pair<int, const GlobalOperator * > > OpenListEntryEHC;
+typedef std::pair<StateID, std::pair<int, const GlobalOperator * > > OpenListEntryEHC;
 
-enum PreferredUsage {
-    PRUNE_BY_PREFERRED, RANK_PREFERRED_FIRST,
+enum class PreferredUsage {
+    PRUNE_BY_PREFERRED,
+    RANK_PREFERRED_FIRST,
     MAX_PREFERRED_USAGE
 };
 
@@ -32,25 +28,28 @@ protected:
     GEvaluator *g_evaluator;
 
     Heuristic *heuristic;
-    bool preferred_contains_eval;
-    vector<Heuristic *> preferred_heuristics;
+    std::vector<Heuristic *> preferred_operator_heuristics;
+    std::set<Heuristic *> heuristics;
     bool use_preferred;
     PreferredUsage preferred_usage;
 
-    GlobalState current_state;
-    int current_h;
-    int current_g;
+    EagerEvaluationContext current_eval_context;
 
-    // statistics
-    map<int, pair<int, int> > d_counts;
+    // Statistics
+    std::map<int, std::pair<int, int> > d_counts;
     int num_ehc_phases;
-    int last_expanded;
+    int last_num_expanded;
 
-    virtual void initialize();
-    virtual SearchStatus step();
+    virtual void initialize() override;
+    virtual SearchStatus step() override;
     SearchStatus ehc();
-    void get_successors(const GlobalState &state, vector<const GlobalOperator *> &ops);
-    void evaluate(const GlobalState &parent, const GlobalOperator *op, const GlobalState &state);
+    void expand(EvaluationContext &eval_context, int d);
+    std::vector<const GlobalOperator *> get_successors(
+        EvaluationContext &eval_context);
+    void reach_state(
+        const GlobalState &parent, const GlobalOperator &op,
+        const GlobalState &state);
+
 public:
     explicit EnforcedHillClimbingSearch(const Options &opts);
     virtual ~EnforcedHillClimbingSearch() override;
