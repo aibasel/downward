1f0645cb90639d42caf564583b9e56c276dc7674
Florian Pommerening <florian.pommerening@unibas.ch>
2015-05-11 14:30:42 +0200

    [issue528] Add issue experiment

---
+++ experiments/issue528/issue528.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from downward import configs, suites
+
+import common_setup
+
+
+SEARCH_REVS = ["issue528-base", "issue528-v1", "issue528-v2"]
+SUITE = suites.suite_optimal_with_ipc11()
+
+CONFIGS = {
+    "astar_lmcut": ["--search", "astar(lmcut())"]
+}
+
+exp = common_setup.IssueExperiment(
+    revisions=SEARCH_REVS,
+    configs=CONFIGS,
+    suite=SUITE,
+    )
+
+exp.add_absolute_report_step()
+exp.add_comparison_table_step()
+
+exp()
