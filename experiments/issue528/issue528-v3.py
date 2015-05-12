a8ee584a9f681a6a26d7d361447edfa25d33bd1a
Florian Pommerening <florian.pommerening@unibas.ch>
2015-05-12 17:53:47 +0200

    [issue528] Add scatter plots to experiment

--- experiments/issue528/issue528-v3.py
+++ experiments/issue528/issue528-v3.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from downward import configs, suites
+from downward.reports.scatter import ScatterPlotReport
 
 import common_setup
 
@@ -21,5 +22,17 @@ exp = common_setup.IssueExperiment(
 
 exp.add_absolute_report_step()
 exp.add_comparison_table_step()
+
+for attr in ("memory", "total_time"):
+    exp.add_report(
+        ScatterPlotReport(
+            attributes=[attr],
+            filter_config=[
+                "issue528-base-astar_lmcut",
+                "issue528-v3-astar_lmcut",
+            ],
+        ),
+        outfile='issue528_base_v3_%s.png' % attr
+    )
 
 exp()
--- experiments/issue528/issue528.py
+++ experiments/issue528/issue528.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from downward import configs, suites
+from downward.reports.scatter import ScatterPlotReport
 
 import common_setup
 
@@ -21,5 +22,17 @@ exp = common_setup.IssueExperiment(
 
 exp.add_absolute_report_step()
 exp.add_comparison_table_step()
+
+for attr in ("memory", "total_time"):
+    exp.add_report(
+        ScatterPlotReport(
+            attributes=[attr],
+            filter_config=[
+                "issue528-base-astar_lmcut",
+                "issue528-v2-astar_lmcut",
+            ],
+        ),
+        outfile='issue528_base_v2_%s.png' % attr
+    )
 
 exp()
