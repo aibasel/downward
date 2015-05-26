247f8a12823574c2f6241686dcd37dc1a8bee5c8
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-05-26 12:09:32 +0200

    [issue600] add documentation

--- src/search/cegar/additive_cartesian_heuristic.h
+++ src/search/cegar/additive_cartesian_heuristic.h
@@ -7,6 +7,50 @@
 #include <memory>
 #include <vector>
 
+/*
+  Overview of classes relevant to Cartesian abstractions:
+
+  AdditiveCartesianHeuristic
+    Get subtasks from Decompositions, adjust their costs by wrapping them in
+    tasks::ModifiedCostsTasks, compute Abstractions, move SplitTrees from
+    Abstractions to CartesianHeuristics, store CartesianHeuristics and
+    compute overall heuristic by adding heuristic values of all
+    CartesianHeuristics.
+
+    Decomposition
+      Create focused subtasks. NoDecomposition returns the original task.
+      GoalDecomposition uses tasks::ModifiedGoalsTask to set a single goal
+      fact. LandmarkDecomposition nests tasks::ModifiedGoalsTask and
+      tasks::DomainAbstractedTask to focus on a single landmark fact.
+
+    CartesianHeuristic
+      Store SplitTree for looking up heuristic values efficiently.
+
+  Abstraction
+    Store the set of AbstractStates, use AbstractSearch to find abstract
+    solutions, find flaws, use SplitSelector to select splits in case of
+    ambiguities, break spurious solutions and maintain the refinement hierarchy
+    in SplitTree.
+
+    AbstractState
+      Store and update abstract Domains and transitions.
+
+      Domains
+        Use a single boost::dynamic_bitset to store the Cartesian set of values
+        in an abstract state.
+
+    AbstractSearch
+      Find an abstract solution using A*. Compute goal distances for abstract
+      states.
+
+    SplitSelector
+      Strategies for selecting splits in case there are multiple possibilities.
+
+    SplitTree
+      Refinement hierarchy that has one tree node for each split. Contains
+      helper nodes for splits that split off multiple facts.
+*/
+
 class CountdownTimer;
 class Decomposition;
 class GlobalState;
