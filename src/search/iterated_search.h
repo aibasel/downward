da5cf205e65b6cf27cbdad7a3d1343430878eafa
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-05-28 00:30:25 +0200

    [issue77] cleanup iterated search

--- src/search/iterated_search.h
+++ src/search/iterated_search.h
@@ -7,6 +7,12 @@
 class Options;
 
 class IteratedSearch : public SearchEngine {
+    const std::vector<ParseTree> engine_configs;
+    bool pass_bound;
+    bool repeat_last_phase;
+    bool continue_on_fail;
+    bool continue_on_solve;
+
     int phase;
     bool last_phase_found_solution;
     int best_bound;
@@ -15,21 +21,17 @@ class IteratedSearch : public SearchEngine {
     SearchEngine *current_search;
     std::string current_search_name;
 
-    const std::vector<ParseTree> engine_configs;
-    bool pass_bound;
-    bool repeat_last_phase;
-    bool continue_on_fail;
-    bool continue_on_solve;
-
     SearchEngine *get_search_engine(int engine_config_start_index);
     SearchEngine *create_phase(int p);
     SearchStatus step_return_value();
 
     virtual void initialize();
     virtual SearchStatus step();
+
 public:
     explicit IteratedSearch(const Options &opts);
-    virtual ~IteratedSearch() override;
+    virtual ~IteratedSearch() override = default;
+
     virtual void save_plan_if_necessary() const;
     virtual void print_statistics() const override;
 };
