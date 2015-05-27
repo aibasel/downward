43ab4af1e64ee29833d578aedca53041f144905a
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-05-27 23:55:07 +0200

    [issue77] assert that no invalid g or preferredness values are used, document constructors

--- src/search/evaluation_context.h
+++ src/search/evaluation_context.h
@@ -53,16 +53,39 @@ class EvaluationContext {
     bool preferred;
     SearchStatistics *statistics;
 
+    static const int INVALID = -1;
+
 public:
+    /*
+      Use existing heuristic cache when looking up heuristic values. Used for
+      example by lazy search.
+    */
     EvaluationContext(
         const HeuristicCache &cache, int g_value, bool is_preferred,
         SearchStatistics *statistics);
+    /*
+      Create new heuristic cache for caching heuristic values. Used for example
+      by eager search.
+    */
     EvaluationContext(
         const GlobalState &state, int g_value, bool is_preferred,
         SearchStatistics *statistics);
+    /*
+      Use the following constructor when you don't care about g values,
+      preferredness (and statistics), e.g. when sampling states for heuristics.
+
+      This constructor sets g_value to -1 and checks that neither get_g_value()
+      nor is_preferred() are called for objects constructed with it.
+
+      TODO: In the long term we might want to separate how path-dependent and
+            path-independent heuristics are evaluated. This change would remove
+            the need to store the g value and preferredness for evaluation
+            contexts that don't need this information.
+    */
     EvaluationContext(
         const GlobalState &state,
         SearchStatistics *statistics = nullptr);
+
     ~EvaluationContext() = default;
 
     const EvaluationResult &get_result(ScalarEvaluator *heur);
