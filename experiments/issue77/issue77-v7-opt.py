cdb7e161d24f84e793e5c75afcc4c2fed8a9f72e
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-06-05 00:07:44 +0200

    [issue77] allow list of scatter attributes

--- experiments/issue77/issue77-v7-opt.py
+++ experiments/issue77/issue77-v7-opt.py
@@ -12,8 +12,8 @@ CONFIGS = configs.default_configs_optimal(ipc=False, extended=False)
 print(sorted(CONFIGS.keys()))
 print(len(CONFIGS))
 
-
 SUITE = downward.suites.suite_optimal_with_ipc11()
+SCATTER_ATTRIBUTES = ["total_time"]
 
 exp = common_setup.IssueExperiment(
     search_revisions=["issue77-v7-base", "issue77-v7"],
@@ -22,11 +22,13 @@ exp = common_setup.IssueExperiment(
     )
 exp.add_absolute_report_step()
 exp.add_comparison_table_step()
-exp.add_scatter_plot_step(attributes=["total_time"])
-exp.add_report(relative_scatter.RelativeScatterPlotReport(
-        filter_config=["issue77-v7-base-astar_blind", "issue77-v7-astar_blind"],
-        attributes=["total_time"],
-        get_category=lambda run1, run2: run1["domain"],),
-    outfile="astar_blind-total_time.png")
+exp.add_scatter_plot_step(attributes=SCATTER_ATTRIBUTES)
+
+for attribute in SCATTER_ATTRIBUTES:
+    exp.add_report(relative_scatter.RelativeScatterPlotReport(
+            filter_config=["issue77-v7-base-astar_blind", "issue77-v7-astar_blind"],
+            attributes=attribute,
+            get_category=lambda run1, run2: run1["domain"],),
+        outfile="astar_blind-%(attribute)s.png" % locals())
 
 exp()
