648b17e9ed8e5ab8145f7ae649ba3f063e0d9f4e
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-06-04 23:30:36 +0200

    [issue77] add RelativeScatterPlot

--- experiments/issue77/issue77-v7-opt.py
+++ experiments/issue77/issue77-v7-opt.py
@@ -5,6 +5,7 @@ import downward.suites
 
 import common_setup
 import configs
+import relative_scatter
 
 CONFIGS = configs.default_configs_optimal(ipc=False, extended=False)
 
@@ -21,6 +22,11 @@ exp = common_setup.IssueExperiment(
     )
 exp.add_absolute_report_step()
 exp.add_comparison_table_step()
-# exp.add_scatter_plot_step()
+exp.add_scatter_plot_step(attributes=["total_time"])
+exp.add_report(relative_scatter.RelativeScatterPlotReport(
+        filter_config=["issue77-v7-base-astar_blind", "issue77-v7-astar_blind"],
+        attributes=["total_time"],
+        get_category=lambda run1, run2: run1["domain"],),
+    outfile="astar_blind-total_time.png")
 
 exp()
