dde2e26217ef7ed8e0153381def8934cc2dd036f
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-06-26 14:53:56 +0200

    [issue542] remove includes, improve error reporting

--- src/search/potentials/potential_optimizer.h
+++ src/search/potentials/potential_optimizer.h
@@ -9,27 +9,17 @@
 class GlobalState;
 class Heuristic;
 class Options;
-class StateRegistry;
 
 namespace potentials {
-
-class PotentialHeuristic;
-
-enum class OptFunc {
-    INITIAL_STATE,
-    ALL_STATES,
-    SAMPLES
-};
-
 class PotentialOptimizer {
     LPSolver lp_solver;
     const double max_potential;
-    int num_cols;
-    std::vector<std::vector<int> > lp_var_ids;
+    int num_facts;
+    std::vector<std::vector<int> > fact_ids;
     std::vector<std::vector<double> > fact_potentials;
 
     void construct_lp();
-    void set_objective(const std::vector<double> &coefficients);
+    void set_lp_objective(const std::vector<double> &coefficients);
     bool solve_lp();
     void extract_lp_solution();
 
@@ -37,12 +27,13 @@ public:
     explicit PotentialOptimizer(const Options &opts);
     ~PotentialOptimizer() = default;
 
+    bool potentials_are_bounded() const;
+
     bool optimize_for_state(const GlobalState &state);
     bool optimize_for_all_states();
     bool optimize_for_samples(const std::vector<GlobalState> &samples);
 
     bool has_optimal_solution() const;
-    bool potentials_are_bounded() const;
 
     std::shared_ptr<Heuristic> get_heuristic() const;
 };
@@ -50,7 +41,5 @@ public:
 void add_common_potential_options_to_parser(OptionParser &parser);
 
 void optimize_for_samples(PotentialOptimizer &optimizer, int num_samples);
-std::shared_ptr<Heuristic> create_potential_heuristic(const Options &opts);
-
 }
 #endif
