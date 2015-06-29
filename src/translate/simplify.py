8fe0aaf6b1e42973b982771e0eb3469da1d65a01
Malte Helmert <malte.helmert@unibas.ch>
2015-06-29 20:52:15 +0200

    [issue533] simplify: add more documentation; add more debugging features; cleanup

--- src/translate/simplify.py
+++ src/translate/simplify.py
@@ -19,7 +19,8 @@ applicable given the mutex information.
 
 Despite the name, the method touches more than the set of facts. For
 example, operators that have preconditions on pruned facts are
-removed, too.
+removed, too. (See also the docstring of
+filter_unreachable_propositions.)
 """
 
 
@@ -38,15 +39,36 @@ DEBUG = False
 
 
 class DomainTransitionGraph(object):
+    """Domain transition graphs.
+
+    Attributes:
+    - init (int): the initial state value of the DTG variable
+    - size (int): the number of values in the domain
+    - arcs (defaultdict: int -> set(int)): the DTG arcs (unlabeled)
+
+    There are no transition labels or goal values.
+
+    The intention is that nodes are represented as ints in {1, ...,
+    domain_size}, but this is not enforced.
+
+    For derived variables, the "fallback value" that is produced by
+    negation by failure should be used for `init`, so that it is
+    always considered reachable.
+    """
+
     def __init__(self, init, size):
+        """Create a DTG with no arcs."""
         self.init = init
         self.size = size
         self.arcs = defaultdict(set)
 
     def add_arc(self, u, v):
+        """Add an arc from u to v."""
         self.arcs[u].add(v)
 
     def reachable(self):
+        """Return the values reachable from the initial value.
+        Represented as a set(int)."""
         queue = [self.init]
         reachable = set(queue)
         while queue:
@@ -57,21 +79,36 @@ class DomainTransitionGraph(object):
         return reachable
 
     def dump(self):
-        print("SIZE", self.size)
-        print("INIT", self.init)
-        print("ARCS:")
+        """Dump the DTG."""
+        print("DTG size:", self.size)
+        print("DTG init value:", self.init)
+        print("DTG arcs:")
         for source, destinations in sorted(self.arcs.items()):
             for destination in sorted(destinations):
                 print("  %d => %d" % (source, destination))
 
 
 def build_dtgs(task):
+    """Build DTGs for all variables of the SASTask `task`.
+    Return a list(DomainTransitionGraph), one for each variable.
+
+    For derived variables, we do not consider the axiom bodies, i.e.,
+    we treat each axiom as if it were an operator with no
+    preconditions. In the case where the only derived variables used
+    are binary and all rules change the value from the default value
+    to the non-default value, this results in the correct DTG.
+    Otherwise, at worst it results in an overapproximation, which
+    would not threaten correctness."""
+
     init_vals = task.init.values
     sizes = task.variables.ranges
     dtgs = [DomainTransitionGraph(init, size)
             for (init, size) in zip(init_vals, sizes)]
 
     def add_arc(var_no, pre_spec, post):
+        """Add a DTG arc for var_no induced by transition pre_spec -> post.
+        pre_spec may be -1, in which case arcs from every value
+        other than post are added."""
         if pre_spec == -1:
             pre_values = set(range(sizes[var_no])).difference([post])
         else:
@@ -79,14 +116,47 @@ def build_dtgs(task):
         for pre in pre_values:
             dtgs[var_no].add_arc(pre, post)
 
-    def get_effective_pre(var_no, pre_spec, cond):
-        # Search pre_spec and cond for all conditions on var_no
-        # and return:
-        # -1    if there is no such condition
-        # val   if there is a condition of the form var_no=val
-        #       (possibly repeated multiple times)
-        # None  if there are contradictory conditions on var_no
-        result = pre_spec
+    def get_combined_preconditions(op):
+        """Return a dict(int -> int) with the combined prevail and
+        preconditions of op.
+
+        Return None if there are internal contradictions (e.g.
+        multiple preconditions with different values)."""
+        conditions = {}
+        def add_condition(var, val):
+            """Try adding condition var->val. Return False if this
+            leads to contradictory conditions."""
+            current_condition = conditions.get(var)
+            if current_condition is None:
+                conditions[var] = val
+            elif current_condition != val:
+                return False
+            return True
+        for var, val in op.prevail:
+            if not add_condition(var, val):
+                return None
+        for var, pre, post, cond in op.pre_post:
+            if pre is not None:
+                if not add_condition(var, pre):
+                    return None
+        return conditions
+
+    def get_effective_pre(var_no, conditions, effect_conditions):
+        """Return combined information on the conditions on `var_no`
+        from operator conditions and effect conditions.
+
+        - conditions: dict(int -> int) containing the combined
+          operator prevail and preconditions (see
+          get_combined_preconditions)
+        - effect_conditions: list(pair(int, int)) containing the
+          effect conditions
+
+        Result:
+        - -1   if there is no condition on var_no
+        - val  if there is a unique condition var_no=val
+        - None if there are contradictory conditions on var_no"""
+
+        result = conditions.get(var_no, -1)
         for cond_var_no, cond_val in cond:
             if cond_var_no == var_no:
                 if result == -1:
@@ -98,12 +168,11 @@ def build_dtgs(task):
         return result
 
     for op in task.operators:
-        for var_no, pre_spec, post, cond in op.pre_post:
-            # Check all conditions on var_no for this effect.
-            # These can come from the pre_spec or from cond.
-            effective_pre = get_effective_pre(var_no, pre_spec, cond)
+        conditions = get_combined_preconditions(op)
+        for var_no, _, post, cond in op.pre_post:
+            effective_pre = get_effective_pre(var_no, conditions, cond)
             if effective_pre is not None:
-                add_arc(var_no, pre_spec, post)
+                add_arc(var_no, effective_pre, post)
     for axiom in task.axioms:
         var_no, val = axiom.effect
         add_arc(var_no, -1, val)
@@ -127,6 +196,25 @@ class VarValueRenaming(object):
         self.new_sizes = []     # indexed by new var_no
         self.new_var_count = 0
         self.num_removed_values = 0
+
+    def dump(self):
+        old_var_count = len(self.new_var_nos)
+        print("variable count: %d => %d" % (
+            old_var_count, self.new_var_count))
+        print("number of removed values: %d" % self.num_removed_values)
+        print("variable conversions:")
+        for old_var_no, (new_var_no, new_values) in enumerate(
+                zip(self.new_var_nos, self.new_values)):
+            old_size = len(new_values)
+            new_size = self.new_sizes[new_var_no]
+            print("variable %d [size %d] => %d [size %d]" % (
+                old_var_no, old_size, new_var_no, new_size))
+            for old_value, new_value in enumerate(new_values):
+                if new_value is always_false:
+                    new_value = "always false"
+                elif new_value is always_true:
+                    new_value = "always true"
+                print("    value %d => %s" % (old_value, new_value))
 
     def register_variable(self, old_domain_size, init_value, new_domain):
         assert 1 <= len(new_domain) <= old_domain_size
@@ -156,6 +244,8 @@ class VarValueRenaming(object):
             self.new_var_count += 1
 
     def apply_to_task(self, task):
+        if DEBUG:
+            self.dump()
         self.apply_to_variables(task.variables)
         self.apply_to_mutexes(task.mutexes)
         self.apply_to_init(task.init)
@@ -171,9 +261,9 @@ class VarValueRenaming(object):
                 new_axiom_layers[new_no] = variables.axiom_layers[old_no]
         assert None not in new_axiom_layers
         variables.axiom_layers = new_axiom_layers
-        self._apply_to_value_names(variables.value_names)
+        self.apply_to_value_names(variables.value_names)
 
-    def _apply_to_value_names(self, value_names):
+    def apply_to_value_names(self, value_names):
         new_value_names = [[None] * size for size in self.new_sizes]
         for var_no, values in enumerate(value_names):
             for value, value_name in enumerate(values):
@@ -225,27 +315,34 @@ class VarValueRenaming(object):
         for op in operators:
             try:
                 self.apply_to_operator(op)
-                new_operators.append(op)
             except (Impossible, DoesNothing):
                 num_removed += 1
                 if DEBUG:
                     print("Removed operator: %s" % op.name)
+            else:
+                new_operators.append(op)
         print("%d operators removed" % num_removed)
         operators[:] = new_operators
 
     def apply_to_axioms(self, axioms):
         new_axioms = []
+        num_removed = 0
         for axiom in axioms:
             try:
                 self.apply_to_axiom(axiom)
-                new_axioms.append(axiom)
             except (Impossible, DoesNothing):
+                num_removed += 1
                 if DEBUG:
                     print("Removed axiom:")
                     axiom.dump()
+            else:
+                new_axioms.append(axiom)
+        print("%d axioms removed" % num_removed)
         axioms[:] = new_axioms
 
     def apply_to_operator(self, op):
+        op.dump()
+
         # The prevail translation may generate an Impossible exception,
         # which is propagated up.
         self.translate_pairs_in_place(op.prevail)
@@ -354,28 +451,32 @@ def build_renaming(dtgs):
 
 
 def filter_unreachable_propositions(sas_task):
-    # This procedure is a bit of an afterthought, and doesn't fit the
-    # overall architecture of the translator too well. We filter away
-    # unreachable propositions here, and then prune away variables
-    # with only one value left.
-    #
-    # Examples of things that are filtered away:
-    # - Constant propositions that are not detected in instantiate.py
-    #   because it only reasons at the predicate level, and some
-    #   predicates such as "at" in Depot is constant for some objects
-    #   (hoists), but not others (trucks).
-    #   Example: "at(hoist1, distributor0)" and the associated
-    #            variable in depots-01.
-    # - "none of those" values that are unreachable.
-    #   Example: at(truck1, ?x) = <none of those> in depots-01.
-    # - Certain values that are relaxed reachable but detected as
-    #   unreachable after SAS instantiation because the only operators
-    #   that set them have inconsistent preconditions.
-    #   Example: on(crate0, crate0) in depots-01.
+    """We remove unreachable propositions and then prune variables
+    with only one value.
+
+    Examples of things that are pruned:
+    - Constant propositions that are not detected in instantiate.py
+      because instantiate.py only reasons at the predicate level, and some
+      predicates such as "at" in Depot are constant for some objects
+      (hoists), but not others (trucks).
+
+      Example: "at(hoist1, distributor0)" and the associated variable
+      in depots-01.
+
+    - "none of those" values that are unreachable.
+
+      Example: at(truck1, ?x) = <none of those> in depots-01.
+
+    - Certain values that are relaxed reachable but detected as
+      unreachable after SAS instantiation because the only operators
+      that set them have inconsistent preconditions.
+
+      Example: on(crate0, crate0) in depots-01.
+    """
 
     dtgs = build_dtgs(sas_task)
     renaming = build_renaming(dtgs)
-    # apply_to_task may propagate up Impossible if the goal is simplified
-    # to False.
+    # apply_to_task may raise Impossible if the goal is detected as
+    # unreachable. We let the exception propagate to the caller.
     renaming.apply_to_task(sas_task)
     print("%d propositions removed" % renaming.num_removed_values)
