e09539bd02ea6f27d7483b54f4ca7f8333751116
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-07-01 19:43:25 +0200

    [issue546] refactor call.py

--- src/driver/call.py
+++ src/driver/call.py
@@ -10,7 +10,9 @@ import subprocess
 import sys
 
 
-def set_limit(kind, soft, hard):
+def _set_limit(kind, soft, hard=None):
+    if hard is None:
+        hard = soft
     try:
         resource.setrlimit(kind, (soft, hard))
     except (OSError, ValueError) as err:
@@ -19,33 +21,38 @@ def set_limit(kind, soft, hard):
               (kind, (soft, hard), err, resource.getrlimit(kind)), file=sys.stderr)
 
 
-def set_limits(timeout=None, memory=None):
-    if timeout is not None:
-        # Don't try to raise the hard limit.
-        _, external_hard_limit = resource.getrlimit(resource.RLIMIT_CPU)
-        if external_hard_limit == resource.RLIM_INFINITY:
-            external_hard_limit = float("inf")
-        # Soft limit reached --> SIGXCPU.
-        # Hard limit reached --> SIGKILL.
-        soft_limit = int(math.ceil(timeout))
-        hard_limit = min(soft_limit + 1, external_hard_limit)
-        print("timeout: %.2f -> (%d, %d)" % (timeout, soft_limit, hard_limit))
-        sys.stdout.flush()
-        set_limit(resource.RLIMIT_CPU, soft_limit, hard_limit)
+def _set_time_limit(timeout):
+    # TODO: Raise soft limit if timeout=None
+    if timeout is None:
+        return
+    # Don't try to raise the hard limit.
+    _, external_hard_limit = resource.getrlimit(resource.RLIMIT_CPU)
+    if external_hard_limit == resource.RLIM_INFINITY:
+        external_hard_limit = float("inf")
+    # Soft limit reached --> SIGXCPU.
+    # Hard limit reached --> SIGKILL.
+    soft_limit = int(math.ceil(timeout))
+    hard_limit = min(soft_limit + 1, external_hard_limit)
+    print("timeout %.2f -> (%d, %d)" % (timeout, soft_limit, hard_limit))
+    sys.stdout.flush()
+    _set_limit(resource.RLIMIT_CPU, soft_limit, hard_limit)
+
+
+def _set_memory_limit(memory):
     if memory is None:
-        set_limit(resource.RLIMIT_AS, -1, -1)
-    else:
-        # Memory in bytes.
-        set_limit(resource.RLIMIT_AS, memory, memory)
+        memory = resource.RLIM_INFINITY
+    # Memory in bytes.
+    _set_limit(resource.RLIMIT_AS, memory)
 
 
 def check_call(cmd, stdin=None, timeout=None, memory=None):
     sys.stdout.flush()
 
-    def set_limits_closure():
-        set_limits(timeout=timeout, memory=memory)
+    def set_limits():
+        _set_time_limit(timeout)
+        _set_memory_limit(memory)
 
-    kwargs = dict(preexec_fn=set_limits_closure)
+    kwargs = dict(preexec_fn=set_limits)
 
     if stdin:
         with open(stdin) as stdin_file:
