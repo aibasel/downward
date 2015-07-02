f1bec4f5c6d91b4300fb14ec1eefddfc1669ca3d
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-07-03 00:16:58 +0200

    [issue542] add admissible_potential_heuristics module

---
+++ src/search/potentials/admissible_potential_heuristics.h
@@ -0,0 +1,12 @@
+#ifndef POTENTIALS_ADMISSIBLE_POTENTIAL_HEURISTICS_H
+#define POTENTIALS_ADMISSIBLE_POTENTIAL_HEURISTICS_H
+
+namespace potentials {
+
+class PotentialOptimizer;
+
+void optimize_for_samples(PotentialOptimizer &optimizer, int num_samples);
+
+}
+
+#endif
