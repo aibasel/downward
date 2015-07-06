2ac1d9af8c328e8f2b87de1ff7b3c44320ac7d92
Florian Pommerening <florian.pommerening@unibas.ch>
2015-07-06 16:25:56 +0200

    [issue67] Replace toolchains by an option

--- src/search/cmake/modules/toolchain-32bit.cmake
+++
@@ -1,7 +0,0 @@
-if(UNIX)
-    set(CMAKE_SYSTEM_LIBRARY_PATH /lib32 /usr/lib32 /usr/local/lib32)
-    set_property(GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS OFF)
-
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32" CACHE STRING "c++ flags")
-    set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -m32" CACHE STRING "c flags")
-endif()
--- src/search/cmake/modules/toolchain-64bit.cmake
+++
@@ -1,6 +0,0 @@
-if(UNIX)
-    set_property(GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS ON)
-
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m64" CACHE STRING "c++ flags")
-    set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -m64" CACHE STRING "c flags")
-endif()
