ccaaaf20f8f41199289a5e0549a593c6f39518e4
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-07-06 15:23:42 +0200

    [issue542] move comment

--- src/search/potentials/potential_heuristics.cc
+++ src/search/potentials/potential_heuristics.cc
@@ -17,8 +17,6 @@ int PotentialHeuristics::compute_heuristic(const GlobalState &global_state) {
     const State state = convert_global_state(global_state);
     int value = 0;
     for (shared_ptr<PotentialFunction> function : functions) {
-        /* TODO: Can we somehow detect that one heuristic detected a dead end
-           and jump out of the loop? */
         value = max(value, function->get_value(state));
     }
     return value;
