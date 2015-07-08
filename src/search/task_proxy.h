2ad5cafc9058e3d182acbea77fe09d03e5d4f5ee
Florian Pommerening <florian.pommerening@unibas.ch>
2015-07-08 18:47:48 +0200

    [issue547] Add compare methods for VariableProxy

--- src/search/task_proxy.h
+++ src/search/task_proxy.h
@@ -236,6 +236,15 @@ public:
         : task(&task), id(id) {}
     ~VariableProxy() = default;
 
+    bool operator==(const VariableProxy &other) const {
+        assert(task == other.task);
+        return id == other.id;
+    }
+
+    bool operator!=(const VariableProxy &other) const {
+        return !(*this == other);
+    }
+
     int get_id() const {
         return id;
     }
