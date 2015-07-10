592f1a9033e502b37fcf9850a04d635bcd1ac5a4
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-07-09 19:46:41 -0700

    [issue542] only compute average operator costs once

--- src/search/sampling.h
+++ src/search/sampling.h
@@ -10,21 +10,20 @@
 #include <exception>
 #include <vector>
 
+
 struct SamplingTimeout : public std::exception {};
+
+double get_average_operator_cost(TaskProxy task_proxy);
 
 template<class Callback>
 std::vector<State> sample_states_with_random_walks(
     TaskProxy task_proxy,
     int num_samples,
     int init_h,
+    int average_operator_cost,
     const Callback &is_dead_end,
     const CountdownTimer &timer) {
     std::vector<State> samples;
-
-    double average_operator_cost = 0;
-    for (OperatorProxy op : task_proxy.get_operators())
-        average_operator_cost += op.get_cost();
-    average_operator_cost /= task_proxy.get_operators().size();
 
     const State &initial_state = task_proxy.get_initial_state();
 
