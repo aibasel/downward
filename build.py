b8f57af82d9f18c1fa8e8b10db3099ee20d9bfb8
Florian Pommerening <florian.pommerening@unibas.ch>
2015-07-22 18:27:24 +0200

    [issue67] update build script

---
+++ build.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import errno
+import os
+import subprocess
+import sys
+
+CONFIGS = {
+    "release32": ["-DCMAKE_BUILD_TYPE=Release"],
+    "debug32":   ["-DCMAKE_BUILD_TYPE=Debug"],
+    "release64": ["-DCMAKE_BUILD_TYPE=Release", "-DALLOW_64_BIT=True", "-DCMAKE_CXX_FLAGS='-m64'", "-DUSE_LP=FALSE"],
+    "debug64":   ["-DCMAKE_BUILD_TYPE=Debug",   "-DALLOW_64_BIT=True", "-DCMAKE_CXX_FLAGS='-m64'", "-DUSE_LP=FALSE"],
+}
+
+
+def get_project_root_path():
+    import __main__
+    return os.path.dirname(__main__.__file__)
+
+
+def get_builds_path():
+    return os.path.join(get_project_root_path(), "builds")
+
+
+def get_src_path():
+    return os.path.join(get_project_root_path(), "src")
+
+
+def get_build_path(config_name):
+    return os.path.join(get_builds_path(), config_name)
+
+
+def build(config_name, cmake_parameters, make_parameters):
+    build_path = get_build_path(config_name)
+    rel_src_path = os.path.relpath(get_src_path(), build_path)
+    try:
+        os.makedirs(build_path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(build_path):
+            pass
+        else:
+            raise
+    subprocess.check_call(["cmake"] + cmake_parameters + [rel_src_path],
+                          cwd=build_path)
+    subprocess.check_call(["make"] + make_parameters, cwd=build_path)
+
+def main():
+    config_names = set()
+    make_parameters = []
+    for arg in sys.argv[1:]:
+        if arg == "--all":
+            config_names |= set(CONFIGS.keys())
+        elif arg in CONFIGS:
+            config_names.add(arg)
+        else:
+            make_parameters.append(arg)
+    if not config_names:
+        config_names.add("release32")
+    for config_name in config_names:
+        build(config_name, CONFIGS[config_name], make_parameters)
+
+
+if __name__ == "__main__":
+    main()
