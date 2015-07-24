67de2b909312a961241b8a1d4aa2a8ea03d340b6
Malte Helmert <malte.helmert@unibas.ch>
2015-07-24 15:41:25 +0200

    [issue561] M&S: added HeuristicRepresentation class (not yet used)

---
+++ src/search/merge_and_shrink/heuristic_representation.cc
@@ -0,0 +1,91 @@
+#include "heuristic_representation.h"
+
+#include "transition_system.h" // TODO: Try to get rid of this.
+
+#include "../task_proxy.h"
+
+#include <numeric>
+
+using namespace std;
+
+
+HeuristicRepresentation::HeuristicRepresentation(int domain_size)
+    : domain_size(domain_size) {
+}
+
+HeuristicRepresentation::~HeuristicRepresentation() {
+}
+
+int HeuristicRepresentation::get_domain_size() const {
+    return domain_size;
+}
+
+
+HeuristicRepresentationLeaf::HeuristicRepresentationLeaf(
+    int domain_size, int var_id)
+    : HeuristicRepresentation(domain_size),
+      var_id(var_id),
+      lookup_table(domain_size) {
+    iota(lookup_table.begin(), lookup_table.end(), 0);
+}
+
+void HeuristicRepresentationLeaf::apply_abstraction_to_lookup_table(
+    const vector<int> &abstraction_mapping) {
+    cout << "leaf: applying abstraction to lookup table" << endl;
+
+    int new_domain_size = 0;
+    for (int &entry : lookup_table) {
+        if (entry != TransitionSystem::PRUNED_STATE) {
+            entry = abstraction_mapping[entry];
+            new_domain_size = max(new_domain_size, entry + 1);
+        }
+    }
+    domain_size = new_domain_size;
+}
+
+int HeuristicRepresentationLeaf::get_abstract_state(const State &state) const {
+    int value = state[var_id].get_value();
+    return lookup_table[value];
+}
+
+
+HeuristicRepresentationMerge::HeuristicRepresentationMerge(
+    HeuristicRepresentation *child1, HeuristicRepresentation *child2)
+    : HeuristicRepresentation(child1->get_domain_size() *
+                              child2->get_domain_size()),
+      children{child1, child2},
+      lookup_table(child1->get_domain_size(),
+                   vector<int>(child2->get_domain_size())) {
+    int counter = 0;
+    for (vector<int> &row : lookup_table) {
+        for (int &entry : row) {
+            entry = counter;
+            ++counter;
+        }
+    }
+}
+
+void HeuristicRepresentationMerge::apply_abstraction_to_lookup_table(
+    const vector<int> &abstraction_mapping) {
+    cout << "merge: applying abstraction to lookup table" << endl;
+    int new_domain_size = 0;
+    for (vector<int> &row : lookup_table) {
+        for (int &entry : row) {
+            if (entry != TransitionSystem::PRUNED_STATE) {
+                entry = abstraction_mapping[entry];
+                new_domain_size = max(new_domain_size, entry + 1);
+            }
+        }
+    }
+    domain_size = new_domain_size;
+}
+
+int HeuristicRepresentationMerge::get_abstract_state(
+    const State &state) const {
+    int state1 = children[0]->get_abstract_state(state);
+    int state2 = children[1]->get_abstract_state(state);
+    if (state1 == TransitionSystem::PRUNED_STATE ||
+        state2 == TransitionSystem::PRUNED_STATE)
+        return TransitionSystem::PRUNED_STATE;
+    return lookup_table[state1][state2];
+}
