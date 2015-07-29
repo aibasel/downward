0681f11543a4bf02d7d1bdcc7504c61924070b9c
Florian Pommerening <florian.pommerening@unibas.ch>
2015-07-29 12:08:19 +0200

    [issue67] Introduce plugin structure in build system

--- src/search/FastDownwardPlugins.cmake
+++ src/search/FastDownwardPlugins.cmake
@@ -8,62 +8,141 @@ set(PLANNER_SOURCES
 
         abstract_task.cc abstract_task.h
         axioms.cc axioms.h
-        causal_graph.cc causal_graph.h
-        combining_evaluator.cc combining_evaluator.h
         cost_adapted_task.cc cost_adapted_task.h
         countdown_timer.cc countdown_timer.h
         delegating_task.cc delegating_task.h
-        doc.h
+        doc.h # TODO: unused?
         domain_transition_graph.cc domain_transition_graph.h
-        eager_search.cc eager_search.h
-        enforced_hill_climbing_search.cc enforced_hill_climbing_search.h
-        equivalence_relation.cc equivalence_relation.h
         evaluation_context.cc evaluation_context.h
         evaluation_result.cc evaluation_result.h
-        exact_timer.cc exact_timer.h
-        g_evaluator.cc g_evaluator.h
         global_operator.cc global_operator.h
         globals.cc globals.h
         global_state.cc global_state.h
         heuristic_cache.cc heuristic_cache.h
         heuristic.cc heuristic.h
         int_packer.cc int_packer.h
-        ipc_max_heuristic.cc ipc_max_heuristic.h
-        iterated_search.cc iterated_search.h
-        lazy_search.cc lazy_search.h
-        lp_internals.cc lp_internals.h
-        lp_solver.cc lp_solver.h
-        max_evaluator.cc max_evaluator.h
         operator_cost.cc operator_cost.h
         option_parser.cc option_parser.h
         option_parser_util.cc option_parser_util.h
-        per_state_information.cc per_state_information.h
         plugin.h
-        pref_evaluator.cc pref_evaluator.h
-        priority_queue.h
-        relaxation_heuristic.cc relaxation_heuristic.h
         rng.cc rng.h
         root_task.cc root_task.h
         scalar_evaluator.cc scalar_evaluator.h
         search_engine.cc search_engine.h
-        search_node_info.cc search_node_info.h
-        search_progress.cc search_progress.h
         search_space.cc search_space.h
         search_statistics.cc search_statistics.h
-        segmented_vector.cc segmented_vector.h
         state_id.cc state_id.h
         state_registry.cc state_registry.h
         successor_generator.cc successor_generator.h
-        sum_evaluator.cc sum_evaluator.h
+        timer.cc timer.h
+        utilities.cc utilities.h
+        utilities_windows.h
+)
+
+## Details of the plugins
+#
+# For now, everything defaults to being enabled - it's up to the user to specify
+#    -DPLUGIN_FOO_ENABLED=FALSE
+# to disable a given plugin.
+#
+# Defining a new plugin:
+#    fast_downward_plugin(
+#        NAME <NAME>
+#        [ DISPLAY_NAME <DISPLAY_NAME> ]
+#        [ HELP <HELP> ]
+#        SOURCES
+#            <FILE 1> [ <FILE 2> ... ]
+#        [ DEPENDS <PLUGIN_NAME 1> [ <PLUGIN_NAME 2> ... ] ]
+#        [ DEACTIVATED ]
+#    )
+#
+# <DISPLAY_NAME> defaults to lower case <NAME> and is used to group
+#                files in IDEs and for messages.
+# <HELP> defaults to <DISPLAY_NAME> and is used to describe the cmake option.
+# DEPENDS lists plugins that will be automatically enabled if this plugin
+# is enabled. If the dependency was not enabled before, this will be logged.
+# DEACTIVATED sets the default value of the generated cmake option to false.
+
+fast_downward_plugin(
+    NAME MAYBE_CORE
+    HELP "Files that maybe should be in the core, even though it can be compiled without them"
+    SOURCES
+        equivalence_relation.cc equivalence_relation.h
+        exact_timer.cc exact_timer.h
+        per_state_information.cc per_state_information.h
+        priority_queue.h
+        search_node_info.cc search_node_info.h
+        search_progress.cc search_progress.h
+        segmented_vector.cc segmented_vector.h
+        tracer.cc tracer.h
+        utilities_hash.cc utilities_hash.h
+        variable_order_finder.cc variable_order_finder.h
+)
+
+fast_downward_plugin(
+    NAME CAUSAL_GRAPH
+    HELP "The causal graph"
+    SOURCES
+        causal_graph.cc causal_graph.h
+)
+
+fast_downward_plugin(
+    NAME TASK_INTERFACE
+    HELP "The task interface"
+    SOURCES
         task_proxy.cc task_proxy.h
         task_tools.cc task_tools.h
-        timer.cc timer.h
-        tracer.cc tracer.h
-        utilities.cc utilities.h
-        utilities_hash.cc utilities_hash.h
-        utilities_windows.h
-        variable_order_finder.cc variable_order_finder.h
+    DEPENDS CAUSAL_GRAPH
+)
+fast_downward_plugin(
+    NAME G_EVALUATOR
+    HELP "The g-evaluator"
+    SOURCES
+        g_evaluator.cc g_evaluator.h
+
+)
+
+fast_downward_plugin(
+    NAME COMBINING_EVALUATOR
+    HELP "The combining evaluator"
+    SOURCES
+        combining_evaluator.cc combining_evaluator.h
+)
+
+fast_downward_plugin(
+    NAME MAX_EVALUATOR
+    HELP "The max evaluator"
+    SOURCES
+        max_evaluator.cc max_evaluator.h
+    DEPENDS COMBINING_EVALUATOR
+)
+
+fast_downward_plugin(
+    NAME PREF_EVALUATOR
+    HELP "The pref evaluator"
+    SOURCES
+        pref_evaluator.cc pref_evaluator.h
+)
+
+fast_downward_plugin(
+    NAME WEIGHTED_EVALUATOR
+    HELP "The weighted evaluator"
+    SOURCES
         weighted_evaluator.cc weighted_evaluator.h
+)
+
+fast_downward_plugin(
+    NAME SUM_EVALUATOR
+    HELP "The sum evaluator"
+    SOURCES
+        sum_evaluator.cc sum_evaluator.h
+    DEPENDS COMBINING_EVALUATOR
+)
+
+fast_downward_plugin(
+    NAME OPEN_LISTS
+    HELP "open lists"
+    SOURCES
         open_lists/alternation_open_list.cc open_lists/alternation_open_list.h
         open_lists/bucket_open_list.cc open_lists/bucket_open_list.h
         open_lists/open_list.cc open_lists/open_list.h
@@ -72,71 +151,118 @@ set(PLANNER_SOURCES
         open_lists/tiebreaking_open_list.cc open_lists/tiebreaking_open_list.h
 )
 
+fast_downward_plugin(
+    NAME EAGER_SEARCH
+    HELP "Eager search algorithm"
+    SOURCES
+        eager_search.cc eager_search.h
+    DEPENDS MAYBE_CORE G_EVALUATOR SUM_EVALUATOR
+)
 
-## Details of the plugins
-#
-# For now, everything defaults to being enabled - it's up to the user to specify -DPLUGIN_FOO=FALSE to disable
-# a given plugin.
-# The recipe for defining a plugin should be fairly self-explanatory. The text in double-quotes is
-# a free-form natural language description, used in the CMake GUI to explain what the flag means.
+fast_downward_plugin(
+    NAME LAZY_SEARCH
+    HELP "Lazy search algorithm"
+    SOURCES
+        lazy_search.cc lazy_search.h
+    DEPENDS MAYBE_CORE G_EVALUATOR SUM_EVALUATOR WEIGHTED_EVALUATOR
+)
 
-option(PLUGIN_ADDITIVE_HEURISTIC "The additive heuristic" TRUE)
-if(PLUGIN_ADDITIVE_HEURISTIC)
-    list(APPEND PLANNER_SOURCES additive_heuristic)
-endif()
+fast_downward_plugin(
+    NAME EHC_SEARCH
+    HELP "Enforced hill-climbing search algorithm"
+    SOURCES
+        enforced_hill_climbing_search.cc enforced_hill_climbing_search.h
+    DEPENDS PREF_EVALUATOR
+)
 
+fast_downward_plugin(
+    NAME ITERATED_SEARCH
+    HELP "Iterated search algorithm"
+    SOURCES
+        iterated_search.cc iterated_search.h
+)
 
-option(PLUGIN_BLIND_SEARCH_HEURISTIC "The 'blind search' heuristic" TRUE)
-if(PLUGIN_BLIND_SEARCH_HEURISTIC)
-    list(APPEND PLANNER_SOURCES blind_search_heuristic)
-endif()
+fast_downward_plugin(
+    NAME LP_SOLVER
+    HELP "Interface to an LP solver"
+    SOURCES
+        lp_internals.cc lp_internals.h
+        lp_solver.cc lp_solver.h
+)
 
+fast_downward_plugin(
+    NAME RELAXATION_HEURISTIC
+    HELP "The base  class for relaxation heuristics"
+    SOURCES
+        relaxation_heuristic.cc relaxation_heuristic.h
+)
 
-option(PLUGIN_CEA_HEURISTIC "The context-enhanced additive heuristic" TRUE)
-if(PLUGIN_CEA_HEURISTIC)
-    list(APPEND PLANNER_SOURCES cea_heuristic)
-endif()
+fast_downward_plugin(
+    NAME IPC_MAX_HEURISTIC
+    HELP "The IPC max heuristic"
+    SOURCES
+        ipc_max_heuristic.cc ipc_max_heuristic.h
+)
 
+fast_downward_plugin(
+    NAME ADDITIVE_HEURISTIC
+    HELP "The additive heuristic"
+    SOURCES additive_heuristic.cc additive_heuristic.h
+)
 
-option(PLUGIN_CG_HEURISTIC "The causal graph heuristic" TRUE)
-if(PLUGIN_CG_HEURISTIC)
-    list(APPEND PLANNER_SOURCES cg_heuristic cg_cache)
-endif()
+fast_downward_plugin(
+    NAME BLIND_SEARCH_HEURISTIC
+    HELP "The 'blind search' heuristic"
+    SOURCES blind_search_heuristic.cc blind_search_heuristic.h
+)
 
+fast_downward_plugin(
+    NAME CEA_HEURISTIC
+    HELP "The context-enhanced additive heuristic"
+    SOURCES cea_heuristic.cc cea_heuristic.h
+)
 
-option(PLUGIN_FF_HEURISTIC "The FF heuristic (an implementation of the RPG heuristic)" TRUE)
-if(PLUGIN_FF_HEURISTIC)
-    list(APPEND PLANNER_SOURCES ff_heuristic)
-endif()
+fast_downward_plugin(
+    NAME CG_HEURISTIC
+    HELP "The causal graph heuristic"
+    SOURCES cg_heuristic.cc cg_heuristic.h
+            cg_cache.cc cg_cache.h
+)
 
+fast_downward_plugin(
+    NAME FF_HEURISTIC
+    HELP "The FF heuristic (an implementation of the RPG heuristic)"
+    SOURCES ff_heuristic.cc ff_heuristic.h
+)
 
-option(PLUGIN_GOAL_COUNT_HEURISTIC "The goal-counting heuristic" TRUE)
-if(PLUGIN_GOAL_COUNT_HEURISTIC)
-    list(APPEND PLANNER_SOURCES goal_count_heuristic)
-endif()
+fast_downward_plugin(
+    NAME GOAL_COUNT_HEURISTIC
+    HELP "The goal-counting heuristic"
+    SOURCES goal_count_heuristic.cc goal_count_heuristic.h
+)
 
+fast_downward_plugin(
+    NAME HM_HEURISTIC
+    HELP "The h^m heuristic"
+    SOURCES hm_heuristic.cc hm_heuristic.h
+)
 
-option(PLUGIN_HM_HEURISTIC "The h^m heuristic" TRUE)
-if(PLUGIN_HM_HEURISTIC)
-    list(APPEND PLANNER_SOURCES hm_heuristic)
-endif()
+fast_downward_plugin(
+    NAME LM_CUT_HEURISTIC
+    HELP "The LM-cut heuristic"
+    SOURCES lm_cut_heuristic.cc lm_cut_heuristic.h
+)
 
+fast_downward_plugin(
+    NAME MAX_HEURISTIC
+    HELP "The Max heuristic"
+    SOURCES max_heuristic.cc max_heuristic.h
+)
 
-option(PLUGIN_LM_CUT_HEURISTIC "The LM-cut heuristic" TRUE)
-if(PLUGIN_LM_CUT_HEURISTIC)
-    list(APPEND PLANNER_SOURCES lm_cut_heuristic)
-endif()
-
-
-option(PLUGIN_MAX_HEURISTIC "The Max heuristic" TRUE)
-if(PLUGIN_MAX_HEURISTIC)
-    list(APPEND PLANNER_SOURCES max_heuristic)
-endif()
-
-
-option(PLUGIN_MAS_HEURISTIC "The Merge-and-Shrink heuristic" TRUE)
-if(PLUGIN_MAS_HEURISTIC)
-    list(APPEND PLANNER_SOURCES
+fast_downward_plugin(
+    NAME MAS_HEURISTIC
+    HELP "The Merge-and-Shrink heuristic"
+    SOURCES
         merge_and_shrink/distances.cc merge_and_shrink/distances.h
         merge_and_shrink/factored_transition_system.cc merge_and_shrink/factored_transition_system.h
         merge_and_shrink/fts_factory.cc merge_and_shrink/fts_factory.h
@@ -152,61 +278,60 @@ if(PLUGIN_MAS_HEURISTIC)
         merge_and_shrink/shrink_random.cc merge_and_shrink/shrink_random.h
         merge_and_shrink/shrink_strategy.cc merge_and_shrink/shrink_strategy.h
         merge_and_shrink/transition_system.cc merge_and_shrink/transition_system.h
-    )
-endif()
+)
 
+fast_downward_plugin(
+    NAME LANDMARKS
+    HELP "Plugin containing the code to reason with landmarks"
+    SOURCES
+        landmarks/exploration.cc landmarks/exploration.h
+        landmarks/h_m_landmarks.cc landmarks/h_m_landmarks.h
+        landmarks/lama_ff_synergy.cc landmarks/lama_ff_synergy.h
+        landmarks/landmark_cost_assignment.cc landmarks/landmark_cost_assignment.h
+        landmarks/landmark_count_heuristic.cc landmarks/landmark_count_heuristic.h
+        landmarks/landmark_factory.cc landmarks/landmark_factory.h
+        landmarks/landmark_factory_rpg_exhaust.cc landmarks/landmark_factory_rpg_exhaust.h
+        landmarks/landmark_factory_rpg_sasp.cc landmarks/landmark_factory_rpg_sasp.h
+        landmarks/landmark_factory_zhu_givan.cc landmarks/landmark_factory_zhu_givan.h
+        landmarks/landmark_graph.cc landmarks/landmark_graph.h
+        landmarks/landmark_graph_merged.cc landmarks/landmark_graph_merged.h
+        landmarks/landmark_status_manager.cc landmarks/landmark_status_manager.h
+        landmarks/landmark_types.h
+        landmarks/util.cc landmarks/util.h
+)
 
-option(PLUGIN_LANDMARKS "Plugin containing the code to reason with landmarks" TRUE)
-if(PLUGIN_LANDMARKS)
-    list(APPEND PLANNER_SOURCES
-        landmarks/exploration
-        landmarks/h_m_landmarks
-        landmarks/lama_ff_synergy
-        landmarks/landmark_cost_assignment
-        landmarks/landmark_count_heuristic
-        landmarks/landmark_factory
-        landmarks/landmark_factory_rpg_exhaust
-        landmarks/landmark_factory_rpg_sasp
-        landmarks/landmark_factory_zhu_givan
-        landmarks/landmark_graph
-        landmarks/landmark_graph_merged
-        landmarks/landmark_status_manager
-        landmarks/landmark_types
-        landmarks/util
-    )
-endif()
+fast_downward_plugin(
+    NAME LEARNING
+    HELP "Plugin containing the code to reason with learning"
+    SOURCES
+        learning/AODE.cc learning/AODE.h
+        learning/classifier.cc learning/classifier.h
+        learning/composite_feature_extractor.cc learning/composite_feature_extractor.h
+        learning/feature_extractor.cc learning/feature_extractor.h
+        learning/maximum_heuristic.cc learning/maximum_heuristic.h
+        learning/naive_bayes_classifier.cc learning/naive_bayes_classifier.h
+        learning/PDB_state_space_sample.cc learning/PDB_state_space_sample.h
+        learning/probe_state_space_sample.cc learning/probe_state_space_sample.h
+        learning/selective_max_heuristic.cc learning/selective_max_heuristic.h
+        learning/state_space_sample.cc learning/state_space_sample.h
+        learning/state_vars_feature_extractor.cc learning/state_vars_feature_extractor.h
+    DEACTIVATED
+)
 
+fast_downward_plugin(
+    NAME PDBS
+    HELP "Plugin containing the code for PDBs"
+    SOURCES
+        pdbs/canonical_pdbs_heuristic.cc pdbs/canonical_pdbs_heuristic.h
+        pdbs/dominance_pruner.cc pdbs/dominance_pruner.h
+        pdbs/match_tree.cc pdbs/match_tree.h
+        pdbs/max_cliques.cc pdbs/max_cliques.h
+        pdbs/pattern_database.cc pdbs/pattern_database.h
+        pdbs/pattern_generation_edelkamp.cc pdbs/pattern_generation_edelkamp.h
+        pdbs/pattern_generation_haslum.cc pdbs/pattern_generation_haslum.h
+        pdbs/pdb_heuristic.cc pdbs/pdb_heuristic.h
+        pdbs/util.cc pdbs/util.h
+        pdbs/zero_one_pdbs_heuristic.cc pdbs/zero_one_pdbs_heuristic.h
+)
 
-option(PLUGIN_LEARNING "Plugin containing the code to reason with learning" FALSE)
-if(PLUGIN_LEARNING)
-    list(APPEND PLANNER_SOURCES
-        learning/AODE
-        learning/classifier
-        learning/composite_feature_extractor
-        learning/feature_extractor
-        learning/maximum_heuristic
-        learning/naive_bayes_classifier
-        learning/PDB_state_space_sample
-        learning/probe_state_space_sample
-        learning/selective_max_heuristic
-        learning/state_space_sample
-        learning/state_vars_feature_extractor
-    )
-endif()
-
-
-option(PLUGIN_PDBS "Plugin containing the code for PDBs" TRUE)
-if(PLUGIN_PDBS)
-    list(APPEND PLANNER_SOURCES
-        pdbs/canonical_pdbs_heuristic
-        pdbs/dominance_pruner
-        pdbs/match_tree
-        pdbs/max_cliques
-        pdbs/pattern_database
-        pdbs/pattern_generation_edelkamp
-        pdbs/pattern_generation_haslum
-        pdbs/pdb_heuristic
-        pdbs/util
-        pdbs/zero_one_pdbs_heuristic
-    )
-endif()
+fast_downward_add_plugin_sources(PLANNER_SOURCES)
