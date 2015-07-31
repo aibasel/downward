cc15557fba3c85496d9fe30f3367ea49377505d4
Florian Pommerening <florian.pommerening@unibas.ch>
2015-07-31 19:00:29 +0200

    [issue67] Automatically add header files to the list of sources

--- src/search/FastDownwardPlugins.cmake
+++ src/search/FastDownwardPlugins.cmake
@@ -1,62 +1,68 @@
 ## == Details of the core source files ==
 ##
 ## If you're adding another file to the codebase which *isn't* a plugin, add
-## it to the following list
+## it to the following list. We assume that every *.cc file has a corresponding
+## *.h file and add headers to the project automatically.
 
 set(PLANNER_SOURCES
         planner.cc
-
-        abstract_task.cc abstract_task.h
-        axioms.cc axioms.h
-        causal_graph.cc causal_graph.h
-        cost_adapted_task.cc cost_adapted_task.h
-        countdown_timer.cc countdown_timer.h
-        delegating_task.cc delegating_task.h
-        domain_transition_graph.cc domain_transition_graph.h
-        equivalence_relation.cc equivalence_relation.h
-        evaluation_context.cc evaluation_context.h
-        evaluation_result.cc evaluation_result.h
-        exact_timer.cc exact_timer.h
-        global_operator.cc global_operator.h
-        globals.cc globals.h
-        global_state.cc global_state.h
-        heuristic_cache.cc heuristic_cache.h
-        heuristic.cc heuristic.h
-        int_packer.cc int_packer.h
-        operator_cost.cc operator_cost.h
-        option_parser.cc option_parser.h
-        option_parser_util.cc option_parser_util.h
-        per_state_information.cc per_state_information.h
-        plugin.cc plugin.h
-        priority_queue.cc priority_queue.h
-        rng.cc rng.h
-        root_task.cc root_task.h
-        scalar_evaluator.cc scalar_evaluator.h
-        search_engine.cc search_engine.h
-        search_node_info.cc search_node_info.h
-        search_progress.cc search_progress.h
-        search_space.cc search_space.h
-        search_statistics.cc search_statistics.h
-        segmented_vector.cc segmented_vector.h
-        state_id.cc state_id.h
-        state_registry.cc state_registry.h
-        successor_generator.cc successor_generator.h
-        task_proxy.cc task_proxy.h
-        task_tools.cc task_tools.h
-        timer.cc timer.h
-        tracer.cc tracer.h
-        utilities.cc utilities.h
-        utilities_hash.cc utilities_hash.h
-        utilities_windows.cc utilities_windows.h
-        variable_order_finder.cc variable_order_finder.h
-
-        open_lists/alternation_open_list.cc open_lists/alternation_open_list.h
-        open_lists/bucket_open_list.cc open_lists/bucket_open_list.h
-        open_lists/open_list.cc open_lists/open_list.h
-        open_lists/pareto_open_list.cc open_lists/pareto_open_list.h
-        open_lists/standard_scalar_open_list.cc open_lists/standard_scalar_open_list.h
-        open_lists/tiebreaking_open_list.cc open_lists/tiebreaking_open_list.h
 )
+
+set(CORE_SOURCES
+        abstract_task.cc
+        axioms.cc
+        causal_graph.cc
+        cost_adapted_task.cc
+        countdown_timer.cc
+        delegating_task.cc
+        domain_transition_graph.cc
+        equivalence_relation.cc
+        evaluation_context.cc
+        evaluation_result.cc
+        exact_timer.cc
+        global_operator.cc
+        globals.cc
+        global_state.cc
+        heuristic_cache.cc
+        heuristic.cc
+        int_packer.cc
+        operator_cost.cc
+        option_parser.cc
+        option_parser_util.cc
+        per_state_information.cc
+        plugin.cc
+        priority_queue.cc
+        rng.cc
+        root_task.cc
+        scalar_evaluator.cc
+        search_engine.cc
+        search_node_info.cc
+        search_progress.cc
+        search_space.cc
+        search_statistics.cc
+        segmented_vector.cc
+        state_id.cc
+        state_registry.cc
+        successor_generator.cc
+        task_proxy.cc
+        task_tools.cc
+        timer.cc
+        tracer.cc
+        utilities.cc
+        utilities_hash.cc
+        utilities_windows.cc
+        variable_order_finder.cc
+
+        open_lists/alternation_open_list.cc
+        open_lists/bucket_open_list.cc
+        open_lists/open_list.cc
+        open_lists/pareto_open_list.cc
+        open_lists/standard_scalar_open_list.cc
+        open_lists/tiebreaking_open_list.cc
+)
+
+fast_downward_add_headers_to_sources_list(CORE_SOURCES)
+list(APPEND PLANNER_SOURCES ${CORE_SOURCES})
 
 ## Details of the plugins
 #
@@ -86,21 +92,21 @@ fast_downward_plugin(
     NAME G_EVALUATOR
     HELP "The g-evaluator"
     SOURCES
-        g_evaluator.cc g_evaluator.h
+        g_evaluator.cc
 )
 
 fast_downward_plugin(
     NAME COMBINING_EVALUATOR
     HELP "The combining evaluator"
     SOURCES
-        combining_evaluator.cc combining_evaluator.h
+        combining_evaluator.cc
 )
 
 fast_downward_plugin(
     NAME MAX_EVALUATOR
     HELP "The max evaluator"
     SOURCES
-        max_evaluator.cc max_evaluator.h
+        max_evaluator.cc
     DEPENDS COMBINING_EVALUATOR
 )
 
@@ -108,21 +114,21 @@ fast_downward_plugin(
     NAME PREF_EVALUATOR
     HELP "The pref evaluator"
     SOURCES
-        pref_evaluator.cc pref_evaluator.h
+        pref_evaluator.cc
 )
 
 fast_downward_plugin(
     NAME WEIGHTED_EVALUATOR
     HELP "The weighted evaluator"
     SOURCES
-        weighted_evaluator.cc weighted_evaluator.h
+        weighted_evaluator.cc
 )
 
 fast_downward_plugin(
     NAME SUM_EVALUATOR
     HELP "The sum evaluator"
     SOURCES
-        sum_evaluator.cc sum_evaluator.h
+        sum_evaluator.cc
     DEPENDS COMBINING_EVALUATOR
 )
 
@@ -130,7 +136,7 @@ fast_downward_plugin(
     NAME EAGER_SEARCH
     HELP "Eager search algorithm"
     SOURCES
-        eager_search.cc eager_search.h
+        eager_search.cc
     DEPENDS G_EVALUATOR SUM_EVALUATOR
 )
 
@@ -138,7 +144,7 @@ fast_downward_plugin(
     NAME LAZY_SEARCH
     HELP "Lazy search algorithm"
     SOURCES
-        lazy_search.cc lazy_search.h
+        lazy_search.cc
     DEPENDS G_EVALUATOR SUM_EVALUATOR WEIGHTED_EVALUATOR
 )
 
@@ -146,7 +152,7 @@ fast_downward_plugin(
     NAME EHC_SEARCH
     HELP "Enforced hill-climbing search algorithm"
     SOURCES
-        enforced_hill_climbing_search.cc enforced_hill_climbing_search.h
+        enforced_hill_climbing_search.cc
     DEPENDS PREF_EVALUATOR G_EVALUATOR
 )
 
@@ -154,86 +160,86 @@ fast_downward_plugin(
     NAME ITERATED_SEARCH
     HELP "Iterated search algorithm"
     SOURCES
-        iterated_search.cc iterated_search.h
+        iterated_search.cc
 )
 
 fast_downward_plugin(
     NAME LP_SOLVER
     HELP "Interface to an LP solver"
     SOURCES
-        lp_internals.cc lp_internals.h
-        lp_solver.cc lp_solver.h
+        lp_internals.cc
+        lp_solver.cc
 )
 
 fast_downward_plugin(
     NAME RELAXATION_HEURISTIC
     HELP "The base class for relaxation heuristics"
     SOURCES
-        relaxation_heuristic.cc relaxation_heuristic.h
+        relaxation_heuristic.cc
 )
 
 fast_downward_plugin(
     NAME IPC_MAX_HEURISTIC
     HELP "The IPC max heuristic"
     SOURCES
-        ipc_max_heuristic.cc ipc_max_heuristic.h
+        ipc_max_heuristic.cc
 )
 
 fast_downward_plugin(
     NAME ADDITIVE_HEURISTIC
     HELP "The additive heuristic"
-    SOURCES additive_heuristic.cc additive_heuristic.h
+    SOURCES additive_heuristic.cc
     DEPENDS RELAXATION_HEURISTIC
 )
 
 fast_downward_plugin(
     NAME BLIND_SEARCH_HEURISTIC
     HELP "The 'blind search' heuristic"
-    SOURCES blind_search_heuristic.cc blind_search_heuristic.h
+    SOURCES blind_search_heuristic.cc
 )
 
 fast_downward_plugin(
     NAME CEA_HEURISTIC
     HELP "The context-enhanced additive heuristic"
-    SOURCES cea_heuristic.cc cea_heuristic.h
+    SOURCES cea_heuristic.cc
 )
 
 fast_downward_plugin(
     NAME CG_HEURISTIC
     HELP "The causal graph heuristic"
-    SOURCES cg_heuristic.cc cg_heuristic.h
-            cg_cache.cc cg_cache.h
+    SOURCES cg_heuristic.cc
+            cg_cache.cc
 )
 
 fast_downward_plugin(
     NAME FF_HEURISTIC
     HELP "The FF heuristic (an implementation of the RPG heuristic)"
-    SOURCES ff_heuristic.cc ff_heuristic.h
+    SOURCES ff_heuristic.cc
     DEPENDS ADDITIVE_HEURISTIC
 )
 
 fast_downward_plugin(
     NAME GOAL_COUNT_HEURISTIC
     HELP "The goal-counting heuristic"
-    SOURCES goal_count_heuristic.cc goal_count_heuristic.h
+    SOURCES goal_count_heuristic.cc
 )
 
 fast_downward_plugin(
     NAME HM_HEURISTIC
     HELP "The h^m heuristic"
-    SOURCES hm_heuristic.cc hm_heuristic.h
+    SOURCES hm_heuristic.cc
 )
 
 fast_downward_plugin(
     NAME LM_CUT_HEURISTIC
     HELP "The LM-cut heuristic"
-    SOURCES lm_cut_heuristic.cc lm_cut_heuristic.h
+    SOURCES lm_cut_heuristic.cc
 )
 
 fast_downward_plugin(
     NAME MAX_HEURISTIC
     HELP "The Max heuristic"
-    SOURCES max_heuristic.cc max_heuristic.h
+    SOURCES max_heuristic.cc
     DEPENDS RELAXATION_HEURISTIC
 )
 
@@ -241,40 +247,40 @@ fast_downward_plugin(
     NAME MAS_HEURISTIC
     HELP "The Merge-and-Shrink heuristic"
     SOURCES
-        merge_and_shrink/distances.cc merge_and_shrink/distances.h
-        merge_and_shrink/factored_transition_system.cc merge_and_shrink/factored_transition_system.h
-        merge_and_shrink/fts_factory.cc merge_and_shrink/fts_factory.h
-        merge_and_shrink/heuristic_representation.cc merge_and_shrink/heuristic_representation.h
-        merge_and_shrink/labels.cc merge_and_shrink/labels.h
-        merge_and_shrink/merge_and_shrink_heuristic.cc merge_and_shrink/merge_and_shrink_heuristic.h
-        merge_and_shrink/merge_dfp.cc merge_and_shrink/merge_dfp.h
-        merge_and_shrink/merge_linear.cc merge_and_shrink/merge_linear.h
-        merge_and_shrink/merge_strategy.cc merge_and_shrink/merge_strategy.h
-        merge_and_shrink/shrink_bisimulation.cc merge_and_shrink/shrink_bisimulation.h
-        merge_and_shrink/shrink_bucket_based.cc merge_and_shrink/shrink_bucket_based.h
-        merge_and_shrink/shrink_fh.cc merge_and_shrink/shrink_fh.h
-        merge_and_shrink/shrink_random.cc merge_and_shrink/shrink_random.h
-        merge_and_shrink/shrink_strategy.cc merge_and_shrink/shrink_strategy.h
-        merge_and_shrink/transition_system.cc merge_and_shrink/transition_system.h
+        merge_and_shrink/distances.cc
+        merge_and_shrink/factored_transition_system.cc
+        merge_and_shrink/fts_factory.cc
+        merge_and_shrink/heuristic_representation.cc
+        merge_and_shrink/labels.cc
+        merge_and_shrink/merge_and_shrink_heuristic.cc
+        merge_and_shrink/merge_dfp.cc
+        merge_and_shrink/merge_linear.cc
+        merge_and_shrink/merge_strategy.cc
+        merge_and_shrink/shrink_bisimulation.cc
+        merge_and_shrink/shrink_bucket_based.cc
+        merge_and_shrink/shrink_fh.cc
+        merge_and_shrink/shrink_random.cc
+        merge_and_shrink/shrink_strategy.cc
+        merge_and_shrink/transition_system.cc
 )
 
 fast_downward_plugin(
     NAME LANDMARKS
     HELP "Plugin containing the code to reason with landmarks"
     SOURCES
-        landmarks/exploration.cc landmarks/exploration.h
-        landmarks/h_m_landmarks.cc landmarks/h_m_landmarks.h
-        landmarks/lama_ff_synergy.cc landmarks/lama_ff_synergy.h
-        landmarks/landmark_cost_assignment.cc landmarks/landmark_cost_assignment.h
-        landmarks/landmark_count_heuristic.cc landmarks/landmark_count_heuristic.h
-        landmarks/landmark_factory.cc landmarks/landmark_factory.h
-        landmarks/landmark_factory_rpg_exhaust.cc landmarks/landmark_factory_rpg_exhaust.h
-        landmarks/landmark_factory_rpg_sasp.cc landmarks/landmark_factory_rpg_sasp.h
-        landmarks/landmark_factory_zhu_givan.cc landmarks/landmark_factory_zhu_givan.h
-        landmarks/landmark_graph.cc landmarks/landmark_graph.h
-        landmarks/landmark_graph_merged.cc landmarks/landmark_graph_merged.h
-        landmarks/landmark_status_manager.cc landmarks/landmark_status_manager.h
-        landmarks/util.cc landmarks/util.h
+        landmarks/exploration.cc
+        landmarks/h_m_landmarks.cc
+        landmarks/lama_ff_synergy.cc
+        landmarks/landmark_cost_assignment.cc
+        landmarks/landmark_count_heuristic.cc
+        landmarks/landmark_factory.cc
+        landmarks/landmark_factory_rpg_exhaust.cc
+        landmarks/landmark_factory_rpg_sasp.cc
+        landmarks/landmark_factory_zhu_givan.cc
+        landmarks/landmark_graph.cc
+        landmarks/landmark_graph_merged.cc
+        landmarks/landmark_status_manager.cc
+        landmarks/util.cc
     DEPENDS LP_SOLVER
 )
 
@@ -282,17 +288,17 @@ fast_downward_plugin(
     NAME LEARNING
     HELP "Plugin containing the code to reason with learning"
     SOURCES
-        learning/AODE.cc learning/AODE.h
-        learning/classifier.cc learning/classifier.h
-        learning/composite_feature_extractor.cc learning/composite_feature_extractor.h
-        learning/feature_extractor.cc learning/feature_extractor.h
-        learning/maximum_heuristic.cc learning/maximum_heuristic.h
-        learning/naive_bayes_classifier.cc learning/naive_bayes_classifier.h
-        learning/PDB_state_space_sample.cc learning/PDB_state_space_sample.h
-        learning/probe_state_space_sample.cc learning/probe_state_space_sample.h
-        learning/selective_max_heuristic.cc learning/selective_max_heuristic.h
-        learning/state_space_sample.cc learning/state_space_sample.h
-        learning/state_vars_feature_extractor.cc learning/state_vars_feature_extractor.h
+        learning/AODE.cc
+        learning/classifier.cc
+        learning/composite_feature_extractor.cc
+        learning/feature_extractor.cc
+        learning/maximum_heuristic.cc
+        learning/naive_bayes_classifier.cc
+        learning/PDB_state_space_sample.cc
+        learning/probe_state_space_sample.cc
+        learning/selective_max_heuristic.cc
+        learning/state_space_sample.cc
+        learning/state_vars_feature_extractor.cc
     DEACTIVATED
 )
 
@@ -300,16 +306,18 @@ fast_downward_plugin(
     NAME PDBS
     HELP "Plugin containing the code for PDBs"
     SOURCES
-        pdbs/canonical_pdbs_heuristic.cc pdbs/canonical_pdbs_heuristic.h
-        pdbs/dominance_pruner.cc pdbs/dominance_pruner.h
-        pdbs/match_tree.cc pdbs/match_tree.h
-        pdbs/max_cliques.cc pdbs/max_cliques.h
-        pdbs/pattern_database.cc pdbs/pattern_database.h
-        pdbs/pattern_generation_edelkamp.cc pdbs/pattern_generation_edelkamp.h
-        pdbs/pattern_generation_haslum.cc pdbs/pattern_generation_haslum.h
-        pdbs/pdb_heuristic.cc pdbs/pdb_heuristic.h
-        pdbs/util.cc pdbs/util.h
-        pdbs/zero_one_pdbs_heuristic.cc pdbs/zero_one_pdbs_heuristic.h
+        pdbs/canonical_pdbs_heuristic.cc
+        pdbs/dominance_pruner.cc
+        pdbs/match_tree.cc
+        pdbs/max_cliques.cc
+        pdbs/pattern_database.cc
+        pdbs/pattern_generation_edelkamp.cc
+        pdbs/pattern_generation_haslum.cc
+        pdbs/pdb_heuristic.cc
+        pdbs/util.cc
+        pdbs/zero_one_pdbs_heuristic.cc
 )
 
 fast_downward_add_plugin_sources(PLANNER_SOURCES)
+
+message(${PLANNER_SOURCES})
