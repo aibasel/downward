b797a0b920664495f9686f2b275ea3f586700d5c
Malte Helmert <malte.helmert@unibas.ch>
2015-08-04 14:52:46 +0200

    [issue481] Made alternation open list a plugin. Temporarily disabled lazy search. Temporarily disabled event handling in main function. Some smallish cleanup.

--- src/search/open_lists/open_list_factory.cc
+++ src/search/open_lists/open_list_factory.cc
@@ -1,1 +1,13 @@
 #include "open_list_factory.h"
+
+#include "open_list.h"
+
+#include "../state_id.h"
+
+using namespace std;
+
+
+template<>
+unique_ptr<OpenList<StateID> > OpenListFactory::create_open_list() {
+    return create_state_open_list();
+}
--- src/search/open_lists/open_list_factory.h
+++ src/search/open_lists/open_list_factory.h
@@ -18,6 +18,15 @@ public:
     OpenListFactory(const OpenListFactory &) = delete;
 
     virtual std::unique_ptr<OpenList<StateID> > create_state_open_list() = 0;
+
+    /*
+      The following template receives manual specializations (in the
+      cc file) for the open list types we want to support. It is
+      intended for templatized callers, e.g. the constructor of
+      AlternationOpenList.
+    */
+    template<typename T>
+    std::unique_ptr<OpenList<T> > create_open_list();
 };
 
 #endif
