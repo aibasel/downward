162d4a87ea1d993249a3242a189aa8f01a5e286c
Malte Helmert <malte.helmert@unibas.ch>
2015-08-04 22:59:16 +0200

    [issue481] search_common: make a local function global and add documentation

--- src/search/search_common.h
+++ src/search/search_common.h
@@ -2,22 +2,51 @@
 #define SEARCH_COMMON_H
 
 /*
-  This module contains some common functionality used by eager and
-  lazy search.
+  This module contains functions for createn open list factories used
+  by the search engines.
+
+  TODO: Think about where this code should ideally live. One possible
+  ordering principle: avoid unnecessary plug-in dependencies.
+
+  This code currently depends on multiple different open list
+  implementations, so it would be good if it would not be a dependency
+  of search engines that don't need all these open lists. Under this
+  maxim, EHC should not depend on this file. A logical solution might
+  be to move every creation function that only uses one particular
+  open list type into the header for this open list type, and leave
+  this file for cases that require more complex set-up and are common
+  to eager and lazy search.
 */
 
 #include <memory>
 
 class OpenListFactory;
 class Options;
+class ScalarEvaluator;
 
 
 /*
-  Open list factory used internally by eager_greedy and lazy_greedy.
-  This is not a user-visible plugin, but rather used internally by the
-  eager_greedy and lazy_greedy shortcuts to create their open lists.
+  Create a standard scalar open list factory with the given "eval" and
+  "pref_only" options.
+*/
 
-  TODO: Say more about the interface and what this does.
+extern std::shared_ptr<OpenListFactory> create_standard_scalar_open_list_factory(
+    ScalarEvaluator *eval, bool pref_only);
+
+/*
+  Create open list factory for the eager_greedy or lazy_greedy plugins.
+
+  Uses "evals", "preferred" and "boost" from the passed-in Options
+  object to construct an open list factory of the appropriate type.
+
+  This is usually an alternation open list with:
+  - one sublist for each heuristic, considering all successors
+  - one sublist for each heuristic, considering only preferred successors
+
+  However, the preferred-only open lists are omitted if no preferred
+  operator heuristics are used, and if there would only be one sublist
+  for the alternation open list, then that sublist is returned
+  directly.
 */
 
 extern std::shared_ptr<OpenListFactory> create_greedy_open_list_factory(
