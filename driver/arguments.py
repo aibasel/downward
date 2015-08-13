e025600a8c747718ac7ad2face4c15f2da0373ab
Florian Pommerening <florian.pommerening@unibas.ch>
2015-08-13 10:12:45 +0200

    [issue67] Document --build argument of driver

--- driver/arguments.py
+++ driver/arguments.py
@@ -305,7 +305,11 @@ def parse_args():
         help="run a config with an alias (e.g. seq-sat-lama-2011)")
     driver_other.add_argument(
         "--build", default=None,
-        help="use specific build like release32 (default), debug32, release64, debug32, or a custom build")
+        help="use build name like release32 (default), debug32, release64, debug32, "
+             "a custom build name, or a path to the planner binaries. "
+             "The driver first looks for the planner binaries in the path 'BUILD'. "
+             "If this path does not exist, it tries the subdirectory 'builds/BUILD/bin' "
+             "of the repository's root directory, where the build script creates them by default.")
     driver_other.add_argument(
         "--debug", action="store_true",
         help="alias for --build=debug32")
