1cedbc60467e37513f27e494a786464ca02e91d5
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-10-09 12:03:15 +0200

    [main] fix extension

--- src/search/DownwardFiles.cmake
+++ src/search/DownwardFiles.cmake
@@ -54,7 +54,7 @@ set(CORE_SOURCES
 
         open_lists/alternation_open_list.cc
         open_lists/bucket_open_list.cc
-        open_lists/epsilon_greedy_open_list.h
+        open_lists/epsilon_greedy_open_list.cc
         open_lists/open_list.cc
         open_lists/pareto_open_list.cc
         open_lists/standard_scalar_open_list.cc
