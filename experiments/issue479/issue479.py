cd3a320fc95a16810527d757e4da95dae819386e
Florian Pommerening <florian.pommerening@unibas.ch>
2015-10-13 16:31:17 +0200

    [issue479] update merge&shrink options in experiment

--- experiments/issue479/issue479.py
+++ experiments/issue479/issue479.py
@@ -12,7 +12,7 @@ import os
 exp = common_setup.IssueExperiment(
     search_revisions=["issue479-v2"],
     configs={
-        'dfp-b-50k': ['--search', 'astar(merge_and_shrink(merge_strategy=merge_dfp,shrink_strategy=shrink_bisimulation(max_states=50000,threshold=1,greedy=false,group_by_h=true),label_reduction_method=all_transition_systems_with_fixpoint))'],
+        'dfp-b-50k': ['--search', 'astar(merge_and_shrink(shrink_strategy=shrink_bisimulation(max_states=100000,threshold=1,greedy=false),merge_strategy=merge_dfp(),label_reduction=label_reduction(before_shrinking=true, before_merging=false)))'],
         'blind': ['--search', 'astar(blind())'],
     },
     suite=['airport'],
