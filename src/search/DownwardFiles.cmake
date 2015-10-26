6b786ed98a72483a6ad37bf881731c0e56ed4c8d
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-10-26 12:42:06 +0100

    [issue580] remove "learning" directory

--- src/search/DownwardFiles.cmake
+++ src/search/DownwardFiles.cmake
@@ -289,24 +289,6 @@ fast_downward_plugin(
 )
 
 fast_downward_plugin(
-    NAME LEARNING
-    HELP "Plugin containing the code to reason with learning"
-    SOURCES
-        learning/AODE.cc
-        learning/classifier.cc
-        learning/composite_feature_extractor.cc
-        learning/feature_extractor.cc
-        learning/maximum_heuristic.cc
-        learning/naive_bayes_classifier.cc
-        learning/PDB_state_space_sample.cc
-        learning/probe_state_space_sample.cc
-        learning/selective_max_heuristic.cc
-        learning/state_space_sample.cc
-        learning/state_vars_feature_extractor.cc
-    DEACTIVATED
-)
-
-fast_downward_plugin(
     NAME PDBS
     HELP "Plugin containing the code for PDBs"
     SOURCES
