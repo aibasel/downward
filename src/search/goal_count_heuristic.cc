e80130829d24e1d3cef76c30011f3ba0d2306c7a
Thomas Keller <tho.keller@unibas.ch>
2015-10-26 13:55:31 +0100

    [issue554] implemented new task interface in goal count heuristic

--- src/search/goal_count_heuristic.cc
+++ src/search/goal_count_heuristic.cc
@@ -1,12 +1,8 @@
 #include "goal_count_heuristic.h"
 
 #include "global_state.h"
-#include "globals.h"
 #include "option_parser.h"
 #include "plugin.h"
-
-
-
 
 GoalCountHeuristic::GoalCountHeuristic(const Options &opts)
     : Heuristic(opts) {
@@ -19,12 +15,16 @@ void GoalCountHeuristic::initialize() {
     cout << "Initializing goal count heuristic..." << endl;
 }
 
-int GoalCountHeuristic::compute_heuristic(const GlobalState &state) {
+int GoalCountHeuristic::compute_heuristic(GlobalState const& global_state) {
+    State const state = convert_global_state(global_state);
     int unsatisfied_goal_count = 0;
-    for (size_t i = 0; i < g_goal.size(); ++i) {
-        int var = g_goal[i].first, value = g_goal[i].second;
-        if (state[var] != value)
+
+    GoalsProxy const goals =  task_proxy.get_goals();
+    for (FactProxy const& goal : goals) {
+        VariableProxy const var = goal.get_variable();
+        if (state[var] != goal) {
             ++unsatisfied_goal_count;
+        }
     }
     return unsatisfied_goal_count;
 }
