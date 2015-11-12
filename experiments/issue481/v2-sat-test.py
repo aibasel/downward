a3dc80a87ea117fb9b68389a385c8503efd31adb
Malte Helmert <malte.helmert@unibas.ch>
2015-11-12 09:53:53 +0100

    [issue481] Added second experiment to test unexplained errors.

---
+++ experiments/issue481/v2-sat-test.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from downward import suites
+
+from common_setup import IssueConfig, IssueExperiment
+
+
+REVS = ["issue481-base", "issue481-v1"]
+SUITE = "floortile-sat11-strips"
+
+CONFIGS = [
+    IssueConfig("ff", ["--search", "eager_greedy(ff())"]),
+    # example using driver options:
+    # IssueConfig("lama", [], driver_options=["--alias", "seq-sat-lama-2011"]),
+]
+
+exp = IssueExperiment(
+    revisions=REVS,
+    configs=CONFIGS,
+    suite=SUITE,
+    email="malte.helmert@unibas.ch"
+)
+
+## TODO: Pick which steps we actually want; probably not all three.
+## scatter plots commented out for now while we don't have a usable matplotlib.
+exp.add_absolute_report_step()
+exp.add_comparison_table_step()
+# exp.add_scatter_plot_step()
+
+exp()
