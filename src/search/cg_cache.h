e2324ba5e1bdd66ed0bcd4b18a88112cfbe03509
Gabi Roeger <gabriele.roeger@unibas.ch>
2015-11-18 13:21:15 +0100

    [issue549] use task proxy in CG cache

--- src/search/cg_cache.h
+++ src/search/cg_cache.h
@@ -1,19 +1,20 @@
 #ifndef CG_CACHE_H
 #define CG_CACHE_H
 
+#include "task_proxy.h"
+
 #include <vector>
 
-class State;
-class TaskProxy;
 struct ValueTransitionLabel;
 
 class CGCache {
+    TaskProxy task_proxy;
     std::vector<std::vector<int>> cache;
     std::vector<std::vector<ValueTransitionLabel *>> helpful_transition_cache;
     std::vector<std::vector<int>> depends_on;
 
     int get_index(int var, const State &state, int from_val, int to_val) const;
-    int compute_required_cache_size(int var,
+    int compute_required_cache_size(int var_id,
                                     const std::vector<int> &depends_on) const;
 public:
     static const int NOT_COMPUTED = -2;
