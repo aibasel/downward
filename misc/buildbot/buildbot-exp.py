a27d5053adc5acc5ba71bca8993a7318a12e5f41
Florian Pommerening <florian.pommerening@unibas.ch>
2015-11-23 11:52:22 +0100

    [main] Update baseline of regression tests

--- misc/buildbot/buildbot-exp.py
+++ misc/buildbot/buildbot-exp.py
@@ -44,7 +44,7 @@ DIR = os.path.dirname(os.path.abspath(__file__))
 REPO = os.path.abspath(os.path.join(DIR, '../../'))
 EXPERIMENTS_DIR = os.path.expanduser('~/experiments')
 
-BASELINE = checkouts.get_global_rev(REPO, '1e20b69c5f43')
+BASELINE = checkouts.get_global_rev(REPO, 'eb9f8c86918f')
 if not BASELINE:
     logging.critical('Baseline not set or not found in repo.')
 CONFIGS = {}
