005e3686727c638e2a0b64ab6c15a1b731d052e9
Silvan Sievers <silvan.sievers@unibas.ch>
2015-12-02 17:55:10 +0100

    [issue601] use unique_ptr for Label also

--- src/search/merge_and_shrink/labels.cc
+++ src/search/merge_and_shrink/labels.cc
@@ -7,17 +7,11 @@
 
 using namespace std;
 
-Labels::Labels(vector<Label *> &&labels_)
-    : labels(move(labels_)),
+Labels::Labels(vector<unique_ptr<Label>> &&labels)
+    : labels(move(labels)),
       max_size(0) {
-    if (!labels.empty()) {
-        max_size = labels.size() * 2 - 1;
-    }
-}
-
-Labels::~Labels() {
-    for (Label *label : labels) {
-        delete label;
+    if (!this->labels.empty()) {
+        max_size = this->labels.size() * 2 - 1;
     }
 }
 
@@ -25,15 +19,14 @@ void Labels::reduce_labels(const vector<int> &old_label_nos) {
     int new_label_cost = labels[old_label_nos[0]]->get_cost();
     for (size_t i = 0; i < old_label_nos.size(); ++i) {
         int old_label_no = old_label_nos[i];
-        delete labels[old_label_no];
-        labels[old_label_no] = 0;
+        labels[old_label_no] = nullptr;
     }
-    labels.push_back(new Label(new_label_cost));
+    labels.push_back(make_unique_ptr<Label>(new_label_cost));
 }
 
 bool Labels::is_current_label(int label_no) const {
     assert(in_bounds(label_no, labels));
-    return labels[label_no];
+    return labels[label_no] != nullptr;
 }
 
 int Labels::get_label_cost(int label_no) const {
