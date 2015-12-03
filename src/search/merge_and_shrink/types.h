9b245026f0382d1c1d5ce1f31c5deaa02c4672b1
Silvan Sievers <silvan.sievers@unibas.ch>
2015-12-03 18:55:30 +0100

    [issue601] use using instead of typedef

--- src/search/merge_and_shrink/types.h
+++ src/search/merge_and_shrink/types.h
@@ -6,19 +6,20 @@
 #include <vector>
 
 // Related to representation of grouped labels
-typedef std::list<int>::const_iterator LabelConstIter;
+using LabelConstIter = std::list<int>::const_iterator;
 
 // Positive infinity. The name "INFINITY" is taken by an ISO C99 macro.
 extern const int INF;
 
-/* An equivalence class is a set of abstract states that shall be
-   mapped (shrunk) to the same abstract state.
+/*
+  An equivalence class is a set of abstract states that shall be
+  mapped (shrunk) to the same abstract state.
 
-   An equivalence relation is a partitioning of states into
-   equivalence classes. It may omit certain states entirely; these
-   will be dropped completely and receive an h value of infinity.
+  An equivalence relation is a partitioning of states into
+  equivalence classes. It may omit certain states entirely; these
+  will be dropped completely and receive an h value of infinity.
 */
-typedef std::forward_list<int> StateEquivalenceClass;
-typedef std::vector<StateEquivalenceClass> StateEquivalenceRelation;
+using StateEquivalenceClass = std::forward_list<int>;
+using StateEquivalenceRelation = std::vector<StateEquivalenceClass>;
 
 #endif
