973215cf8f4459896ffab02c3270514cc05a72ce
Malte Helmert <malte.helmert@unibas.ch>
2015-12-08 18:56:18 +0100

    [issue605] type-based open list: separate methods from class declaration

--- src/search/open_lists/type_based_open_list.cc
+++ src/search/open_lists/type_based_open_list.cc
@@ -25,93 +25,114 @@ class TypeBasedOpenList : public OpenList<Entry> {
 
 protected:
     virtual void do_insertion(
-        EvaluationContext &eval_context, const Entry &entry) override {
-        vector<int> key;
-        key.reserve(evaluators.size());
-        for (ScalarEvaluator *evaluator : evaluators) {
-            key.push_back(
-                eval_context.get_heuristic_value_or_infinity(evaluator));
-        }
-
-        auto it = key_to_bucket_index.find(key);
-        if (it == key_to_bucket_index.end()) {
-            key_to_bucket_index[key] = keys_and_buckets.size();
-            keys_and_buckets.push_back({move(key), {entry}
-                                       });
-        } else {
-            size_t bucket_index = it->second;
-            assert(in_bounds(bucket_index, keys_and_buckets));
-            keys_and_buckets[bucket_index].second.push_back(entry);
-        }
-    }
+        EvaluationContext &eval_context, const Entry &entry) override;
 
 public:
-    explicit TypeBasedOpenList(const Options &opts)
-        : evaluators(opts.get_list<ScalarEvaluator *>("evaluators")) {
-    }
-
+    explicit TypeBasedOpenList(const Options &opts);
     virtual ~TypeBasedOpenList() override = default;
 
-    virtual Entry remove_min(vector<int> *key = 0) override {
-        size_t bucket_id = g_rng(keys_and_buckets.size());
-        auto &key_and_bucket = keys_and_buckets[bucket_id];
-        const Key &min_key = key_and_bucket.first;
-        Bucket &bucket = key_and_bucket.second;
-
-        if (key) {
-            assert(key->empty());
-            *key = min_key;
-        }
-
-        int pos = g_rng(bucket.size());
-        Entry result = swap_and_pop_from_vector(bucket, pos);
-
-        if (bucket.empty()) {
-            // Swap the empty bucket with the last bucket, then delete it.
-            key_to_bucket_index[keys_and_buckets.back().first] = bucket_id;
-            key_to_bucket_index.erase(min_key);
-            swap_and_pop_from_vector(keys_and_buckets, bucket_id);
-        }
-        return result;
-    }
-
-    virtual bool empty() const override {
-        return keys_and_buckets.empty();
-    }
-
-    virtual void clear() override {
-        keys_and_buckets.clear();
-        key_to_bucket_index.clear();
-    }
-
-    virtual bool is_dead_end(EvaluationContext &eval_context) const override {
-        // If one evaluator is sure we have a dead end, return true.
-        if (is_reliable_dead_end(eval_context))
-            return true;
-        // Otherwise, return true if all evaluators agree this is a dead-end.
-        for (ScalarEvaluator *evaluator : evaluators) {
-            if (!eval_context.is_heuristic_infinite(evaluator))
-                return false;
-        }
-        return true;
-    }
-
+    virtual Entry remove_min(vector<int> *key = nullptr) override;
+    virtual bool empty() const override;
+    virtual void clear() override;
+    virtual bool is_dead_end(EvaluationContext &eval_context) const override;
     virtual bool is_reliable_dead_end(
-        EvaluationContext &eval_context) const override {
-        for (ScalarEvaluator *evaluator : evaluators) {
-            if (evaluator->dead_ends_are_reliable() &&
-                eval_context.is_heuristic_infinite(evaluator))
-                return true;
-        }
-        return false;
+        EvaluationContext &eval_context) const override;
+    virtual void get_involved_heuristics(set<Heuristic *> &hset) override;
+};
+
+template<class Entry>
+void TypeBasedOpenList<Entry>::do_insertion(
+    EvaluationContext &eval_context, const Entry &entry) {
+    vector<int> key;
+    key.reserve(evaluators.size());
+    for (ScalarEvaluator *evaluator : evaluators) {
+        key.push_back(
+            eval_context.get_heuristic_value_or_infinity(evaluator));
     }
 
-    virtual void get_involved_heuristics(set<Heuristic *> &hset) override {
-        for (ScalarEvaluator *evaluator : evaluators) {
-            evaluator->get_involved_heuristics(hset);
-        }
+    auto it = key_to_bucket_index.find(key);
+    if (it == key_to_bucket_index.end()) {
+        key_to_bucket_index[key] = keys_and_buckets.size();
+        keys_and_buckets.push_back({move(key), {entry}});
+    } else {
+        size_t bucket_index = it->second;
+        assert(in_bounds(bucket_index, keys_and_buckets));
+        keys_and_buckets[bucket_index].second.push_back(entry);
     }
-};
+}
+
+template<class Entry>
+TypeBasedOpenList<Entry>::TypeBasedOpenList(const Options &opts)
+    : evaluators(opts.get_list<ScalarEvaluator *>("evaluators")) {
+}
+
+template<class Entry>
+Entry TypeBasedOpenList<Entry>::remove_min(vector<int> *key) {
+    size_t bucket_id = g_rng(keys_and_buckets.size());
+    auto &key_and_bucket = keys_and_buckets[bucket_id];
+    const Key &min_key = key_and_bucket.first;
+    Bucket &bucket = key_and_bucket.second;
+
+    if (key) {
+        assert(key->empty());
+        *key = min_key;
+    }
+
+    int pos = g_rng(bucket.size());
+    Entry result = swap_and_pop_from_vector(bucket, pos);
+
+    if (bucket.empty()) {
+        // Swap the empty bucket with the last bucket, then delete it.
+        key_to_bucket_index[keys_and_buckets.back().first] = bucket_id;
+        key_to_bucket_index.erase(min_key);
+        swap_and_pop_from_vector(keys_and_buckets, bucket_id);
+    }
+    return result;
+}
+
+template<class Entry>
+bool TypeBasedOpenList<Entry>::empty() const {
+    return keys_and_buckets.empty();
+}
+
+template<class Entry>
+void TypeBasedOpenList<Entry>::clear() {
+    keys_and_buckets.clear();
+    key_to_bucket_index.clear();
+}
+
+template<class Entry>
+bool TypeBasedOpenList<Entry>::is_dead_end(
+    EvaluationContext &eval_context) const {
+    // If one evaluator is sure we have a dead end, return true.
+    if (is_reliable_dead_end(eval_context))
+        return true;
+    // Otherwise, return true if all evaluators agree this is a dead-end.
+    for (ScalarEvaluator *evaluator : evaluators) {
+        if (!eval_context.is_heuristic_infinite(evaluator))
+            return false;
+    }
+    return true;
+}
+
+template<class Entry>
+bool TypeBasedOpenList<Entry>::is_reliable_dead_end(
+    EvaluationContext &eval_context) const {
+    for (ScalarEvaluator *evaluator : evaluators) {
+        if (evaluator->dead_ends_are_reliable() &&
+            eval_context.is_heuristic_infinite(evaluator))
+            return true;
+    }
+    return false;
+}
+
+template<class Entry>
+void TypeBasedOpenList<Entry>::get_involved_heuristics(
+    set<Heuristic *> &hset) {
+    for (ScalarEvaluator *evaluator : evaluators) {
+        evaluator->get_involved_heuristics(hset);
+    }
+}
 
 TypeBasedOpenListFactory::TypeBasedOpenListFactory(
     const Options &options)
