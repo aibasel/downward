526a43ea97b0020217e747b50893e8625b65776b
Florian Pommerening <florian.pommerening@unibas.ch>
2015-12-09 11:09:36 +0100

    [issue607] Create CMake core plugin and stubs for headers

---
+++ src/search/utilities.h
@@ -0,0 +1,8 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+/* We use this temporary stub to avoid merge conflicts while we move the
+   utility code into a subdirectory. */
+#include "utils/utilities.h"
+
+#endif
