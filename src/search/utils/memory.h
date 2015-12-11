e205ab922590ca6b4be55f72d7eb1ddff9c2a64c
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-12-11 15:49:49 +0100

    [issue613] add functions for reserving and releasing extra memory

--- src/search/utils/memory.h
+++ src/search/utils/memory.h
@@ -15,6 +15,17 @@ template<typename T, typename ... Args>
 std::unique_ptr<T> make_unique_ptr(Args && ... args) {
     return std::unique_ptr<T>(new T(std::forward<Args>(args) ...));
 }
+
+/*
+  Reserve some memory that we can release and be able to continue
+  afterwards, once we hit the memory limit. Due to memory fragmentation
+  the planner often doesn't have enough memory to continue if we don't
+  reserve enough memory. For CEGAR heuristics reserving 75 MB worked
+  best.
+*/
+extern void reserve_extra_memory_padding(int memory_in_mb);
+extern void release_extra_memory_padding();
+extern bool extra_memory_padding_is_reserved();
 }
 
 #endif
