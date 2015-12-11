592ebbdc26e248c2353bffecb1d2a02186861a15
Jendrik Seipp <jendrik.seipp@unibas.ch>
2015-12-11 10:38:16 +0100

    [issue600] Add typedef for LandmarkGraph

--- src/search/cegar/utils_landmarks.h
+++ src/search/cegar/utils_landmarks.h
@@ -14,19 +14,14 @@ class LandmarkGraph;
 
 namespace CEGAR {
 using VarToValues = std::unordered_map<int, std::vector<int>>;
+using SharedGraph = std::shared_ptr<Landmarks::LandmarkGraph>;
 
-// TODO: Add typedef for LandmarkGraph.
+extern SharedGraph get_landmark_graph();
+extern std::vector<Fact> get_fact_landmarks(SharedGraph landmark_graph);
+extern VarToValues get_prev_landmarks(SharedGraph, Fact fact);
 
-extern std::shared_ptr<Landmarks::LandmarkGraph> get_landmark_graph();
-extern std::vector<Fact> get_fact_landmarks(
-    std::shared_ptr<Landmarks::LandmarkGraph> landmark_graph);
-extern VarToValues get_prev_landmarks(
-    std::shared_ptr<Landmarks::LandmarkGraph>, Fact fact);
-
-extern void dump_landmark_graph(
-    std::shared_ptr<Landmarks::LandmarkGraph> graph);
-extern void write_landmark_graph_dot_file(
-    std::shared_ptr<Landmarks::LandmarkGraph> graph);
+extern void dump_landmark_graph(SharedGraph graph);
+extern void write_landmark_graph_dot_file(SharedGraph graph);
 }
 
 #endif
