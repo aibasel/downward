7aa39a7fa47ef3f5d3b769137ce33f1847e2d6fd
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-01-02 14:56:42 +0100

    [issue600] add TODO

--- src/search/cegar/utils_landmarks.h
+++ src/search/cegar/utils_landmarks.h
@@ -12,11 +12,13 @@ namespace Landmarks {
 class LandmarkGraph;
 }
 
+// TODO: Move into "Landmarks" namespace and directory?
 namespace CEGAR {
 using VarToValues = std::unordered_map<int, std::vector<int>>;
 
 extern std::unique_ptr<Landmarks::LandmarkGraph> get_landmark_graph();
-extern std::vector<Fact> get_fact_landmarks(const Landmarks::LandmarkGraph &graph);
+extern std::vector<Fact> get_fact_landmarks(
+    const Landmarks::LandmarkGraph &graph);
 
 /*
   Do a breadth-first search through the landmark graph ignoring
@@ -24,7 +26,8 @@ extern std::vector<Fact> get_fact_landmarks(const Landmarks::LandmarkGraph &grap
   variable the facts that have to be made true before the given fact
   can be true for the first time.
 */
-extern VarToValues get_prev_landmarks(const Landmarks::LandmarkGraph &graph, const Fact &fact);
+extern VarToValues get_prev_landmarks(
+    const Landmarks::LandmarkGraph &graph, const Fact &fact);
 }
 
 #endif
