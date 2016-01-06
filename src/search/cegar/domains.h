b9b104745608724b688fdb09e8a6ca022e516de4
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-01-06 23:23:41 +0100

    [issue600] remove static Domains members

--- src/search/cegar/domains.h
+++ src/search/cegar/domains.h
@@ -28,23 +28,8 @@ class Domains {
     */
     std::vector<Bitset> bits;
 
-    static std::vector<int> orig_domain_sizes;
-    // Total number of facts and size of the bitset.
-    static int num_facts;
-    // The first bit for var is at borders[var].
-    static std::vector<int> borders;
-    // masks[var][pos] is set iff pos belongs to var.
-    static std::vector<Bitset> masks;
-    static std::vector<Bitset> inverse_masks;
-    // Temporary bitset for speeding up calculations.
-    static Bitset temp_bits;
-
-    static void initialize_static_members(std::vector<int> &&domain_sizes);
-
-    int pos(int var, int value) const {return borders[var] + value; }
-
 public:
-    explicit Domains(std::vector<int> &&domain_sizes);
+    explicit Domains(const std::vector<int> &domain_sizes);
     ~Domains() = default;
 
     void add(int var, int value);
