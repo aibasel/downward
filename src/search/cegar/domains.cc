714f639566674ec20b20ca5e224d240a2d82cfb3
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-01-06 23:23:41 +0100

    [issue600] remove static Domains members

--- src/search/cegar/domains.cc
+++ src/search/cegar/domains.cc
@@ -5,45 +5,12 @@
 using namespace std;
 
 namespace CEGAR {
-vector<int> Domains::orig_domain_sizes;
-int Domains::num_facts = -1;
-vector<int> Domains::borders;
-vector<Bitset> Domains::masks;
-vector<Bitset> Domains::inverse_masks;
-Bitset Domains::temp_bits;
-
-Domains::Domains(vector<int> &&domain_sizes) {
-    initialize_static_members(move(domain_sizes));
-    for (int domain_size : orig_domain_sizes) {
+Domains::Domains(const vector<int> &domain_sizes) {
+    for (int domain_size : domain_sizes) {
         Bitset domain(domain_size);
         domain.set();
         bits.push_back(move(domain));
     }
-}
-
-void Domains::initialize_static_members(vector<int> &&domain_sizes) {
-    orig_domain_sizes = move(domain_sizes);
-
-    masks.clear();
-    inverse_masks.clear();
-    borders.clear();
-    num_facts = 0;
-    int num_vars = orig_domain_sizes.size();
-    for (int var = 0; var < num_vars; ++var) {
-        borders.push_back(num_facts);
-        num_facts += orig_domain_sizes[var];
-    }
-    for (int var = 0; var < num_vars; ++var) {
-        Bitset mask(borders[var]);
-        // Example mask: 0000-----
-        mask.resize(borders[var] + orig_domain_sizes[var], true);
-        // Example mask: 0000111--
-        mask.resize(num_facts, false);
-        // Example mask: 000011100
-        masks.push_back(mask);
-        inverse_masks.push_back(~mask);
-    }
-    temp_bits.resize(num_facts);
 }
 
 void Domains::add(int var, int value) {
