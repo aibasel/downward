531b13820db51d3150d96a0e6648f5eef20b9dd2
Florian Pommerening <florian.pommerening@unibas.ch>
2016-01-07 16:51:27 +0100

    [main] Visual Studio 2013 does not support noexcept

--- src/search/utils/language.h
+++ src/search/utils/language.h
@@ -1,11 +1,18 @@
 #ifndef UTILS_LANGUAGE_H
 #define UTILS_LANGUAGE_H
 
-// TODO: this should depend on the compiler, not on the OS.
-#if defined(_WIN32)
+#if defined(_MSC_VER)
 #define NO_RETURN __declspec(noreturn)
 #else
 #define NO_RETURN __attribute__((noreturn))
+#endif
+
+/* The Visual Studio 2013 compiler does not support noexcept.
+   http://stackoverflow.com/questions/18387640 */
+#if defined(_MSC_FULL_VER) && _MSC_FULL_VER < 180021114
+#define NOEXCEPT
+#else
+#define NOEXCEPT noexcept
 #endif
 
 namespace Utils {
