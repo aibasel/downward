839fb358ce4e4f51391a12a310de09c47fc1eeae
Florian Pommerening <florian.pommerening@unibas.ch>
2016-01-07 17:09:28 +0100

    [main] Test for VS2015 using only major version

--- src/search/utils/language.h
+++ src/search/utils/language.h
@@ -9,7 +9,7 @@
 
 /* The Visual Studio 2013 compiler does not support noexcept.
    http://stackoverflow.com/questions/18387640 */
-#if defined(_MSC_FULL_VER) && _MSC_FULL_VER < 180021114
+#if defined(_MSC_VER) && _MSC_VER < 1900
 #define NOEXCEPT
 #else
 #define NOEXCEPT noexcept
