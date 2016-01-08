ef409ee5e09c1b65e7e7f6beaadba5bd105f63d3
Martin Wehrle <martin.wehrle@unibas.ch>
2016-01-08 18:59:17 +0100

    [issue499] register PORMethod in option_parser

--- src/search/options/option_parser.cc
+++ src/search/options/option_parser.cc
@@ -27,6 +27,7 @@ using namespace std;
 // TODO (post-issue586): Remove this once we no longer need it.
 class AbstractTask;
 class OpenListFactory;
+class PORMethod;
 class SearchEngine;
 
 namespace Landmarks {
@@ -94,6 +95,7 @@ static void get_help(string k) {
     get_help_templ<shared_ptr<OperatorCounting::ConstraintGenerator>>(pt);
     get_help_templ<shared_ptr<PDBs::PatternCollectionGenerator>>(pt);
     get_help_templ<shared_ptr<PDBs::PatternGenerator>>(pt);
+    get_help_templ<shared_ptr<PORMethod>>(pt);
 }
 
 template<typename T>
@@ -122,6 +124,7 @@ static void get_full_help() {
     get_full_help_templ<shared_ptr<OperatorCounting::ConstraintGenerator>>();
     get_full_help_templ<shared_ptr<PDBs::PatternCollectionGenerator>>();
     get_full_help_templ<shared_ptr<PDBs::PatternGenerator>>();
+    get_full_help_templ<shared_ptr<PORMethod>>();
 }
 
 
