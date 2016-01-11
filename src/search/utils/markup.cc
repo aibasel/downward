45c7f19182c777b646b538c3cb826817897de6f4
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-01-11 16:01:45 +0100

    [issue618] pass authors as vector of strings

--- src/search/utils/markup.cc
+++ src/search/utils/markup.cc
@@ -1,5 +1,6 @@
 #include "markup.h"
 
+#include <cassert>
 #include <sstream>
 
 using namespace std;
@@ -9,12 +10,27 @@ static string t2t_escape(const std::string &s) {
     return "\"\"" + s + "\"\"";
 }
 
+static string format_authors(const vector<string> &authors) {
+    assert(!authors.empty());
+    stringstream ss;
+    for (size_t i = 0; i < authors.size(); ++i) {
+        const string &author = authors[i];
+        ss << t2t_escape(author);
+        if (i < authors.size() - 2) {
+            ss << ", ";
+        } else if (i == authors.size() - 2) {
+            ss << " and ";
+        }
+    }
+    return ss.str();
+}
+
 string format_paper_reference(
-    const string &authors, const string &title, const string &url,
+    const vector<string> &authors, const string &title, const string &url,
     const string &conference, const string &pages, const string &publisher) {
     stringstream ss;
     ss << "\n\n"
-       << " * " << t2t_escape(authors) << ".<<BR>>\n"
+       << " * " << format_authors(authors) << ".<<BR>>\n"
        << " [" << t2t_escape(title) << " " << t2t_escape(url) << "].<<BR>>\n"
        << " In //" << t2t_escape(conference) << "//,"
        << " pp. " << t2t_escape(pages) << ". "
