68e2b16114c1d91ef95167e032139a0f7bd3d8c1
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-01-11 19:02:32 +0100

    [issue618] use Utils::format_paper_reference for remaining M&S docs

--- src/search/merge_and_shrink/shrink_fh.cc
+++ src/search/merge_and_shrink/shrink_fh.cc
@@ -8,6 +8,7 @@
 #include "../plugin.h"
 
 #include "../utils/collections.h"
+#include "../utils/markup.h"
 
 #include <algorithm>
 #include <cassert>
@@ -170,12 +171,15 @@ void ShrinkFH::dump_strategy_specific_options() const {
 static shared_ptr<ShrinkStrategy>_parse(OptionParser &parser) {
     parser.document_synopsis(
         "f-preserving shrink strategy",
-        "This shrink strategy implements the algorithm described in the paper:\n\n"
-        " * Malte Helmert, Patrik Haslum and Joerg Hoffmann.<<BR>>\n"
-        " [Flexible Abstraction Heuristics for Optimal Sequential Planning "
-        "http://ai.cs.unibas.ch/papers/helmert-et-al-icaps2007.pdf].<<BR>>\n "
-        "In //Proceedings of the Seventeenth International Conference on "
-        "Automated Planning and Scheduling (ICAPS 2007)//, pp. 176-183. 2007. ");
+        "This shrink strategy implements the algorithm described in"
+        " the paper:" + Utils::format_paper_reference(
+            {"Malte Helmert", "Patrik Haslum", "Joerg Hoffmann"},
+            "Flexible Abstraction Heuristics for Optimal Sequential Planning",
+            "http://ai.cs.unibas.ch/papers/helmert-et-al-icaps2007.pdf",
+            "Proceedings of the Seventeenth International Conference on"
+            " Automated Planning and Scheduling (ICAPS 2007)",
+            "176-183",
+            "2007"));
     parser.document_note(
         "shrink_fh(max_states=N)",
         "f-preserving shrinking of transition systems "
