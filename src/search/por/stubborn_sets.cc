15074561201efcd19f398f2ebde66b86afd48bc8
Florian Pommerening <florian.pommerening@unibas.ch>
2016-01-11 21:35:34 +0100

    [issue499] Factor out common code

--- src/search/por/stubborn_sets.cc
+++ src/search/por/stubborn_sets.cc
@@ -15,63 +15,41 @@ struct SortFactsByVariable {
     }
 };
 
+// Relies on both fact sets being sorted by variable.
+bool contain_conflicting_fact(const std::vector<Fact> &fact_set_1,
+                              const std::vector<Fact> &fact_set_2) {
+    auto f1 = fact_set_1.begin();
+    auto f2 = fact_set_2.begin();
+    while (f1 != fact_set_1.end() && f2 != fact_set_2.end()) {
+        if (f1->var < f2->var) {
+            ++f1;
+        } else if (f1->var > f2->var) {
+            ++f2;
+        } else {
+            if (f1->val != f2->val)
+                return true;
+            ++f1;
+            ++f2;
+        }
+    }
+    return false;
+}
+
 StubbornSets::StubbornSets()
     : num_unpruned_successors_generated(0),
       num_pruned_successors_generated(0) {
     verify_no_axioms_no_conditional_effects();
 }
 
-/* can_disable relies on op_preconds and op_effects being sorted by
-   variable */
+// Relies on op_preconds and op_effects being sorted by variable.
 bool StubbornSets::can_disable(int op1_no, int op2_no) {
-    int i = 0;
-    int j = 0;
-    int num_op1_effects = op_effects[op1_no].size();
-    int num_op2_preconditions = op_preconds[op2_no].size();
-    while (i < num_op2_preconditions && j < num_op1_effects) {
-        int read_var = op_preconds[op2_no][i].var;
-        int write_var = op_effects[op1_no][j].var;
-        if (read_var < write_var) {
-            i++;
-        } else if (read_var > write_var) {
-            j++;
-        } else {
-            int read_value = op_preconds[op2_no][i].val;
-            int write_value = op_effects[op1_no][j].val;
-            if (read_value != write_value)
-                return true;
-            i++;
-            j++;
-        }
-    }
-    return false;
+    return contain_conflicting_fact(op_effects[op1_no], op_preconds[op2_no]);
 }
 
-// can_conflict relies on op_effect being sorted by variable
+// Relies on op_effect being sorted by variable.
 bool StubbornSets::can_conflict(int op1_no, int op2_no) {
-    int i = 0;
-    int j = 0;
-    int num_op1_effects = op_effects[op1_no].size();
-    int num_op2_effects = op_effects[op2_no].size();
-    while (i < num_op1_effects && j < num_op2_effects) {
-        int var1 = op_effects[op1_no][i].var;
-        int var2 = op_effects[op2_no][j].var;
-        if (var1 < var2) {
-            i++;
-        } else if (var1 > var2) {
-            j++;
-        } else {
-            int value1 = op_effects[op1_no][i].val;
-            int value2 = op_effects[op2_no][j].val;
-            if (value1 != value2)
-                return true;
-            i++;
-            j++;
-        }
-    }
-    return false;
+    return contain_conflicting_fact(op_effects[op1_no], op_effects[op2_no]);
 }
-
 
 void StubbornSets::compute_sorted_operators() {
     assert(op_preconds.empty());
