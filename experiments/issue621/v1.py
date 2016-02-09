9d86b1f65c70b4cc56d258c6595dad5c45af3f5a
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-02-09 10:12:35 +0100

    [issue621] add relative scatter plot

--- experiments/issue621/v1.py
+++ experiments/issue621/v1.py
@@ -4,6 +4,7 @@
 from downward import suites
 
 from common_setup import IssueConfig, IssueExperiment
+from relativescatter import RelativeScatterPlotReport
 
 
 configs = [
@@ -21,5 +22,18 @@ exp = IssueExperiment(
 )
 
 exp.add_comparison_table_step()
+
+for attribute in ["memory", "total_time"]:
+    for config in configs:
+        exp.add_report(
+            RelativeScatterPlotReport(
+                attributes=[attribute],
+                filter_config=[
+                    "issue627-base-%s" % config.nick,
+                    "issue627-v1-%s" % config.nick],
+                get_category=lambda run1, run2: run1.get("domain"),
+            ),
+            outfile="issue621_base_v1_{}_{}.png".format(attribute, config.nick)
+        )
 
 exp()
