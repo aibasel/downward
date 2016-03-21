b1427ef470c03c158dc4e58c6a88448519179121
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-03-21 21:39:21 +0100

    [issue581] pass benchmarks dir to test-translator.py

--- misc/tests/test-translator.py
+++ misc/tests/test-translator.py
@@ -1,24 +1,13 @@
 #! /usr/bin/env python
 
-"""
-Run the translator on supported Python versions and test that the log
-and the output file are the same for all versions.
-
-# Run small test on a few problems.
-./test-translator.py
-
-# Run bigger test on the first problem of each domain.
-./test-translator.py first
-
-# Run even bigger test on the whole benchmark suite.
-./test-translator.py all
-
-# Run test on specific problems.
-./test-translator.py gripper:prob01.pddl depot:pfile1
-"""
-
 from __future__ import print_function
 
+HELP = """\
+Run the translator on supported Python versions and test that the log
+and the output file are the same for all versions.
+"""
+
+import argparse
 from collections import defaultdict
 from distutils.spawn import find_executable
 import itertools
@@ -38,15 +27,29 @@ BENCHMARKS = os.path.join(REPO, "misc", "tests", "benchmarks")
 # Translating these problems covers 84% of the translator code. Translating all
 # first problems covers 85%.
 TASKS = [
-    "airport:p01-airport1-p1.pddl",
-    "airport-adl:p01-airport1-p1.pddl",
-    "assembly:prob01.pddl",
-    "barman-opt11-strips:pfile01-001.pddl",
-    "miconic-fulladl:f1-0.pddl",
-    "psr-large:p01-s29-n2-l5-f30.pddl",
-    "transport-opt08-strips:p01.pddl",
-    "woodworking-opt11-strips:p01.pddl",
+    "airport/p01-airport1-p1.pddl",
+    "airport-adl/p01-airport1-p1.pddl",
+    "assembly/prob01.pddl",
+    "barman-opt11-strips/pfile01-001.pddl",
+    "miconic-fulladl/f1-0.pddl",
+    "psr-large/p01-s29-n2-l5-f30.pddl",
+    "transport-opt08-strips/p01.pddl",
+    "woodworking-opt11-strips/p01.pddl",
     ]
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=HELP)
+    parser.add_argument(
+        "benchmarks_dir",
+        help="path to benchmark directory")
+    parser.add_argument(
+        "--first", action="store_true",
+        help="test first task of each domain")
+    parser.add_argument(
+        "--all", action="store_true",
+        help="test complete benchmark suite")
+    return parser.parse_args()
 
 
 def get_task_name(path):
@@ -74,33 +77,26 @@ def translate_task(python, python_version, task_file):
     return output
 
 
-def _get_all_tasks_by_domain():
+def _get_all_tasks_by_domain(benchmarks_dir):
     tasks = defaultdict(list)
-    for domain in os.listdir(BENCHMARKS):
-        path = os.path.join(BENCHMARKS, domain)
-        tasks[domain] = [os.path.join(BENCHMARKS, domain, f)
+    for domain in os.listdir(benchmarks_dir):
+        path = os.path.join(benchmarks_dir, domain)
+        tasks[domain] = [os.path.join(benchmarks_dir, domain, f)
                          for f in sorted(os.listdir(path)) if not "domain" in f]
-    return tasks
+    return sorted(tasks.values())
 
 
-def get_tasks():
-    if "first" in sys.argv:
+def get_tasks(args):
+    if args.first:
         # Use the first problem of each domain.
-        return [tasks[0] for tasks in sorted(_get_all_tasks_by_domain().values())]
-
-    if "all" in sys.argv:
+        return [tasks[0] for tasks in _get_all_tasks_by_domain(args.benchmarks_dir)]
+    elif args.all:
         # Use the whole benchmark suite.
         return itertools.chain.from_iterable(
-                tasks for tasks in _get_all_tasks_by_domain().values())
-
-    # Use the problems given on the commandline.
-    tasks = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
-    if not tasks:
+                tasks for tasks in _get_all_tasks_by_domain(args.benchmarks_dir))
+    else:
         # Use predefined problems.
-        tasks = TASKS
-
-    # Make the paths absolute.
-    return [os.path.join(BENCHMARKS, *task.split(":")) for task in tasks]
+        return [os.path.join(args.benchmarks_dir, task) for task in TASKS]
 
 
 def cleanup():
@@ -134,7 +130,8 @@ def main():
     cleanup()
     interpreter_paths = [get_abs_interpreter_path("python{}".format(version)) for version in VERSIONS]
     interpreter_versions = [get_python_version_info(path) for path in interpreter_paths]
-    for task in get_tasks():
+    args = parse_args()
+    for task in get_tasks(args):
         for python, python_version in zip(interpreter_paths, interpreter_versions):
             log = translate_task(python, python_version, task)
             with open(python_version + ".sas", "w") as combined_output:
