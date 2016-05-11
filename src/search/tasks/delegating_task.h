7f433c92e5e2b8935ab69c92d8f83e4bbb7c9bc3
Florian Pommerening <florian.pommerening@unibas.ch>
2016-05-11 19:38:10 +0200

    [issue627] Use template method to avoid code duplication

--- src/search/tasks/delegating_task.h
+++ src/search/tasks/delegating_task.h
@@ -49,9 +49,12 @@ public:
     virtual Fact get_goal_fact(int index) const override;
 
     virtual std::vector<int> get_initial_state_values() const override;
+
     virtual std::vector<int> convert_state_values(
         const std::vector<int> &ancestor_state_values,
-        const AbstractTask *ancestor_task) const override;
+        const AbstractTask *ancestor_task) const final override;
+    virtual std::vector<int> convert_state_values_from_parent(
+        const std::vector<int> &&parent_state_values) const;
 };
 }
 
