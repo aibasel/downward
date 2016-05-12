626e69a5803e57ec2a8f31844e33e51022ce4d36
Florian Pommerening <florian.pommerening@unibas.ch>
2016-05-12 09:29:16 +0200

    [issue627] fixed revision in exp4

--- experiments/issue627/v4.py
+++ experiments/issue627/v4.py
@@ -43,7 +43,7 @@ def main(revisions=None):
         exp.add_report(
             RelativeScatterPlotReport(
                 attributes=["total_time"],
-                filter_config=["issue627-base-%s" % config.nick,
+                filter_config=["issue627-v4-base-%s" % config.nick,
                                "issue627-v4-%s" % config.nick],
                 get_category=lambda run1, run2: run1.get("domain"),
             ),
