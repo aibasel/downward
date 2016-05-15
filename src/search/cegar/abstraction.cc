6ce589804b51e1d6d744f0d119a38857ddf88306
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-05-15 13:59:36 +0200

    [issue657] add max_arcs parameter

--- src/search/cegar/abstraction.cc
+++ src/search/cegar/abstraction.cc
@@ -69,6 +69,7 @@ Abstraction::Abstraction(
     const shared_ptr<AbstractTask> task,
     int max_states,
     int max_transitions,
+    int max_arcs,
     double max_time,
     bool use_general_costs,
     PickSplit pick,
@@ -76,6 +77,7 @@ Abstraction::Abstraction(
     : task_proxy(*task),
       max_states(max_states),
       max_transitions(max_transitions),
+      max_arcs(max_arcs),
       use_general_costs(use_general_costs),
       abstract_search(get_operator_costs(task_proxy), states),
       split_selector(task, pick),
@@ -89,6 +91,7 @@ Abstraction::Abstraction(
     g_log << "Start building abstraction." << endl;
     cout << "Maximum number of states: " << max_states << endl;
     cout << "Maximum number of transitions: " << max_transitions << endl;
+    cout << "Maximum number of arcs: " << max_arcs << endl;
     build();
     g_log << "Done building abstraction." << endl;
     cout << "Time for building abstraction: " << timer << endl;
@@ -146,6 +149,7 @@ bool Abstraction::may_keep_refining() const {
     return utils::extra_memory_padding_is_reserved() &&
            get_num_states() < max_states &&
            compute_num_transitions() < max_transitions &&
+           compute_num_arcs() < max_arcs &&
            !timer.is_expired();
 }
 
@@ -212,7 +216,8 @@ void Abstraction::refine(AbstractState *state, int var, const vector<int> &wante
     if (num_states % 1000 == 0) {
         g_log << num_states << "/" << max_states << " states, "
               << compute_num_transitions() << "/" << max_transitions
-              << " transitions" << endl;
+              << " transitions, "
+              << compute_num_arcs() << "/" << max_arcs << " arcs" << endl;
     }
 
     delete state;
@@ -296,6 +301,14 @@ int Abstraction::compute_num_transitions() const {
     return num_transitions;
 }
 
+int Abstraction::compute_num_arcs() const {
+    int num_arcs = 0;
+    for (const AbstractState *state : states) {
+        num_arcs += state->get_outgoing_arcs().size();
+    }
+    return num_arcs;
+}
+
 int Abstraction::get_h_value_of_initial_state() const {
     return init->get_h_value();
 }
@@ -371,6 +384,7 @@ void Abstraction::print_statistics() {
 
     cout << "Self-loops/transitions: "
          << total_loops << "/" << compute_num_transitions() << endl;
+    cout << "Arcs: " << total_outgoing_arcs << endl;
 
     cout << "Deviations: " << deviations << endl;
     cout << "Unmet preconditions: " << unmet_preconditions << endl;
