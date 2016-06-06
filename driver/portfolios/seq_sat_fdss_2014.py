81a96cd68a9313002418428710f95718b513b3f7
Manuel Heusner <manuel.heusner@unibas.ch>
2016-06-06 19:17:57 +0200

    [main] changed aliases

--- driver/portfolios/seq_sat_fdss_2014.py
+++ driver/portfolios/seq_sat_fdss_2014.py
@@ -6,27 +6,27 @@ CONFIGS = [
     # add_lm_lazy_greedy
     (114, ["--heuristic", "hadd=add(cost_type=H_COST_TYPE)",
          "--heuristic",
-         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2,cost_type=H_COST_TYPE))", "--search",
+         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2),cost_type=H_COST_TYPE)", "--search",
          "lazy_greedy([hadd,hlm],preferred=[hadd,hlm],cost_type=S_COST_TYPE,bound=BOUND)"]),
     # ff_lm_lazy_greedy
     (187, ["--heuristic", "hff=ff(cost_type=H_COST_TYPE)",
          "--heuristic",
-         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2,cost_type=H_COST_TYPE))", "--search",
+         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2),cost_type=H_COST_TYPE)", "--search",
          "lazy_greedy([hff,hlm],preferred=[hff,hlm],cost_type=S_COST_TYPE,bound=BOUND)"]),
     # add_lm_eager_greedy
     (33, ["--heuristic", "hadd=add(cost_type=H_COST_TYPE)",
          "--heuristic",
-         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2,cost_type=H_COST_TYPE))", "--search",
+         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2),cost_type=H_COST_TYPE)", "--search",
          "eager_greedy([hadd,hlm],preferred=[hadd,hlm],cost_type=S_COST_TYPE,bound=BOUND)"]),
     # ff_lm_eager_greedy
     (35, ["--heuristic", "hff=ff(cost_type=H_COST_TYPE)",
          "--heuristic",
-         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2,cost_type=H_COST_TYPE))", "--search",
+         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2),cost_type=H_COST_TYPE)", "--search",
          "eager_greedy([hff,hlm],preferred=[hff,hlm],cost_type=S_COST_TYPE,bound=BOUND)"]),
     # cea_lm_lazy_greedy
     (39, ["--heuristic", "hcea=cea(cost_type=H_COST_TYPE)",
          "--heuristic",
-         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2,cost_type=H_COST_TYPE))", "--search",
+         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2),cost_type=H_COST_TYPE)", "--search",
          "lazy_greedy([hcea,hlm],preferred=[hcea,hlm],cost_type=S_COST_TYPE,bound=BOUND)"]),
     # add_ff_eager_greedy
     (120, ["--heuristic", "hff=ff(cost_type=H_COST_TYPE)",
@@ -49,20 +49,20 @@ CONFIGS = [
           "lazy_greedy([hadd,hcg],preferred=[hadd,hcg],cost_type=S_COST_TYPE,bound=BOUND)"]),
     # add_lm_lazy_wastar
     (79, ["--heuristic", "hadd=add(cost_type=H_COST_TYPE)", "--heuristic",
-         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2,cost_type=H_COST_TYPE))", "--search",
+         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2),cost_type=H_COST_TYPE)", "--search",
           "lazy_wastar([hadd,hlm],w=3,preferred=[hadd,hlm],cost_type=S_COST_TYPE,bound=BOUND)"]),
     # ff_lm_lazy_wastar
     (159, ["--heuristic", "hff=ff(cost_type=H_COST_TYPE)", "--heuristic",
-         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2,cost_type=H_COST_TYPE))", "--search",
+         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2),cost_type=H_COST_TYPE)", "--search",
           "lazy_wastar([hff,hlm],w=3,preferred=[hff,hlm],cost_type=S_COST_TYPE,bound=BOUND)"]),
     # cea_lm_lazy_wastar
     (39, ["--heuristic", "hcea=cea(cost_type=H_COST_TYPE)", "--heuristic",
-         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2,cost_type=H_COST_TYPE))", "--search",
+         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2),cost_type=H_COST_TYPE)", "--search",
           "lazy_wastar([hcea,hlm],w=3,preferred=[hcea,hlm],cost_type=S_COST_TYPE,bound=BOUND)"]),
     # cg_lm_eager_greedy
     (78, ["--heuristic", "hcg=cg(cost_type=H_COST_TYPE)",
          "--heuristic",
-         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2,cost_type=H_COST_TYPE))", "--search",
+         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2),cost_type=H_COST_TYPE)", "--search",
          "eager_greedy([hcg,hlm],preferred=[hcg,hlm],cost_type=S_COST_TYPE,bound=BOUND)"]),
     # cea_ff_lazy_wastar
     (39, ["--heuristic", "hcea=cea(cost_type=H_COST_TYPE)",
@@ -70,7 +70,7 @@ CONFIGS = [
           "lazy_wastar([hcea,hff],w=3,preferred=[hcea,hff],cost_type=S_COST_TYPE,bound=BOUND)"]),
     # cea_lm_eager_wastar
     (37, ["--heuristic", "hcea=cea(cost_type=H_COST_TYPE)", "--heuristic",
-         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2,cost_type=H_COST_TYPE))", "--search",
+         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2),cost_type=H_COST_TYPE)", "--search",
          "eager(alt([single(sum([g(), weight(hcea, 3)])),single(sum([g(),weight(hcea,3)]),pref_only=true),single(sum([g(), weight(hlm,3)])),single(sum([g(),weight(hlm,3)]),pref_only=true)]),preferred=[hcea,hlm],cost_type=S_COST_TYPE,bound=BOUND)"]),
     # cg_ff_lazy_wastar
     (40, ["--heuristic", "hcg=cg(cost_type=H_COST_TYPE)",
@@ -78,7 +78,7 @@ CONFIGS = [
           "lazy_wastar([hcg,hff],w=3,preferred=[hcg,hff],cost_type=S_COST_TYPE,bound=BOUND)"]),
     # ff_lm_eager_wastar
     (40, ["--heuristic", "hff=ff(cost_type=H_COST_TYPE)", "--heuristic",
-         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2,cost_type=H_COST_TYPE))", "--search",
+         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2),cost_type=H_COST_TYPE)", "--search",
          "eager(alt([single(sum([g(),weight(hff,3)])),single(sum([g(),weight(hff,3)]),pref_only=true),single(sum([g(),weight(hlm,3)])),single(sum([g(),weight(hlm,3)]),pref_only=true)]),preferred=[hff,hlm],cost_type=S_COST_TYPE,bound=BOUND)"]),
     # add_eager_wastar
     (77, ["--heuristic", "hadd=add(cost_type=H_COST_TYPE)",
@@ -90,7 +90,7 @@ CONFIGS = [
          "eager(alt([single(sum([g(),weight(hff,3)])),single(sum([g(),weight(hff,3)]),pref_only=true),single(sum([g(),weight(hcea,3)])),single(sum([g(),weight(hcea,3)]),pref_only=true)]),preferred=[hff,hcea],cost_type=S_COST_TYPE,bound=BOUND)"]),
     # cg_lm_eager_wastar
     (78, ["--heuristic", "hcg=cg(cost_type=H_COST_TYPE)", "--heuristic",
-         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2,cost_type=H_COST_TYPE))", "--search",
+         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2),cost_type=H_COST_TYPE)", "--search",
          "eager(alt([single(sum([g(),weight(hcg,3)])),single(sum([g(),weight(hcg,3)]),pref_only=true),single(sum([g(),weight(hlm,3)])),single(sum([g(),weight(hlm,3)]),pref_only=true)]),preferred=[hcg,hlm],cost_type=S_COST_TYPE,bound=BOUND)"]),
     # cea_eager_greedy
     (40, ["--heuristic", "hcea=cea(cost_type=H_COST_TYPE)",
@@ -98,7 +98,7 @@ CONFIGS = [
          "eager_greedy(hcea,preferred=hcea,cost_type=S_COST_TYPE,bound=BOUND)"]),
     # cg_lm_lazy_wastar
     (39, ["--heuristic", "hcg=cg(cost_type=H_COST_TYPE)", "--heuristic",
-         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2,cost_type=H_COST_TYPE))", "--search",
+         "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2),cost_type=H_COST_TYPE)", "--search",
           "lazy_wastar([hcg,hlm],w=3,preferred=[hcg,hlm],cost_type=S_COST_TYPE,bound=BOUND)"]),
     # cea_lazy_wastar
     (40, ["--heuristic", "hcea=cea(cost_type=H_COST_TYPE)",
@@ -126,6 +126,6 @@ CONFIGS = [
 FINAL_CONFIG = [
     "--heuristic", "hff=ff(cost_type=H_COST_TYPE)",
     "--heuristic",
-    "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2,cost_type=H_COST_TYPE))",
+    "hlm=lmcount(lm_rhw(reasonable_orders=true,lm_cost_type=2),cost_type=H_COST_TYPE)",
     "--search",
     "iterated(eager(alt([single(sum([g(),weight(hff,3)])),single(sum([g(),weight(hff,3)]),pref_only=true),single(sum([g(),weight(hlm,3)])),single(sum([g(),weight(hlm,3)]),pref_only=true)]),preferred=[hff,hlm],cost_type=S_COST_TYPE,bound=BOUND),bound=BOUND,repeat_last=true)"]
