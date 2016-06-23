e316a7746ad62a48ebcb6f8068ff2ef31d616fe0
Silvan Sievers <silvan.sievers@unibas.ch>
2016-06-23 17:22:30 +0200

    [issue644] backwards compatibility: merge_linear plugin

--- src/search/merge_and_shrink/merge_tree_factory_linear.cc
+++ src/search/merge_and_shrink/merge_tree_factory_linear.cc
@@ -1,5 +1,6 @@
 #include "merge_tree_factory_linear.h"
 
+#include "merge_strategy_factory_precomputed.h"
 #include "merge_tree.h"
 
 #include "../variable_order_finder.h"
@@ -39,7 +40,7 @@ void MergeTreeFactoryLinear::dump_options() const {
                                  options.get_enum("variable_order")));
 }
 
-static shared_ptr<MergeTreeFactory>_parse(options::OptionParser &parser) {
+static shared_ptr<MergeTreeFactory> _parse(options::OptionParser &parser) {
     parser.document_synopsis(
         "Linear merge trees",
         "These merge trees implement several linear merge orders, which "
@@ -70,5 +71,42 @@ static shared_ptr<MergeTreeFactory>_parse(options::OptionParser &parser) {
         return make_shared<MergeTreeFactoryLinear>(opts);
 }
 
+static shared_ptr<MergeStrategyFactory> _parse_strategy(options::OptionParser &parser) {
+    parser.document_synopsis(
+        "Linear merge trees",
+        "These merge trees implement several linear merge orders, which "
+        "are described in the paper:" + utils::format_paper_reference(
+            {"Malte Helmert", "Patrik Haslum", "Joerg Hoffmann"},
+            "Flexible Abstraction Heuristics for Optimal Sequential Planning",
+            "http://ai.cs.unibas.ch/papers/helmert-et-al-icaps2007.pdf",
+            "Proceedings of the Seventeenth International Conference on"
+            " Automated Planning and Scheduling (ICAPS 2007)",
+            "176-183",
+            "2007"));
+    vector<string> merge_strategies;
+    merge_strategies.push_back("CG_GOAL_LEVEL");
+    merge_strategies.push_back("CG_GOAL_RANDOM");
+    merge_strategies.push_back("GOAL_CG_LEVEL");
+    merge_strategies.push_back("RANDOM");
+    merge_strategies.push_back("LEVEL");
+    merge_strategies.push_back("REVERSE_LEVEL");
+    parser.add_enum_option("variable_order", merge_strategies,
+                           "the order in which atomic transition systems are merged",
+                           "CG_GOAL_LEVEL");
+    utils::add_rng_options(parser);
+
+    options::Options opts = parser.parse();
+    if (parser.dry_run())
+        return nullptr;
+
+    shared_ptr<MergeTreeFactoryLinear> tree_factory = make_shared<MergeTreeFactoryLinear>(opts);
+
+    options::Options strategy_factory_options;
+    strategy_factory_options.set<shared_ptr<MergeTreeFactory>>("merge_tree", tree_factory);
+
+    return make_shared<MergeStrategyFactoryPrecomputed>(strategy_factory_options);
+}
+
 static options::PluginShared<MergeTreeFactory> _plugin("linear", _parse);
+static options::PluginShared<MergeStrategyFactory> _plugin_strategy("merge_linear", _parse_strategy);
 }
