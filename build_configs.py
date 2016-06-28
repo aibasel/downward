10943339e835e9c22dd1b89324187f7c26e435e4
Florian Pommerening <florian.pommerening@unibas.ch>
2016-06-28 17:59:20 +0200

    [main] add option to compile without installed LP solvers

--- build_configs.py
+++ build_configs.py
@@ -1,8 +1,10 @@
 release32 = ["-DCMAKE_BUILD_TYPE=Release"]
 debug32 = ["-DCMAKE_BUILD_TYPE=Debug"]
+release32nolp = ["-DCMAKE_BUILD_TYPE=Release", "-DUSE_LP=NO"]
+debug32nolp = ["-DCMAKE_BUILD_TYPE=Debug", "-DUSE_LP=NO"]
 release64 = ["-DCMAKE_BUILD_TYPE=Release", "-DALLOW_64_BIT=True", "-DCMAKE_CXX_FLAGS='-m64'"]
 debug64 = ["-DCMAKE_BUILD_TYPE=Debug",   "-DALLOW_64_BIT=True", "-DCMAKE_CXX_FLAGS='-m64'"]
 minimal = ["-DCMAKE_BUILD_TYPE=Release", "-DDISABLE_PLUGINS_BY_DEFAULT=YES"]
- 
+
 DEFAULT = "release32"
 DEBUG = "debug32"
