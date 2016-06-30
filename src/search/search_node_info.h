cbf368242786ec394864f52612271d9909580e4c
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-06-30 12:54:54 +0200

    [main] 64bit builds use multiple of 8 for SearchNodeInfo size

--- src/search/search_node_info.h
+++ src/search/search_node_info.h
@@ -1,11 +1,12 @@
 #ifndef SEARCH_NODE_INFO_H
 #define SEARCH_NODE_INFO_H
 
-#include "global_operator.h"
 #include "state_id.h"
 
 // For documentation on classes relevant to storing and working with registered
 // states see the file state_registry.h.
+
+class GlobalOperator;
 
 struct SearchNodeInfo {
     enum NodeStatus {NEW = 0, OPEN = 1, CLOSED = 2, DEAD_END = 3};
@@ -18,14 +19,8 @@ struct SearchNodeInfo {
 
     SearchNodeInfo()
         : status(NEW), g(-1), parent_state_id(StateID::no_state),
-          creating_operator(0), real_g(-1) {
+          creating_operator(nullptr), real_g(-1) {
     }
 };
-
-static_assert(
-    sizeof(SearchNodeInfo) ==
-    sizeof(int) + sizeof(StateID) + sizeof(GlobalOperator *) + sizeof(int),
-    "The size of SearchNodeInfo is larger than expected. This probably means "
-    "that packing two field into one integer using bitfields is not supported.");
 
 #endif
