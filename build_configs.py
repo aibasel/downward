c959928d63d7415c09b5bf452537884598b1dfa5
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-06-30 12:54:54 +0200

    [main] 64bit builds use multiple of 8 for SearchNodeInfo size

--- build_configs.py
+++ build_configs.py
@@ -4,6 +4,8 @@ release32nolp = ["-DCMAKE_BUILD_TYPE=Release", "-DUSE_LP=NO"]
 debug32nolp = ["-DCMAKE_BUILD_TYPE=Debug", "-DUSE_LP=NO"]
 release64 = ["-DCMAKE_BUILD_TYPE=Release", "-DALLOW_64_BIT=True", "-DCMAKE_CXX_FLAGS='-m64'"]
 debug64 = ["-DCMAKE_BUILD_TYPE=Debug",   "-DALLOW_64_BIT=True", "-DCMAKE_CXX_FLAGS='-m64'"]
+release64nolp = ["-DCMAKE_BUILD_TYPE=Release", "-DALLOW_64_BIT=True", "-DCMAKE_CXX_FLAGS='-m64'", "-DUSE_LP=NO"]
+debug64nolp = ["-DCMAKE_BUILD_TYPE=Debug",   "-DALLOW_64_BIT=True", "-DCMAKE_CXX_FLAGS='-m64'", "-DUSE_LP=NO"]
 minimal = ["-DCMAKE_BUILD_TYPE=Release", "-DDISABLE_PLUGINS_BY_DEFAULT=YES"]
 
 DEFAULT = "release32"
