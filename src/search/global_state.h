da278ce8d2abdfd7d8ec1dd7219682c3bb276ad1
Florian Pommerening <florian.pommerening@unibas.ch>
2016-08-12 20:25:54 +0200

    [issue665] store number of variables in GlobalState

--- src/search/global_state.h
+++ src/search/global_state.h
@@ -20,15 +20,21 @@ class GlobalState {
     friend class StateRegistry;
     template<typename Entry>
     friend class PerStateInformation;
+
     // Values for vars are maintained in a packed state and accessed on demand.
     const PackedStateBin *buffer;
+
     // registry isn't a reference because we want to support operator=
     const StateRegistry *registry;
     StateID id;
+
+    int num_variables;
+
     // Only used by the state registry.
     GlobalState(const PackedStateBin *buffer,
                 const StateRegistry &registry,
-                StateID id);
+                StateID id,
+                int num_variables);
 
     const PackedStateBin *get_packed_buffer() const {
         return buffer;
