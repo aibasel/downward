7aa4b2fa19b8118f01ece66e4ef7b8dbb4bf2a41
Florian Pommerening <florian.pommerening@unibas.ch>
2016-08-15 12:32:22 +0200

    [issue665] code review

--- src/search/global_state.cc
+++ src/search/global_state.cc
@@ -11,7 +11,9 @@ using namespace std;
 
 
 GlobalState::GlobalState(
-    const PackedStateBin *buffer, const StateRegistry &registry, StateID id,
+    const PackedStateBin *buffer,
+    const StateRegistry &registry,
+    StateID id,
     int num_variables)
     : buffer(buffer),
       registry(&registry),
@@ -22,6 +24,7 @@ GlobalState::GlobalState(
 }
 
 int GlobalState::operator[](int var) const {
+    assert(var >= 0);
     assert(var < num_variables);
     return registry->get_state_value(buffer, var);
 }
@@ -34,15 +37,13 @@ vector<int> GlobalState::get_values() const {
 }
 
 void dump_pddl(const GlobalState &global_state, const AbstractTask &task) {
-    vector<int> values = global_state.get_values();
-    State state(task, move(values));
+    State state(task, global_state.get_values());
     state.dump_pddl();
 
 }
 
 void dump_fdr(const GlobalState &global_state, const AbstractTask &task) {
-    vector<int> values = global_state.get_values();
-    State state(task, move(values));
+    State state(task, global_state.get_values());
     state.dump_fdr();
 
 }
