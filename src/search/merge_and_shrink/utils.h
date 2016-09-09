7e22ca143764dee46d06edc57145c44ca0ba3629
Silvan Sievers <silvan.sievers@unibas.ch>
2016-09-09 09:46:38 +0200

    [issue668] documentation

--- src/search/merge_and_shrink/utils.h
+++ src/search/merge_and_shrink/utils.h
@@ -10,20 +10,28 @@ class ShrinkStrategy;
 enum class Verbosity;
 
 /*
-  If any of size1 and size2 is larger than max_states_before_merge or
-  if the product of size1 and size2 is larger than max_states, this method
-  computes balanced sizes so that these limits are respected again.
+  Compute target sizes for shrinking two transition systems with sizes size1
+  and size2 before they are merged. Use the following rules:
+  1) Right before merging, the transition systems may have at most
+     max_states_before_merge states.
+  2) Right after merging, the product has at most max_states states.
+  3) Transition systems shrunk as little as necessary to satisfy the above
+     constraints. (If possible, neither is shrunk at all.)
+  There is often a Pareto frontier of solutions following these rules. In this
+  case, balanced solutions (where the target sizes are close to each other)
+  are preferred over less balanced ones.
 */
 extern std::pair<int, int> compute_shrink_sizes(
     int size1, int size2, int max_states, int max_states_before_merge);
 
 /*
   This method checks if the transition system specified via index violates
-  the hard size limit (given via new_size) or the soft size limit
-  (shrink_threshold_before_merge) and uses shrink_strategy to reduce
-  the size of the transition system if necessary.
+  the size limit (given via new_size as computed by compute_shrink_sizes) or
+  the threshold that triggers shrinking even if the size limit is not violated
+  (shrink_threshold_before_merge). If so, the given shrink strategy
+  (shrink_strategy) is used to reduce the size of the transition system to at
+  most new_size.
 */
-
 extern bool shrink_transition_system(
     FactoredTransitionSystem &fts,
     int index,
@@ -32,6 +40,14 @@ extern bool shrink_transition_system(
     const std::shared_ptr<ShrinkStrategy> &shrink_strategy,
     Verbosity verbosity);
 
+/*
+  This method copies the entries given via ts_index1 and ts_index2, shrinks
+  them according to the usual rules (i.e. by using compute_shrink_sizes and
+  shrink_transition_system), and then merges them. The given factored
+  transition system fts is extended with those three new entries. The caller
+  is responsible to delete those copies afterwards with a call to
+  fts.release_copies().
+*/
 extern int shrink_and_merge_temporarily(
     FactoredTransitionSystem &fts,
     int ts_index1,
