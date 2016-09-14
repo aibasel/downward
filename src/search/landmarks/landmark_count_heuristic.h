60a83c0db866fefe00833a721f2d2da877c2afe1
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-09-14 20:13:44 +0200

    [issue551] retrieve OperatorsProxy only once, return vector of facts by value

--- src/search/landmarks/landmark_count_heuristic.h
+++ src/search/landmarks/landmark_count_heuristic.h
@@ -22,27 +22,27 @@ class LandmarkCountHeuristic : public Heuristic {
     std::unique_ptr<LandmarkStatusManager> lm_status_manager;
     std::unique_ptr<LandmarkCostAssignment> lm_cost_assignment;
 
-    int get_heuristic_value(const GlobalState &state);
+    int get_heuristic_value(const GlobalState &global_state);
 
-    void collect_lm_leaves(bool disjunctive_lms, LandmarkSet &result, std::vector<FactPair> &leaves);
+    std::vector<FactPair> collect_lm_leaves(
+        bool disjunctive_lms, LandmarkSet &result);
 
-    bool check_node_orders_disobeyed(const LandmarkNode &node,
-                                     const LandmarkSet &reached) const;
+    bool check_node_orders_disobeyed(
+        const LandmarkNode &node, const LandmarkSet &reached) const;
 
     void add_node_children(LandmarkNode &node, const LandmarkSet &reached) const;
 
-    bool landmark_is_interesting(const State &s, const LandmarkSet &reached,
-                                 LandmarkNode &lm) const;
-    bool generate_helpful_actions(const State &state,
-                                  const LandmarkSet &reached);
-    void set_exploration_goals(const GlobalState &state);
+    bool landmark_is_interesting(
+        const State &s, const LandmarkSet &reached, LandmarkNode &lm) const;
+    bool generate_helpful_actions(
+        const State &state, const LandmarkSet &reached);
+    void set_exploration_goals(const GlobalState &global_state);
 
     void convert_lms(LandmarkSet &lms_set, const std::vector<bool> &lms_vec);
 protected:
     virtual int compute_heuristic(const GlobalState &state) override;
 public:
     explicit LandmarkCountHeuristic(const options::Options &opts);
-    ~LandmarkCountHeuristic() = default;
 
     virtual void notify_initial_state(const GlobalState &initial_state) override;
     virtual bool notify_state_transition(const GlobalState &parent_state,
