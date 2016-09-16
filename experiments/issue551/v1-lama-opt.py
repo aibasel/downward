67f3149a6faa503c7484ba93b2004187cfcfe786
Manuel Heusner <manuel.heusner@unibas.ch>
2016-09-16 18:04:04 +0200

    [issue551] changed experiments

--- experiments/issue551/v1-lama-opt.py
+++ experiments/issue551/v1-lama-opt.py
@@ -12,27 +12,6 @@ SUITE = suites.suite_optimal_strips()
 
 CONFIGS = [
     IssueConfig("seq-opt-bjolp", [], driver_options=["--alias", "seq-opt-bjolp"]),
-    IssueConfig("lm_hm", [
-        "--landmarks", "lm=lm_hm(2)",
-        "--heuristic", "hlm=lmcount(lm)",
-        "--search",    "astar(hlm)"]),
-    IssueConfig("lm_exhaust", [
-        "--landmarks", "lm=lm_exhaust()",
-        "--heuristic", "hlm=lmcount(lm)",
-        "--search",    "astar(hlm)"]),
-    IssueConfig("lm_rhw", [
-        "--landmarks", "lm=lm_rhw()",
-        "--heuristic", "hlm=lmcount(lm)",
-        "--search",    "astar(hlm)"]),
-    IssueConfig("lm_zg", [
-        "--landmarks", "lm=lm_zg()",
-        "--heuristic", "hlm=lmcount(lm)",
-        "--search",    "astar(hlm)"]),
-    IssueConfig("lm_hm_max", [
-        "--landmarks", "lm=lm_hm(2)",
-        "--heuristic", "h1=lmcount(lm,admissible=true)",
-        "--heuristic", "h2=lmcount(lm,admissible=false)",
-        "--search",    "astar(max([h1,h2]))"]),
 ]
 
 exp = IssueExperiment(
--- experiments/issue551/v1-lama-sat.py
+++ experiments/issue551/v1-lama-sat.py
@@ -11,8 +11,23 @@ BENCHMARKS = os.path.expanduser('~/downward-benchmarks')
 SUITE = suites.suite_satisficing()
 
 CONFIGS = [
-    IssueConfig("seq-sat-lama-2011", [], driver_options=["--alias", "seq-sat-lama-2011"]),
     IssueConfig("lama-first", [], driver_options=["--alias", "lama-first"]),
+    IssueConfig("lm_hm", [
+        "--landmarks", "lm=lm_hm(2)",
+        "--heuristic", "hlm=lmcount(lm)",
+        "--search",    "lazy_greedy(hlm)"]),
+    IssueConfig("lm_exhaust", [
+        "--landmarks", "lm=lm_exhaust()",
+        "--heuristic", "hlm=lmcount(lm)",
+        "--search",    "lazy_greedy(hlm)"]),
+    IssueConfig("lm_rhw", [
+        "--landmarks", "lm=lm_rhw()",
+        "--heuristic", "hlm=lmcount(lm)",
+        "--search",    "lazy_greedy(hlm)"]),
+    IssueConfig("lm_zg", [
+        "--landmarks", "lm=lm_zg()",
+        "--heuristic", "hlm=lmcount(lm)",
+        "--search",    "lazy_greedy(hlm)"]),
 ]
 
 exp = IssueExperiment(
