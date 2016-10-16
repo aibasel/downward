f6000162ec65cc1ff69ac0b63b28f31356d8e4d3
Florian Pommerening <florian.pommerening@unibas.ch>
2016-10-16 11:45:10 +0200

    [issue680] Add build configs for different OSI versions

---
+++ issue680_build_configs.py
@@ -0,0 +1,2 @@
+issue680_OSI103 = ["-DCMAKE_BUILD_TYPE=Release", "-DDOWNWARD_COIN_ROOT32=/infai/pommeren/local/opt/coin-0.103.0-32"]
+issue680_OSI107 = ["-DCMAKE_BUILD_TYPE=Release", "-DDOWNWARD_COIN_ROOT32=/infai/pommeren/local/opt/coin-0.107.8-32"]
