69ab0eae1a17db7108f94ab44a5abc7ae425438c
Silvan Sievers <silvan.sievers@unibas.ch>
2016-10-21 13:29:08 +0200

    [issue682] only compute merge strategy if the atomic fts is not already unsolvable

--- src/search/merge_and_shrink/merge_and_shrink_heuristic.cc
+++ src/search/merge_and_shrink/merge_and_shrink_heuristic.cc
@@ -242,12 +242,12 @@ void MergeAndShrinkHeuristic::build_transition_system(const utils::Timer &timer)
     print_time(timer, "after computation of atomic transition systems");
     cout << endl;
 
-    unique_ptr<MergeStrategy> merge_strategy =
-        merge_strategy_factory->compute_merge_strategy(task, *fts);
-    merge_strategy_factory = nullptr;
-
     int final_index = -1; // TODO: get rid of this
     if (fts->is_solvable()) { // All atomic transition system are solvable.
+        unique_ptr<MergeStrategy> merge_strategy =
+            merge_strategy_factory->compute_merge_strategy(task, *fts);
+        merge_strategy_factory = nullptr;
+
         int number_of_merges = task_proxy.get_variables().size() - 1;
         for (int i = 0; i < number_of_merges; ++i) {
             // Choose next transition systems to merge
