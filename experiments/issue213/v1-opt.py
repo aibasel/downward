fec15326c84992543259711d3ac818c4c4dbc516
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-11-28 23:52:32 +0100

    [issue213] add experiment comparing optimal configs

--- experiments/issue213/v1-opt.py
+++ experiments/issue213/v1-opt.py
@@ -5,27 +5,42 @@ import os
 
 from lab.environments import LocalEnvironment, MaiaEnvironment
 
-from common_setup import IssueConfig, IssueExperiment, is_test_run
+from downward.reports.compare import ComparativeReport
+
+import common_setup
+from common_setup import IssueConfig, IssueExperiment
 
 
 BENCHMARKS_DIR = os.environ["DOWNWARD_BENCHMARKS"]
-REVISIONS = ["issue591-base", "issue591-v1"]
-CONFIGS = [
-    IssueConfig(heuristic, ["--search", "astar({})".format(heuristic)])
-    for heuristic in [
-        "blind()", "cegar(max_states=10000)", "hm()", "lmcut()", "hmax()"]
+REVISIONS = ["issue213-base"]
+BUILDS = ["release32", "release64"]
+SEARCHES = [
+    ("bjolp", "astar(lmcount(lm_merged([lm_rhw(), lm_hm(m=1)]), admissible=true), mpd=true)"),
+    ("blind", "astar(blind())"),
+    ("cegar", "astar(cegar())"),
+    ("divpot", "astar(diverse_potentials())"),
+    ("ipdb", "astar(ipdb(max_time=900))"),
+    ("lmcut", "astar(lmcut())"),
+    ("mas",
+        "astar(merge_and_shrink(shrink_strategy=shrink_bisimulation(greedy=false), "
+        "merge_strategy=merge_dfp(), "
+        "label_reduction=exact(before_shrinking=true, before_merging=false), max_states=100000, threshold_before_merge=1))"),
+    ("seq", "astar(operatorcounting([state_equation_constraints()]))"),
 ]
-SUITE = [
-    'barman-opt14-strips', 'cavediving-14-adl', 'childsnack-opt14-strips',
-    'citycar-opt14-adl', 'floortile-opt14-strips', 'ged-opt14-strips',
-    'hiking-opt14-strips', 'maintenance-opt14-adl',
-    'openstacks-opt14-strips', 'parking-opt14-strips',
-    'tetris-opt14-strips', 'tidybot-opt14-strips', 'transport-opt14-strips',
-    'visitall-opt14-strips']
+CONFIGS = [
+    IssueConfig(
+        "{nick}-{build}".format(**locals()),
+        ["--search", search],
+        build_options=[build],
+        driver_options=["--build", build])
+    for nick, search in SEARCHES
+    for build in BUILDS
+]
+SUITE = common_setup.DEFAULT_OPTIMAL_SUITE
 ENVIRONMENT = MaiaEnvironment(
     priority=0, email="jendrik.seipp@unibas.ch")
 
-if is_test_run():
+if common_setup.is_test_run():
     SUITE = IssueExperiment.DEFAULT_TEST_SUITE
     ENVIRONMENT = LocalEnvironment(processes=1)
 
@@ -37,7 +52,10 @@ exp = IssueExperiment(
 exp.add_suite(BENCHMARKS_DIR, SUITE)
 
 exp.add_absolute_report_step()
-exp.add_comparison_table_step()
-exp.add_scatter_plot_step(attributes=["total_time"])
+
+algorithm_pairs = []
+exp.add_report(ComparativeReport(algorithm_pairs))
+#exp.add_scatter_plot_step(attributes=["total_time", "memory"])
+exp.add_absolute_report_step()
 
 exp()
