86d8a16a7d09c55696a8d1692325a3ef15f0bce0
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-11-28 23:52:32 +0100

    [issue213] add experiment comparing optimal configs

--- experiments/issue213/common_setup.py
+++ experiments/issue213/common_setup.py
@@ -12,7 +12,7 @@ from lab import tools
 
 from downward.experiment import FastDownwardExperiment
 from downward.reports.absolute import AbsoluteReport
-from downward.reports.compare import CompareConfigsReport
+from downward.reports.compare import ComparativeReport
 from downward.reports.scatter import ScatterPlotReport
 
 from relativescatter import RelativeScatterPlotReport
@@ -29,6 +29,57 @@ def parse_args():
     return ARGPARSER.parse_args()
 
 ARGS = parse_args()
+
+
+DEFAULT_OPTIMAL_SUITE = [
+    'airport', 'barman-opt11-strips', 'barman-opt14-strips', 'blocks',
+    'childsnack-opt14-strips', 'depot', 'driverlog',
+    'elevators-opt08-strips', 'elevators-opt11-strips',
+    'floortile-opt11-strips', 'floortile-opt14-strips', 'freecell',
+    'ged-opt14-strips', 'grid', 'gripper', 'hiking-opt14-strips',
+    'logistics00', 'logistics98', 'miconic', 'movie', 'mprime',
+    'mystery', 'nomystery-opt11-strips', 'openstacks-opt08-strips',
+    'openstacks-opt11-strips', 'openstacks-opt14-strips',
+    'openstacks-strips', 'parcprinter-08-strips',
+    'parcprinter-opt11-strips', 'parking-opt11-strips',
+    'parking-opt14-strips', 'pathways-noneg', 'pegsol-08-strips',
+    'pegsol-opt11-strips', 'pipesworld-notankage',
+    'pipesworld-tankage', 'psr-small', 'rovers', 'satellite',
+    'scanalyzer-08-strips', 'scanalyzer-opt11-strips',
+    'sokoban-opt08-strips', 'sokoban-opt11-strips', 'storage',
+    'tetris-opt14-strips', 'tidybot-opt11-strips',
+    'tidybot-opt14-strips', 'tpp', 'transport-opt08-strips',
+    'transport-opt11-strips', 'transport-opt14-strips',
+    'trucks-strips', 'visitall-opt11-strips', 'visitall-opt14-strips',
+    'woodworking-opt08-strips', 'woodworking-opt11-strips',
+    'zenotravel']
+
+DEFAULT_SATISFICING_SUITE = [
+    'airport', 'assembly', 'barman-sat11-strips',
+    'barman-sat14-strips', 'blocks', 'cavediving-14-adl',
+    'childsnack-sat14-strips', 'citycar-sat14-adl', 'depot',
+    'driverlog', 'elevators-sat08-strips', 'elevators-sat11-strips',
+    'floortile-sat11-strips', 'floortile-sat14-strips', 'freecell',
+    'ged-sat14-strips', 'grid', 'gripper', 'hiking-sat14-strips',
+    'logistics00', 'logistics98', 'maintenance-sat14-adl', 'miconic',
+    'miconic-fulladl', 'miconic-simpleadl', 'movie', 'mprime',
+    'mystery', 'nomystery-sat11-strips', 'openstacks',
+    'openstacks-sat08-adl', 'openstacks-sat08-strips',
+    'openstacks-sat11-strips', 'openstacks-sat14-strips',
+    'openstacks-strips', 'optical-telegraphs', 'parcprinter-08-strips',
+    'parcprinter-sat11-strips', 'parking-sat11-strips',
+    'parking-sat14-strips', 'pathways', 'pathways-noneg',
+    'pegsol-08-strips', 'pegsol-sat11-strips', 'philosophers',
+    'pipesworld-notankage', 'pipesworld-tankage', 'psr-large',
+    'psr-middle', 'psr-small', 'rovers', 'satellite',
+    'scanalyzer-08-strips', 'scanalyzer-sat11-strips', 'schedule',
+    'sokoban-sat08-strips', 'sokoban-sat11-strips', 'storage',
+    'tetris-sat14-strips', 'thoughtful-sat14-strips',
+    'tidybot-sat11-strips', 'tpp', 'transport-sat08-strips',
+    'transport-sat11-strips', 'transport-sat14-strips', 'trucks',
+    'trucks-strips', 'visitall-sat11-strips', 'visitall-sat14-strips',
+    'woodworking-sat08-strips', 'woodworking-sat11-strips',
+    'zenotravel']
 
 
 def get_script():
@@ -120,7 +171,7 @@ class IssueExperiment(FastDownwardExperiment):
         "error",
         "evaluations",
         "expansions",
-        "expansions_until_last_jump",
+        "expanded_until_last_jump",
         "generated",
         "memory",
         "quality",
@@ -270,7 +321,7 @@ class IssueExperiment(FastDownwardExperiment):
                         ("%s-%s" % (rev1, config_nick),
                          "%s-%s" % (rev2, config_nick),
                          "Diff (%s)" % config_nick))
-                report = CompareConfigsReport(compared_configs, **kwargs)
+                report = ComparativeReport(compared_configs, **kwargs)
                 outfile = os.path.join(
                     self.eval_dir,
                     "%s-%s-%s-compare.%s" % (
