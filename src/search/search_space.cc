4a5e5dae0e5ed3bb41f2925d33f7d786791a78dd
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-12-11 21:35:17 +0100

    [issue692] remove "else" block

--- src/search/search_space.cc
+++ src/search/search_space.cc
@@ -134,11 +134,10 @@ void SearchSpace::trace_path(const GlobalState &goal_state,
         if (info.creating_operator == -1) {
             assert(info.parent_state_id == StateID::no_state);
             break;
-        } else {
-            assert(utils::in_bounds(info.creating_operator, g_operators));
-            const GlobalOperator *op = &g_operators[info.creating_operator];
-            path.push_back(op);
         }
+        assert(utils::in_bounds(info.creating_operator, g_operators));
+        const GlobalOperator *op = &g_operators[info.creating_operator];
+        path.push_back(op);
         current_state = state_registry.lookup_state(info.parent_state_id);
     }
     reverse(path.begin(), path.end());
