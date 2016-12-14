c0a91a2735a2dba4fb8f899ade0cbca5f51a937c
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-12-14 10:09:44 +0100

    [issue693] turn macros into inline functions

--- src/search/utils/hash.cc
+++ src/search/utils/hash.cc
@@ -10,7 +10,12 @@ namespace utils {
   in lookup3.c.
 */
 
-#define rot(x, k) (((x) << (k)) | ((x) >> (32 - (k))))
+/*
+  Circular rotation.
+*/
+inline unsigned int rot(unsigned int x, unsigned int k) {
+    return (x << k) | (x >> (32 - k));
+}
 
 /*
   mix 3 32-bit values reversibly.
@@ -18,15 +23,14 @@ namespace utils {
   Any information in (a,b,c) before mix() is still in (a,b,c) after
   mix().
 */
-#define mix(a, b, c) \
-    { \
-        a -= c;  a ^= rot(c, 4);  c += b; \
-        b -= a;  b ^= rot(a, 6);  a += c; \
-        c -= b;  c ^= rot(b, 8);  b += a; \
-        a -= c;  a ^= rot(c, 16);  c += b; \
-        b -= a;  b ^= rot(a, 19);  a += c; \
-        c -= b;  c ^= rot(b, 4);  b += a; \
-    }
+inline void mix(unsigned int &a, unsigned int &b, unsigned int &c) {
+    a -= c; a ^= rot(c, 4); c += b;
+    b -= a; b ^= rot(a, 6); a += c;
+    c -= b; c ^= rot(b, 8); b += a;
+    a -= c; a ^= rot(c, 16); c += b;
+    b -= a; b ^= rot(a, 19); a += c;
+    c -= b; c ^= rot(b, 4); b += a;
+}
 
 /*
   final mixing of 3 32-bit values (a,b,c) into c.
@@ -34,16 +38,15 @@ namespace utils {
   Pairs of (a,b,c) values differing in only a few bits will usually
   produce values of c that look totally different.
 */
-#define final(a, b, c) \
-    { \
-        c ^= b; c -= rot(b, 14); \
-        a ^= c; a -= rot(c, 11); \
-        b ^= a; b -= rot(a, 25); \
-        c ^= b; c -= rot(b, 16); \
-        a ^= c; a -= rot(c, 4);  \
-        b ^= a; b -= rot(a, 14); \
-        c ^= b; c -= rot(b, 24); \
-    }
+inline void final(unsigned int &a, unsigned int &b, unsigned int &c) {
+    c ^= b; c -= rot(b, 14);
+    a ^= c; a -= rot(c, 11);
+    b ^= a; b -= rot(a, 25);
+    c ^= b; c -= rot(b, 16);
+    a ^= c; a -= rot(c, 4);
+    b ^= a; b -= rot(a, 14);
+    c ^= b; c -= rot(b, 24);
+}
 
 unsigned int hash_unsigned_int_sequence(
     const unsigned int *k, unsigned int length, unsigned int initval) {
