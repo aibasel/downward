6f2430154b9a0cf705d062441418f434d1e2209c
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-12-14 15:38:32 +0100

    [issue693] adapt variable names

--- src/search/utils/hash.cc
+++ src/search/utils/hash.cc
@@ -2,8 +2,9 @@
 
 namespace utils {
 /*
-  Code for rot(), mix(), final() and hash_unsigned_int_sequence() taken
-  from lookup3.c, by Bob Jenkins, May 2006, Public Domain.
+  The functions rotate(), mix(), final_mix() and
+  hash_unsigned_int_sequence() have been taken and adapted from
+  lookup3.c, by Bob Jenkins, May 2006, Public Domain.
   (http://www.burtleburtle.net/bob/c/lookup3.c)
 
   hash_unsigned_int_sequence() corresponds to the hashword() function
@@ -13,86 +14,86 @@ namespace utils {
 /*
   Circular rotation.
 */
-static inline unsigned int rot(unsigned int x, unsigned int k) {
-    return (x << k) | (x >> (32 - k));
+static inline unsigned int rotate(unsigned int value, unsigned int offset) {
+    return (value << offset) | (value >> (32 - offset));
 }
 
 /*
   mix 3 32-bit values reversibly.
 
-  Any information in (a,b,c) before mix() is still in (a,b,c) after
+  Any information in (a, b, c) before mix() is still in (a, b, c) after
   mix().
 */
 static inline void mix(unsigned int &a, unsigned int &b, unsigned int &c) {
     a -= c;
-    a ^= rot(c, 4);
+    a ^= rotate(c, 4);
     c += b;
     b -= a;
-    b ^= rot(a, 6);
+    b ^= rotate(a, 6);
     a += c;
     c -= b;
-    c ^= rot(b, 8);
+    c ^= rotate(b, 8);
     b += a;
     a -= c;
-    a ^= rot(c, 16);
+    a ^= rotate(c, 16);
     c += b;
     b -= a;
-    b ^= rot(a, 19);
+    b ^= rotate(a, 19);
     a += c;
     c -= b;
-    c ^= rot(b, 4);
+    c ^= rotate(b, 4);
     b += a;
 }
 
 /*
-  final mixing of 3 32-bit values (a,b,c) into c.
+  final mixing of 3 32-bit values (a, b, c) into c.
 
   Pairs of (a,b,c) values differing in only a few bits will usually
   produce values of c that look totally different.
 */
-static inline void final (unsigned int &a, unsigned int &b, unsigned int &c) {
+static inline void final_mix(unsigned int &a, unsigned int &b, unsigned int &c) {
     c ^= b;
-    c -= rot(b, 14);
+    c -= rotate(b, 14);
     a ^= c;
-    a -= rot(c, 11);
+    a -= rotate(c, 11);
     b ^= a;
-    b -= rot(a, 25);
+    b -= rotate(a, 25);
     c ^= b;
-    c -= rot(b, 16);
+    c -= rotate(b, 16);
     a ^= c;
-    a -= rot(c, 4);
+    a -= rotate(c, 4);
     b ^= a;
-    b -= rot(a, 14);
+    b -= rotate(a, 14);
     c ^= b;
-    c -= rot(b, 24);
+    c -= rotate(b, 24);
 }
 
 unsigned int hash_unsigned_int_sequence(
-    const unsigned int *k, unsigned int length, unsigned int initval) {
+    const unsigned int *key, unsigned int length, unsigned int initial_value) {
     unsigned int a, b, c;
 
     // Set up the internal state.
-    a = b = c = 0xdeadbeef + (length << 2) + initval;
+    a = b = c = 0xdeadbeef + (length << 2) + initial_value;
 
     // Handle most of the key.
     while (length > 3) {
-        a += k[0];
-        b += k[1];
-        c += k[2];
+        a += key[0];
+        b += key[1];
+        c += key[2];
         mix(a, b, c);
         length -= 3;
-        k += 3;
+        key += 3;
     }
 
     // Handle the last 3 unsigned ints. All case statements fall through.
     switch (length) {
     case 3:
-        c += k[2];
+        c += key[2];
     case 2:
-        b += k[1];
+        b += key[1];
     case 1:
-        a += k[0];
-        final (a, b, c);
+        a += key[0];
+        final_mix(a, b, c);
     // case 0: nothing left to add.
     case 0:
         break;
