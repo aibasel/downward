ff37739a84d97ed8583782b9452b5630e3c0f305
Florian Pommerening <florian.pommerening@unibas.ch>
2016-12-20 12:09:23 +0100

    [issue638] fix import

--- experiments/issue638/v1.py
+++ experiments/issue638/v1.py
@@ -17,8 +17,8 @@ CONFIGS = [
         "operatorcounting([pho_constraints(patterns=systematic(2))])"]
 ]
 
-sys.path.append(os.path.dirname(BENCHMARKS_DIR))
-from benchmarks import suites
+sys.path.append(BENCHMARKS_DIR)
+import suites
 
 SUITE = suites.suite_optimal_strips()
 ENVIRONMENT = MaiaEnvironment(
