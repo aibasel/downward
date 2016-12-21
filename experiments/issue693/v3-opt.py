28639f3c32f89635977ddbb49af96ef8c0255e25
Jendrik Seipp <jendrik.seipp@unibas.ch>
2016-12-21 20:20:50 +0100

    [issue693] add experiments for v3

---
+++ experiments/issue693/v3-opt.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import itertools
+import os
+
+from lab.environments import LocalEnvironment, MaiaEnvironment
+
+from downward.reports.compare import ComparativeReport
+
+import common_setup
+from common_setup import IssueConfig, IssueExperiment, RelativeScatterPlotReport
+
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+BENCHMARKS_DIR = os.environ["DOWNWARD_BENCHMARKS"]
+REVISIONS = ["issue693-v2", "issue693-v3"]
+BUILDS = ["release32", "release64"]
+SEARCHES = [
+    ("blind", "astar(blind())"),
+    ("divpot", "astar(diverse_potentials())"),
+    ("lmcut", "astar(lmcut())"),
+    ("cegar", "astar(cegar())"),
+    ("systematic2", "astar(cpdbs(systematic(2)))"),
+]
+CONFIGS = [
+    IssueConfig(
+        "{nick}-{build}".format(**locals()),
+        ["--search", search],
+        build_options=[build],
+        driver_options=["--build", build])
+    for nick, search in SEARCHES
+    for build in BUILDS
+]
+SUITE = common_setup.DEFAULT_OPTIMAL_SUITE
+ENVIRONMENT = MaiaEnvironment(
+    priority=0, email="jendrik.seipp@unibas.ch")
+
+if common_setup.is_test_run():
+    SUITE = IssueExperiment.DEFAULT_TEST_SUITE
+    ENVIRONMENT = LocalEnvironment(processes=1)
+
+exp = IssueExperiment(
+    revisions=REVISIONS,
+    configs=CONFIGS,
+    environment=ENVIRONMENT,
+)
+exp.add_suite(BENCHMARKS_DIR, SUITE)
+exp.add_absolute_report_step()
+exp.add_comparison_table_step()
+
+exp.run_steps()
