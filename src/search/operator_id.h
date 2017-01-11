bd6071eb7d9c2bd34a01075c64975621e0e36ffb
Florian Pommerening <florian.pommerening@unibas.ch>
2017-01-11 11:50:55 +0100

    [issue688] introduce operator id

---
+++ src/search/operator_id.h
@@ -0,0 +1,48 @@
+#ifndef OPERATOR_ID_H
+#define OPERATOR_ID_H
+
+#include <iostream>
+
+/*
+  Operator ids identify operators as opposed to action ids that can
+  identify operators or axioms. The task they refer to is not stored,
+  so it is the user's responsibility to avoid mixing ids from
+  different tasks.
+*/
+class OperatorID {
+    int index;
+
+public:
+    explicit OperatorID(int index)
+        : index(index) {
+    }
+
+    int get_index() {
+        return index;
+    }
+
+    bool operator==(const OperatorID &other) const {
+        return index == other.index;
+    }
+
+    bool operator!=(const OperatorID &other) const {
+        return !(*this == other);
+    }
+
+    size_t hash() const {
+        return index;
+    }
+};
+
+std::ostream &operator<<(std::ostream &os, OperatorID id);
+
+namespace std {
+template<>
+struct hash<OperatorID> {
+    size_t operator()(OperatorID id) const {
+        return id.hash();
+    }
+};
+}
+
+#endif
