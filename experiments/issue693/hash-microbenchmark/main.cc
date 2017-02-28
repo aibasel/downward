c2689b7424bc266f168775cfa9695a509352a8da
Jendrik Seipp <jendrik.seipp@unibas.ch>
2017-02-28 10:47:57 +0100

    [issue693] repeat whole benchmark instead of individual tests

--- experiments/issue693/hash-microbenchmark/main.cc
+++ experiments/issue693/hash-microbenchmark/main.cc
@@ -13,16 +13,13 @@ using namespace std;
 static void benchmark(const string &desc, int num_calls,
                       const function<void()> &func) {
     cout << "Running " << desc << " " << num_calls << " times:" << flush;
-    const int REPETITIONS = 10;
-    for (int i = 0; i < REPETITIONS; ++i) {
-        clock_t start = clock();
-        for (int j = 0; j < num_calls; ++j)
-            func();
-        clock_t end = clock();
-        double duration = static_cast<double>(end - start) / CLOCKS_PER_SEC;
-        cout << " " << duration << "s" << flush;
-    }
-    cout << endl;
+
+    clock_t start = clock();
+    for (int j = 0; j < num_calls; ++j)
+        func();
+    clock_t end = clock();
+    double duration = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+    cout << " " << duration << "s" << endl;
 }
 
 
@@ -32,73 +29,77 @@ static int scramble(int i) {
 
 
 int main(int, char **) {
-    const int NUM_ITERATIONS = 10000;
+    const int REPETITIONS = 5;
+    const int NUM_CALLS = 10000;
     const int NUM_INSERTIONS = 100;
 
-    benchmark("nothing", NUM_ITERATIONS, [] () {});
-    cout << endl;
-    benchmark("insert int into std::unordered_set", NUM_ITERATIONS,
-              [&]() {
-        unordered_set<int> s;
-        for (int i = 0; i < NUM_INSERTIONS; ++i) {
-            s.insert(scramble(i));
-        }
-    });
-    benchmark("insert int into utils::HashSet", NUM_ITERATIONS,
-              [&]() {
-        utils::HashSet<int> s;
-        for (int i = 0; i < NUM_INSERTIONS; ++i) {
-            s.insert(scramble(i));
-        }
-    });
-    cout << endl;
-
-    benchmark("insert pair<int, int> into std::unordered_set", NUM_ITERATIONS,
-              [&]() {
-        unordered_set<pair<int, int>> s;
-        for (int i = 0; i < NUM_INSERTIONS; ++i) {
-            s.insert(make_pair(scramble(i), scramble(i + 1)));
-        }
-    });
-    benchmark("insert pair<int, int> into utils::HashSet", NUM_ITERATIONS,
-              [&]() {
-        utils::HashSet<pair<int, int>> s;
-        for (int i = 0; i < NUM_INSERTIONS; ++i) {
-            s.insert(make_pair(scramble(i), scramble(i + 1)));
-        }
-    });
-    cout << endl;
-
-    for (int length : {1, 10, 100, 101}
-         ) {
-        benchmark(
-            "insert vector<int> of size " + to_string(length) +
-            " into std::unordered_set", NUM_ITERATIONS,
-            [&]() {
-            unordered_set<vector<int>> s;
+    for (int i = 0; i < REPETITIONS; ++i) {
+        benchmark("nothing", NUM_CALLS, [] () {});
+        cout << endl;
+        benchmark("insert int into std::unordered_set", NUM_CALLS,
+                  [&]() {
+            unordered_set<int> s;
             for (int i = 0; i < NUM_INSERTIONS; ++i) {
-                vector<int> v;
-                v.reserve(length);
-                for (int j = 0; j < length; ++j) {
-                    v.push_back(scramble(NUM_INSERTIONS * length + j));
-                }
-                s.insert(v);
+                s.insert(scramble(i));
             }
         });
-        benchmark(
-            "insert vector<int> of size " + to_string(length) +
-            " into utils::HashSet", NUM_ITERATIONS,
-            [&]() {
-            utils::HashSet<vector<int>> s;
+        benchmark("insert int into utils::HashSet", NUM_CALLS,
+                  [&]() {
+            utils::HashSet<int> s;
             for (int i = 0; i < NUM_INSERTIONS; ++i) {
-                vector<int> v;
-                v.reserve(length);
-                for (int j = 0; j < length; ++j) {
-                    v.push_back(scramble(NUM_INSERTIONS * length + j));
-                }
-                s.insert(v);
+                s.insert(scramble(i));
             }
         });
+        cout << endl;
+
+        benchmark("insert pair<int, int> into std::unordered_set", NUM_CALLS,
+                  [&]() {
+            unordered_set<pair<int, int>> s;
+            for (int i = 0; i < NUM_INSERTIONS; ++i) {
+                s.insert(make_pair(scramble(i), scramble(i + 1)));
+            }
+        });
+        benchmark("insert pair<int, int> into utils::HashSet", NUM_CALLS,
+                  [&]() {
+            utils::HashSet<pair<int, int>> s;
+            for (int i = 0; i < NUM_INSERTIONS; ++i) {
+                s.insert(make_pair(scramble(i), scramble(i + 1)));
+            }
+        });
+        cout << endl;
+
+        for (int length : {1, 10, 100, 101}
+             ) {
+            benchmark(
+                "insert vector<int> of size " + to_string(length) +
+                " into std::unordered_set", NUM_CALLS,
+                [&]() {
+                unordered_set<vector<int>> s;
+                for (int i = 0; i < NUM_INSERTIONS; ++i) {
+                    vector<int> v;
+                    v.reserve(length);
+                    for (int j = 0; j < length; ++j) {
+                        v.push_back(scramble(NUM_INSERTIONS * length + j));
+                    }
+                    s.insert(v);
+                }
+            });
+            benchmark(
+                "insert vector<int> of size " + to_string(length) +
+                " into utils::HashSet", NUM_CALLS,
+                [&]() {
+                utils::HashSet<vector<int>> s;
+                for (int i = 0; i < NUM_INSERTIONS; ++i) {
+                    vector<int> v;
+                    v.reserve(length);
+                    for (int j = 0; j < length; ++j) {
+                        v.push_back(scramble(NUM_INSERTIONS * length + j));
+                    }
+                    s.insert(v);
+                }
+            });
+            cout << endl;
+        }
         cout << endl;
     }
 
