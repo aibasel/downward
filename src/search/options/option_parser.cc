c8e29bc361a5252fd9ca9d2ebd750356689c5af7
Jendrik Seipp <jendrik.seipp@unibas.ch>
2017-04-26 13:29:02 +0200

    [issue719] use range-based for-loop

--- src/search/options/option_parser.cc
+++ src/search/options/option_parser.cc
@@ -334,8 +334,8 @@ SearchEngine *OptionParser::parse_cmd_line_aux(
             if (helpiands.empty()) {
                 get_full_help();
             } else {
-                for (size_t j = 0; j != helpiands.size(); ++j) {
-                    get_help(helpiands[j]);
+                for (const string &helpiand : helpiands) {
+                    get_help(helpiand);
                 }
             }
             DocPrinter *dp;
