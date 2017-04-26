a1c9465a21930469370f4098488f66af6012aae7
Daniel Killenberger <daniel.killenberger@unibas.ch>
2017-04-26 22:03:05 +0200

    [issue720] Fix include ordering and dependencies

--- src/search/pdbs/pattern_collection_generator_genetic.cc
+++ src/search/pdbs/pattern_collection_generator_genetic.cc
@@ -7,12 +7,12 @@
 #include "../plugin.h"
 #include "../task_proxy.h"
 
+#include "../task_utils/causal_graph.h"
 #include "../utils/markup.h"
 #include "../utils/math.h"
 #include "../utils/rng.h"
 #include "../utils/rng_options.h"
 #include "../utils/timer.h"
-#include "../task_utils/causal_graph.h"
 
 #include <algorithm>
 #include <cassert>
