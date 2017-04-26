3920fef12e0c81a5cf769442e1061c655481a0d2
Silvan Sievers <silvan.sievers@unibas.ch>
2017-04-26 17:59:40 +0200

    [issue667] change class SCCs into a function compute_maximal_sccs

--- src/search/algorithms/sccs.h
+++ src/search/algorithms/sccs.h
@@ -1,43 +1,35 @@
 #ifndef ALGORITHMS_SCCS_H
 #define ALGORITHMS_SCCS_H
 
-/*
-  This implements Tarjan's linear-time algorithm for finding the maximal
-  strongly connected components. It takes time proportional to the sum
-  of the number of vertices and arcs.
-
-  Instantiate class SCC with a graph represented as a vector of vectors,
-  where graph[i] is the vector of successors of vertex i.
-
-  Method get_result() returns a vector of strongly connected components,
-  each of which is a vector of vertices (ints).
-  This is a partitioning of all vertices where each SCC is a maximal subset
-  such that each node in an SCC is reachable from all other nodes in the SCC.
-  Note that the derived graph where each SCC is a single "supernode" is
-  necessarily acyclic. The SCCs returned by get_result() are in a topological
-  sort order with regard to this derived DAG.
-*/
-
 #include <vector>
 
 namespace sccs {
-class SCCs {
-    const std::vector<std::vector<int>> &graph;
+void dfs(
+    const std::vector<std::vector<int>> &graph,
+    int vertex,
+    std::vector<int> &dfs_numbers,
+    std::vector<int> &dfs_minima,
+    std::vector<int> &stack_indices,
+    std::vector<int> &stack,
+    int &current_dfs_number,
+    std::vector<std::vector<int>> &sccs);
 
-    // The following three are indexed by vertex number.
-    std::vector<int> dfs_numbers;
-    std::vector<int> dfs_minima;
-    std::vector<int> stack_indices;
+/*
+  This function implements Tarjan's linear-time algorithm for finding the
+  maximal strongly connected components. It takes time proportional to the sum
+  of the number of vertices and arcs.
 
-    std::vector<int> stack; // This is indexed by the level of recursion.
-    std::vector<std::vector<int>> sccs;
+  Parameter: a graph represented as a vector of vectors, where graph[i] is the
+  vector of successors of vertex i.
 
-    int current_dfs_number;
-
-    void dfs(int vertex);
-public:
-    SCCs(const std::vector<std::vector<int>> &graph);
-    const std::vector<std::vector<int>> &get_result();
-};
+  Return value: a vector of strongly connected components, each of which is a
+  vector of vertices (ints). This is a partitioning of all vertices where each
+  SCC is a maximal subset such that each node in an SCC is reachable from all
+  other nodes in the SCC. Note that the derived graph where each SCC is a
+  single "supernode" is necessarily acyclic. The SCCs returned by this function
+  are in a topological sort order with regard to this derived DAG.
+*/
+std::vector<std::vector<int>> compute_maximal_sccs(
+    const std::vector<std::vector<int>> &graph);
 }
 #endif
