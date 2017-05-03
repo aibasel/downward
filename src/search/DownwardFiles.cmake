fd6bcb3e482024a3f57cfd1a25cd01e7ac612a4d
Daniel Killenberger <daniel.killenberger@unibas.ch>
2017-05-03 15:32:36 +0200

    [issue721] Extract parsing options for lazy_greedy into plugin_lazy_greedy.cc

--- src/search/DownwardFiles.cmake
+++ src/search/DownwardFiles.cmake
@@ -348,6 +348,14 @@ fast_downward_plugin(
 )
 
 fast_downward_plugin(
+    NAME PLUGIN_LAZY_GREEDY
+    HELP "Eager greedy best-first search"
+    SOURCES
+        search_engines/plugin_lazy_greedy
+    DEPENDS LAZY_SEARCH
+)
+
+fast_downward_plugin(
     NAME ENFORCED_HILL_CLIMBING_SEARCH
     HELP "Lazy enforced hill-climbing search algorithm"
     SOURCES
