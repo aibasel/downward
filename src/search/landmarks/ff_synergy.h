7a597ad29d483047192f4fb54928b8f9a72799c1
Cedric Geissmann <cedric.geissmann@unibas.ch>
2017-06-26 16:35:26 +0200

    [issue717] Changed documentation

--- src/search/landmarks/ff_synergy.h
+++ src/search/landmarks/ff_synergy.h
@@ -6,7 +6,7 @@
 namespace landmarks {
 class LamaSynergyHeuristic;
 
-// See documentation of LamaSynergyHeuristic
+// See documentation of LamaSynergyHeuristic.
 class FFSynergyHeuristic : public Heuristic {
     LamaSynergyHeuristic *master;
 
