6c0a6da6309c5ed75575ce5d7e6f6e8e94251272
Silvan Sievers <silvan.sievers@unibas.ch>
2017-07-06 09:29:26 +0200

    [issue707] code review: no default implementation for 'require_*_distances'

--- src/search/merge_and_shrink/merge_strategy_factory.h
+++ src/search/merge_and_shrink/merge_strategy_factory.h
@@ -21,14 +21,8 @@ public:
     virtual std::unique_ptr<MergeStrategy> compute_merge_strategy(
         const TaskProxy &task_proxy,
         const FactoredTransitionSystem &fts) = 0;
-
-    virtual bool requires_init_distances() const {
-        return false;
-    }
-
-    virtual bool requires_goal_distances() const {
-        return false;
-    }
+    virtual bool requires_init_distances() const = 0;
+    virtual bool requires_goal_distances() const = 0;
 };
 }
 
