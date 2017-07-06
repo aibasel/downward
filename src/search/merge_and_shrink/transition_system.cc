2a5559379d893c15cc84de40d9a80614896e291e
Silvan Sievers <silvan.sievers@unibas.ch>
2017-07-06 10:37:14 +0200

    [issue707] use explicit constant

--- src/search/merge_and_shrink/transition_system.cc
+++ src/search/merge_and_shrink/transition_system.cc
@@ -214,6 +214,7 @@ unique_ptr<TransitionSystem> TransitionSystem::merge(
         label_equivalence_relation->add_label_group(dead_labels);
     }
 
+    const compute_label_equivalence_relation = false;
     return utils::make_unique_ptr<TransitionSystem>(
         num_variables,
         move(incorporated_variables),
@@ -222,7 +223,7 @@ unique_ptr<TransitionSystem> TransitionSystem::merge(
         num_states,
         move(goal_states),
         init_state,
-        false
+        compute_label_equivalence_relation
         );
 }
 
