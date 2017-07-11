02c43b4b2aa9efd94ecb470c180953b73f0a1375
Silvan Sievers <silvan.sievers@unibas.ch>
2017-07-11 21:35:34 +0200

    [issue668] simplify temporary merges further: only copy TS if necessary, do not copy distances

--- src/search/merge_and_shrink/utils.cc
+++ src/search/merge_and_shrink/utils.cc
@@ -6,6 +6,7 @@
 #include "transition_system.h"
 
 #include "../utils/math.h"
+#include "../utils/memory.h"
 
 #include <algorithm>
 #include <cassert>
@@ -220,25 +221,39 @@ bool is_goal_relevant(const TransitionSystem &ts) {
     return false;
 }
 
-void shrink_ts(
+/*
+  Compute a state equivalence relation for the given transition system with
+  the given shrink strategy, respecting the given size limit new_size. If the
+  result of applying it actually reduced the size of the transition system,
+  copy the transition system, apply the state equivalence relation to it and
+  return the result. Return nullptr otherwise.
+*/
+unique_ptr<TransitionSystem> copy_and_shrink_ts(
+    const TransitionSystem &ts,
+    const Distances &distances,
     const ShrinkStrategy &shrink_strategy,
-    TransitionSystem &ts,
-    const Distances &dist,
     int new_size,
     Verbosity verbosity) {
     StateEquivalenceRelation equivalence_relation =
-        shrink_strategy.compute_equivalence_relation(ts, dist, new_size);
+        shrink_strategy.compute_equivalence_relation(ts, distances, new_size);
     // TODO: We currently violate this; see issue250
     //assert(equivalence_relation.size() <= target_size);
     int new_num_states = equivalence_relation.size();
+
     if (new_num_states < ts.get_size()) {
-        /* Compute the abstraction mapping based on the given state equivalence
-           relation. */
+        /*
+          If we actually shrink the transition system, we first need to copy it,
+          then shrink it and return it.
+        */
         vector<int> abstraction_mapping = compute_abstraction_mapping(
             ts.get_size(), equivalence_relation);
-        ts.apply_abstraction(
+        unique_ptr<TransitionSystem> ts_copy =
+            utils::make_unique_ptr<TransitionSystem>(ts);
+        ts_copy->apply_abstraction(
             equivalence_relation, abstraction_mapping, verbosity);
-        // Not applying abstraction to distances because we don't need to.
+        return ts_copy;
+    } else {
+        return nullptr;
     }
 }
 
@@ -250,27 +265,55 @@ unique_ptr<TransitionSystem> shrink_before_merge_externally(
     int max_states,
     int max_states_before_merge,
     int shrink_threshold_before_merge) {
-    // Copy the transition systems.
-    TransitionSystem ts1(fts.get_ts(index1));
-    TransitionSystem ts2(fts.get_ts(index2));
+    const TransitionSystem &original_ts1 = fts.get_ts(index1);
+    const TransitionSystem &original_ts2 = fts.get_ts(index2);
 
-    // Imitate shrinking before merging as done in the merge-and-shrink loop.
+    /*
+      Determine size limits and if shrinking is necessary or possible as done
+      in the merge-and-shrink loop.
+    */
     pair<int, int> new_sizes = compute_shrink_sizes(
-        ts1.get_size(),
-        ts2.get_size(),
+        original_ts1.get_size(),
+        original_ts2.get_size(),
         max_states_before_merge,
         max_states);
+    bool must_shrink_ts1 = original_ts1.get_size() > min(new_sizes.first, shrink_threshold_before_merge);
+    bool must_shrink_ts2 = original_ts2.get_size() > min(new_sizes.second, shrink_threshold_before_merge);
+
+    /*
+      If we need to shrink, copy_and_shrink_ts will take care of computing
+      a copy, shrinking it, and returning it. (In cases where shrinking is
+      only triggered due to the threshold being passed but no perfect
+      shrinking is possible, the method returns a null pointer.)
+    */
     Verbosity verbosity = Verbosity::SILENT;
-    if (ts1.get_size() > min(new_sizes.first, shrink_threshold_before_merge)) {
-        Distances dist1(ts1, fts.get_distances(index1));
-        shrink_ts(shrink_strategy, ts1, dist1, new_sizes.first, verbosity);
+    unique_ptr<TransitionSystem> ts1 = nullptr;
+    if (must_shrink_ts1) {
+        ts1 = copy_and_shrink_ts(
+            original_ts1,
+            fts.get_distances(index1),
+            shrink_strategy,
+            new_sizes.first,
+            verbosity);
     }
-    if (ts2.get_size() > min(new_sizes.second, shrink_threshold_before_merge)) {
-        Distances dist2(ts2, fts.get_distances(index2));
-        shrink_ts(shrink_strategy, ts2, dist2, new_sizes.second, verbosity);
+    unique_ptr<TransitionSystem> ts2 = nullptr;
+    if (must_shrink_ts2) {
+        ts2 = copy_and_shrink_ts(
+            original_ts2,
+            fts.get_distances(index2),
+            shrink_strategy,
+            new_sizes.second,
+            verbosity);
     }
 
-    // Return the product.
-    return TransitionSystem::merge(fts.get_labels(), ts1, ts2, verbosity);
+    /*
+      Return the product, using either the original transition systems or
+      the copied and shrunk ones.
+    */
+    return TransitionSystem::merge(
+        fts.get_labels(),
+        (ts1 ? *ts1 : original_ts1),
+        (ts2 ? *ts2: original_ts2),
+        verbosity);
 }
 }
