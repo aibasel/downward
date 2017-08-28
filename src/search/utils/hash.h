009b83490e45e1ce2ac847a75ee181ae2f57c8ae
Jendrik Seipp <jendrik.seipp@unibas.ch>
2017-08-28 10:58:39 +0200

    [issue731] use utils::Hash{Map,Set} for hashing vectors

--- src/search/utils/hash.h
+++ src/search/utils/hash.h
@@ -304,45 +304,6 @@ using HashMap = std::unordered_map<T1, T2, Hash<T1>>;
 
 template<typename T>
 using HashSet = std::unordered_set<T, Hash<T>>;
-
-
-/*
-  Legacy hash functions.
-
-  We plan to remove these legacy hash functions since implementing std::hash<T>
-  for non-user-defined types T causes undefined behaviour
-  (http://en.cppreference.com/w/cpp/language/extending_std) and maintaining
-  only one set of user-defined hash functions is easier.
-*/
-
-template<typename T>
-inline void hash_combine(size_t &hash, const T &value) {
-    std::hash<T> hasher;
-    /*
-      The combination of hash values is based on issue 6.18 in
-      http://www.open-std.org/JTC1/SC22/WG21/docs/papers/2005/n1756.pdf.
-      Boost combines hash values in the same way.
-    */
-    hash ^= hasher(value) + 0x9e3779b9 + (hash << 6) + (hash >> 2);
-}
-
-template<typename Sequence>
-size_t hash_sequence(const Sequence &data, size_t length) {
-    size_t hash = 0;
-    for (size_t i = 0; i < length; ++i) {
-        hash_combine(hash, data[i]);
-    }
-    return hash;
-}
-}
-
-namespace std {
-template<typename T>
-struct hash<std::vector<T>> {
-    size_t operator()(const std::vector<T> &vec) const {
-        return utils::hash_sequence(vec, vec.size());
-    }
-};
 }
 
 #endif
