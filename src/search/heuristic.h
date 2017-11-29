9f963d2195d728e71119589e4aeafe5c0aa9451e
Jendrik Seipp <jendrik.seipp@unibas.ch>
2017-11-29 17:38:03 +0100

    [issue750] move Heuristic::description to Evaluator

--- src/search/heuristic.h
+++ src/search/heuristic.h
@@ -32,8 +32,6 @@ class Heuristic : public Evaluator {
     };
     static_assert(sizeof(HEntry) == 4, "HEntry has unexpected size.");
 
-    std::string description;
-
     /*
       TODO: We might want to get rid of the preferred_operators
       attribute. It is currently only used by compute_result() and the
@@ -102,8 +100,6 @@ public:
 
     virtual EvaluationResult compute_result(
         EvaluationContext &eval_context) override;
-
-    std::string get_description() const;
 };
 
 #endif
