7039e83ae0a65fa2820b10ea1dc7b6f3d17c0118
Silvan Sievers <silvan.sievers@unibas.ch>
2017-12-03 18:16:19 +0100

    [issue739] move printing of component settings and callstrings to calls.check_call

--- driver/call.py
+++ driver/call.py
@@ -5,12 +5,30 @@ from __future__ import print_function
 """Make subprocess calls with time and memory limits."""
 
 from . import limits
+from . import util
 
+import logging
 import subprocess
 import sys
 
 
-def check_call(cmd, stdin=None, time_limit=None, memory_limit=None):
+def print_call_settings(nick, cmd, stdin, time_limit, memory_limit):
+    if stdin is not None:
+        stdin = util.shell_escape(stdin)
+    logging.info("{} stdin: {}".format(nick, stdin))
+    if time_limit is not None:
+        time_limit = str(time_limit) + "s"
+    logging.info("{} time limit: {}".format(nick, time_limit))
+    if memory_limit is not None:
+        memory_limit = int(limits.convert_to_mb(memory_limit))
+        memory_limit = str(memory_limit) + " MB"
+    logging.info("{} memory limit: {}".format(nick, memory_limit))
+    logging.info("{} command line string: {}".format(nick, cmd))
+
+
+def check_call(nick, cmd, stdin=None, time_limit=None, memory_limit=None):
+    print_call_settings(nick, cmd, stdin, time_limit, memory_limit)
+
     def set_limits():
         limits.set_time_limit(time_limit)
         limits.set_memory_limit(memory_limit)
