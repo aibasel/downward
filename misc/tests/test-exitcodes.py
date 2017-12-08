f87918cd091edd6c13512316dccfc20b0c96c0d7
Silvan Sievers <silvan.sievers@unibas.ch>
2017-12-08 13:37:30 +0100

    [issue739] change memory limit back; use ubuntu 16.04 for pipelines

--- misc/tests/test-exitcodes.py
+++ misc/tests/test-exitcodes.py
@@ -23,7 +23,7 @@ TRANSLATE_TASKS = {
 TRANSLATE_TESTS = [
     ("small", [], [], returncodes.SUCCESS),
     ("large", ["--translate-time-limit", "1s"], [], returncodes.TRANSLATE_OUT_OF_TIME),
-    ("large", ["--translate-memory-limit", "150M"], [], returncodes.TRANSLATE_OUT_OF_MEMORY),
+    ("large", ["--translate-memory-limit", "50M"], [], returncodes.TRANSLATE_OUT_OF_MEMORY),
 ]
 
 SEARCH_TASKS = {
