47b2edb7b8effbd141308512caf6d243016e16f7
Jendrik Seipp <jendrik.seipp@unibas.ch>
2018-04-11 00:17:26 +0200

    [main] use UNIT_MAX macro instead of numeric_limits function since MSVC 12.0 lacks support for constexpr

--- src/search/algorithms/int_hash_set.h
+++ src/search/algorithms/int_hash_set.h
@@ -7,6 +7,7 @@
 
 #include <algorithm>
 #include <cassert>
+#include <climits>
 #include <iostream>
 #include <limits>
 #include <utility>
@@ -77,7 +78,9 @@ class IntHashSet {
 
     // Max distance from the ideal bucket to the actual bucket for each key.
     static const int MAX_DISTANCE = 32;
-    static const unsigned int MAX_BUCKETS = std::numeric_limits<unsigned int>::max();
+    // MSVC 12.0 lacks support for constexpr. Other compilers allow using
+    // std::numeric_limits<unsigned int>::max() instead of the UINT_MAX macro.
+    static const unsigned int MAX_BUCKETS = UINT_MAX;
 
     struct Bucket {
         KeyType key;
