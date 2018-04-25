543eb1ba3a7efe712ac2aba4974e73192f7dbd40
Jendrik Seipp <jendrik.seipp@unibas.ch>
2018-04-25 23:50:13 +0200

    [issue781] Add new stubborn set variant.

---
+++ src/search/pruning/stubborn_sets_queue.cc
@@ -0,0 +1,193 @@
+#include "stubborn_sets_queue.h"
+
+#include "../option_parser.h"
+#include "../plugin.h"
+
+#include "../utils/markup.h"
+
+using namespace std;
+
+namespace stubborn_sets_queue {
+StubbornSetsQueue::StubbornSetsQueue(const options::Options &opts)
+    : StubbornSets(opts) {
+}
+
+void StubbornSetsQueue::initialize(const shared_ptr<AbstractTask> &task) {
+    StubbornSets::initialize(task);
+    cout << "pruning method: stubborn sets queue" << endl;
+
+    TaskProxy task_proxy(*task);
+
+    for (VariableProxy var : task_proxy.get_variables()) {
+        marked_producers.emplace_back(var.get_domain_size(), false);
+        marked_consumers.emplace_back(var.get_domain_size(), false);
+    }
+    marked_ops.resize(num_operators, false);
+
+    compute_consumers(task_proxy);
+}
+
+void StubbornSetsQueue::compute_consumers(const TaskProxy &task_proxy) {
+    consumers = utils::map_vector<vector<vector<int>>>(
+        task_proxy.get_variables(), [](const VariableProxy &var) {
+            return vector<vector<int>>(var.get_domain_size());
+        });
+
+    for (const OperatorProxy op : task_proxy.get_operators()) {
+        int op_id = op.get_id();
+        for (const FactProxy fact_proxy : op.get_preconditions()) {
+            FactPair fact = fact_proxy.get_pair();
+            consumers[fact.var][fact.value].push_back(op_id);
+        }
+    }
+}
+
+bool StubbornSetsQueue::operator_is_applicable(int op, const State &state) {
+    return find_unsatisfied_precondition(op, state) == FactPair::no_fact;
+}
+
+void StubbornSetsQueue::enqueue_producers(const FactPair &fact) {
+    if (!marked_producers[fact.var][fact.value]) {
+        marked_producers[fact.var][fact.value] = true;
+        producer_queue.push_back(fact);
+    }
+}
+
+void StubbornSetsQueue::enqueue_consumers(const FactPair &fact) {
+    if (!marked_consumers[fact.var][fact.value]) {
+        marked_consumers[fact.var][fact.value] = true;
+        consumer_queue.push_back(fact);
+    }
+}
+
+void StubbornSetsQueue::enqueue_sibling_producers(const FactPair &fact) {
+    int domain_size = consumers[fact.var].size();
+    for (int value = 0; value < domain_size; ++value) {
+        if (value != fact.value) {
+            enqueue_producers(FactPair(fact.var, value));
+        }
+    }
+}
+
+void StubbornSetsQueue::enqueue_sibling_consumers(const FactPair &fact) {
+    int domain_size = consumers[fact.var].size();
+    for (int value = 0; value < domain_size; ++value) {
+        if (value != fact.value) {
+            enqueue_consumers(FactPair(fact.var, value));
+        }
+    }
+}
+
+void StubbornSetsQueue::enqueue_nes(int op, const State &state) {
+    FactPair fact = find_unsatisfied_precondition(op, state);
+    assert(fact != FactPair::no_fact);
+    enqueue_producers(fact);
+}
+
+void StubbornSetsQueue::enqueue_interferers(int op) {
+    for (const FactPair &fact : sorted_op_preconditions[op]) {
+        enqueue_sibling_producers(fact);
+    }
+    for (const FactPair &fact : sorted_op_effects[op]) {
+        enqueue_sibling_producers(fact);
+        enqueue_sibling_consumers(fact);
+    }
+}
+
+void StubbornSetsQueue::initialize_stubborn_set(const State &state) {
+    assert(producer_queue.empty());
+    assert(consumer_queue.empty());
+    for (auto &facts : marked_producers) {
+        fill(facts.begin(), facts.end(), false);
+    }
+    for (auto &facts : marked_consumers) {
+        fill(facts.begin(), facts.end(), false);
+    }
+
+    fill(marked_ops.begin(), marked_ops.end(), false);
+
+    FactPair unsatisfied_goal = find_unsatisfied_goal(state);
+    assert(unsatisfied_goal != FactPair::no_fact);
+    enqueue_producers(unsatisfied_goal);
+}
+
+void StubbornSetsQueue::handle_stubborn_operator(const State &state, int op) {
+    if (!marked_ops[op]) {
+        marked_ops[op] = true;
+        if (operator_is_applicable(op, state)) {
+            enqueue_interferers(op);
+        } else {
+            enqueue_nes(op, state);
+        }
+    }
+}
+
+void StubbornSetsQueue::prune_operators(
+    const State &state, vector<OperatorID> &op_ids) {
+    initialize_stubborn_set(state);
+
+    while (!producer_queue.empty() || !consumer_queue.empty()) {
+        if (!producer_queue.empty()) {
+            FactPair fact = producer_queue.back();
+            producer_queue.pop_back();
+            for (int op : achievers[fact.var][fact.value]) {
+                handle_stubborn_operator(state, op);
+            }
+        } else {
+            FactPair fact = consumer_queue.back();
+            consumer_queue.pop_back();
+            for (int op : consumers[fact.var][fact.value]) {
+                handle_stubborn_operator(state, op);
+            }
+        }
+    }
+
+    vector<OperatorID> remaining_op_ids;
+    remaining_op_ids.reserve(op_ids.size());
+    for (OperatorID op_id : op_ids) {
+        if (marked_ops[op_id.get_index()]) {
+            remaining_op_ids.push_back(op_id);
+        }
+    }
+
+    op_ids.swap(remaining_op_ids);
+}
+
+static shared_ptr<PruningMethod> _parse(OptionParser &parser) {
+    parser.document_synopsis(
+        "Stubborn sets queue",
+        "Stubborn sets represent a state pruning method which computes a subset "
+        "of applicable operators in each state such that completeness and "
+        "optimality of the overall search is preserved. For details, see the "
+        "following papers: "
+        + utils::format_paper_reference(
+            {"Yusra Alkhazraji", "Martin Wehrle", "Robert Mattmueller", "Malte Helmert"},
+            "A Stubborn Set Algorithm for Optimal Planning",
+            "http://ai.cs.unibas.ch/papers/alkhazraji-et-al-ecai2012.pdf",
+            "Proceedings of the 20th European Conference on Artificial Intelligence "
+            "(ECAI 2012)",
+            "891-892",
+            "IOS Press 2012")
+        + utils::format_paper_reference(
+            {"Martin Wehrle", "Malte Helmert"},
+            "Efficient Stubborn Sets: Generalized Algorithms and Selection Strategies",
+            "http://www.aaai.org/ocs/index.php/ICAPS/ICAPS14/paper/view/7922/8042",
+            "Proceedings of the 24th International Conference on Automated Planning "
+            " and Scheduling (ICAPS 2014)",
+            "323-331",
+            "AAAI Press, 2014"));
+
+    // TODO: Setting these options currently has no effect.
+    stubborn_sets::add_pruning_options(parser);
+
+    Options opts = parser.parse();
+
+    if (parser.dry_run()) {
+        return nullptr;
+    }
+
+    return make_shared<StubbornSetsQueue>(opts);
+}
+
+static PluginShared<PruningMethod> _plugin("stubborn_sets_queue", _parse);
+}
