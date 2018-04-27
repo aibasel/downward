a24b2763383c9f0ced800532190889621eadc821
Jendrik Seipp <jendrik.seipp@unibas.ch>
2018-04-28 01:07:31 +0200

    [issue780] move parser manipulation to SCP file

--- src/search/cost_saturation/max_cost_partitioning_heuristic.cc
+++ src/search/cost_saturation/max_cost_partitioning_heuristic.cc
@@ -2,22 +2,15 @@
 
 #include "abstraction.h"
 #include "cost_partitioned_heuristic.h"
-#include "cost_partitioning_collection_generator.h"
 #include "utils.h"
 
 #include "../option_parser.h"
-#include "../plugin.h"
 
 #include "../utils/logging.h"
-#include "../utils/rng.h"
-#include "../utils/rng_options.h"
 
 using namespace std;
 
 namespace cost_saturation {
-class AbstractionGenerator;
-class OrderGenerator;
-
 static int compute_max_h(
     const vector<CostPartitionedHeuristic> &cp_heuristics,
     const vector<int> &local_state_ids) {
@@ -88,83 +81,12 @@ int MaxCostPartitioningHeuristic::compute_heuristic(const GlobalState &global_st
     return compute_heuristic(state);
 }
 
-int MaxCostPartitioningHeuristic::compute_heuristic(const State &state) {
+int MaxCostPartitioningHeuristic::compute_heuristic(const State &state) const {
     vector<int> local_state_ids = get_local_state_ids(abstractions, state);
     int max_h = compute_max_h(cp_heuristics, local_state_ids);
     if (max_h == INF) {
         return DEAD_END;
     }
     return max_h;
-}
-
-void add_cost_partitioning_collection_options_to_parser(OptionParser &parser) {
-    parser.add_option<int>(
-        "max_orders",
-        "maximum number of orders",
-        "infinity",
-        Bounds("0", "infinity"));
-    parser.add_option<double>(
-        "max_time",
-        "maximum time for finding orders",
-        "10",
-        Bounds("0", "infinity"));
-    parser.add_option<bool>(
-        "diversify",
-        "only keep orders that have a higher heuristic value than all previous"
-        " orders for any of the samples",
-        "true");
-    parser.add_option<int>(
-        "samples",
-        "number of samples for diversification",
-        "1000",
-        Bounds("1", "infinity"));
-    parser.add_option<double>(
-        "max_optimization_time",
-        "maximum time for optimizing each order with hill climbing",
-        "0.0",
-        Bounds("0.0", "infinity"));
-    utils::add_rng_options(parser);
-}
-
-void prepare_parser_for_cost_partitioning_heuristic(
-    options::OptionParser &parser) {
-    parser.document_language_support("action costs", "supported");
-    parser.document_language_support(
-        "conditional effects",
-        "not supported (the heuristic supports them in theory, but none of "
-        "the currently implemented abstraction generators do)");
-    parser.document_language_support(
-        "axioms",
-        "not supported (the heuristic supports them in theory, but none of "
-        "the currently implemented abstraction generators do)");
-    parser.document_property("admissible", "yes");
-    parser.document_property("consistent", "yes");
-    parser.document_property("safe", "yes");
-    parser.document_property("preferred operators", "no");
-
-    parser.add_list_option<shared_ptr<AbstractionGenerator>>(
-        "abstraction_generators",
-        "classes that generate abstractions");
-    parser.add_option<shared_ptr<OrderGenerator>>(
-        "orders",
-        "order generator",
-        "greedy_orders()");
-    parser.add_option<bool>(
-        "debug",
-        "print debugging information",
-        "false");
-    Heuristic::add_options_to_parser(parser);
-}
-
-CostPartitioningCollectionGenerator get_cp_collection_generator_from_options(
-    const options::Options &opts) {
-    return CostPartitioningCollectionGenerator(
-        opts.get<shared_ptr<OrderGenerator>>("orders"),
-        opts.get<int>("max_orders"),
-        opts.get<double>("max_time"),
-        opts.get<bool>("diversify"),
-        opts.get<int>("samples"),
-        opts.get<double>("max_optimization_time"),
-        utils::parse_rng_from_options(opts));
 }
 }
--- src/search/cost_saturation/max_cost_partitioning_heuristic.h
+++ src/search/cost_saturation/max_cost_partitioning_heuristic.h
@@ -5,7 +5,6 @@
 
 #include "../heuristic.h"
 
-#include <memory>
 #include <vector>
 
 namespace options {
@@ -14,14 +13,13 @@ class Options;
 
 namespace cost_saturation {
 class CostPartitionedHeuristic;
-class CostPartitioningCollectionGenerator;
 
 class MaxCostPartitioningHeuristic : public Heuristic {
     Abstractions abstractions;
     const std::vector<CostPartitionedHeuristic> cp_heuristics;
     const bool debug;
 
-    virtual int compute_heuristic(const State &state);
+    int compute_heuristic(const State &state) const;
 
 protected:
     virtual int compute_heuristic(const GlobalState &global_state) override;
@@ -33,15 +31,6 @@ public:
         std::vector<CostPartitionedHeuristic> &&cp_heuristics);
     virtual ~MaxCostPartitioningHeuristic() override;
 };
-
-extern void add_cost_partitioning_collection_options_to_parser(
-    options::OptionParser &parser);
-
-extern void prepare_parser_for_cost_partitioning_heuristic(
-    options::OptionParser &parser);
-
-extern CostPartitioningCollectionGenerator get_cp_collection_generator_from_options(
-    const options::Options &opts);
 }
 
 #endif
