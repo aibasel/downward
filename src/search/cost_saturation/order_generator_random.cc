f7a9635a343faa8793fb4257174deb9687371150
Jendrik Seipp <jendrik.seipp@unibas.ch>
2018-04-27 10:50:27 +0200

    [issue780] add OrderGeneratorRandom

---
+++ src/search/cost_saturation/order_generator_random.cc
@@ -0,0 +1,49 @@
+#include "order_generator_random.h"
+
+#include "utils.h"
+
+#include "../option_parser.h"
+#include "../plugin.h"
+
+#include "../utils/logging.h"
+#include "../utils/rng.h"
+#include "../utils/rng_options.h"
+
+using namespace std;
+
+namespace cost_saturation {
+OrderGeneratorRandom::OrderGeneratorRandom(const Options &opts)
+    : rng(utils::parse_rng_from_options(opts)) {
+}
+
+void OrderGeneratorRandom::initialize(
+    const TaskProxy &,
+    const vector<unique_ptr<Abstraction>> &abstractions,
+    const vector<int> &) {
+    utils::Log() << "Initialize random order generator" << endl;
+    random_order = get_default_order(abstractions.size());
+}
+
+Order OrderGeneratorRandom::get_next_order(
+    const TaskProxy &,
+    const vector<unique_ptr<Abstraction>> &,
+    const vector<int> &,
+    const vector<int> &,
+    bool) {
+    utils::Timer greedy_timer;
+    rng->shuffle(random_order);
+    return random_order;
+}
+
+
+static shared_ptr<OrderGenerator> _parse_greedy(OptionParser &parser) {
+    utils::add_rng_options(parser);
+    Options opts = parser.parse();
+    if (parser.dry_run())
+        return nullptr;
+    else
+        return make_shared<OrderGeneratorRandom>(opts);
+}
+
+static PluginShared<OrderGenerator> _plugin_greedy("random_orders", _parse_greedy);
+}
---
+++ src/search/cost_saturation/order_generator_random.h
@@ -0,0 +1,32 @@
+#ifndef COST_SATURATION_ORDER_GENERATOR_RANDOM_H
+#define COST_SATURATION_ORDER_GENERATOR_RANDOM_H
+
+#include "order_generator.h"
+
+namespace utils {
+class RandomNumberGenerator;
+}
+
+namespace cost_saturation {
+class OrderGeneratorRandom : public OrderGenerator {
+    const std::shared_ptr<utils::RandomNumberGenerator> rng;
+    std::vector<int> random_order;
+
+public:
+    explicit OrderGeneratorRandom(const options::Options &opts);
+
+    virtual void initialize(
+        const TaskProxy &task_proxy,
+        const std::vector<std::unique_ptr<Abstraction>> &abstractions,
+        const std::vector<int> &costs) override;
+
+    virtual Order get_next_order(
+        const TaskProxy &task_proxy,
+        const std::vector<std::unique_ptr<Abstraction>> &abstractions,
+        const std::vector<int> &costs,
+        const std::vector<int> &local_state_ids,
+        bool verbose) override;
+};
+}
+
+#endif
