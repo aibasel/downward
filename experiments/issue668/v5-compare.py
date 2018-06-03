162829dbb2bdc35f1ff1112bb5e2dc8d595d1831
Silvan Sievers <silvan.sievers@unibas.ch>
2018-06-03 17:59:33 +0200

    [issue668] fix comparison names

--- experiments/issue668/v5-compare.py
+++ experiments/issue668/v5-compare.py
@@ -68,7 +68,7 @@ exp.add_report(ComparativeReport(attributes=attributes,algorithm_pairs=[
     ('%s-sbf-miasm-rnd-otn-abp-b50k' % 'issue668-v5-hack','%s-sbf-miasm-rnd-otn-abp-b50k' % 'issue668-v5-clean'),
     ('%s-sbf-miasm-rnd-nto-abp-b50k' % 'issue668-v5-hack','%s-sbf-miasm-rnd-nto-abp-b50k' % 'issue668-v5-clean'),
     ('%s-sbf-miasm-rnd-rnd-abp-b50k' % 'issue668-v5-hack','%s-sbf-miasm-rnd-rnd-abp-b50k' % 'issue668-v5-clean'),
-]),outfile='issue668-v5-hack-v4-compare-abp.html')
+]),outfile='issue668-v5-hack-vs-clean-abp.html')
 exp.add_report(ComparativeReport(attributes=attributes,algorithm_pairs=[
     ('%s-sbf-miasm-rl-otn-pba-b50k' % 'issue668-v5-hack','%s-sbf-miasm-rl-otn-pba-b50k' % 'issue668-v5-clean'),
     ('%s-sbf-miasm-rl-nto-pba-b50k' % 'issue668-v5-hack','%s-sbf-miasm-rl-nto-pba-b50k' % 'issue668-v5-clean'),
@@ -79,7 +79,7 @@ exp.add_report(ComparativeReport(attributes=attributes,algorithm_pairs=[
     ('%s-sbf-miasm-rnd-otn-pba-b50k' % 'issue668-v5-hack','%s-sbf-miasm-rnd-otn-pba-b50k' % 'issue668-v5-clean'),
     ('%s-sbf-miasm-rnd-nto-pba-b50k' % 'issue668-v5-hack','%s-sbf-miasm-rnd-nto-pba-b50k' % 'issue668-v5-clean'),
     ('%s-sbf-miasm-rnd-rnd-pba-b50k' % 'issue668-v5-hack','%s-sbf-miasm-rnd-rnd-pba-b50k' % 'issue668-v5-clean'),
-]),outfile='issue668-v5-hack-v4-compare-pba.html')
+]),outfile='issue668-v5-hack-vs-clean-pba.html')
 exp.add_report(ComparativeReport(attributes=attributes,algorithm_pairs=[
     ('%s-sbf-miasm-rl-nto-abp-b50k' % 'issue668-v5-hack','%s-sbf-miasm-rl-nto-abp-b50k' % 'issue668-v5-clean'),
     ('%s-sbf-miasm-l-nto-abp-b50k' % 'issue668-v5-hack','%s-sbf-miasm-l-nto-abp-b50k' % 'issue668-v5-clean'),
@@ -88,7 +88,7 @@ exp.add_report(ComparativeReport(attributes=attributes,algorithm_pairs=[
     ('%s-sbf-miasm-l-nto-pba-b50k' % 'issue668-v5-hack','%s-sbf-miasm-l-nto-pba-b50k' % 'issue668-v5-clean'),
     ('%s-sbf-miasm-rnd-nto-pba-b50k' % 'issue668-v5-hack','%s-sbf-miasm-rnd-nto-pba-b50k' % 'issue668-v5-clean'),
     ('%s-sbf-miasm-allrnd-b50k' % 'issue668-v5-hack','%s-sbf-miasm-allrnd-b50k' % 'issue668-v5-clean'),
-]),outfile='issue668-v5-hack-v4-compare-paper.html')
+]),outfile='issue668-v5-hack-vs-clean-paper.html')
 
 exp.run_steps()
 
