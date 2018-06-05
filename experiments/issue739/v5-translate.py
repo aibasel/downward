f651bbbd2f9b612edd16333ac6d058349f80372a
Silvan Sievers <silvan.sievers@unibas.ch>
2018-06-05 11:31:53 +0200

    [issue739] adapt exp to latest Lab

--- experiments/issue739/v5-translate.py
+++ experiments/issue739/v5-translate.py
@@ -32,9 +32,10 @@ exp = IssueExperiment(
 )
 exp.add_suite(BENCHMARKS_DIR, SUITE)
 
-exp.add_parser('lab_driver_parser', exp.LAB_DRIVER_PARSER)
-exp.add_parser('exitcode_parser', exp.EXITCODE_PARSER)
-exp.add_parser('translator_parser', exp.TRANSLATOR_PARSER)
+exp.add_parser(exp.LAB_STATIC_PROPERTIES_PARSER)
+exp.add_parser(exp.LAB_DRIVER_PARSER)
+exp.add_parser(exp.EXITCODE_PARSER)
+exp.add_parser(exp.TRANSLATOR_PARSER)
 
 exp.add_step('build', exp.build)
 exp.add_step('start', exp.start_runs)
