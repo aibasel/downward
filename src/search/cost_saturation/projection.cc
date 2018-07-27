ea7ec5a99ab6fe3555b18c771b8b62db5189d761
Jendrik Seipp <jendrik.seipp@unibas.ch>
2018-07-27 14:50:43 +0200

    [issue780] Compute saturated costs with Projection::for_each_transition().

--- src/search/cost_saturation/projection.cc
+++ src/search/cost_saturation/projection.cc
@@ -383,6 +383,29 @@ vector<int> Projection::compute_saturated_costs(
     sort(old_transitions.begin(), old_transitions.end());
     sort(new_transitions.begin(), new_transitions.end());
     assert(old_transitions == new_transitions);
+
+    vector<int> saturated_costs_new(num_operators, -INF);
+
+    /* To prevent negative cost cycles, we ensure that all operators
+       inducing self-loops have non-negative costs. */
+    for (int op_id : looping_operators) {
+        saturated_costs_new[op_id] = 0;
+    }
+
+    for_each_transition(
+        [&saturated_costs_new, &h_values](const Transition &t) {
+            assert(utils::in_bounds(t.src, h_values));
+            assert(utils::in_bounds(t.target, h_values));
+            int src_h = h_values[t.src];
+            int target_h = h_values[t.target];
+            if (src_h == INF || target_h == INF) {
+                return;
+            }
+            int &needed_costs = saturated_costs_new[t.op];
+            needed_costs = max(needed_costs, src_h - target_h);
+        });
+
+    assert(saturated_costs == saturated_costs_new);
     return saturated_costs;
 }
 
