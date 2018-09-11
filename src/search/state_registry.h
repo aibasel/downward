bc17168be4dd626bb344fb6a72dea988f8a72f67
Malte Helmert <malte.helmert@unibas.ch>
2018-09-11 14:49:08 +0000

    [issue695] Improve language in comment.

--- src/search/state_registry.h
+++ src/search/state_registry.h
@@ -91,10 +91,10 @@
   Problem:
     In the LMcount heuristic each state should store which landmarks are
     already reached when this state is reached. This should only require
-    additional memory, when the LMcount heuristic is used.
+    additional memory when the LMcount heuristic is used.
 
   Solution:
-    The heuristic object uses a field of type PerStateBitset to store for each
+    The heuristic object uses an attribute of type PerStateBitset to store for each
     state and each landmark whether it was reached in this state.
 */
 
