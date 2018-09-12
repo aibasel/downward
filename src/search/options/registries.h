056637d81284956a9d297f1a2ca07fcbd4c58d23
Malte Helmert <malte.helmert@unibas.ch>
2018-09-12 12:21:21 +0200

    [issue749] Add registry for plugin groups plugins.

--- src/search/options/registries.h
+++ src/search/options/registries.h
@@ -141,6 +141,36 @@ public:
 
     static PluginTypeRegistry *instance();
 };
+
+
+/*
+  The plugin group registry collects information about plugin groups.
+  A plugin group is a set of plugins (which should be of the same
+  type, although the code does not enforce this) that should be
+  grouped together in user documentation.
+
+  For example, all PDB heuristics could be grouped together so that
+  the documention page for the heuristics looks nicer.
+*/
+
+// TODO: Reduce code duplication with Registry<T>.
+struct PluginGroupInfo {
+    std::string group_id;
+    std::string doc_title;
+};
+
+class PluginGroupRegistry {
+    using Map = std::map<std::string, PluginGroupInfo>;
+    PluginGroupRegistry() = default;
+    ~PluginGroupRegistry() = default;
+    Map registry;
+public:
+    void insert(const PluginGroupInfo &info);
+
+    const PluginGroupInfo &get(const std::string &key) const;
+
+    static PluginGroupRegistry *instance();
+};
 }
 
 #endif
