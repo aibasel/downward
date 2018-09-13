d56ca3bda12264809b677442a5364023d11e1d71
Salome Eriksson <salome.eriksson@unibas.ch>
2018-09-13 11:49:18 +0200

    [issue695] Close issue695 branch.

--- src/search/per_state_array.h
+++ src/search/per_state_array.h
@@ -117,14 +117,15 @@ public:
         return ArrayView<Element>((*entries)[state_id], default_array.size());
     }
 
-    void operator[](const GlobalState &state) const {
+    ArrayView<Element> operator[](const GlobalState &state) const {
         ABORT("PerStateArray::operator[] const not implemented. "
               "See source code for more information.");
         /*
           This method is not implemented because it is currently not used and
           would require quite a bit of boilerplate, introducing a ConstArrayView
           class similar to ArrayView. If you need it, it should be easy to
-          implement based on PerStateInformation:operator[] const.
+          implement based on PerStateInformation:operator[] const. This method
+          should return a ConstArrayView<Element>.
         */
     }
 
