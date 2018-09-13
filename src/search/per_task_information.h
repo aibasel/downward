1b187b24bb0185180d00649e57ad5b6fb4a673b7
Florian Pommerening <florian.pommerening@unibas.ch>
2018-09-13 22:33:55 +0200

    [issue774] documentation of PerTaskInformation

--- src/search/per_task_information.h
+++ src/search/per_task_information.h
@@ -9,12 +9,35 @@
 #include <functional>
 #include <unordered_map>
 
+/*
+  A PerTaskInformation<T> acts like an unordered_map<const AbstractTask *, T>
+  with two main differences:
+  (1) If an entry is accessed that does not exist yet, it is created using a
+      factory function that is passed to the PerTaskInformation in its
+      constructor.
+  (2) If a task is destroyed, its associated data in all PerTaskInformation
+      objects is automatically destroyed as well.
+
+*/
 template<class Entry>
 class PerTaskInformation : public subscriber::Subscriber<AbstractTask> {
+    /*
+      EntryConstructor is the type of a function that can create objects for
+      a given task if the PerTaskInformation is accessed for a task that has no
+      associated entry yet. It receives a TaskProxy instead of the AbstractTask
+      because AbstractTask is an internal implementation detail as far as other
+      classes are concerned. It should return a unique_ptr to the newly created
+      object.
+    */
     using EntryConstructor = std::function<std::unique_ptr<Entry>(const TaskProxy &)>;
     EntryConstructor entry_constructor;
     std::unordered_map<const AbstractTask *, std::unique_ptr<Entry>> entries;
 public:
+    /*
+      If no entry_constructor is passed to the PerTaskInformation explicitly,
+      we assume the class Entry has a constructor that takes a single TaskProxy
+      parameter.
+    */
     PerTaskInformation()
         : entry_constructor(
               [](const TaskProxy &task_proxy) {
