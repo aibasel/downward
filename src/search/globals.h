781693a400237499aca25dc9366772d4ce381a45
Florian Pommerening <florian.pommerening@unibas.ch>
2018-09-18 19:43:26 +0200

    [issue793] move global logger to utils/logging.h

--- src/search/globals.h
+++ src/search/globals.h
@@ -3,15 +3,9 @@
 
 #include <istream>
 
-namespace utils {
-struct Log;
-}
-
 void read_everything(std::istream &in);
 
 // The following function is deprecated. Use task_properties.h instead.
 bool is_unit_cost();
-
-extern utils::Log g_log;
 
 #endif
