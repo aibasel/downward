4ed55946a8fbb786152525d36ae272c587708608
Salome Eriksson <salome.eriksson@unibas.ch>
2018-09-18 14:59:41 +0200

    [issue827] move function definitions to cc files

--- src/search/options/bounds.cc
+++ src/search/options/bounds.cc
@@ -5,6 +5,19 @@
 using namespace std;
 
 namespace options {
+
+Bounds::Bounds(const std::string &min, const std::string &max)
+    : min(min), max(max) {
+}
+
+bool Bounds::has_bound() const {
+    return !min.empty() || !max.empty();
+}
+
+Bounds Bounds::unlimited() {
+    return Bounds("", "");
+}
+
 ostream &operator<<(ostream &out, const Bounds &bounds) {
     if (!bounds.min.empty() || !bounds.max.empty())
         out << "[" << bounds.min << ", " << bounds.max << "]";
