d2a937242fc1d6c6bc95b0d352e3282ad03a411c
Patrick Ferber <patrick.ferber@unibas.ch>
2018-09-19 10:01:41 +0200

    [issue826] Maltes Comments

--- src/search/options/command_line.h
+++ src/search/options/command_line.h
@@ -3,14 +3,10 @@
 
 #include <memory>
 #include <string>
-#include <vector>
 
 class SearchEngine;
 
 namespace options {
-std::shared_ptr<SearchEngine> parse_cmd_line_aux(
-    const std::vector<std::string> &args, bool dry_run);
-
 std::shared_ptr<SearchEngine> parse_cmd_line(
     int argc, const char **argv, bool dry_run, bool is_unit_cost);
 
