7c1f8c168e81e655b64380d8a0d91b0cfa0bab3e
Patrick Ferber <patrick.ferber@unibas.ch>
2018-09-21 11:53:44 +0200

    [issue842] Moved registry insert method to private

--- src/search/options/plugin.cc
+++ src/search/options/plugin.cc
@@ -5,12 +5,13 @@
 using namespace std;
 
 namespace options {
+/*
 void register_plugin_type_plugin(
     const type_info &type, const string &type_name, const string &documentation) {
     PluginTypeInfo info(type_index(type), type_name, documentation);
     Registry::instance()->insert_type_info(info);
 }
-
+*/
 PluginGroupPlugin::PluginGroupPlugin(const string &group_id,
                                      const string &doc_title) {
     RegistryDataCollection::instance()->insert_plugin_group_data(group_id,
