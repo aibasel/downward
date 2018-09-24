fceccefe97cde4664a9a7667b0bef2448bfcab3c
Patrick Ferber <patrick.ferber@unibas.ch>
2018-09-24 17:38:13 +0200

    [issue842] Added checks to 2 phase plugin registration

--- src/search/options/registries.cc
+++ src/search/options/registries.cc
@@ -1,7 +1,10 @@
 #include "registries.h"
 
+#include "string_utils.h"
+
 #include <iostream>
-#include <unordered_set>
+#include <sstream>
+#include <typeindex>
 
 using namespace std;
 using utils::ExitCode;
@@ -111,46 +114,124 @@ Registry::Registry(const RegistryDataCollection& collection) {
     collect_plugin_types(collection, errors);
     collect_plugin_groups(collection, errors);
     collect_plugins(collection, errors);
+    if (errors.size() > 0) {
+        cerr << endl << "Plugin initialization errors:" << endl;
+        cerr << join(errors.begin(), errors.end(), "\n") <<endl;
+        utils::exit_with(utils::ExitCode::SEARCH_CRITICAL_ERROR);
+    }
+}
+
+template<typename K, typename T>
+static void generate_name_clash_errors(
+    const unordered_map<K, T> &occurrences,
+    vector<string> &errors,
+    function<bool(const T& obj)> f_cnd,
+    function<string(const K &first,const T &second)> f_msg) {
+    vector<string> name_clash_errors;
+    
+    typename unordered_map<K, T>::const_iterator it;
+    for(it = occurrences.begin(); it != occurrences.end(); ++it) {
+        if (f_cnd(it->second)){
+            name_clash_errors.push_back(f_msg(it->first, it->second));
+        }
+    }
+    sort(name_clash_errors.begin(), name_clash_errors.end());
+    errors.insert(errors.end(), name_clash_errors.begin(), name_clash_errors.end());
 }
 
 void Registry::collect_plugin_types(const RegistryDataCollection& collection,
-    vector<string> &/*errors*/) {
+    vector<string> &errors) {
+    unordered_map<string, vector<type_index>> occurrences_names;
+    unordered_map<type_index, vector<string>> occurrences_types;
     for (PluginTypeData ptd : collection.get_plugin_type_data()) {
         const string &type_name = std::get<0>(ptd);
         const string &documentation = std::get<1>(ptd);
         const type_index type = std::get<2>(ptd);
-        cout << "START" << endl;
-        //cout << "TYPE" << type << endl;
-        cout << "NAME " << type_name << endl;
-        cout << "DOC" << documentation << endl;
-        PluginTypeInfo info(type, type_name, documentation);
-        insert_type_info(info);
+        occurrences_names[type_name].push_back(type);
+        occurrences_types[type].push_back(type_name);
+        if (occurrences_names[type_name].size() == 1 && 
+                occurrences_types[type].size() == 1) {
+            PluginTypeInfo info(type, type_name, documentation);
+            insert_type_info(info);
+        }
     }
+    
+    generate_name_clash_errors<string, vector<type_index>>(
+        occurrences_names, errors, 
+        [](const vector<type_index> &types) {return types.size() > 1;},
+        [](const string &type_name, const vector<type_index> &types) {
+            return "Multiple PluginTypePlugins with the same name: "  +
+                type_name + " (types: " +
+                join<vector<type_index>::const_iterator, type_index>(types.begin(),
+                types.end(), [](const type_index &type) {return type.name();}) + ")";
+        });
+        
+    generate_name_clash_errors<type_index, vector<string>>(
+        occurrences_types, errors, 
+        [](const vector<string> &names) {return names.size() > 1;},
+        [](const type_index &type, const vector<string> &names) {
+            return "Multiple PluginTypePlugins with the same type: " +
+                string(type.name()) + " (names: " + join(names.begin(), names.end()) + ")";
+        });
 }
 
 void Registry::collect_plugin_groups(const RegistryDataCollection& collection,
-    vector<string> &/*errors*/) {
+    vector<string> &errors) {
+    unordered_map<string, int> occurrences;
     for (PluginGroupData pgd : collection.get_plugin_group_data()) {
         const string &group_id = std::get<0>(pgd);
         const string &doc_title = std::get<1>(pgd);
-        PluginGroupInfo info {group_id, doc_title};
-        insert_group_info(info);
+        occurrences[group_id]++;
+        if (occurrences[group_id] == 1) {
+            PluginGroupInfo info {group_id, doc_title};
+            insert_group_info(info);
+        }
     }
+    
+    generate_name_clash_errors<string, int>(occurrences, errors, 
+        [](const int &count) {return count > 1;},
+        [](const string &first, const int &count) {
+            return "Multiple PluginGroupPlugins: " + first +
+                " (count: " + to_string(count) + ")";
+        });
 }
 
 void Registry::collect_plugins(const RegistryDataCollection& collection,
-    vector<string> &/*errors*/) {
+    vector<string> &errors) {
+    vector<string> other_plugin_errors;
+    unordered_map<string, vector<type_index>> occurrences;
     for (PluginData pd : collection.get_plugin_data()) {
         const string &key = std::get<0>(pd);
         const Any &factory = std::get<1>(pd);
         const string &group = std::get<2>(pd);
         const PluginTypeNameGetter &type_name_factory = std::get<3>(pd);
         const DocFactory &doc_factory = std::get<4>(pd);
-        type_index type = std::get<5>(pd);
-        
-        insert_plugin(key, factory, type_name_factory, doc_factory, group, 
-            type);
+        const type_index type = std::get<5>(pd);
+        occurrences[key].push_back(type);
+        if (occurrences[key].size() == 1) {
+            insert_plugin(key, factory, type_name_factory, doc_factory, group, 
+                type);
+        }
+        if (group != "" && !plugin_group_infos.count(group)) {
+            other_plugin_errors.push_back("Missing PluginGroupPlugin for "
+            "Plugin " + key + " of type " + type.name() + ": " + group);
+        }
+        if (!plugin_type_infos.count(type)) {
+            other_plugin_errors.push_back("Missing PluginTypePlugin for "
+            "Plugin " + key + ": " + type.name());
+        }
     }
+    
+    generate_name_clash_errors<string, vector<type_index>>(occurrences, errors, 
+        [](const vector<type_index> &types) {return types.size() > 1;},
+        [](const string &type_name, const vector<type_index> &types) {
+            return "Multiple Plugins: " + type_name + " (types: " +
+                join<vector<type_index>::const_iterator, type_index>(types.begin(),
+                types.end(), [](const type_index &type) {return type.name();}) + ")";
+        });
+        
+    sort(other_plugin_errors.begin(), other_plugin_errors.end());
+    errors.insert(errors.end(), other_plugin_errors.begin(), other_plugin_errors.end());
 }
 
 void Registry::insert_plugin(const std::string &key, Any factory,
