9a3644974ca5ad1591890bd9d7edb6304bc05827
Jendrik Seipp <jendrik.seipp@unibas.ch>
2018-10-01 12:54:35 +0200

    [main] Cast vector size to uint64_t instead of unsigned int before passing it to feed() function.

--- src/search/utils/hash.h
+++ src/search/utils/hash.h
@@ -248,8 +248,11 @@ void feed(HashState &hash_state, const std::vector<T> &vec) {
     /*
       Feed vector size to ensure that no two different vectors of the same type
       have the same code prefix.
+
+      Using uint64_t is wasteful on 32-bit platforms but feeding a size_t breaks
+      the build on MacOS (see msg7812).
     */
-    feed(hash_state, static_cast<unsigned int>(vec.size()));
+    feed(hash_state, static_cast<uint64_t>(vec.size()));
     for (const T &item : vec) {
         feed(hash_state, item);
     }
