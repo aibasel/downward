d69c57e11b4cd9c5128773b7c7b73c7da31c713b
Patrick Ferber <patrick.ferber@unibas.ch>
2018-11-27 14:03:39 +0100

    [issue842] Moved PlutinTypeInfo methods which were forgotten to move earlier

--- src/search/options/doc_utils.cc
+++ src/search/options/doc_utils.cc
@@ -14,4 +14,28 @@ void PluginInfo::fill_docs() {
 string PluginInfo::get_type_name() const {
     return type_name_factory(registry);
 }
+
+PluginTypeInfo::PluginTypeInfo(const type_index &type,
+                               const string &type_name,
+                               const string &documentation)
+    : type(type),
+      type_name(type_name),
+      documentation(documentation) {
+}
+
+const type_index &PluginTypeInfo::get_type() const {
+    return type;
+}
+
+const string &PluginTypeInfo::get_type_name() const {
+    return type_name;
+}
+
+const string &PluginTypeInfo::get_documentation() const {
+    return documentation;
+}
+
+bool PluginTypeInfo::operator<(const PluginTypeInfo &other) const {
+    return make_pair(type_name, type) < make_pair(other.type_name, other.type);
+}
 }
--- src/search/options/registries.cc
+++ src/search/options/registries.cc
@@ -13,31 +13,6 @@ using namespace std;
 using utils::ExitCode;
 
 namespace options {
-
-PluginTypeInfo::PluginTypeInfo(const type_index &type,
-                               const string &type_name,
-                               const string &documentation)
-    : type(type),
-      type_name(type_name),
-      documentation(documentation) {
-}
-
-const type_index &PluginTypeInfo::get_type() const {
-    return type;
-}
-
-const string &PluginTypeInfo::get_type_name() const {
-    return type_name;
-}
-
-const string &PluginTypeInfo::get_documentation() const {
-    return documentation;
-}
-
-bool PluginTypeInfo::operator<(const PluginTypeInfo &other) const {
-    return make_pair(type_name, type) < make_pair(other.type_name, other.type);
-}
-
 void Registry::insert_type_info(const PluginTypeInfo &info) {
     if (plugin_type_infos.count(info.get_type())) {
         cerr << "duplicate type in registry: "
