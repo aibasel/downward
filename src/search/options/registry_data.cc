f00f474c30abcd3dde8b6d4a92c08e7216471da2
Patrick Ferber <patrick.ferber@unibas.ch>
2018-11-27 14:53:34 +0100

    [issue842] ...Forgot to add options/registry_data.{cc,h}

---
+++ src/search/options/registry_data.cc
@@ -0,0 +1,64 @@
+#include "registry_data.h"
+
+using namespace std;
+
+namespace options{
+PluginTypeData::PluginTypeData(const string type_name,
+                               const string documentation,
+                               const type_index type)
+    : type_name(type_name),
+      documentation(documentation),
+      type(type) { }
+
+PluginGroupData::PluginGroupData(const string group_id,
+                                 const string doc_title)
+    : group_id(group_id),
+      doc_title(doc_title) { }
+
+PluginData::PluginData(const string key,
+                       const Any factory,
+                       const string group,
+                       const PluginTypeNameGetter type_name_factory,
+                       const DocFactory doc_factory,
+                       const type_index type)
+    : key(key),
+      factory(factory),
+      group(group),
+      type_name_factory(type_name_factory),
+      doc_factory(doc_factory),
+      type(type) { }
+
+
+
+
+void RegistryData::insert_plugin_type_data(
+    const string &type_name, const string &documentation, type_index type) {
+    plugin_types.emplace_back(type_name, documentation, type);
+}
+
+void RegistryData::insert_plugin_group_data(
+    const string &group_id, const string &doc_title) {
+    plugin_groups.emplace_back(group_id, doc_title);
+}
+
+void RegistryData::insert_plugin_data(
+    const string &key, const Any &factory, const string &group,
+    PluginTypeNameGetter type_name_factory, DocFactory doc_factory,
+    type_index type) {
+    plugins.emplace_back(key, factory, group, type_name_factory, doc_factory,
+                         type);
+}
+
+const vector<PluginTypeData> &RegistryData::get_plugin_type_data() const {
+    return plugin_types;
+}
+
+const vector<PluginGroupData> &RegistryData::get_plugin_group_data() const {
+    return plugin_groups;
+}
+
+const vector<PluginData> &RegistryData::get_plugin_data() const {
+    return plugins;
+}
+
+}
---
+++ src/search/options/registry_data.h
@@ -0,0 +1,73 @@
+#ifndef OPTIONS_REGISTRY_DATA
+#define OPTIONS_REGISTRY_DATA
+
+#include "any.h"
+#include "doc_utils.h"
+
+#include <vector>
+
+
+
+namespace options {
+
+struct PluginTypeData {
+    std::string type_name;
+    std::string documentation;
+    std::type_index type;
+
+    PluginTypeData(std::string type_name, std::string documentation,
+                   std::type_index type);
+};
+
+struct PluginGroupData {
+    std::string group_id;
+    std::string doc_title;
+
+    PluginGroupData(std::string group_id, std::string doc_title);
+};
+
+struct PluginData {
+    std::string key;
+    Any factory;
+    std::string group;
+    PluginTypeNameGetter type_name_factory;
+    DocFactory doc_factory;
+    std::type_index type;
+
+    PluginData(std::string key, Any factory, std::string group,
+               PluginTypeNameGetter type_name_factory,
+               DocFactory doc_factory, std::type_index type);
+};
+
+
+class RegistryData {
+    std::vector<PluginTypeData> plugin_types;
+    std::vector<PluginGroupData> plugin_groups;
+    std::vector<PluginData> plugins;
+
+public:
+    void insert_plugin_type_data(
+        const std::string &type_name, const std::string &documentation,
+        std::type_index type);
+
+    void insert_plugin_group_data(
+        const std::string &group_id, const std::string &doc_title);
+
+    void insert_plugin_data(
+        const std::string &key, const Any &factory, const std::string &group,
+        PluginTypeNameGetter type_name_factory, DocFactory doc_factory,
+        std::type_index type);
+
+    const std::vector<PluginTypeData> &get_plugin_type_data() const;
+    const std::vector<PluginGroupData> &get_plugin_group_data() const;
+    const std::vector<PluginData> &get_plugin_data() const;
+
+    static RegistryData *instance() {
+        static RegistryData instance_;
+        return &instance_;
+    }
+};
+}
+
+#endif
+
