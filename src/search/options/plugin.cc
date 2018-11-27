f21c2f498492f5128306039c3b1229410fd84319
Patrick Ferber <patrick.ferber@unibas.ch>
2018-11-27 14:02:08 +0100

    [issue842] Moved RegistryDataCollection into own file & rename RegistryData

--- src/search/options/plugin.cc
+++ src/search/options/plugin.cc
@@ -5,7 +5,7 @@ using namespace std;
 namespace options {
 PluginGroupPlugin::PluginGroupPlugin(const string &group_id,
                                      const string &doc_title) {
-    RegistryDataCollection::instance()->insert_plugin_group_data(group_id,
+    RegistryData::instance()->insert_plugin_group_data(group_id,
                                                                  doc_title);
 }
 }
