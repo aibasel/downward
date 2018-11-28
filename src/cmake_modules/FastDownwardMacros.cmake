04d235d871ff6e82038f6678b16082b1e6de8970
Jendrik Seipp <jendrik.seipp@unibas.ch>
2018-11-28 14:38:47 +0100

    [issue870] Don't force static builds.

--- src/cmake_modules/FastDownwardMacros.cmake
+++ src/cmake_modules/FastDownwardMacros.cmake
@@ -21,14 +21,6 @@ macro(fast_downward_set_compiler_flags)
         set(CMAKE_CXX_FLAGS_DEBUG "-O3 -D_GLIBCXX_DEBUG")
         set(CMAKE_CXX_FLAGS_PROFILE "-O3 -pg")
     elseif(MSVC)
-        # We force linking to be static because the dynamically linked code is
-        # about 10% slower on Linux (see issue67). On Windows this is a compiler
-        # setting, not a linker setting.
-        set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT")
-        string(REPLACE "/MD" "/MT" CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE}")
-        set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MTd")
-        string(REPLACE "/MDd" "/MTd" CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG}")
-
         # Enable exceptions.
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc")
 
@@ -69,59 +61,6 @@ endmacro()
 macro(fast_downward_set_linker_flags)
     if(UNIX)
         set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -g")
-    endif()
-
-    # Fixing the linking to static or dynamic is only supported on Unix.
-    # We don't support the option on MacOS because static linking is
-    # not supported by Apple: https://developer.apple.com/library/mac/qa/qa1118/_index.html
-    # We don't support it on Windows because we don't have a use case
-    # and it's not possible to distinguish static and dynamic libraries
-    # by their file name.
-    if(FORCE_DYNAMIC_BUILD AND NOT UNIX)
-        message(FATAL_ERROR "Forcing dynamic builds is only supported on Unix.")
-    endif()
-
-    if(UNIX AND NOT APPLE)
-        # By default, we try to force linking to be static because the
-        # dynamically linked code is about 10% slower on Linux (see issue67)
-        # but we offer the option to force a dynamic build for debugging
-        # purposes (for example, valgrind's memcheck requires a dynamic build).
-        # To force a dynamic build, set FORCE_DYNAMIC_BUILD to true by passing
-        # -DFORCE_DYNAMIC_BUILD=YES to cmake. We do not introduce an option for
-        # this because it cannot be changed after the first cmake run.
-        if(FORCE_DYNAMIC_BUILD)
-            message(STATUS "Forcing dynamic build.")
-
-            # Any libraries that are implicitly added to the end of the linker
-            # command should be linked dynamically.
-            set(LINK_SEARCH_END_STATIC FALSE)
-
-            # Only look for dynamic libraries.
-            set(CMAKE_FIND_LIBRARY_SUFFIXES .so)
-        else()
-            message(STATUS "Forcing static build.")
-
-            # Any libraries that are implicitly added to the end of the linker
-            # command should be linked statically.
-            set(LINK_SEARCH_END_STATIC TRUE)
-
-            # Only look for static libraries.
-            set(CMAKE_FIND_LIBRARY_SUFFIXES .a)
-
-            # Set linker flags to link statically.
-            if(CMAKE_COMPILER_IS_GNUCXX)
-                set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static -static-libgcc")
-            elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
-                set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static -static-libstdc++")
-
-                # CMake automatically adds the -rdynamic flag to the
-                # following two variables, which causes an error in our
-                # static builds with clang. Therefore we explicitly
-                # clear the variables.
-                set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
-                set(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "")
-            endif()
-        endif()
     endif()
 endmacro()
 
