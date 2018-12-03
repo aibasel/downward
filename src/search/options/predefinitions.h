19dff4ea68d7b7b953fcf4ab0f5a167871deb21e
Patrick Ferber <patrick.ferber@unibas.ch>
2018-12-03 15:45:42 +0100

    [issue831] predefined from map<type, map<key, obj>> to map<key, pair<type, obj>> and checks

--- src/search/options/predefinitions.h
+++ src/search/options/predefinitions.h
@@ -3,31 +3,60 @@
 
 #include "any.h"
 
+#include "../utils/system.h"
+
+#include <iostream>
 #include <string>
 #include <typeindex>
 #include <unordered_map>
+#include <utility>
 
 namespace options {
 class Predefinitions {
-    std::unordered_map<std::type_index, std::unordered_map<std::string, Any>> predefined;
+    std::unordered_map<std::string, std::pair<std::type_index, Any>> predefined;
 public:
     Predefinitions() = default;
 
     template<typename T>
     void predefine(const std::string &key, T object) {
-        predefined[std::type_index(typeid(T))][key] = object;
+        if (predefined.count(key)) {
+            std::cerr << "Multiple predefinitions for the key: " 
+                      << key << std::endl;
+            utils::exit_with(utils::ExitCode::SEARCH_CRITICAL_ERROR);
+        }
+        predefined.emplace(
+            key,
+            std::pair<std::type_index, Any>(std::type_index(typeid(T)), object));
     }
 
     template<typename T>
     bool contains(const std::string &key) const {
         std::type_index type(typeid(T));
-        return predefined.count(type) && predefined.at(type).count(key);
+        auto it = predefined.find(key);
+        if (it == predefined.end()) {
+            return false;
+        } else if (it->second.first == type) {
+            return true;
+        } else {
+            std::cerr << "Attempt to use a predefinition with a wrong type: "
+                      << key << "(type: " << typeid(T).name()
+                      << ")" << std::endl;
+            //Add demangling hint from issue842
+            utils::exit_with(utils::ExitCode::SEARCH_CRITICAL_ERROR);
+        }
     }
 
     template<typename T>
     T get(const std::string &key) const {
-        std::type_index type(typeid(T));
-        return any_cast<T>(predefined.at(type).at(key));
+        try {
+            return any_cast<T>(predefined.at(key).second);
+        } catch (BadAnyCast&) {
+            std::cerr << "Attempt to use a predefinition with a wrong type: "
+                      << key << "(type: " << typeid(T).name()
+                      << ")" << std::endl;
+            //Add demangling hint from issue842
+            utils::exit_with(utils::ExitCode::SEARCH_CRITICAL_ERROR);
+        }
     }
 };
 }
