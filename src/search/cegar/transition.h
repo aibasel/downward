e75ac489a45513501ef96f0ce0eed0e36d6847cf
Jendrik Seipp <jendrik.seipp@unibas.ch>
2018-12-15 17:11:41 +0100

    [main] Allow printing cegar::Transition.

--- src/search/cegar/transition.h
+++ src/search/cegar/transition.h
@@ -1,6 +1,8 @@
 #ifndef CEGAR_TRANSITION_H
 #define CEGAR_TRANSITION_H
 
+#include <iostream>
+
 namespace cegar {
 struct Transition {
     int op_id;
@@ -11,8 +13,12 @@ struct Transition {
           target_id(target_id) {
     }
 
-    bool operator==(const Transition &other) {
+    bool operator==(const Transition &other) const {
         return op_id == other.op_id && target_id == other.target_id;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Transition &t) {
+        return os << "[" << t.op_id << "," << t.target_id << "]";
     }
 };
 }
