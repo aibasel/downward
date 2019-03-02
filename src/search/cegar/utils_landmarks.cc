f570be7ecc7fa8c0fbde25f2ba6d49a51693446c
Jendrik Seipp <jendrik.seipp@unibas.ch>
2019-03-02 20:20:00 +0100

    [issue899] Use unique_ptr to store landmark nodes.

--- src/search/cegar/utils_landmarks.cc
+++ src/search/cegar/utils_landmarks.cc
@@ -38,7 +38,7 @@ vector<FactPair> get_fact_landmarks(const LandmarkGraph &graph) {
     vector<FactPair> facts;
     const LandmarkGraph::Nodes &nodes = graph.get_nodes();
     facts.reserve(nodes.size());
-    for (const LandmarkNode *node : nodes) {
+    for (auto &node : nodes) {
         facts.push_back(get_fact(*node));
     }
     sort(facts.begin(), facts.end());
