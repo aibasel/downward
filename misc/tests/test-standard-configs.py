c7d56d330c427ff01b47cd666c0662d9acc84f36
Jendrik Seipp <jendrik.seipp@unibas.ch>
2019-03-02 11:00:53 +0100

    [main] test-standard-configs.py: translate each task only once.

--- misc/tests/test-standard-configs.py
+++ misc/tests/test-standard-configs.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 
 import os
+import pipes
 import subprocess
 import sys
 
@@ -12,6 +13,8 @@ DIR = os.path.dirname(os.path.abspath(__file__))
 REPO = os.path.dirname(os.path.dirname(DIR))
 BENCHMARKS_DIR = os.path.join(REPO, "misc", "tests", "benchmarks")
 FAST_DOWNWARD = os.path.join(REPO, "fast-downward.py")
+SAS_FILE = os.path.join(REPO, "test.sas")
+PLAN_FILE = os.path.join(REPO, "test.plan")
 
 TASKS = [os.path.join(BENCHMARKS_DIR, path) for path in [
     "miconic/s1-0.pddl",
@@ -22,10 +25,12 @@ CONFIGS.update(configs.default_configs_optimal(core=True, extended=True))
 CONFIGS.update(configs.default_configs_satisficing(core=True, extended=True))
 
 
-def run_plan_script(task, nick, config, debug):
-    cmd = [sys.executable, FAST_DOWNWARD]
-    if os.name != "nt":
-        cmd.extend(["--search-time-limit", "30m"])
+def escape_list(l):
+    return " ".join(pipes.quote(x) for x in l)
+
+
+def run_plan_script(task, config, debug):
+    cmd = [sys.executable, FAST_DOWNWARD, "--plan-file", PLAN_FILE]
     if debug:
         cmd.append("--debug")
     if "--alias" in config:
@@ -33,13 +38,19 @@ def run_plan_script(task, nick, config, debug):
         cmd += config + [task]
     else:
         cmd += [task] + config
-    print("\nRun {}:".format(cmd))
+    print("\nRun: {}:".format(escape_list(cmd)))
     sys.stdout.flush()
     subprocess.check_call(cmd)
 
 
+def translate(task):
+    subprocess.check_call([
+        sys.executable, FAST_DOWNWARD, "--sas-file", SAS_FILE, "--translate", task], cwd=REPO)
+
+
 def cleanup():
-    subprocess.check_call([sys.executable, FAST_DOWNWARD, "--cleanup"])
+    os.remove(SAS_FILE)
+    os.remove(PLAN_FILE)
 
 
 def main():
@@ -50,14 +61,15 @@ def main():
     if os.name == "posix":
         subprocess.check_call(["./build.py", "release", "debug"], cwd=REPO)
     for task in TASKS:
-        for nick, config in CONFIGS.items():
+        translate(task)
+        for config in sorted(CONFIGS.values()):
             for debug in [False, True]:
                 try:
-                    run_plan_script(task, nick, config, debug)
+                    run_plan_script(SAS_FILE, config, debug)
                 except subprocess.CalledProcessError:
                     sys.exit(
                         "\nError: {} failed to solve {} (debug={})".format(
-                            nick, task, debug))
-                cleanup()
+                            config, task, debug))
+    cleanup()
 
 main()
