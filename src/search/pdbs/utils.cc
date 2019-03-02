06d3eb4e7537013b66e2b23d07cae883f9954589
Silvan Sievers <silvan.sievers@unibas.ch>
2019-03-02 13:52:11 +0100

    [issue896] add generic function for dumping pattern/pdb collection statistics

--- src/search/pdbs/utils.cc
+++ src/search/pdbs/utils.cc
@@ -1,6 +1,12 @@
 #include "utils.h"
 
+#include "pattern_database.h"
+
+#include "../utils/logging.h"
+
 #include "../task_proxy.h"
+
+using namespace std;
 
 namespace pdbs {
 int compute_pdb_size(const TaskProxy &task_proxy, const Pattern &pattern) {
@@ -18,5 +24,43 @@ int compute_total_pdb_size(
         size += compute_pdb_size(task_proxy, pattern);
     }
     return size;
+}
+
+void dump_pattern_collection_statistics(
+    const TaskProxy &task_proxy,
+    string identifier,
+    utils::Duration runtime,
+    const std::shared_ptr<PatternCollection> &pattern_collection,
+    const std::shared_ptr<PDBCollection> &pdbs) {
+    assert(pattern_collection || pdbs);
+
+    int num_patterns;
+    int total_pdb_size = 0;
+    if (pdbs) {
+        for (const auto &pdb : *pdbs) {
+            total_pdb_size += pdb->get_size();
+        }
+        num_patterns = pdbs->size();
+    } else {
+        total_pdb_size = compute_total_pdb_size(task_proxy, *pattern_collection);
+        num_patterns = pattern_collection->size();
+    }
+
+    cout << identifier << " collection: ";
+    if (pattern_collection) {
+        cout << *pattern_collection << endl;
+    } else {
+        cout << "[";
+        string sep = "";
+        for (const auto &pdb : *pdbs) {
+            cout << sep << pdb->get_pattern();
+            sep = ", ";
+        }
+        cout << "]";
+    }
+    cout << endl;
+    cout << identifier << " number of patterns: " << num_patterns << endl;
+    cout << identifier << " total PDB size: " << total_pdb_size << endl;
+    cout << identifier << " runtime: " << runtime << endl;
 }
 }
--- src/search/pdbs/utils.h
+++ src/search/pdbs/utils.h
@@ -3,12 +3,32 @@
 
 #include "types.h"
 
+#include "../utils/timer.h"
+
+#include <memory>
+#include <string>
+
 class TaskProxy;
 
 namespace pdbs {
 extern int compute_pdb_size(const TaskProxy &task_proxy, const Pattern &pattern);
 extern int compute_total_pdb_size(
     const TaskProxy &task_proxy, const PatternCollection &pattern_collection);
+/*
+   Dumps the following information for a given pattern collection or PDB
+   collection, one of which has to be given as input:
+   - the collection itself
+   - the number of elements in the collection
+   - the total PDB size of the collection
+   - the runtime in seconds it took to compute the collection
+   All output is prepended with the given string identifier.
+*/
+extern void dump_pattern_collection_statistics(
+    const TaskProxy &task_proxy,
+    std::string identifier,
+    utils::Duration runtime,
+    const std::shared_ptr<PatternCollection> &pattern_collection = nullptr,
+    const std::shared_ptr<PDBCollection> &pdbs = nullptr);
 }
 
 #endif
