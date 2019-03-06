f2a0cfd948c671743b12bbeaf7bfd7f93853f790
Jendrik Seipp <jendrik.seipp@unibas.ch>
2019-03-06 09:04:41 +0100

    [issue703] CMake: write compiler ID and version to the cache.

--- misc/tests/test-memory-leaks.py
+++ misc/tests/test-memory-leaks.py
@@ -15,7 +15,8 @@ DIR = os.path.dirname(os.path.abspath(__file__))
 REPO = os.path.dirname(os.path.dirname(DIR))
 BENCHMARKS_DIR = os.path.join(REPO, "misc", "tests", "benchmarks")
 FAST_DOWNWARD = os.path.join(REPO, "fast-downward.py")
-DOWNWARD_BIN = os.path.join(REPO, "builds", "release", "bin", "downward")
+BUILD_DIR = os.path.join(REPO, "builds", "release")
+DOWNWARD_BIN = os.path.join(BUILD_DIR, "bin", "downward")
 SAS_FILE = os.path.join(REPO, "test.sas")
 PLAN_FILE = os.path.join(REPO, "test.plan")
 VALGRIND_GCC5_SUPPRESSION_FILE = os.path.join(
@@ -35,16 +36,11 @@ def escape_list(l):
     return " ".join(pipes.quote(x) for x in l)
 
 
-def get_major_gcc_version():
-    """
-    Read major GCC version from binary. Return None for other compilers.
-    """
-    output = subprocess.check_output(["strings", "-a", DOWNWARD_BIN])
-    match = re.search("GCC: \(.*\) (\d+).\d+.\d+ \d{8}\n", output)
-    if match:
-        return int(match.group(1))
-    else:
-        return None
+def get_compiler_and_version():
+    output = subprocess.check_output(["cmake", "-LA", "-N", "../../src/"], cwd=BUILD_DIR)
+    compiler = re.search("^DOWNWARD_CXX_COMPILER_ID:STRING=(.+)$", output, re.M).group(1)
+    version = re.search("^DOWNWARD_CXX_COMPILER_VERSION:STRING=(.+)$", output, re.M).group(1)
+    return compiler, version
 
 
 def run_plan_script(task, config, suppression_files):
@@ -93,11 +89,12 @@ def cleanup():
 
 def main():
     subprocess.check_call(["./build.py"], cwd=REPO)
-    major_gcc_version = get_major_gcc_version()
-    print("Major GCC version:", major_gcc_version)
+    compiler, compiler_version = get_compiler_and_version()
+    print("Compiler:", compiler, compiler_version)
     suppression_files = []
-    if major_gcc_version == 5:
-        # See http://issues.fast-downward.org/issue703.
+    if compiler == "GNU" and compiler_version.split(".")[0] == "5":
+        print("Using leak suppression file for GCC 5 "
+              "(see http://issues.fast-downward.org/issue703).")
         suppression_files.append(VALGRIND_GCC5_SUPPRESSION_FILE)
     for task in TASKS:
         translate(task)
