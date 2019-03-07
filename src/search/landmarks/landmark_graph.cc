8c66eb5f9f2ebdd1fbd3a4d453e0780d681c2f9f
Jendrik Seipp <jendrik.seipp@unibas.ch>
2019-03-07 10:09:09 +0100

    [issue899] Fix typo in method name.

--- src/search/landmarks/landmark_graph.cc
+++ src/search/landmarks/landmark_graph.cc
@@ -174,7 +174,7 @@ LandmarkNode &LandmarkGraph::landmark_add_conjunctive(const set<FactPair> &lm) {
     return *new_node_p;
 }
 
-void LandmarkGraph::remove_node_occurences(LandmarkNode *node) {
+void LandmarkGraph::remove_node_occurrences(LandmarkNode *node) {
     for (const auto &parent : node->parents) {
         LandmarkNode &parent_node = *(parent.first);
         parent_node.children.erase(node);
@@ -200,7 +200,7 @@ void LandmarkGraph::remove_node_if(
     const function<bool (const LandmarkNode &)> &remove_node) {
     for (auto &node : nodes) {
         if (remove_node(*node)) {
-            remove_node_occurences(node.get());
+            remove_node_occurrences(node.get());
         }
     }
     nodes.erase(remove_if(nodes.begin(), nodes.end(),
