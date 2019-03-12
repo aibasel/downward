ec9a7532b5995df865032d2e5e032021f43f8fef
Jendrik Seipp <jendrik.seipp@unibas.ch>
2019-03-12 20:23:32 +0100

    [issue909] Add utils::format_journal_article_reference().

--- src/search/utils/markup.cc
+++ src/search/utils/markup.cc
@@ -41,4 +41,18 @@ string format_paper_reference(
     ss << t2t_escape(year) << ".\n\n\n";
     return ss.str();
 }
+
+string format_journal_article_reference(
+    const std::vector<string> &authors, const string &title, const string &url,
+    const string &journal, const string &volume, const string &pages,
+    const string &year) {
+    stringstream ss;
+    ss << "\n\n"
+       << " * " << format_authors(authors) << ".<<BR>>\n"
+       << " [" << t2t_escape(title) << " " << url << "].<<BR>>\n"
+       << " //" << t2t_escape(journal) << "// "
+       << t2t_escape(volume) << ":" << t2t_escape(pages) << ". "
+       << t2t_escape(year) << ".\n\n\n";
+    return ss.str();
+}
 }
--- src/search/utils/markup.h
+++ src/search/utils/markup.h
@@ -9,6 +9,11 @@ extern std::string format_paper_reference(
     const std::vector<std::string> &authors, const std::string &title,
     const std::string &url, const std::string &venue, const std::string &pages,
     const std::string &publisher, const std::string &year);
+
+extern std::string format_journal_article_reference(
+    const std::vector<std::string> &authors, const std::string &title,
+    const std::string &url, const std::string &journal, const std::string &volume,
+    const std::string &pages, const std::string &year);
 }
 
 #endif
