4c3832a6e2c3bd030f77d91bf4456359c972527e
Silvan Sievers <silvan.sievers@unibas.ch>
2019-05-08 11:20:00 +0200

    [issue915] do not copy potentially uninitialized iterators

--- src/search/merge_and_shrink/label_equivalence_relation.cc
+++ src/search/merge_and_shrink/label_equivalence_relation.cc
@@ -22,9 +22,14 @@ LabelEquivalenceRelation::LabelEquivalenceRelation(
 LabelEquivalenceRelation::LabelEquivalenceRelation(
     const LabelEquivalenceRelation &other)
     : labels(other.labels),
-      /* We copy label_to_positions to have identical vectors even on
-      "unused" positions (for label numbers that do not exist any more). */
-      label_to_positions(other.label_to_positions) {
+      /*
+        Note that we do not copy label_to_positions because copying iterators
+        from potentially uninitialized iterators causes problems in debug mode.
+        This also means that label_to_positions contains uninitialized values
+        at all positions corresponding to already reduced labels (inactive
+        labels).
+      */
+      label_to_positions(other.label_to_positions.size()) {
     // For the reserve call, see the comment in the constructor above.
     grouped_labels.reserve(labels.get_max_size());
     for (size_t other_group_id = 0;
