39ab63fda92cb32af871227f5006f56d2101db53
Jendrik Seipp <jendrik.seipp@unibas.ch>
2019-10-10 00:23:48 +0200

    [issue935] Use pytest to test standard configs.

--- misc/tests/test-standard-configs.py
+++ misc/tests/test-standard-configs.py
@@ -1,11 +1,11 @@
-#! /usr/bin/env python
-
 from __future__ import print_function
 
 import os
 import pipes
 import subprocess
 import sys
+
+import pytest
 
 import configs
 
@@ -15,10 +15,7 @@ BENCHMARKS_DIR = os.path.join(REPO, "misc", "tests", "benchmarks")
 FAST_DOWNWARD = os.path.join(REPO, "fast-downward.py")
 SAS_FILE = os.path.join(REPO, "test.sas")
 PLAN_FILE = os.path.join(REPO, "test.plan")
-
-TASKS = [os.path.join(BENCHMARKS_DIR, path) for path in [
-    "miconic/s1-0.pddl",
-]]
+TASK = os.path.join(BENCHMARKS_DIR, "miconic/s1-0.pddl")
 
 CONFIGS = {}
 CONFIGS.update(configs.default_configs_optimal(core=True, extended=True))
@@ -40,7 +37,7 @@ def run_plan_script(task, config, debug):
         cmd += [task] + config
     print("\nRun: {}:".format(escape_list(cmd)))
     sys.stdout.flush()
-    subprocess.check_call(cmd)
+    subprocess.check_call(cmd, cwd=REPO)
 
 
 def translate(task):
@@ -53,23 +50,21 @@ def cleanup():
     os.remove(PLAN_FILE)
 
 
-def main():
+def setup_module(module):
     # On Windows, ./build.py has to be called from the correct environment.
     # Since we want this script to work even when we are in a regular
     # shell, we do not build on Windows. If the planner is not yet built,
     # the driver script will complain about this.
     if os.name == "posix":
         subprocess.check_call(["./build.py", "release", "debug"], cwd=REPO)
-    for task in TASKS:
-        translate(task)
-        for config in sorted(CONFIGS.values()):
-            for debug in [False, True]:
-                try:
-                    run_plan_script(SAS_FILE, config, debug)
-                except subprocess.CalledProcessError:
-                    sys.exit(
-                        "\nError: {} failed to solve {} (debug={})".format(
-                            config, task, debug))
-    cleanup()
+    translate(TASK)
 
-main()
+
+@pytest.mark.parametrize("config", sorted(CONFIGS.values()))
+@pytest.mark.parametrize("debug", [False, True])
+def test_configs(config, debug):
+    run_plan_script(SAS_FILE, config, debug)
+
+
+def teardown_module(module):
+    cleanup()
