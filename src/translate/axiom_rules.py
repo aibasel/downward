2e175e34a2e132526eda08fbb163bb2e10112fc5
Salome Eriksson <salome.eriksson@unibas.ch>
2020-07-16 12:05:30 +0200

    [issue453] Change how axioms removed by simplify are reported.

--- src/translate/axiom_rules.py
+++ src/translate/axiom_rules.py
@@ -194,7 +194,7 @@ def compute_clusters(axioms, goals, operators):
                 old_size = len(cluster.axioms[variable])
                 cluster.axioms[variable] = compute_simplified_axioms(cluster.axioms[variable])
                 removed += old_size - len(cluster.axioms[variable])
-    print("Simplifying axioms removed {} axioms".format(removed))
+    print(f"Translator axioms removed by simplifying: {removed}")
 
     # Create links between clusters (positive dependencies).
     for from_variable, depends_on in dependencies.positive_dependencies.items():
