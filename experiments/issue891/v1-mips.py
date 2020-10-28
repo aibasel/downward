a334dfa03405705910bacbdca0adec8d0e1b8b78
Florian Pommerening <florian.pommerening@unibas.ch>
2020-10-28 13:37:34 +0100

    [issue891] Update experiment reports.

--- experiments/issue891/v1-mips.py
+++ experiments/issue891/v1-mips.py
@@ -6,6 +6,7 @@ import os
 from lab.environments import LocalEnvironment, BaselSlurmEnvironment
 
 from downward.reports.compare import ComparativeReport
+from downward.reports.scatter import ScatterPlotReport
 
 import common_setup
 from common_setup import IssueConfig, IssueExperiment
@@ -44,13 +45,25 @@ exp.add_step('build', exp.build)
 exp.add_step('start', exp.start_runs)
 exp.add_fetcher(name='fetch')
 
-#exp.add_absolute_report_step()
-exp.add_comparison_table_step()
-exp.add_scatter_plot_step(relative=True, attributes=["total_time", "memory"],
+exp.add_report(ComparativeReport(
+    [("issue891-v1-opcount-lp", "issue891-v1-opcount-mip", "Diff (LP/MIP)")],
+    attributes=exp.DEFAULT_TABLE_ATTRIBUTES + ["initial_h_value"]))
+
+exp.add_scatter_plot_step(relative=False, attributes=["total_time", "memory"],
     additional=[
         ("opcount-lp", "opcount-mip", "issue891-v1", "issue891-v1", "total_time"),
         ("opcount-lp", "opcount-mip", "issue891-v1", "issue891-v1", "memory"),
-        ("opcount-lp", "opcount-mip", "issue891-v1", "issue891-v1", "initial_h_value")
     ])
+
+def interesting_h_value(run):
+    if "initial_h_value" in run and run["initial_h_value"] > 50:
+        run["initial_h_value"] = 51
+    return run
+
+exp.add_report(ScatterPlotReport(
+    attributes=["initial_h_value"],
+    filter=interesting_h_value,
+    get_category=lambda run1, run2: run1["domain"],
+))
 
 exp.run_steps()
