909c6579b93ea2b0214aa3468d24d854b1329638
Florian Pommerening <florian.pommerening@unibas.ch>
2021-01-30 19:19:32 +0100

    [issue989] Use extern for pure functions.

--- src/search/landmarks/util.h
+++ src/search/landmarks/util.h
@@ -11,18 +11,18 @@ namespace landmarks {
 class LandmarkNode;
 class LandmarkGraph;
 
-std::unordered_map<int, int> _intersect(
+extern std::unordered_map<int, int> _intersect(
     const std::unordered_map<int, int> &a,
     const std::unordered_map<int, int> &b);
 
-bool _possibly_reaches_lm(const OperatorProxy &op,
-                          const std::vector<std::vector<int>> &lvl_var,
-                          const LandmarkNode *lmp);
+extern bool _possibly_reaches_lm(
+    const OperatorProxy &op, const std::vector<std::vector<int>> &lvl_var,
+    const LandmarkNode *lmp);
 
-OperatorProxy get_operator_or_axiom(const TaskProxy &task_proxy, int op_or_axiom_id);
-int get_operator_or_axiom_id(const OperatorProxy &op);
+extern OperatorProxy get_operator_or_axiom(const TaskProxy &task_proxy, int op_or_axiom_id);
+extern int get_operator_or_axiom_id(const OperatorProxy &op);
 
-void dump(const TaskProxy &task_proxy, const LandmarkGraph &graph);
+extern void dump(const TaskProxy &task_proxy, const LandmarkGraph &graph);
 }
 
 #endif
