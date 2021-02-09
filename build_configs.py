a4a12309586a9773ca974e67240ec2f9cd592030
Florian Pommerening <florian.pommerening@unibas.ch>
2021-02-09 18:08:30 +0100

    [issue982] Remove nolp build configs and make debug build compilable with LP solvers.

--- build_configs.py
+++ build_configs.py
@@ -1,8 +1,7 @@
 release = ["-DCMAKE_BUILD_TYPE=Release"]
-debug = ["-DCMAKE_BUILD_TYPE=Debug","-DUSE_GLIBCXX_DEBUG=YES"]
-releasenolp = ["-DCMAKE_BUILD_TYPE=Release", "-DUSE_LP=NO"]
-debugnolp = ["-DCMAKE_BUILD_TYPE=Debug", "-DUSE_LP=NO", "-DUSE_GLIBCXX_DEBUG=YES"]
-debuglp = ["-DCMAKE_BUILD_TYPE=Debug","-DUSE_GLIBCXX_DEBUG=NO"]
+debug = ["-DCMAKE_BUILD_TYPE=Debug"]
+# USE_GLIBCXX_DEBUG is not compatible with USE_LP (see issue983).
+glibcxx_debug = ["-DCMAKE_BUILD_TYPE=Debug", "-DUSE_LP=NO", "-DUSE_GLIBCXX_DEBUG=YES"]
 minimal = ["-DCMAKE_BUILD_TYPE=Release", "-DDISABLE_PLUGINS_BY_DEFAULT=YES"]
 
 DEFAULT = "release"
