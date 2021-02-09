baaec9628c493ab32de560c1cbcddaaf7742f362
Florian Pommerening <florian.pommerening@unibas.ch>
2021-02-09 12:40:36 +0100

    [issue982] Add option for disabling GLIBCXX_DEBUG flag.

--- build_configs.py
+++ build_configs.py
@@ -1,7 +1,8 @@
 release = ["-DCMAKE_BUILD_TYPE=Release"]
-debug = ["-DCMAKE_BUILD_TYPE=Debug"]
+debug = ["-DCMAKE_BUILD_TYPE=Debug","-DUSE_GLIBCXX_DEBUG=YES"]
 releasenolp = ["-DCMAKE_BUILD_TYPE=Release", "-DUSE_LP=NO"]
-debugnolp = ["-DCMAKE_BUILD_TYPE=Debug", "-DUSE_LP=NO"]
+debugnolp = ["-DCMAKE_BUILD_TYPE=Debug", "-DUSE_LP=NO", "-DUSE_GLIBCXX_DEBUG=YES"]
+debuglp = ["-DCMAKE_BUILD_TYPE=Debug","-DUSE_GLIBCXX_DEBUG=NO"]
 minimal = ["-DCMAKE_BUILD_TYPE=Release", "-DDISABLE_PLUGINS_BY_DEFAULT=YES"]
 
 DEFAULT = "release"
