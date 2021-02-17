728f3d57015e301b8adf14bd3c1e6eb83cf48bc1
Thomas Keller <tho.keller@unibas.ch>
2021-02-17 07:09:31 +0100

    [issue1007] remove unnecessary std::

--- src/search/pdbs/cegar.cc
+++ src/search/pdbs/cegar.cc
@@ -18,14 +18,14 @@ using namespace std;
 namespace pdbs {
 struct SearchNode {
     State state;
-    std::size_t hash;
+    size_t hash;
     int g;
     int cost;
-    std::shared_ptr<SearchNode> predecessor;
+    shared_ptr<SearchNode> predecessor;
 
-    SearchNode(State state, std::size_t hash, int g, int cost,
-               std::shared_ptr<SearchNode> predecessor)
-            : state(std::move(state)), hash(hash), g(g), cost(cost),
+    SearchNode(State state, size_t hash, int g, int cost,
+               shared_ptr<SearchNode> predecessor)
+            : state(move(state)), hash(hash), g(g), cost(cost),
               predecessor(predecessor) {
     }
 };
@@ -39,35 +39,35 @@ inline void feed(HashState &hash_state, const pdbs::SearchNode &search_node) {
 
 namespace pdbs {
 class AbstractSolutionData {
-    std::shared_ptr<PatternDatabase> pdb;
+    shared_ptr<PatternDatabase> pdb;
     tasks::ProjectedTask abstracted_task;
     TaskProxy abs_task_proxy;
-    std::vector<std::vector<OperatorID>> wildcard_plan;
+    vector<vector<OperatorID>> wildcard_plan;
     bool wildcard_plans;
     bool is_solvable;
     bool solved;
 
-    std::vector<State> extract_state_sequence(
+    vector<State> extract_state_sequence(
             const SearchNode &goal_node) const;
-    std::vector<State> bfs_for_improving_state(
+    vector<State> bfs_for_improving_state(
             const successor_generator::SuccessorGenerator &succ_gen,
-            const std::shared_ptr<utils::RandomNumberGenerator> &rng,
+            const shared_ptr<utils::RandomNumberGenerator> &rng,
             const int f_star,
-            std::shared_ptr<SearchNode> &current_node) const;
-    std::vector<State> steepest_ascent_enforced_hillclimbing(
+            shared_ptr<SearchNode> &current_node) const;
+    vector<State> steepest_ascent_enforced_hillclimbing(
             const successor_generator::SuccessorGenerator &succ_gen,
-            const std::shared_ptr<utils::RandomNumberGenerator> &rng,
+            const shared_ptr<utils::RandomNumberGenerator> &rng,
             utils::Verbosity verbosity) const;
     void turn_state_sequence_into_plan(
             const successor_generator::SuccessorGenerator &succ_gen,
-            const std::shared_ptr<utils::RandomNumberGenerator> &rng,
+            const shared_ptr<utils::RandomNumberGenerator> &rng,
             utils::Verbosity verbosity,
-            const std::vector<State> &state_sequence);
+            const vector<State> &state_sequence);
 public:
     AbstractSolutionData(
-            const std::shared_ptr<AbstractTask> &concrete_task,
+            const shared_ptr<AbstractTask> &concrete_task,
             const Pattern &pattern,
-            const std::shared_ptr<utils::RandomNumberGenerator> &rng,
+            const shared_ptr<utils::RandomNumberGenerator> &rng,
             bool wildcard_plans,
             utils::Verbosity verbosity);
 
@@ -75,7 +75,7 @@ public:
         return pdb->get_pattern();
     }
 
-    const std::shared_ptr<PatternDatabase> &get_pdb() {
+    const shared_ptr<PatternDatabase> &get_pdb() {
         return pdb;
     }
 
@@ -91,12 +91,12 @@ public:
         return solved;
     }
 
-    const std::vector<std::vector<OperatorID>> &get_plan() const {
+    const vector<vector<OperatorID>> &get_plan() const {
         return wildcard_plan;
     }
 
     OperatorID get_concrete_op_id_for_abs_op_id(
-            OperatorID abs_op_id, const std::shared_ptr<AbstractTask> &parent_task) const;
+            OperatorID abs_op_id, const shared_ptr<AbstractTask> &parent_task) const;
 
     void print_plan() const;
 };
