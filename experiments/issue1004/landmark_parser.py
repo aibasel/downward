17fc65f83425e0b1f8e07702ffc0bb2b33aaef73
Thomas Keller <tho.keller@unibas.ch>
2021-02-20 00:11:16 +0100

    [issue1004] adjust landmark parser to altered statistic output

--- experiments/issue1004/landmark_parser.py
+++ experiments/issue1004/landmark_parser.py
@@ -5,19 +5,25 @@ import re
 from lab.parser import Parser
 
 parser = Parser()
-parser.add_pattern("lm_graph_generation_time", r"Landmark graph generation time: (.+)s")
+parser.add_pattern(
+    "lmgraph_generation_time",
+    r"Landmark graph generation time: (.+)s",
+    type=float)
 parser.add_pattern(
     "landmarks", 
-    r"Landmark graph contains (\d+) landmarks, of which \d+ are disjunctive and \d+ are conjunctive."
-)
+    r"Landmark graph contains (\d+) landmarks, of which \d+ are disjunctive and \d+ are conjunctive.",
+    type=int)
 parser.add_pattern(
-    "disjunctive_landmarks", 
-    r"Landmark graph contains \d+ landmarks, of which (\d+) are disjunctive and \d+ are conjunctive."
-)
+    "landmarks_disjunctive", 
+    r"Landmark graph contains \d+ landmarks, of which (\d+) are disjunctive and \d+ are conjunctive.",
+    type=int)
 parser.add_pattern(
-    "conjunctive_landmarks", 
-    r"Landmark graph contains \d+ landmarks, of which \d+ are disjunctive and (\d+) are conjunctive."
-)
-parser.add_pattern("orderings", r"Landmark graph contains (\d+) orderings.")
+    "landmarks_conjunctive", 
+    r"Landmark graph contains \d+ landmarks, of which \d+ are disjunctive and (\d+) are conjunctive.",
+    type=int)
+parser.add_pattern(
+    "orderings",
+    r"Landmark graph contains (\d+) orderings.",
+    type=int)
 
 parser.parse()
