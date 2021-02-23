bfba36fbf6cbc237cbf793f74494d9fff11f43c3
Silvan Sievers <silvan.sievers@unibas.ch>
2021-02-23 12:22:18 +0100

    [issue1007] projected task factory: use vector instead of hash map for projecting variables

--- src/search/tasks/projected_task_factory.cc
+++ src/search/tasks/projected_task_factory.cc
@@ -20,69 +20,63 @@ shared_ptr<AbstractTask> build_projected_task(
         ABORT("ProjectedTask doesn't support conditional effects.");
     }
 
-    unordered_map<int, int> var_to_index;
     vector<int> operator_indices;
     vector<vector<FactPair>> operator_preconditions;
     vector<vector<FactPair>> operator_effects;
     vector<FactPair> goals;
 
-    // remember where in the pattern each variable is stored
+    // Map variables of the parent to indices in the projection.
+    vector<int> var_to_index(parent_proxy.get_variables().size(), -1);
     for (size_t i = 0; i < variables.size(); ++i) {
         int var = variables[i];
         var_to_index[var] = i;
     }
 
-    // make a list of operator indices for operators which are
-    // relevant to at least one variable in the pattern
-    for (int opi = 0; opi < parent->get_num_operators(); ++opi) {
+    /*
+      Compute the preconditions and effects (both set of facts) of all
+      operators relevant to the projection, i.e., operators which have an
+      effect on some variable of the projection.
+    */
+    int num_operators = parent->get_num_operators();
+    for (int op_index = 0; op_index < num_operators; ++op_index) {
         vector<FactPair> effects;
 
-        for (int effi = 0;
-             effi < parent->get_num_operator_effects(opi, false); ++effi) {
+        int num_effects = parent->get_num_operator_effects(op_index, false);
+        for (int eff_index = 0; eff_index < num_effects; ++eff_index) {
             FactPair effect =
-                parent->get_operator_effect(opi, effi, false);
+                parent->get_operator_effect(op_index, eff_index, false);
             int var = effect.var;
-
-            if (var_to_index.count(var) != 0) {
-                effects.emplace_back(var_to_index[effect.var], effect.value);
+            if (var_to_index[var] != -1) {
+                effects.emplace_back(var_to_index[var], effect.value);
             }
         }
 
-        if (effects.empty()) {
-            continue;
-        } else {
-            operator_indices.push_back(opi);
-            operator_effects.push_back(effects);
-        }
+        if (!effects.empty()) {
+            operator_indices.push_back(op_index);
+            operator_effects.push_back(move(effects));
 
-        vector<FactPair> preconditions;
-        for (int condi = 0;
-             condi < parent->get_num_operator_preconditions(opi, false); ++condi) {
-            FactPair precondition =
-                parent->get_operator_precondition(opi, condi, false);
-            int var = precondition.var;
-
-            // check if the precondition variable appears in the pattern
-            if (var_to_index.count(var) != 0) {
-                preconditions.emplace_back(
-                    var_to_index[precondition.var], precondition.value);
+            vector<FactPair> preconditions;
+            int num_preconditions = parent->get_num_operator_preconditions(op_index, false);
+            for (int fact_index = 0; fact_index < num_preconditions; ++fact_index) {
+                FactPair precondition =
+                    parent->get_operator_precondition(op_index, fact_index, false);
+                int var = precondition.var;
+                if (var_to_index[var] != -1) {
+                    preconditions.emplace_back(var_to_index[var], precondition.value);
+                }
             }
+            operator_preconditions.push_back(move(preconditions));
         }
-        operator_preconditions.push_back(preconditions);
     }
 
-    for (int goali = 0;
-         goali < parent->get_num_goals(); ++goali) {
-        FactPair goal =
-            parent->get_goal_fact(goali);
+    int num_goals = parent->get_num_goals();
+    for (int goal_index = 0; goal_index < num_goals; ++goal_index) {
+        FactPair goal = parent->get_goal_fact(goal_index);
         int var = goal.var;
-
-        auto res = find(variables.begin(), variables.end(), var);
-        if (res != variables.end()) {
-            goals.emplace_back(var_to_index[goal.var], goal.value);
+        if (find(variables.begin(), variables.end(), var) != variables.end()) {
+            goals.emplace_back(var_to_index[var], goal.value);
         }
     }
-    assert((unsigned)parent->get_num_goals() >= goals.size());
 
     return make_shared<ProjectedTask>(
         parent,
