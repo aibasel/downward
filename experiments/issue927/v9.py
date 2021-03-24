e513d4dfe67d3cb0696020ee1a31d5001fc2e4e7
Silvan Sievers <silvan.sievers@unibas.ch>
2021-03-24 10:06:54 +0100

    [issue927] add missing library paths

--- experiments/issue927/v9.py
+++ experiments/issue927/v9.py
@@ -36,7 +36,13 @@ SUITE = common_setup.DEFAULT_OPTIMAL_SUITE
 ENVIRONMENT = BaselSlurmEnvironment(
     partition="infai_2",
     email="silvan.sievers@unibas.ch",
-    export=["PATH", "DOWNWARD_BENCHMARKS"])
+    # paths obtained via:
+    # module purge
+    # module -q load CMake/3.15.3-GCCcore-8.3.0
+    # module -q load GCC/8.3.0
+    # echo $PATH
+    # echo $LD_LIBRARY_PATH
+    setup='export PATH=/scicore/soft/apps/binutils/2.32-GCCcore-8.3.0/bin:/scicore/soft/apps/CMake/3.15.3-GCCcore-8.3.0/bin:/scicore/soft/apps/cURL/7.66.0-GCCcore-8.3.0/bin:/scicore/soft/apps/bzip2/1.0.8-GCCcore-8.3.0/bin:/scicore/soft/apps/ncurses/6.1-GCCcore-8.3.0/bin:/scicore/soft/apps/GCCcore/8.3.0/bin:/infai/sieverss/repos/bin:/infai/sieverss/local:/export/soft/lua_lmod/centos7/lmod/lmod/libexec:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:$PATH\nexport LD_LIBRARY_PATH=/scicore/soft/apps/binutils/2.32-GCCcore-8.3.0/lib:/scicore/soft/apps/cURL/7.66.0-GCCcore-8.3.0/lib:/scicore/soft/apps/bzip2/1.0.8-GCCcore-8.3.0/lib:/scicore/soft/apps/zlib/1.2.11-GCCcore-8.3.0/lib:/scicore/soft/apps/ncurses/6.1-GCCcore-8.3.0/lib:/scicore/soft/apps/GCCcore/8.3.0/lib64:/scicore/soft/apps/GCCcore/8.3.0/lib')
 
 if common_setup.is_test_run():
     SUITE = IssueExperiment.DEFAULT_TEST_SUITE
@@ -63,7 +69,7 @@ exp.add_fetcher(name='fetch')
 perfect_heuristic = Attribute('perfect_heuristic', absolute=True, min_wins=False)
 
 # m&s attributes
-ms_construction_time = Attribute('ms_construction_time', absolute=False, min_wins=True, functions=[geometric_mean])
+ms_construction_time = Attribute('ms_construction_time', absolute=False, min_wins=True, function=geometric_mean)
 score_ms_construction_time = Attribute('score_ms_construction_time', min_wins=False, digits=4)
 ms_atomic_construction_time = Attribute('ms_atomic_construction_time', absolute=False, min_wins=True, functions=[geometric_mean])
 ms_abstraction_constructed = Attribute('ms_abstraction_constructed', absolute=True, min_wins=False)
