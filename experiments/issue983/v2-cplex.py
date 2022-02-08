f43e05e06f07dc7bf91af6092f5c48673f265d1c
Florian Pommerening <florian.pommerening@unibas.ch>
2022-02-08 12:37:45 +0100

    [issue983] report for comparing h^+

--- experiments/issue983/v2-cplex.py
+++ experiments/issue983/v2-cplex.py
@@ -3,6 +3,7 @@
 import os
 
 from lab.environments import LocalEnvironment, BaselSlurmEnvironment
+from downward.reports.compare import ComparativeReport
 
 import common_setup
 from common_setup import IssueConfig, IssueExperiment
@@ -86,36 +87,24 @@ exp.add_fetcher(name='fetch')
 def get_task(id):
     return f"{id[1]}:{id[2]}"
 
-
-lmcut_costs = {}
-def get_lmcut_costs(run):
-    if "lmcut" in run["algorithm"]:
-        lmcut_costs[get_task(run["id"])] = run["cost"]
-    return run
-
-
-def add_diff_relaxed_cost(run):
-    h = run["initial_h_value"]
-    diff = h - lmcut_costs[get_task(run["id"])]
-    run["diff_relaxed_cost"] = diff
-
-    inadmissible = 0
-    not_equal = 0
+def add_hplus(run):
     if "hplus" in run["algorithm"]:
-        inadmissible = diff > 0
-        if "-int-time" in run["algorithm"]:
-            not_equal = diff != 0
-    run["hplus_inadmissible"] = inadmissible
-    run["hplus_not_equal_relaxed_cost"] = not_equal
-
+        run["hplus"] = run.get("initial_h_value")
+    elif "relaxed-lmcut" in run["algorithm"]:
+        run["hplus"] = run.get("cost")
     return run
+
+
+exp.add_report(ComparativeReport(
+    filter=[add_hplus],
+    attributes=["hplus"],
+    algorithm_pairs=[("issue983-v1-opcount-hplus-int-time", "relaxed-lmcut"), ("issue983-v2-opcount-hplus-int-time", "relaxed-lmcut")]),
+    outfile="issue983-v2-compare-hplus.html")
 
 
 exp.add_comparison_table_step(
-    filter=[get_lmcut_costs, add_diff_relaxed_cost],
     attributes=exp.DEFAULT_TABLE_ATTRIBUTES + [
-        "initial_h_value", "diff_relaxed_cost",
-        "hplus_not_equal_relaxed_cost", "hplus_inadmissible",
+        "initial_h_value"
     ]
 )
 
