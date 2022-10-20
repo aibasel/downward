a608ac6554eb7723a8833b73cacb35af0f8dd30a
Gabriele Roeger <gabriele.roeger@unibas.ch>
2022-10-20 10:31:09 +0200

    [issue1067] Ignore code leading to unused variables in release build
    Clang 14 complains about unused-but-set-variables which we only use in an
    assertion. I remove all code setting the variable by means of a preprocessor
    directive when building without debug flag.

--- src/search/tasks/domain_abstracted_task_factory.cc
+++ src/search/tasks/domain_abstracted_task_factory.cc
@@ -106,11 +106,15 @@ string DomainAbstractedTaskFactory::get_combined_fact_name(
 void DomainAbstractedTaskFactory::combine_values(int var, const ValueGroups &groups) {
     vector<string> combined_fact_names;
     unordered_set<int> groups_union;
+#ifndef NDEBUG
     int num_merged_values = 0;
+#endif
     for (const ValueGroup &group : groups) {
         combined_fact_names.push_back(get_combined_fact_name(var, group));
         groups_union.insert(group.begin(), group.end());
+#ifndef NDEBUG
         num_merged_values += group.size();
+#endif
     }
     assert(static_cast<int>(groups_union.size()) == num_merged_values);
 
