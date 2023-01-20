7adc203ffe333ecd330ef73d627e66a79d98993b
Florian Pommerening <florian.pommerening@unibas.ch>
2023-01-20 10:55:54 +0100

    [issue1040] fix newlines

--- driver/portfolios/seq_sat_fdss_2014.py
+++ driver/portfolios/seq_sat_fdss_2014.py
@@ -48,15 +48,18 @@ CONFIGS = [
           "lazy_greedy([hadd,hcg],preferred=[hadd,hcg],cost_type=S_COST_TYPE,bound=BOUND)))"]),
     # add_lm_lazy_wastar
     (79, ["--search",
-         "let(hadd, add(transform=H_COST_TRANSFORM)," "let(hlm, lmcount(lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(plusone)),"
+         "let(hadd, add(transform=H_COST_TRANSFORM),"
+         "let(hlm, lmcount(lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(plusone)),"
           "lazy_wastar([hadd,hlm],w=3,preferred=[hadd,hlm],cost_type=S_COST_TYPE,bound=BOUND)))"]),
     # ff_lm_lazy_wastar
     (159, ["--search",
-         "let(hff, ff(transform=H_COST_TRANSFORM)," "let(hlm, lmcount(lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(plusone)),"
+         "let(hff, ff(transform=H_COST_TRANSFORM),"
+         "let(hlm, lmcount(lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(plusone)),"
           "lazy_wastar([hff,hlm],w=3,preferred=[hff,hlm],cost_type=S_COST_TYPE,bound=BOUND)))"]),
     # cea_lm_lazy_wastar
     (39, ["--search",
-         "let(hcea, cea(transform=H_COST_TRANSFORM)," "let(hlm, lmcount(lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(plusone)),"
+         "let(hcea, cea(transform=H_COST_TRANSFORM),"
+         "let(hlm, lmcount(lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(plusone)),"
           "lazy_wastar([hcea,hlm],w=3,preferred=[hcea,hlm],cost_type=S_COST_TYPE,bound=BOUND)))"]),
     # cg_lm_eager_greedy
     (78, ["--search",
@@ -70,7 +73,8 @@ CONFIGS = [
           "lazy_wastar([hcea,hff],w=3,preferred=[hcea,hff],cost_type=S_COST_TYPE,bound=BOUND)))"]),
     # cea_lm_eager_wastar
     (37, ["--search",
-         "let(hcea, cea(transform=H_COST_TRANSFORM)," "let(hlm, lmcount(lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(plusone)),"
+         "let(hcea, cea(transform=H_COST_TRANSFORM),"
+         "let(hlm, lmcount(lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(plusone)),"
          "eager(alt([single(sum([g(), weight(hcea, 3)])),single(sum([g(),weight(hcea,3)]),pref_only=true),single(sum([g(), weight(hlm,3)])),single(sum([g(),weight(hlm,3)]),pref_only=true)]),preferred=[hcea,hlm],cost_type=S_COST_TYPE,bound=BOUND)))"]),
     # cg_ff_lazy_wastar
     (40, ["--search",
@@ -79,7 +83,8 @@ CONFIGS = [
           "lazy_wastar([hcg,hff],w=3,preferred=[hcg,hff],cost_type=S_COST_TYPE,bound=BOUND)))"]),
     # ff_lm_eager_wastar
     (40, ["--search",
-         "let(hff, ff(transform=H_COST_TRANSFORM)," "let(hlm, lmcount(lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(plusone)),"
+         "let(hff, ff(transform=H_COST_TRANSFORM),"
+         "let(hlm, lmcount(lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(plusone)),"
          "eager(alt([single(sum([g(),weight(hff,3)])),single(sum([g(),weight(hff,3)]),pref_only=true),single(sum([g(),weight(hlm,3)])),single(sum([g(),weight(hlm,3)]),pref_only=true)]),preferred=[hff,hlm],cost_type=S_COST_TYPE,bound=BOUND)))"]),
     # add_eager_wastar
     (77, ["--search",
@@ -87,11 +92,13 @@ CONFIGS = [
           "eager(alt([single(sum([g(), weight(hadd, 3)])),single(sum([g(), weight(hadd,3)]),pref_only=true)]),preferred=[hadd],cost_type=S_COST_TYPE,bound=BOUND))"]),
     # cea_ff_eager_wastar
     (40, ["--search",
-         "let(hff, ff(transform=H_COST_TRANSFORM)," "let(hcea, cea(transform=H_COST_TRANSFORM),"
+         "let(hff, ff(transform=H_COST_TRANSFORM),"
+         "let(hcea, cea(transform=H_COST_TRANSFORM),"
          "eager(alt([single(sum([g(),weight(hff,3)])),single(sum([g(),weight(hff,3)]),pref_only=true),single(sum([g(),weight(hcea,3)])),single(sum([g(),weight(hcea,3)]),pref_only=true)]),preferred=[hff,hcea],cost_type=S_COST_TYPE,bound=BOUND)))"]),
     # cg_lm_eager_wastar
     (78, ["--search",
-         "let(hcg, cg(transform=H_COST_TRANSFORM)," "let(hlm, lmcount(lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(plusone)),"
+         "let(hcg, cg(transform=H_COST_TRANSFORM),"
+         "let(hlm, lmcount(lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(plusone)),"
          "eager(alt([single(sum([g(),weight(hcg,3)])),single(sum([g(),weight(hcg,3)]),pref_only=true),single(sum([g(),weight(hlm,3)])),single(sum([g(),weight(hlm,3)]),pref_only=true)]),preferred=[hcg,hlm],cost_type=S_COST_TYPE,bound=BOUND)))"]),
     # cea_eager_greedy
     (40, ["--search",
@@ -99,7 +106,8 @@ CONFIGS = [
          "eager_greedy([hcea],preferred=[hcea],cost_type=S_COST_TYPE,bound=BOUND))"]),
     # cg_lm_lazy_wastar
     (39, ["--search",
-         "let(hcg, cg(transform=H_COST_TRANSFORM)," "let(hlm, lmcount(lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(plusone)),"
+         "let(hcg, cg(transform=H_COST_TRANSFORM),"
+         "let(hlm, lmcount(lm_reasonable_orders_hps(lm_rhw()),transform=adapt_costs(plusone)),"
           "lazy_wastar([hcg,hlm],w=3,preferred=[hcg,hlm],cost_type=S_COST_TYPE,bound=BOUND)))"]),
     # cea_lazy_wastar
     (40, ["--search",
