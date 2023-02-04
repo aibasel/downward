345e0697f844cfad5f6e06aff386f4e8c16c94d8
Gabriele Roeger <gabriele.roeger@unibas.ch>
2023-02-04 17:29:20 +0100

    [issue1079] Reintroduce some old code.
    There can be atoms in a mutex group that are not represented in the
    strips2sas dictionary (freecell and settlers).

--- src/translate/translate.py
+++ src/translate/translate.py
@@ -625,9 +625,12 @@ def build_mutex_key(strips_to_sas, groups):
     for group in groups:
         group_key = []
         for fact in group:
-            represented_by = strips_to_sas[fact]
-            assert len(represented_by) == 1
-            group_key.append(represented_by[0])
+            represented_by = strips_to_sas.get(fact)
+            if represented_by:
+            	assert len(represented_by) == 1
+            	group_key.append(represented_by[0])
+            else:
+                print("not in strips_to_sas, left out:", fact)
         group_keys.append(group_key)
     return group_keys
 
