ab2f99db4abcf1b2889df69820e14f831236b144
Silvan Sievers <silvan.sievers@unibas.ch>
2023-02-09 14:00:13 +0100

    [issue927] only sort labels before removing old ones from group

--- src/search/merge_and_shrink/transition_system.cc
+++ src/search/merge_and_shrink/transition_system.cc
@@ -43,6 +43,7 @@ void LocalLabelInfo::apply_same_cost_label_mapping(
 void LocalLabelInfo::remove_labels(const vector<int> &old_labels) {
     assert(is_consistent());
     assert(utils::is_sorted_unique(old_labels));
+    sort(label_group.begin(), label_group.end());
     auto it = set_difference(
         label_group.begin(), label_group.end(),
         old_labels.begin(), old_labels.end(),
@@ -74,7 +75,6 @@ void LocalLabelInfo::merge_local_label_info(LocalLabelInfo &local_label_info) {
         make_move_iterator(local_label_info.label_group.end()));
     cost = min(cost, local_label_info.cost);
     local_label_info.deactivate();
-    sort(label_group.begin(), label_group.end());
     assert(is_consistent());
 }
 
@@ -85,8 +85,7 @@ void LocalLabelInfo::deactivate() {
 }
 
 bool LocalLabelInfo::is_consistent() const {
-    return utils::is_sorted_unique(label_group) &&
-        utils::is_sorted_unique(transitions);
+    return utils::is_sorted_unique(transitions);
 }
 
 
@@ -251,7 +250,6 @@ unique_ptr<TransitionSystem> TransitionSystem::merge(
                 dead_labels.insert(dead_labels.end(), new_labels.begin(), new_labels.end());
             } else {
                 sort(new_transitions.begin(), new_transitions.end());
-                sort(new_labels.begin(), new_labels.end());
                 int new_local_label = local_label_infos.size();
                 int cost = INF;
                 for (int label : new_labels) {
@@ -271,7 +269,6 @@ unique_ptr<TransitionSystem> TransitionSystem::merge(
       All dead labels should form one single label group.
     */
     if (!dead_labels.empty()) {
-        sort(dead_labels.begin(), dead_labels.end());
         int new_local_label = local_label_infos.size();
         int cost = INF;
         for (int label : dead_labels) {
@@ -473,7 +470,7 @@ void TransitionSystem::apply_label_reduction(
           Remove all labels of all affected local labels and recompute the
           cost of these affected local labels.
         */
-        for (auto &entry : local_label_to_old_labels) {
+        for (pair<const int, vector<int>> &entry : local_label_to_old_labels) {
             sort(entry.second.begin(), entry.second.end());
             local_label_infos[entry.first].remove_labels(entry.second);
             local_label_infos[entry.first].recompute_cost(labels);
