6a5626bdaa56a3c508b504faaea39f9ebeeaba29
Silvan Sievers <silvan.sievers@unibas.ch>
2023-02-09 11:54:09 +0100

    [issue1019] guard dump output with if (log) clause

--- src/search/pdbs/abstract_operator.cc
+++ src/search/pdbs/abstract_operator.cc
@@ -21,16 +21,18 @@ AbstractOperator::AbstractOperator(
 void AbstractOperator::dump(const Pattern &pattern,
                             const VariablesProxy &variables,
                             utils::LogProxy &log) const {
-    log << "AbstractOperator:" << endl;
-    log << "Preconditions:" << endl;
-    for (size_t i = 0; i < regression_preconditions.size(); ++i) {
-        int var_id = regression_preconditions[i].var;
-        int val = regression_preconditions[i].value;
-        log << "Pattern variable: " << var_id
-            << " (Concrete variable: " << pattern[var_id]
-            << "  True name: " << variables[pattern[var_id]].get_name()
-            << ") Value: " << val << endl;
+    if (log.is_at_least_debug()) {
+        log << "AbstractOperator:" << endl;
+        log << "Preconditions:" << endl;
+        for (size_t i = 0; i < regression_preconditions.size(); ++i) {
+            int var_id = regression_preconditions[i].var;
+            int val = regression_preconditions[i].value;
+            log << "Pattern variable: " << var_id
+                << " (Concrete variable: " << pattern[var_id]
+                << "  True name: " << variables[pattern[var_id]].get_name()
+                << ") Value: " << val << endl;
+        }
+        log << "Hash effect: " << hash_effect << endl;
     }
-    log << "Hash effect: " << hash_effect << endl;
 }
 }
