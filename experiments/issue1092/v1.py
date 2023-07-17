557657e015652be24c13db414d916eb81783ba51
Silvan Sievers <silvan.sievers@unibas.ch>
2023-07-17 18:18:05 +0200

    env

--- experiments/issue1092/v1.py
+++ experiments/issue1092/v1.py
@@ -37,14 +37,19 @@ CONFIGS = [
 
 SUITE = common_setup.DEFAULT_OPTIMAL_SUITE
 ENVIRONMENT = BaselSlurmEnvironment(
-    partition="infai_2",
+    partition="infai_3",
     email="silvan.sievers@unibas.ch",
+    memory_per_cpu="3940M",
+    cpus_per_task=2,
+    export=["PATH"],
     # paths obtained via:
     # module purge
-    # module -q CMake/3.23.1-GCCcore-11.3.0
+    # module -q load Python/3.10.4-GCCcore-11.3.0
+    # module -q load GCC/11.3.0
+    # module -q load CMake/3.23.1-GCCcore-11.3.0
     # echo $PATH
     # echo $LD_LIBRARY_PATH
-    setup='export PATH=/scicore/soft/apps/binutils/2.36.1-GCCcore-10.3.0/bin:/scicore/soft/apps/CMake/3.23.1-GCCcore-10.3.0/bin:/scicore/soft/apps/OpenSSL/1.1/bin:/scicore/soft/apps/libarchive/3.5.1-GCCcore-10.3.0/bin:/scicore/soft/apps/XZ/5.2.5-GCCcore-10.3.0/bin:/scicore/soft/apps/cURL/7.76.0-GCCcore-10.3.0/bin:/scicore/soft/apps/bzip2/1.0.8-GCCcore-10.3.0/bin:/scicore/soft/apps/ncurses/6.2-GCCcore-10.3.0/bin:/scicore/soft/apps/GCCcore/10.3.0/bin:/infai/sieverss/repos/bin:/infai/sieverss/local:/export/soft/lua_lmod/centos7/lmod/lmod/libexec:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:$PATH\nexport LD_LIBRARY_PATH=/scicore/soft/apps/binutils/2.36.1-GCCcore-10.3.0/lib:/scicore/soft/apps/OpenSSL/1.1/lib:/scicore/soft/apps/libarchive/3.5.1-GCCcore-10.3.0/lib:/scicore/soft/apps/XZ/5.2.5-GCCcore-10.3.0/lib:/scicore/soft/apps/cURL/7.76.0-GCCcore-10.3.0/lib:/scicore/soft/apps/bzip2/1.0.8-GCCcore-10.3.0/lib:/scicore/soft/apps/zlib/1.2.11-GCCcore-10.3.0/lib:/scicore/soft/apps/ncurses/6.2-GCCcore-10.3.0/lib:/scicore/soft/apps/GCCcore/10.3.0/lib64')
+    setup='export PATH=/scicore/soft/apps/CMake/3.23.1-GCCcore-11.3.0/bin:/scicore/soft/apps/libarchive/3.6.1-GCCcore-11.3.0/bin:/scicore/soft/apps/cURL/7.83.0-GCCcore-11.3.0/bin:/scicore/soft/apps/Python/3.10.4-GCCcore-11.3.0/bin:/scicore/soft/apps/OpenSSL/1.1/bin:/scicore/soft/apps/XZ/5.2.5-GCCcore-11.3.0/bin:/scicore/soft/apps/SQLite/3.38.3-GCCcore-11.3.0/bin:/scicore/soft/apps/Tcl/8.6.12-GCCcore-11.3.0/bin:/scicore/soft/apps/ncurses/6.3-GCCcore-11.3.0/bin:/scicore/soft/apps/bzip2/1.0.8-GCCcore-11.3.0/bin:/scicore/soft/apps/binutils/2.38-GCCcore-11.3.0/bin:/scicore/soft/apps/GCCcore/11.3.0/bin:/infai/sieverss/repos/bin:/infai/sieverss/local:/export/soft/lua_lmod/centos7/lmod/lmod/libexec:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:$PATH\nexport LD_LIBRARY_PATH=/scicore/soft/apps/libarchive/3.6.1-GCCcore-11.3.0/lib:/scicore/soft/apps/cURL/7.83.0-GCCcore-11.3.0/lib:/scicore/soft/apps/Python/3.10.4-GCCcore-11.3.0/lib:/scicore/soft/apps/OpenSSL/1.1/lib:/scicore/soft/apps/libffi/3.4.2-GCCcore-11.3.0/lib64:/scicore/soft/apps/GMP/6.2.1-GCCcore-11.3.0/lib:/scicore/soft/apps/XZ/5.2.5-GCCcore-11.3.0/lib:/scicore/soft/apps/SQLite/3.38.3-GCCcore-11.3.0/lib:/scicore/soft/apps/Tcl/8.6.12-GCCcore-11.3.0/lib:/scicore/soft/apps/libreadline/8.1.2-GCCcore-11.3.0/lib:/scicore/soft/apps/ncurses/6.3-GCCcore-11.3.0/lib:/scicore/soft/apps/bzip2/1.0.8-GCCcore-11.3.0/lib:/scicore/soft/apps/binutils/2.38-GCCcore-11.3.0/lib:/scicore/soft/apps/zlib/1.2.12-GCCcore-11.3.0/lib:/scicore/soft/apps/GCCcore/11.3.0/lib64')
 
 if common_setup.is_test_run():
     SUITE = IssueExperiment.DEFAULT_TEST_SUITE
