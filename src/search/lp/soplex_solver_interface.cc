86bc072cce66aae61f59c7011f5198df2593aaee
Florian Pommerening <florian.pommerening@unibas.ch>
2023-07-19 00:59:37 +0200

    [issue1076] do not force representation or algorithm

--- src/search/lp/soplex_solver_interface.cc
+++ src/search/lp/soplex_solver_interface.cc
@@ -52,8 +52,6 @@ static LPColSetReal variables_to_col_set(const named_vector::NamedVector<LPVaria
 SoPlexSolverInterface::SoPlexSolverInterface() : SolverInterface() {
     soplex.setIntParam(SoPlex::VERBOSITY, SoPlex::VERBOSITY_ERROR);
     soplex.setIntParam(SoPlex::SIMPLIFIER, SoPlex::SIMPLIFIER_OFF);
-    soplex.setIntParam(SoPlex::REPRESENTATION, SoPlex::REPRESENTATION_COLUMN);
-    soplex.setIntParam(SoPlex::ALGORITHM, SoPlex::ALGORITHM_DUAL);
 }
 
 void SoPlexSolverInterface::load_problem(const LinearProgram &lp) {
