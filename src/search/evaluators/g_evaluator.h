8d058fb49e2c21e32d5dc57263976d8ab239d320
Simon Dold <simon.dold@stud.unibas.ch>
2023-07-19 23:32:51 +0200

    Move unpacking of options to GEvaluatorFeature.

--- src/search/evaluators/g_evaluator.h
+++ src/search/evaluators/g_evaluator.h
@@ -7,6 +7,11 @@ namespace g_evaluator {
 class GEvaluator : public Evaluator {
 public:
     explicit GEvaluator(const plugins::Options &opts);
+    explicit GEvaluator(std::basic_string<char> unparsed_config,
+                        bool use_for_reporting_minima,
+                        bool use_for_boosting,
+                        bool use_for_counting_evaluations,
+                        utils::LogProxy log);
     virtual ~GEvaluator() override = default;
 
     virtual EvaluationResult compute_result(
