8517d976fab9e1e6e14b494bfd53fee99f925010
Salomé Eriksson <salome.eriksson@unibas.ch>
2023-07-19 15:28:37 +0200

    Add prototype build and set it as default.

--- build_configs.py
+++ build_configs.py
@@ -4,6 +4,10 @@ release_no_lp = ["-DCMAKE_BUILD_TYPE=Release", "-DUSE_LP=NO"]
 # USE_GLIBCXX_DEBUG is not compatible with USE_LP (see issue983).
 glibcxx_debug = ["-DCMAKE_BUILD_TYPE=Debug", "-DUSE_LP=NO", "-DUSE_GLIBCXX_DEBUG=YES"]
 minimal = ["-DCMAKE_BUILD_TYPE=Release", "-DDISABLE_LIBRARIES_BY_DEFAULT=YES"]
+prototype = ["-DCMAKE_BUILD_TYPE=Release", "-DDISABLE_LIBRARIES_BY_DEFAULT=YES",
+             "-DPLUGIN_PLUGIN_ASTAR_ENABLED=YES",
+             "-DPLUGIN_LANDMARK_CUT_HEURISTIC_ENABLED=YES"]
+#TODO 559REBASE replace '[...]_PLUGIN_[...]' by '[...]_LIBRARY_[...]'?
 
-DEFAULT = "release"
+DEFAULT = "prototype"
 DEBUG = "debug"
