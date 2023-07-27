f0c682a501252adf4c9b737ed107e63840aa8f39
Simon Dold <simon.dold@stud.unibas.ch>
2023-07-27 12:28:18 +0200

    add TI-g.

--- src/search/evaluators/g_evaluator.cc
+++ src/search/evaluators/g_evaluator.cc
@@ -4,6 +4,8 @@
 #include "../evaluation_result.h"
 #include "../plugins/plugin.h"
 
+#include "../utils/logging.h"
+
 using namespace std;
 
 namespace g_evaluator {
@@ -25,9 +27,28 @@ EvaluationResult GEvaluator::compute_result(EvaluationContext &eval_context) {
     return result;
 }
 
-class GEvaluatorFeature : public plugins::TypedFeature<Evaluator, GEvaluator> {
+
+
+TaskIndependentGEvaluator::TaskIndependentGEvaluator(utils::LogProxy log,
+                                                     std::basic_string<char> unparsed_config,
+                                                     bool use_for_reporting_minima,
+                                                     bool use_for_boosting,
+                                                     bool use_for_counting_evaluations)
+        : TaskIndependentEvaluator(log, unparsed_config, use_for_reporting_minima, use_for_boosting, use_for_counting_evaluations),
+          unparsed_config(unparsed_config), log(log){
+}
+
+TaskIndependentGEvaluator::~TaskIndependentGEvaluator() {
+}
+
+shared_ptr<Evaluator> TaskIndependentGEvaluator::create_task_specific([[maybe_unused]] shared_ptr<AbstractTask> &task) {
+    return make_shared<GEvaluator>(log, unparsed_config);
+}
+
+
+class TaskIndependentGEvaluatorFeature : public plugins::TypedFeature<TaskIndependentEvaluator, TaskIndependentGEvaluator> {
 public:
-    GEvaluatorFeature() : TypedFeature("g") {
+    TaskIndependentGEvaluatorFeature() : TypedFeature("g") {
         document_subcategory("evaluators_basic");
         document_title("g-value evaluator");
         document_synopsis(
@@ -35,9 +56,9 @@ public:
         add_evaluator_options_to_feature(*this);
     }
 
-    virtual shared_ptr<GEvaluator> create_component(
+    virtual shared_ptr<TaskIndependentGEvaluator> create_component(
         const plugins::Options &opts, const utils::Context &) const override {
-        return make_shared<GEvaluator>(utils::get_log_from_options(opts),
+        return make_shared<TaskIndependentGEvaluator>(utils::get_log_from_options(opts),
                                        opts.get_unparsed_config(),
                                        opts.get<bool>("use_for_reporting_minima"),
                                        opts.get<bool>("use_for_boosting"),
@@ -45,5 +66,5 @@ public:
     }
 };
 
-static plugins::FeaturePlugin<GEvaluatorFeature> _plugin;
+static plugins::FeaturePlugin<TaskIndependentGEvaluatorFeature> _plugin;
 }
