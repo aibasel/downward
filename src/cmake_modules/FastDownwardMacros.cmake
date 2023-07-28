e4ccc422850b35e9b8105908fce73da08290fc20
Florian Pommerening <florian.pommerening@unibas.ch>
2023-07-28 11:01:47 +0200

    use generator expressions again

--- src/cmake_modules/FastDownwardMacros.cmake
+++ src/cmake_modules/FastDownwardMacros.cmake
@@ -1,64 +1,54 @@
 include(CMakeParseArguments)
 
 
-    add_library(fd_warnings INTERFACE)
-
-    if(("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-       OR ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang"))
-        target_compile_options(fd_warnings INTERFACE
-            "-Wall" "-Wextra" "-Wpedantic" "-Wnon-virtual-dtor" "-Wfloat-conversion" "-Wmissing-declarations" "-Wzero-as-null-pointer-constant")
-
-
-        if (("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-            AND NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 12
-            AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 13)
-            ## We ignore the warning "restrict" because of a bug in GCC 12:
-            ## https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105651
-            target_compile_options(fd_warnings INTERFACE "-Wno-restrict")
-        endif()
-    elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-        # Use warning level 4 (/W4).
-        # /Wall currently detects too many warnings outside of our code to be useful.
-        target_compile_options(fd_warnings INTERFACE "/W4")
-        # TODO: before, we replaced W3 with W4. Do we still need to do that?
-
-        # Disable warnings that currently trigger in the code until we fix them.
-        target_compile_options(fd_warnings INTERFACE "/wd4456") # declaration hides previous local declaration
-        target_compile_options(fd_warnings INTERFACE "/wd4458") # declaration hides class member
-        target_compile_options(fd_warnings INTERFACE "/wd4459") # declaration hides global declaration
-        target_compile_options(fd_warnings INTERFACE "/wd4244") # conversion with possible loss of data
-        target_compile_options(fd_warnings INTERFACE "/wd4267") # conversion from size_t to int with possible loss of data
-    else()
-        message(FATAL_ERROR "Unsupported compiler: ${CMAKE_CXX_COMPILER_ID}")
-    endif()
-endmacro()
-
 macro(define_interface_library)
     add_library(fd_interface_library INTERFACE)
     target_compile_features(fd_interface_library INTERFACE cxx_std_20)
 
-    define_warnings_library()
+    set(gcc_like_cxx "$<COMPILE_LANG_AND_ID:CXX,ARMClang,AppleClang,Clang,GNU,LCC>")
+    set(gcc_cxx "$<COMPILE_LANG_AND_ID:CXX,GNU>")
+    set(msvc_cxx "$<COMPILE_LANG_AND_ID:CXX,MSVC>")
+    set(gcc_like_release "$<AND:${gcc_like_cxx},$<CONFIG:RELEASE>>")
+    set(gcc_like_debug "$<AND:${gcc_like_cxx},$<CONFIG:DEBUG>>")
+    set(should_use_glibcxx_debug "$<AND:${gcc_like_debug},$<BOOL:USE_GLIBCXX_DEBUG>>")
+
+    target_compile_options(fd_interface_library INTERFACE
+        "$<${gcc_like_cxx}:-O3;-g>")
+    target_compile_options(fd_interface_library INTERFACE
+        "$<${gcc_like_release}:-DNDEBUG;-fomit-frame-pointer>")
+    target_compile_definitions(fd_interface_library INTERFACE
+        "$<${should_use_glibcxx_debug}:_GLIBCXX_DEBUG>")
+    # Enable exceptions for MSVC.
+    target_compile_options(fd_interface_library INTERFACE
+        "$<${msvc_cxx}:/EHsc>")
+
+    add_library(fd_warnings INTERFACE)
+    target_compile_options(fd_warnings INTERFACE
+        "$<${gcc_like_cxx}:-Wall;-Wextra;-Wpedantic;-Wnon-virtual-dtor;-Wfloat-conversion;-Wmissing-declarations;-Wzero-as-null-pointer-constant>")
+
+    ## We ignore the warning "restrict" because of a bug in GCC 12:
+    ## https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105651
+    set(v12_or_later "$<VERSION_GREATER_EQUAL:$<CXX_COMPILER_VERSION>,12>")
+    set(before_v13 "$<VERSION_LESS:$<CXX_COMPILER_VERSION>,13>")
+    set(bugged_gcc_cxx "$<AND:${gcc_cxx},${v12_or_later},${before_v13}>")
+    target_compile_options(fd_warnings INTERFACE
+        "$<${bugged_gcc_cxx}:-Wno-restrict>")
+
+    # For MSVC, use warning level 4 (/W4) because /Wall currently detects too
+    # many warnings outside of our code to be useful.
+    target_compile_options(fd_warnings INTERFACE
+        "$<${msvc_cxx}:/W4;/wd4456;/wd4458;/wd4459;/wd4244;/wd4267>")
+        # Disable warnings that currently trigger in the code until we fix them.
+        #   /wd4456: declaration hides previous local declaration
+        #   /wd4458: declaration hides class member
+        #   /wd4459: declaration hides global declaration
+        #   /wd4244: conversion with possible loss of data
+        #   /wd4267: conversion from size_t to int with possible loss of data
     target_link_libraries(fd_interface_library INTERFACE fd_warnings)
 
     # TODO: rework. The idea was that downward inherits this property, but it doesn't
     # Instead, maybe an "install" command would be sensible?
     set_target_properties(fd_interface_library PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/bin)
-
-    if((${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
-       OR (${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
-       OR (${CMAKE_CXX_COMPILER_ID} STREQUAL "AppleClang"))
-        target_compile_options(fd_interface_library INTERFACE
-            "-O3" "-g" "$<$<CONFIG:RELEASE>:-DNDEBUG;-fomit-frame-pointer>")
-
-        if(USE_GLIBCXX_DEBUG)
-            target_compile_definitions(fd_interface_library INTERFACE "$<$<CONFIG:DEBUG>:_GLIBCXX_DEBUG>")
-        endif()
-    elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
-        # Enable exceptions.
-        target_compile_options(fd_interface_library INTERFACE "/EHsc")        
-    else()
-        message(FATAL_ERROR "Unsupported compiler: ${CMAKE_CXX_COMPILER_ID}")
-    endif()
 endmacro()
 
 # TODO: I'm not sure if we need this anymore? I could not find a corresponding
@@ -144,11 +134,11 @@ function(fast_downward_plugin)
     endif()
 
     add_library(downward_${_PLUGIN_NAME} INTERFACE)
+    target_link_libraries(downward_${_PLUGIN_NAME} INTERFACE fd_interface_library)
     target_sources(downward_${_PLUGIN_NAME} INTERFACE ${_PLUGIN_SOURCES})
     foreach(DEPENDENCY ${_PLUGIN_DEPENDS})
         target_link_libraries(downward_${_PLUGIN_NAME} INTERFACE downward_${DEPENDENCY})
     endforeach()
-    set_target_properties(downward_${_PLUGIN_NAME} PROPERTIES LINKER_LANGUAGE CXX)
 
     if (_PLUGIN_CORE_PLUGIN OR PLUGIN_${_PLUGIN_NAME}_ENABLED)
         target_link_libraries(downward PUBLIC downward_${_PLUGIN_NAME})
