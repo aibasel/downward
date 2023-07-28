8a8a3b09734ad46269be76e43476f007c3175224
Salomé Eriksson <salome.eriksson@unibas.ch>
2023-07-28 13:55:02 +0200

    Rename interface and warning library and build it as soon as FastDownwardMacros is included.

--- src/cmake_modules/FastDownwardMacros.cmake
+++ src/cmake_modules/FastDownwardMacros.cmake
@@ -1,58 +1,56 @@
 include(CMakeParseArguments)
 
 
-macro(define_interface_library)
-    add_library(fd_interface_library INTERFACE)
-    target_compile_features(fd_interface_library INTERFACE cxx_std_20)
 
-    set(gcc_like_cxx "$<COMPILE_LANG_AND_ID:CXX,ARMClang,AppleClang,Clang,GNU,LCC>")
-    set(gcc_cxx "$<COMPILE_LANG_AND_ID:CXX,GNU>")
-    set(msvc_cxx "$<COMPILE_LANG_AND_ID:CXX,MSVC>")
-    set(gcc_like_release "$<AND:${gcc_like_cxx},$<CONFIG:RELEASE>>")
-    set(gcc_like_debug "$<AND:${gcc_like_cxx},$<CONFIG:DEBUG>>")
-    set(should_use_glibcxx_debug "$<AND:${gcc_like_debug},$<BOOL:USE_GLIBCXX_DEBUG>>")
+add_library(cxx_options INTERFACE)
+target_compile_features(cxx_options INTERFACE cxx_std_20)
 
-    target_compile_options(fd_interface_library INTERFACE
-        "$<${gcc_like_cxx}:-O3;-g>")
-    target_compile_options(fd_interface_library INTERFACE
-        "$<${gcc_like_release}:-DNDEBUG;-fomit-frame-pointer>")
-    target_compile_definitions(fd_interface_library INTERFACE
-        "$<${should_use_glibcxx_debug}:_GLIBCXX_DEBUG>")
-    # Enable exceptions for MSVC.
-    target_compile_options(fd_interface_library INTERFACE
-        "$<${msvc_cxx}:/EHsc>")
+set(gcc_like "$<COMPILE_LANG_AND_ID:CXX,ARMClang,AppleClang,Clang,GNU,LCC>")
+set(gcc "$<COMPILE_LANG_AND_ID:CXX,GNU>")
+set(msvc "$<COMPILE_LANG_AND_ID:CXX,MSVC>")
+set(gcc_like_release "$<AND:${gcc_like},$<CONFIG:RELEASE>>")
+set(gcc_like_debug "$<AND:${gcc_like},$<CONFIG:DEBUG>>")
+set(should_use_glibcxx_debug "$<AND:${gcc_like_debug},$<BOOL:USE_GLIBCXX_DEBUG>>")
 
-    add_library(fd_warnings INTERFACE)
-    target_compile_options(fd_warnings INTERFACE
-        "$<${gcc_like_cxx}:-Wall;-Wextra;-Wpedantic;-Wnon-virtual-dtor;-Wfloat-conversion;-Wmissing-declarations;-Wzero-as-null-pointer-constant>")
+target_compile_options(cxx_options INTERFACE
+    "$<${gcc_like}:-O3;-g>")
+target_compile_options(cxx_options INTERFACE
+    "$<${gcc_like_release}:-DNDEBUG;-fomit-frame-pointer>")
+target_compile_definitions(cxx_options INTERFACE
+    "$<${should_use_glibcxx_debug}:_GLIBCXX_DEBUG>")
+# Enable exceptions for MSVC.
+target_compile_options(cxx_options INTERFACE
+    "$<${msvc}:/EHsc>")
+if(UNIX) #TODO: Maybe we can also replace this with a generator expression?
+    target_link_options(cxx_options INTERFACE "-g")
+endif()
 
-    ## We ignore the warning "restrict" because of a bug in GCC 12:
-    ## https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105651
-    set(v12_or_later "$<VERSION_GREATER_EQUAL:$<CXX_COMPILER_VERSION>,12>")
-    set(before_v13 "$<VERSION_LESS:$<CXX_COMPILER_VERSION>,13>")
-    set(bugged_gcc_cxx "$<AND:${gcc_cxx},${v12_or_later},${before_v13}>")
-    target_compile_options(fd_warnings INTERFACE
-        "$<${bugged_gcc_cxx}:-Wno-restrict>")
+add_library(cxx_warnings INTERFACE)
+target_compile_options(cxx_warnings INTERFACE
+    "$<${gcc_like}:-Wall;-Wextra;-Wpedantic;-Wnon-virtual-dtor;-Wfloat-conversion;-Wmissing-declarations;-Wzero-as-null-pointer-constant>")
 
-    # For MSVC, use warning level 4 (/W4) because /Wall currently detects too
-    # many warnings outside of our code to be useful.
-    target_compile_options(fd_warnings INTERFACE
-        "$<${msvc_cxx}:/W4;/wd4456;/wd4458;/wd4459;/wd4244;/wd4267>")
-        # Disable warnings that currently trigger in the code until we fix them.
-        #   /wd4456: declaration hides previous local declaration
-        #   /wd4458: declaration hides class member
-        #   /wd4459: declaration hides global declaration
-        #   /wd4244: conversion with possible loss of data
-        #   /wd4267: conversion from size_t to int with possible loss of data
-    target_link_libraries(fd_interface_library INTERFACE fd_warnings)
+## We ignore the warning "restrict" because of a bug in GCC 12:
+## https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105651
+set(v12_or_later "$<VERSION_GREATER_EQUAL:$<CXX_COMPILER_VERSION>,12>")
+set(before_v13 "$<VERSION_LESS:$<CXX_COMPILER_VERSION>,13>")
+set(bugged_gcc "$<AND:${gcc},${v12_or_later},${before_v13}>")
+target_compile_options(cxx_warnings INTERFACE
+    "$<${bugged_gcc}:-Wno-restrict>")
 
-    if(UNIX)
-        target_link_options(fd_interface_library INTERFACE "-g")
-    endif()
-endmacro()
+# For MSVC, use warning level 4 (/W4) because /Wall currently detects too
+# many warnings outside of our code to be useful.
+target_compile_options(cxx_warnings INTERFACE
+    "$<${msvc}:/W4;/wd4456;/wd4458;/wd4459;/wd4244;/wd4267>")
+    # Disable warnings that currently trigger in the code until we fix them.
+    #   /wd4456: declaration hides previous local declaration
+    #   /wd4458: declaration hides class member
+    #   /wd4459: declaration hides global declaration
+    #   /wd4244: conversion with possible loss of data
+    #   /wd4267: conversion from size_t to int with possible loss of data
+target_link_libraries(cxx_options INTERFACE cxx_warnings)
 
 
-
+# TODO: Reopening the question whether we need it; Silvan could build in VS Code without it.
 macro(fast_downward_default_to_release_build)
     # Only for single-config generators (like Makefiles) that choose the build type at generation time.
     if(NOT CMAKE_CONFIGURATION_TYPES AND NOT CMAKE_BUILD_TYPE)
@@ -126,13 +124,13 @@ function(create_fast_downward_library)
     endif()
 
     add_library(downward_${_LIBRARY_NAME} INTERFACE)
-    target_link_libraries(downward_${_LIBRARY_NAME} INTERFACE fd_interface_library)
+    target_link_libraries(downward_${_LIBRARY_NAME} INTERFACE cxx_options)
     target_sources(downward_${_LIBRARY_NAME} INTERFACE ${_LIBRARY_SOURCES})
     foreach(DEPENDENCY ${_LIBRARY_DEPENDS})
         target_link_libraries(downward_${_LIBRARY_NAME} INTERFACE downward_${DEPENDENCY})
     endforeach()
 
-    if (_LIBRARY_CORE_LIBRARY OR PLUGIN_${_LIBRARY_NAME}_ENABLED)
+    if (_LIBRARY_CORE_LIBRARY OR LIBRARY_${_LIBRARY_NAME}_ENABLED)
         target_link_libraries(downward PUBLIC downward_${_LIBRARY_NAME})
     endif()
 endfunction()
