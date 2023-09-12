1a7093733a7963a25798bd930e6d909ed8869881
Florian Pommerening <florian.pommerening@unibas.ch>
2023-09-12 10:10:19 +0200

    restructure cmake files

--- src/search/cmake/macros.cmake
+++ src/search/cmake/macros.cmake
@@ -1,57 +1,10 @@
+include_guard(GLOBAL)
+
 include(CMakeParseArguments)
+include(cxx_options)
+include(project_options)
 
-
-
-add_library(cxx_options INTERFACE)
-target_compile_features(cxx_options INTERFACE cxx_std_20)
-
-set(using_gcc_like "$<CXX_COMPILER_ID:ARMClang,AppleClang,Clang,GNU,LCC>")
-set(using_gcc "$<CXX_COMPILER_ID:GNU>")
-set(using_msvc "$<CXX_COMPILER_ID:MSVC>")
-set(using_gcc_like_release "$<AND:${using_gcc_like},$<CONFIG:RELEASE>>")
-set(using_gcc_like_debug "$<AND:${using_gcc_like},$<CONFIG:DEBUG>>")
-set(should_use_glibcxx_debug "$<AND:${using_gcc_like_debug},$<BOOL:USE_GLIBCXX_DEBUG>>")
-
-target_compile_options(cxx_options INTERFACE
-    "$<${using_gcc_like}:-O3;-g>")
-target_link_options(cxx_options INTERFACE
-    "$<${using_gcc_like}:-g>")
-target_compile_options(cxx_options INTERFACE
-    "$<${using_gcc_like_release}:-DNDEBUG;-fomit-frame-pointer>")
-target_compile_definitions(cxx_options INTERFACE
-    "$<${should_use_glibcxx_debug}:_GLIBCXX_DEBUG>")
-# Enable exceptions for MSVC.
-target_compile_options(cxx_options INTERFACE
-    "$<${using_msvc}:/EHsc>")
-
-add_library(cxx_warnings INTERFACE)
-target_compile_options(cxx_warnings INTERFACE
-    "$<${using_gcc_like}:-Wall;-Wextra;-Wpedantic;-Wnon-virtual-dtor;-Wfloat-conversion;-Wmissing-declarations;-Wzero-as-null-pointer-constant>")
-
-## We ignore the warning "restrict" because of a bug in GCC 12:
-## https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105651
-set(v12_or_later "$<VERSION_GREATER_EQUAL:$<CXX_COMPILER_VERSION>,12>")
-set(before_v13 "$<VERSION_LESS:$<CXX_COMPILER_VERSION>,13>")
-set(bugged_gcc "$<AND:${using_gcc},${v12_or_later},${before_v13}>")
-target_compile_options(cxx_warnings INTERFACE
-    "$<${bugged_gcc}:-Wno-restrict>")
-
-# For MSVC, use warning level 4 (/W4) because /Wall currently detects too
-# many warnings outside of our code to be useful.
-target_compile_options(cxx_warnings INTERFACE
-    "$<${using_msvc}:/W4;/wd4456;/wd4458;/wd4459;/wd4244;/wd4267>")
-    # Disable warnings that currently trigger in the code until we fix them.
-    #   /wd4456: declaration hides previous local declaration
-    #   /wd4458: declaration hides class member
-    #   /wd4459: declaration hides global declaration
-    #   /wd4244: conversion with possible loss of data
-    #   /wd4267: conversion from size_t to int with possible loss of data
-target_link_libraries(cxx_options INTERFACE cxx_warnings)
-
-
-function(fast_downward_set_up_build_types)
-    set(allowedBuildTypes Debug Release)
-
+function(set_up_build_types allowedBuildTypes)
     get_property(isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
     if(isMultiConfig)
         # Set the possible choices for multi-config generators like (like
@@ -71,8 +24,7 @@ function(fast_downward_set_up_build_types)
     endif()
 endfunction()
 
-
-macro(fast_downward_report_bitwidth)
+macro(report_bitwidth)
     if(${CMAKE_SIZEOF_VOID_P} EQUAL 4)
         message(STATUS "Building for 32-bit.")
     elseif(${CMAKE_SIZEOF_VOID_P} EQUAL 8)
@@ -82,7 +34,7 @@ macro(fast_downward_report_bitwidth)
     endif()
 endmacro()
 
-function(fast_downward_add_existing_sources_to_list _SOURCES_LIST_VAR)
+function(add_existing_sources_to_list _SOURCES_LIST_VAR)
     set(_ALL_FILES)
     foreach(SOURCE_FILE ${${_SOURCES_LIST_VAR}})
         get_filename_component(_SOURCE_FILE_DIR ${SOURCE_FILE} PATH)
@@ -114,7 +66,7 @@ function(create_fast_downward_library)
         message(FATAL_ERROR "fast_downward_library: 'SOURCES' argument required.")
     endif()
 
-    fast_downward_add_existing_sources_to_list(_LIBRARY_SOURCES)
+    add_existing_sources_to_list(_LIBRARY_SOURCES)
 
     if (NOT _LIBRARY_CORE_LIBRARY AND NOT _LIBRARY_DEPENDENCY_ONLY)
         # Decide whether the plugin should be enabled by default.
@@ -132,23 +84,16 @@ function(create_fast_downward_library)
     foreach(DEPENDENCY ${_LIBRARY_DEPENDS})
         target_link_libraries(downward_${_LIBRARY_NAME} INTERFACE downward_${DEPENDENCY})
     endforeach()
-    # TODO: This feels very hacky. I'd rather have the ability to specify
-    # external dependencies in DownwardFiles.cmake (i.e. new parameter
-    # DEPENDSEXTERNAL) but how to specify conditions there?
-    if (${_LIBRARY_NAME} STREQUAL "UTILS")
-        # On Linux, find the rt library for clock_gettime().
-        if(UNIX AND NOT APPLE)
-            target_link_libraries(downward_${_LIBRARY_NAME} INTERFACE rt)
-        endif()
-
-        # On Windows, find the psapi library for determining peak memory.
-        if(WIN32)
-            cmake_policy(SET CMP0074 NEW)
-            target_link_libraries(downward_${_LIBRARY_NAME} INTERFACE psapi)
-        endif()
-    endif()
 
     if (_LIBRARY_CORE_LIBRARY OR LIBRARY_${_LIBRARY_NAME}_ENABLED)
         target_link_libraries(downward PUBLIC downward_${_LIBRARY_NAME})
     endif()
+endfunction()
+
+function(copy_dlls_to_binary_dir _TARGET_NAME)
+    # https://cmake.org/cmake/help/latest/manual/cmake-generator-expressions.7.html#genex:_TARGET_RUNTIME_DLLS
+    add_custom_command(TARGET ${_TARGET_NAME} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy -t $<TARGET_FILE_DIR:${_TARGET_NAME}> $<TARGET_RUNTIME_DLLS:${_TARGET_NAME}>
+        COMMAND_EXPAND_LISTS
+    )
 endfunction()
