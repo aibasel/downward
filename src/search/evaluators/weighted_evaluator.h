0b3b0e8f1fdfbc4b201c3d2f2f2835a9bf76d84a
Simon Dold <simon.dold@stud.unibas.ch>
2023-09-14 15:20:19 +0200

    add unpacked constructor to WeightedEvaluator.

--- src/search/evaluators/weighted_evaluator.h
+++ src/search/evaluators/weighted_evaluator.h
@@ -12,10 +12,18 @@ class Options;
 namespace weighted_evaluator {
 class WeightedEvaluator : public Evaluator {
     std::shared_ptr<Evaluator> evaluator;
-    int w;
+    int weight;
 
 public:
     explicit WeightedEvaluator(const plugins::Options &opts);
+    explicit WeightedEvaluator(utils::LogProxy log,
+                               std::shared_ptr<Evaluator> evaluator,
+                               int weight,
+                               std::basic_string<char> unparsed_config,
+                               bool use_for_reporting_minima,
+                               bool use_for_boosting,
+                               bool use_for_counting_evaluations,
+                               std::vector<std::shared_ptr<Evaluator>> subevaluators);
     virtual ~WeightedEvaluator() override;
 
     virtual bool dead_ends_are_reliable() const override;
