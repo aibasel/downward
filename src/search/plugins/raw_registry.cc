8ee93c46e2c2aa820965217ef6a899ce7d18ba57
SimonDold <48084373+SimonDold@users.noreply.github.com>
2023-09-22 14:16:55 +0200

    fix message.

--- src/search/plugins/raw_registry.cc
+++ src/search/plugins/raw_registry.cc
@@ -178,7 +178,7 @@ Features RawRegistry::collect_features(
             int parameter_occurrence = pair.second;
             if (parameter_occurrence > 1) {
                 errors.push_back(
-                    "The Parameter '" + parameter + "' in '" + feature_key + "' is defined " +
+                    "The parameter '" + parameter + "' in '" + feature_key + "' is defined " +
                     to_string(parameter_occurrence) + " times.");
             }
         }
