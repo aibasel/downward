94efb2f78a614ee2b1d9b5824ed1f69d091c622e
Simon Dold <simon.dold@stud.unibas.ch>
2023-12-29 23:54:05 +0100

    add blind heuristic.

--- src/search/heuristics/blind_search_heuristic.cc
+++ src/search/heuristics/blind_search_heuristic.cc
@@ -12,11 +12,14 @@
 using namespace std;
 
 namespace blind_search_heuristic {
-BlindSearchHeuristic::BlindSearchHeuristic(const plugins::Options &opts)
-    : Heuristic(opts),
+BlindSearchHeuristic::BlindSearchHeuristic(const string &name,
+                                           utils::Verbosity verbosity,
+                                           const shared_ptr<AbstractTask> task,
+                                           bool cache_evaluator_values)
+    : Heuristic(name, verbosity, task, cache_evaluator_values),
       min_operator_cost(task_properties::get_min_operator_cost(task_proxy)) {
     if (log.is_at_least_normal()) {
-        log << "Initializing blind search heuristic..." << endl;
+        log << "Initializing blind search heuristic '" << name << "'..." << endl;
     }
 }
 
@@ -31,7 +34,47 @@ int BlindSearchHeuristic::compute_heuristic(const State &ancestor_state) {
         return min_operator_cost;
 }
 
-class BlindSearchHeuristicFeature : public plugins::TypedFeature<Evaluator, BlindSearchHeuristic> {
+
+
+
+TaskIndependentBlindSearchHeuristic::TaskIndependentBlindSearchHeuristic(
+        const string &name,
+        utils::Verbosity verbosity,
+        shared_ptr<TaskIndependentAbstractTask> task_transformation,
+        bool cache_evaluator_values)
+        : TaskIndependentHeuristic(name, verbosity, task_transformation, cache_evaluator_values) {
+}
+
+TaskIndependentBlindSearchHeuristic::~TaskIndependentBlindSearchHeuristic() {
+}
+
+
+shared_ptr<Evaluator> TaskIndependentBlindSearchHeuristic::create_task_specific(
+        const shared_ptr<AbstractTask> &task,
+        std::unique_ptr<ComponentMap> &component_map,
+        int depth) const {
+    shared_ptr<BlindSearchHeuristic> task_specific_x;
+    if (component_map->count(static_cast<const TaskIndependentComponent *>(this))) {
+        log << std::string(depth, ' ') << "Reusing task specific BlindSearchHeuristic '" + name + "'..." << endl;
+        task_specific_x = dynamic_pointer_cast<BlindSearchHeuristic>(
+                component_map->at(static_cast<const TaskIndependentComponent *>(this)));
+    } else {
+        log << std::string(depth, ' ') << "Creating task specific BlindSearchHeuristic '" + name + "'..." << endl;
+
+        task_specific_x = make_shared<BlindSearchHeuristic>(name,
+                                                            verbosity,
+                                                            task_transformation->create_task_specific(
+                                                                    task, component_map,
+                                                                    depth >= 0 ? depth + 1 : depth),
+                                                            cache_evaluator_values);
+        component_map->insert(make_pair<const TaskIndependentComponent *, std::shared_ptr<Component>>(
+                static_cast<const TaskIndependentComponent *>(this), task_specific_x));
+    }
+    return task_specific_x;
+}
+
+
+class BlindSearchHeuristicFeature : public plugins::TypedFeature<TaskIndependentEvaluator, TaskIndependentBlindSearchHeuristic> {
 public:
     BlindSearchHeuristicFeature() : TypedFeature("blind") {
         document_title("Blind heuristic");
@@ -39,7 +82,7 @@ public:
             "Returns cost of cheapest action for non-goal states, "
             "0 for goal states");
 
-        Heuristic::add_options_to_feature(*this);
+        Heuristic::add_options_to_feature(*this, "blind");
 
         document_language_support("action costs", "supported");
         document_language_support("conditional effects", "supported");
@@ -49,6 +92,16 @@ public:
         document_property("consistent", "yes");
         document_property("safe", "yes");
         document_property("preferred operators", "no");
+    }
+
+    virtual shared_ptr<TaskIndependentBlindSearchHeuristic> create_component(
+            const plugins::Options &opts, const utils::Context &) const override {
+        return make_shared<TaskIndependentBlindSearchHeuristic>(opts.get<string>("name"),
+                                                                opts.get<utils::Verbosity>("verbosity"),
+                                                                opts.get<shared_ptr<TaskIndependentAbstractTask>>(
+                                                                        "transform"),
+                                                                opts.get<bool>("cache_estimates")
+        );
     }
 };
 
