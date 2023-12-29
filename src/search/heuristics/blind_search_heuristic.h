f31bd83961d0216784b94d96ae78f962b0e53f17
Simon Dold <simon.dold@stud.unibas.ch>
2023-12-29 23:54:05 +0100

    add blind heuristic.

--- src/search/heuristics/blind_search_heuristic.h
+++ src/search/heuristics/blind_search_heuristic.h
@@ -9,8 +9,27 @@ class BlindSearchHeuristic : public Heuristic {
 protected:
     virtual int compute_heuristic(const State &ancestor_state) override;
 public:
-    BlindSearchHeuristic(const plugins::Options &opts);
+    BlindSearchHeuristic(const std::string &name,
+                         utils::Verbosity verbosity,
+                         const std::shared_ptr<AbstractTask> task,
+                         bool cache_evaluator_values);
     ~BlindSearchHeuristic();
+};
+
+class TaskIndependentBlindSearchHeuristic : public TaskIndependentHeuristic {
+private:
+    bool cache_evaluator_values;
+public:
+    explicit TaskIndependentBlindSearchHeuristic(const std::string &name,
+                                                 utils::Verbosity verbosity,
+                                                 std::shared_ptr<TaskIndependentAbstractTask> task_transformation,
+                                                 bool cache_evaluator_values);
+
+    virtual ~TaskIndependentBlindSearchHeuristic() override;
+
+    std::shared_ptr<Evaluator>
+    create_task_specific(const std::shared_ptr<AbstractTask> &task, std::unique_ptr<ComponentMap> &component_map,
+                         int depth = -1) const override;
 };
 }
 
