bde4387f1fb7f06df97490cc5e1a824453db378c
Gabriele Roeger <gabriele.roeger@unibas.ch>
2024-01-12 13:33:41 +0100

    [issue1133] add some comments and TODOs to invariant code

--- src/translate/invariants.py
+++ src/translate/invariants.py
@@ -21,9 +21,28 @@ def invert_list(alist):
 
 
 def instantiate_factored_mapping(pairs):
+    """Input pairs is a list [(preimg1, img1), ..., (preimgn, imgn)].
+       For entry (preimg, img), preimg and img is a list of numbers of the same
+       length. All preimages (and all imgage) are pairwise disjoint, as well as
+       the components of each preimage/image.
+
+       The function determines all possible bijections between the union of
+       preimgs and the union of imgs such that for every entry (preimg, img),
+       values from preimg are mapped to values from img.
+       It yields one permutation after the other, each represented as a list
+       of pairs (x,y), meaning x is mapped to y.
+       """
+    # for every entry (preimg, img) in pairs, determine all possible bijections
+    # from preimg to img.
     part_mappings = [[list(zip(preimg, perm_img)) for perm_img in itertools.permutations(img)]
                      for (preimg, img) in pairs]
+    # all possibilities to pick one bijection for each entry
     return tools.cartesian_product(part_mappings)
+    # TODO: this has a TODO note in tools. It probably can simply be entirely
+    # replaced by using itertools.product and chain as follows:
+    # for x in itertools.product(*part_mapping):
+    #     for y in itertools.chain(*x):
+    #         yield list(y)
 
 
 def find_unique_variables(action, invariant):
@@ -54,7 +73,16 @@ def ensure_conjunction_sat(system, *parts):
        conjunction of all parts is satisfiable.
 
        Each part must be an iterator, generator, or an iterable over
-       literals."""
+       literals.
+
+       We add the following constraints for each literal to the system:
+
+       - for (not (= x y)): x != y (as a NegativeClause with one entry(x,y)),
+       - for (= x y): x = y
+       - for predicates that occur with a positive and negative literal, we
+         consider every combination of a positive one (e.g. P(x, y, z)) and
+         a negative one (e.g. (not P(a, b, c))) and add a constraint
+         (x != y or y != b or z != c)."""
     pos = defaultdict(set)
     neg = defaultdict(set)
     for literal in itertools.chain(*parts):
@@ -64,6 +92,8 @@ def ensure_conjunction_sat(system, *parts):
                 system.add_negative_clause(n)
             else:
                 a = constraints.Assignment([literal.args])
+                # TODO an assignment x=y expects x to be a variable, not an
+                # object. Do we need to handle this here?
                 system.add_assignment_disjunction([a])
         else:
             if literal.negated:
@@ -86,15 +116,20 @@ def ensure_cover(system, literal, invariant, inv_vars):
        invariant covers the literal"""
     a = invariant.get_covering_assignments(inv_vars, literal)
     assert len(a) == 1
-    # if invariants could contain several parts of one predicate, this would
-    # not be true but the depending code in parts relies on this assumption
+    # if invariants could contain several parts of one predicate, this
+    # assertion would not be true but the depending code in parts relies on
+    # this assumption.
     system.add_assignment_disjunction(a)
 
 
 def ensure_inequality(system, literal1, literal2):
     """Modifies the constraint system such that it is only solvable if the
        literal instantiations are not equal (ignoring whether one is negated and
-       the other is not)"""
+       the other is not).
+
+       If the literals have different predicates, there is nothing to do.
+       Otherwise we add for P(x, y, z) and P(a, b, c) a contraint
+       (x != a or y != b or z != c)"""
     if (literal1.predicate == literal2.predicate and
         literal1.args):
         parts = list(zip(literal1.args, literal2.args))
@@ -103,6 +138,15 @@ def ensure_inequality(system, literal1, literal2):
 
 class InvariantPart:
     def __init__(self, predicate, order, omitted_pos=-1):
+        """There is one InvariantPart for every predicate mentioned in the
+           invariant. The parameters of the invariant do not have fixed names
+           in the code (to avoid conflicts with existing variable names in
+           operators), instead we think of the 0th, 1st, 2nd, ...  parameter.
+           The order of the invariant part ties these parameters to the
+           arguments of the predicate: the i-th number n_i in order specifies
+           that the i-th invariant parameter occurs at position n_i as an
+           argument of the predicate. The omitted_pos is the remaining argument
+           position, or -1 if there is none."""
         self.predicate = predicate
         self.order = order
         self.omitted_pos = omitted_pos
@@ -134,11 +178,15 @@ class InvariantPart:
         return len(self.order)
 
     def get_assignment(self, parameters, literal):
+        """Returns an assignment constraint that requires the invariant
+           parameters to correspond to the values in which they occur in the
+           literal."""
         equalities = [(arg, literal.args[argpos])
                       for arg, argpos in zip(parameters, self.order)]
         return constraints.Assignment(equalities)
 
     def get_parameters(self, literal):
+        """Returns the values of the invariant parameters in the literal."""
         return [literal.args[pos] for pos in self.order]
 
     def instantiate(self, parameters):
@@ -148,18 +196,68 @@ class InvariantPart:
         return pddl.Atom(self.predicate, args)
 
     def possible_mappings(self, own_literal, other_literal):
+        """This method is used when an action had an unbalanced add effect
+           own_literal. The action has a delete effect on literal
+           other_literal, so we try to refine the invariant such that it also
+           covers the delete effect.
+
+           For this purpose, we need to identify all bijections m between
+           argument positions of of other_literal and argument positions of
+           own_literal, such that other_literal can balance own_literal
+           (possibly omitting one argument position of own_literal).
+
+           In most cases this is straight-forward, e.g. with own_literal being
+           P(?a, ?b, ?c) and other_literal being Q(?b, ?a, ?c), we would need
+           to map the first position of Q to the second position of P, the
+           second one of Q to the first one of P, and the third to the third
+           one (with 0-indexing represented as [(0,1), (1,0), (2,2)]).
+
+           Most complication stems from the fact that in rare cases arguments
+           in the literals can be repeated, e.g. P(?a, ?b, ?a) and Q(?b, ?a,
+           ?a). In invariants, we don't have repetitions, so we need to
+           identify the more general invariants for which this repetition is
+           a special case (if invariant parameters are equal in its
+           instantiation). So in this example, we need to consider two
+           possible mappings: [(0, 1), (1, 0), (2, 2)] and [(0, 1), (1, 2), (2,
+           1)].
+
+           The method returns [] if there is no possible mapping and otherwise
+           yields the mappings one by one.
+           """
         allowed_omissions = len(other_literal.args) - len(self.order)
+        # All parts of an invariant always use all non-counted variables, so
+        # len(self.order) corresponds to the number of invariant parameters
+        # (the arity of the invariant). So we can/must omit allowed_omissions
+        # many arguments of other_literal when matching invariant parameters
+        # with arguments.
         if allowed_omissions not in (0, 1):
+            # There may be at most one counted variable.
             return []
         own_parameters = self.get_parameters(own_literal)
+        # own_parameters is a list containing at position i the value of the
+        # i-th invariant parameter in own_literal.
         arg_to_ordered_pos = invert_list(own_parameters)
         other_arg_to_pos = invert_list(other_literal.args)
+        # other_arg_to_pos maps every argument of other_literal to the
+        # lists of positions in which it is occuring in other_literal, e.g.
+        # for P(?a, ?b, ?a), other_arg_to_pos["?a"] = [0, 2].
         factored_mapping = []
 
+        # We iterate over all values occuring as arguments in other_literal
+        # and compare the number of occurrences in other_literal to those in
+        # own_literal. If the difference of these numbers allows us to cover
+        # other_literal with the (still) permitted number of counted variables,
+        # we remember the correspondance of positions for this value in
+        # factored_mapping. If all values can be covered, we instatiate the
+        # complete factored_mapping, computing all possibilities to map
+        # positions from own_literal to positions from other_literal.
         for key, other_positions in other_arg_to_pos.items():
             own_positions = arg_to_ordered_pos.get(key, [])
+            # all positions at which key occurs as argument in own_literal
             len_diff = len(own_positions) - len(other_positions)
             if len_diff >= 1 or len_diff <= -2 or len_diff == -1 and not allowed_omissions:
+                # mapping of the literals is not possible with at most one
+                # counted variable.
                 return []
             if len_diff:
                 own_positions.append(-1)
@@ -168,16 +266,39 @@ class InvariantPart:
         return instantiate_factored_mapping(factored_mapping)
 
     def possible_matches(self, own_literal, other_literal):
+        """This method is used when an action had an unbalanced add effect
+           own_literal. The action has a delete effect on literal
+           other_literal, so we try to refine the invariant such that it also
+           covers the delete effect.
+
+           For this purpose, we consider all possible mappings from the
+           parameter positions of other_literal to the parameter positions of
+           own_literal such that the extended invariant can use other_literal
+           to balance own_literal. From these position mapping, we can extract
+           the order field for the invariant part.
+
+           Consider for an example of the "self" InvariantPart "forall ?u,
+           ?v, ?w(P(?u, ?v, ?w) is non-increasing" and let own_literal be P(?a, ?b,
+           ?c) and other_literal be Q(?b, ?c, ?d, ?a)). The only possible mapping
+           from positions of Q to positions of P (or -1) is [0->1, 1->2, 2->-1,
+           3->0] for which we create a new Invariant Part Q(?v, ?w, _. ?u) with
+           the third argument being counted.
+        """
         assert self.predicate == own_literal.predicate
         result = []
         for mapping in self.possible_mappings(own_literal, other_literal):
             new_order = [None] * len(self.order)
             omitted = -1
-            for (key, value) in mapping:
-                if value == -1:
-                    omitted = key
+            for (other_pos, own_pos) in mapping:
+                if own_pos == -1:
+                    omitted = other_pos
                 else:
-                    new_order[value] = key
+                    new_order[own_pos] = other_pos
+                    # TODO This only works because in the initial invariant
+                    # candidates the numbering of invariant parameters matches
+                    # the order of the corresponding arguments in the
+                    # predicate. To make this work in general, we need to
+                    # consider a potential order difference in self.order.
             result.append(InvariantPart(other_literal.predicate, new_order, omitted))
         return result
 
