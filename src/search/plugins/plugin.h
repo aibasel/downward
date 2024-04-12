b12fd9dc8a90e2d5ce0f11a04abee26406edd603
Simon Dold <simon.dold@stud.unibas.ch>
2024-04-12 22:02:24 +0200

    style

--- src/search/plugins/plugin.h
+++ src/search/plugins/plugin.h
@@ -126,7 +126,7 @@ public:
 template<typename T, typename ... Arguments>
 std::shared_ptr<T> make_shared_from_arg_tuples(Arguments... arguments) {
     return std::apply(
-        [](auto&&... flattened_args) {
+        [](auto &&... flattened_args) {
             return std::make_shared<T>(
                 std::forward<decltype(flattened_args)>(flattened_args) ...);
         },
