faafea1c1c0516de8eb305d83adb4c2475f52946
Simon Dold <simon.dold@stud.unibas.ch>
2024-04-13 16:48:15 +0200

    add output

--- misc/tests/test-parameters.py
+++ misc/tests/test-parameters.py
@@ -249,7 +249,7 @@ def main():
     search_dir = os.path.join(SRC_DIR, "search")
     cc_files = get_src_files(search_dir, (".cc",))
     # assert len(cc_files) > 0
-    if not len(cc_files) > 0 or True:
+    if not len(cc_files) > 0:
         print("WARNING: Not .cc files found.")
         print("search_dir: ", search_dir)
         tree = subprocess.run(["tree", str(search_dir), "-L", "2"], stdout=subprocess.PIPE, text=True)
