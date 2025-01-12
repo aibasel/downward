ed2bdb00e2f1a72f042af6477a42ce0f1c16e5b3
Masataro Asai <guicho2.71828@gmail.com>
2025-01-12 13:46:15 -0500

    BitsetView: use long int to make popcount faster (callgrind ir/call 2227 -> 1135)

--- src/search/per_state_bitset.h
+++ src/search/per_state_bitset.h
@@ -9,7 +9,7 @@
 
 class BitsetMath {
 public:
-    using Block = unsigned int;
+    using Block = unsigned long int;
     static_assert(
         !std::numeric_limits<Block>::is_signed,
         "Block type must be unsigned");
