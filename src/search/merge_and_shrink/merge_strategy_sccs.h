ec918bf64a0b4da19f8d656e52cfb10b9beb295b
Silvan Sievers <silvan.sievers@unibas.ch>
2025-02-02 10:33:49 +0100

    [issue1171] M&S SCC merge strategy: new option to allow working on any SCC
    Previously, the merge selector used for selecting pairs of M&S factors
    only considered one SCC partition after the other. With the new option
    set to true, it considers the pairs of factors from any cluster of
    factors that has not been fully merged yet.

--- src/search/merge_and_shrink/merge_strategy_sccs.h
+++ src/search/merge_and_shrink/merge_strategy_sccs.h
@@ -10,14 +10,14 @@ namespace merge_and_shrink {
 class MergeSelector;
 class MergeStrategySCCs : public MergeStrategy {
     std::shared_ptr<MergeSelector> merge_selector;
-    std::vector<std::vector<int>> non_singleton_cg_sccs;
-
-    std::vector<int> current_ts_indices;
+    std::vector<std::vector<int>> unfinished_clusters;
+    bool allow_working_on_all_clusters;
 public:
     MergeStrategySCCs(
         const FactoredTransitionSystem &fts,
         const std::shared_ptr<MergeSelector> &merge_selector,
-        std::vector<std::vector<int>> &&non_singleton_cg_sccs);
+        std::vector<std::vector<int>> &&unfinished_clusters,
+        bool allow_working_on_all_clusters);
     virtual ~MergeStrategySCCs() override;
     virtual std::pair<int, int> get_next() override;
 };
