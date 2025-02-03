6ed449e069b06115c1c2509379e9d6defc795961
Simon Dold <simon.dold@stud.unibas.ch>
2025-02-03 17:25:13 +0100

    mark affected parts in the code.

--- src/search/merge_and_shrink/merge_and_shrink_heuristic.cc
+++ src/search/merge_and_shrink/merge_and_shrink_heuristic.cc
@@ -241,7 +241,6 @@ public:
         const plugins::Options &opts,
         const utils::Context &) const override {
         plugins::Options options_copy(opts);
-
         return plugins::make_shared_from_arg_tuples<MergeAndShrinkHeuristic>(
             get_merge_and_shrink_algorithm_arguments_from_options(options_copy),
             get_heuristic_arguments_from_options(options_copy)
