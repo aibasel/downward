794d0f566239521e30d8edec3e2e1b0db06c6e54
Silvan Sievers <sievers.silvan@gmail.com>
2025-02-06 14:23:36 +0100

    [issue1170] SCC merge strategy: remove option to use a merge tree.
    Conceptually, it doesn't make too much sense to use a precomputed merge strategy for a subset of factors, and the option was never used in a paper. We remove it to simplify the code and facilitate the integration of other options.
    ---------
    
    Co-authored-by: Silvan Sievers <silvan.sievers@unibas.ch>

--- src/search/merge_and_shrink/merge_strategy_sccs.cc
+++ src/search/merge_and_shrink/merge_strategy_sccs.cc
@@ -15,16 +15,11 @@ using namespace std;
 namespace merge_and_shrink {
 MergeStrategySCCs::MergeStrategySCCs(
     const FactoredTransitionSystem &fts,
-    const TaskProxy &task_proxy,
-    const shared_ptr<MergeTreeFactory> &merge_tree_factory,
     const shared_ptr<MergeSelector> &merge_selector,
     vector<vector<int>> &&non_singleton_cg_sccs)
     : MergeStrategy(fts),
-      task_proxy(task_proxy),
-      merge_tree_factory(merge_tree_factory),
       merge_selector(merge_selector),
-      non_singleton_cg_sccs(move(non_singleton_cg_sccs)),
-      current_merge_tree(nullptr) {
+      non_singleton_cg_sccs(move(non_singleton_cg_sccs)) {
 }
 
 MergeStrategySCCs::~MergeStrategySCCs() {
@@ -53,31 +48,13 @@ pair<int, int> MergeStrategySCCs::get_next() {
             current_ts_indices = move(current_scc);
             non_singleton_cg_sccs.erase(non_singleton_cg_sccs.begin());
         }
-
-        // If using a merge tree factory, compute a merge tree for this set.
-        if (merge_tree_factory) {
-            current_merge_tree = merge_tree_factory->compute_merge_tree(
-                task_proxy, fts, current_ts_indices);
-        }
     } else {
         // Add the most recent product to the current index set.
         current_ts_indices.push_back(fts.get_size() - 1);
     }
 
-    // Select the next merge from the current index set, either using the
-    // tree or the selector.
-    pair<int, int > next_pair;
-    int merged_ts_index = fts.get_size();
-    if (current_merge_tree) {
-        assert(!current_merge_tree->done());
-        next_pair = current_merge_tree->get_next_merge(merged_ts_index);
-        if (current_merge_tree->done()) {
-            current_merge_tree = nullptr;
-        }
-    } else {
-        assert(merge_selector);
-        next_pair = merge_selector->select_merge(fts, current_ts_indices);
-    }
+    // Select the next merge for the current set of indices.
+    pair<int, int > next_pair = merge_selector->select_merge(fts, current_ts_indices);
 
     // Remove the two merged indices from the current index set.
     for (vector<int>::iterator it = current_ts_indices.begin();
--- src/search/merge_and_shrink/merge_strategy_sccs.h
+++ src/search/merge_and_shrink/merge_strategy_sccs.h
@@ -6,25 +6,16 @@
 #include <memory>
 #include <vector>
 
-class TaskProxy;
-
 namespace merge_and_shrink {
 class MergeSelector;
-class MergeTreeFactory;
-class MergeTree;
 class MergeStrategySCCs : public MergeStrategy {
-    const TaskProxy &task_proxy;
-    std::shared_ptr<MergeTreeFactory> merge_tree_factory;
     std::shared_ptr<MergeSelector> merge_selector;
     std::vector<std::vector<int>> non_singleton_cg_sccs;
 
-    std::unique_ptr<MergeTree> current_merge_tree;
     std::vector<int> current_ts_indices;
 public:
     MergeStrategySCCs(
         const FactoredTransitionSystem &fts,
-        const TaskProxy &task_proxy,
-        const std::shared_ptr<MergeTreeFactory> &merge_tree_factory,
         const std::shared_ptr<MergeSelector> &merge_selector,
         std::vector<std::vector<int>> &&non_singleton_cg_sccs);
     virtual ~MergeStrategySCCs() override;
