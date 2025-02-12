aab0e2077310ad907e600c8c219d1e52a8b6c0b2
Simon Dold <simon.dold@stud.unibas.ch>
2025-02-12 18:58:09 +0100

    style.

--- src/search/utils/component_errors.cc
+++ src/search/utils/component_errors.cc
@@ -1,7 +1,9 @@
 #include "component_errors.h"
 
+using namespace std;
+
 namespace utils {
-void verify_argument(bool b, const std::string &message) {
+void verify_argument(bool b, const string &message) {
     if (!b) {
         throw ComponentArgumentError(message);
     }
