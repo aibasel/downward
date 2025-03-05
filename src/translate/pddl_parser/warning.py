16a82a7a3f3949cb47d756e8280eefc8e4e5d4fa
Salome Eriksson <salome.eriksson@unibas.ch>
2025-03-05 10:18:51 +0100

    style fix

--- src/translate/pddl_parser/warning.py
+++ src/translate/pddl_parser/warning.py
@@ -5,6 +5,6 @@ PRINTED_WARNINGS = set()
 
 def print_warning(msg):
     global PRINTED_WARNINGS
-    if not msg in PRINTED_WARNINGS:
+    if msg not in PRINTED_WARNINGS:
         print(f"Warning: {msg}", file=sys.stderr)
         PRINTED_WARNINGS.add(msg)
