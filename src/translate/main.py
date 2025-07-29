69dad3cc5296905f64febe3dc79891dd27525ce7
Gabriele Roeger <gabriele.roeger@unibas.ch>
2025-07-29 11:44:41 +0200

    introduce __main__.py and move content from main.py there

--- src/translate/main.py
+++ src/translate/main.py
@@ -1,10 +1,7 @@
 #! /usr/bin/env python3
 
 
-import os
-import signal
 import sys
-import traceback
 from typing import Dict, List, Optional, Tuple, Union
 
 VarValPair = Tuple[int, int]
@@ -44,13 +41,6 @@ from translate import variable_order
 # non-derived).
 
 DEBUG = False
-
-
-## For a full list of exit codes, please see driver/returncodes.py. Here,
-## we only list codes that are used by the translator component of the planner.
-TRANSLATE_OUT_OF_MEMORY = 20
-TRANSLATE_OUT_OF_TIME = 21
-TRANSLATE_INPUT_ERROR = 31
 
 simplified_effect_condition_counter = 0
 added_implied_precondition_counter = 0
@@ -722,37 +712,3 @@ def main():
         with open(options.sas_file, "w") as output_file:
             sas_task.output(output_file)
     print("Done! %s" % timer)
-
-
-def handle_sigxcpu(signum, stackframe):
-    print()
-    print("Translator hit the time limit")
-    # sys.exit() is not safe to be called from within signal handlers, but
-    # os._exit() is.
-    os._exit(TRANSLATE_OUT_OF_TIME)
-
-
-if __name__ == "__main__":
-    try:
-        signal.signal(signal.SIGXCPU, handle_sigxcpu)
-    except AttributeError:
-        print("Warning! SIGXCPU is not available on your platform. "
-              "This means that the planner cannot be gracefully terminated "
-              "when using a time limit, which, however, is probably "
-              "supported on your platform anyway.")
-    try:
-        # Reserve about 10 MB of emergency memory.
-        # https://stackoverflow.com/questions/19469608/
-        emergency_memory = b"x" * 10**7
-        main()
-    except MemoryError:
-        del emergency_memory
-        print()
-        print("Translator ran out of memory, traceback:")
-        print("=" * 79)
-        traceback.print_exc(file=sys.stdout)
-        print("=" * 79)
-        sys.exit(TRANSLATE_OUT_OF_MEMORY)
-    except pddl_parser.ParseError as e:
-        print(e)
-        sys.exit(TRANSLATE_INPUT_ERROR)
