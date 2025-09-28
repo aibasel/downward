b01b321088b3fce85362d9c4de216fd781a3b163
Silvan Sievers <silvan.sievers@unibas.ch>
2025-09-28 20:14:19 +0200

    [issue1191] Refactor interface of MergeSelector.
    MergeSelector now has two variants of the method for choosing the best
    merge candidate for a given factored transition system: one that
    considers all merge candidates for the given factored transition
    system, and another one that only considers the given (sub)set of merge
    candidates.

--- src/search/merge_and_shrink/merge_selector.cc
+++ src/search/merge_and_shrink/merge_selector.cc
@@ -11,34 +11,28 @@
 using namespace std;
 
 namespace merge_and_shrink {
-vector<pair<int, int>> MergeSelector::compute_merge_candidates(
-    const FactoredTransitionSystem &fts,
-    const vector<int> &indices_subset) const {
+static vector<pair<int, int>> compute_merge_candidates(
+    const FactoredTransitionSystem &fts) {
     vector<pair<int, int>> merge_candidates;
-    if (indices_subset.empty()) {
-        for (int ts_index1 = 0; ts_index1 < fts.get_size(); ++ts_index1) {
-            if (fts.is_active(ts_index1)) {
-                for (int ts_index2 = ts_index1 + 1; ts_index2 < fts.get_size();
-                     ++ts_index2) {
-                    if (fts.is_active(ts_index2)) {
-                        merge_candidates.emplace_back(ts_index1, ts_index2);
-                    }
+    merge_candidates.reserve(
+        (fts.get_num_active_entries() * (fts.get_num_active_entries() - 1)) /
+        2);
+    for (int ts_index1 = 0; ts_index1 < fts.get_size(); ++ts_index1) {
+        if (fts.is_active(ts_index1)) {
+            for (int ts_index2 = ts_index1 + 1; ts_index2 < fts.get_size();
+                 ++ts_index2) {
+                if (fts.is_active(ts_index2)) {
+                    merge_candidates.emplace_back(ts_index1, ts_index2);
                 }
-            }
-        }
-    } else {
-        assert(indices_subset.size() > 1);
-        for (size_t i = 0; i < indices_subset.size(); ++i) {
-            int ts_index1 = indices_subset[i];
-            assert(fts.is_active(ts_index1));
-            for (size_t j = i + 1; j < indices_subset.size(); ++j) {
-                int ts_index2 = indices_subset[j];
-                assert(fts.is_active(ts_index2));
-                merge_candidates.emplace_back(ts_index1, ts_index2);
             }
         }
     }
     return merge_candidates;
+}
+
+pair<int, int> MergeSelector::select_merge(
+    const FactoredTransitionSystem &fts) const {
+    return select_merge_from_candidates(fts, compute_merge_candidates(fts));
 }
 
 void MergeSelector::dump_options(utils::LogProxy &log) const {
