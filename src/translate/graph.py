a61a4cbbdfb815e654c9d455baeebc8b51decc06
Malte Helmert <malte.helmert@unibas.ch>
2025-12-25 22:34:00 +0100

    [issue1063] translator: Compute indirect subtypes more efficiently.

--- src/translate/graph.py
+++ src/translate/graph.py
@@ -1,5 +1,41 @@
 #! /usr/bin/env python3
 
+from collections import defaultdict
+
+
+def transitive_closure(pairs):
+    """Compute the transitive closure of a relation.
+
+    The input must be a sequence of pairs.
+    The output is a sorted list of pairs."""
+
+    successors = defaultdict(list)
+    sources = set()
+    for u, v in pairs:
+        sources.add(u)
+        successors[u].append(v)
+
+    result = []
+    for source in sources:
+        # Perform a depth-first search from this source. The
+        # implementation is non-recursive to avoid hitting Python's
+        # recursion limit.
+
+        # Note that we don't want reflexive closure, so we add to the
+        # result when processing "successor", not when processing
+        # "current".
+        reached = set()
+        pending = [source]
+        while pending:
+            current = pending.pop()
+            for successor in successors[current]:
+                if successor not in reached:
+                    result.append((source, successor))
+                    reached.add(successor)
+                    pending.append(successor)
+    result.sort()
+    return result
+
 
 class Graph:
     def __init__(self, nodes):
@@ -25,21 +61,25 @@ class Graph:
         return sorted(result)
 
 
-def transitive_closure(pairs):
-    # Warshall's algorithm.
-    result = set(pairs)
-    nodes = {u for (u, v) in pairs} | {v for (u, v) in pairs}
-    for k in nodes:
-        for i in nodes:
-            for j in nodes:
-                if (i, j) not in result and (i, k) in result and (k, j) in result:
-                    result.add((i, j))
-    return sorted(result)
-
-
 if __name__ == "__main__":
     g = Graph([1, 2, 3, 4, 5, 6])
     g.connect(1, 2)
     g.connect(1, 3)
     g.connect(4, 5)
-    print(g.connected_components())
+    assert g.connected_components() == [[1, 2, 3], [4, 5], [6]]
+
+    assert transitive_closure([
+        ("A", "B"),
+        ("A", "C"),
+        ("B", "C"),
+        ("C", "B"),
+        ("C", "D")]) == [
+        ("A", "B"),
+        ("A", "C"),
+        ("A", "D"),
+        ("B", "B"),
+        ("B", "C"),
+        ("B", "D"),
+        ("C", "B"),
+        ("C", "C"),
+        ("C", "D")]
