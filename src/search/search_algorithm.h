49c0dd5b4f5303e9b9d34b47a4dcd64eccc31ed4
Claudia Grundke <claudia.grundke@unibas.ch>
2026-02-12 17:57:41 +0100

    Add some comments.

--- src/search/search_algorithm.h
+++ src/search/search_algorithm.h
@@ -69,6 +69,10 @@ public:
         utils::Verbosity verbosity);
     virtual void print_statistics() const = 0;
     virtual void save_plan_if_necessary();
+    /*
+      is_complete returns true if the search algorithm cannot "overlook" any
+      solvable state.
+    */
     virtual bool is_complete() const = 0;
     bool found_solution() const;
     SearchStatus get_status() const;
