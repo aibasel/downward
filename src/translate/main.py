bdb596b5cbc7b498ccd890b14c50ea164cf91e14
speckdavid <davidjakob.speck@gmail.com>
2026-07-24 11:26:51 +0200

    Use get_options in translator for getting the condition normalization strategy

--- src/translate/main.py
+++ src/translate/main.py
@@ -696,7 +696,7 @@ def main():
             domain_filename=get_options().domain, problem_filename=get_options().problem)
 
     with timers.timing(f"Normalizing task with strategy {get_options().condition_normalization_strategy}"):
-        normalize.normalize(task, get_options().condition_normalization_strategy)
+        normalize.normalize(task)
 
     if get_options().generate_relaxed_task:
         # Remove delete effects.
