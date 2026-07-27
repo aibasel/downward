c247f1b6d36e2b0338179ff99cd4213ebf10461f
Gabriele Roeger <gabriele.roeger@unibas.ch>
2026-07-27 12:01:21 +0200

    Revert "rename task to problem when talking about PDDL files"
    This reverts commit baa1af81b080df6f1bbba23e03e65360dd845e71.

--- src/translate/options.py
+++ src/translate/options.py
@@ -33,7 +33,7 @@ def get_arg_parser():
     argparser.add_argument(
         "domain", metavar="DOMAIN", help="path to domain pddl file")
     argparser.add_argument(
-        "problem", metavar="PROBLEM", help="path to problem pddl file")
+        "task", metavar="TASK", help="path to task pddl file")
     argparser.add_argument(
         "--relaxed", dest="generate_relaxed_task", action="store_true",
         help="output relaxed task (no delete effects)")
